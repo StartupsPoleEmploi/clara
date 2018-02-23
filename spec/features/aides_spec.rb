4680ff91092bbe030ca4cbe203fd0dc856e2bd4a
David Boureau <bdavidxyz@gmail.com>
2018-02-23 11:04:55 +0100

    added test, removed before(:all)

--- spec/features/aides_spec.rb
+++ spec/features/aides_spec.rb
@@ -4,19 +4,19 @@ require 'support/gon_extraction_helper'
 feature 'Aides page' do 
 
   context 'No active user' do
-    before(:all) do
+    before do
       create_2_different_aids
       disable_http_service
       visit aides_path
+    end
+    after do
+      enable_http_service
     end
     scenario 'Should display 2 aids NOT related to any eligibility' do
       display_2_aids_unrelated_to_eligibility
     end
     scenario 'Should not display breadcrumb' do
       expect(page).not_to have_css('.c-breadcrumb')
-    end
-    after(:all) do
-      enable_http_service
     end
   end
   
