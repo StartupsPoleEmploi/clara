681c083aba77824ea6623c3008456979a6590376
David Boureau <bdavidxyz@gmail.com>
2018-02-23 15:48:09 +0100

    all tests done, as should have existed

--- spec/views/breadcrumb_spec.rb
+++ spec/views/breadcrumb_spec.rb
@@ -62,41 +62,19 @@ describe 'Breadcrumb partial' do
         render partial: 'shared/breadcrumb.haml', locals: {context: context_aides_without_forid}
         expect(rendered).to have_css(selector_under_test)
       end
+      it 'Display print, when detail_path, with for_id', type: :view do
+        render partial: 'shared/breadcrumb.haml', locals: {context: context_detail_with_forid}
+        expect(rendered).to have_css(selector_under_test)
+      end
+      it 'Display print, when detail_path, without for_id', type: :view do
+        render partial: 'shared/breadcrumb.haml', locals: {context: context_detail_without_forid}
+        expect(rendered).to have_css(selector_under_test)
+      end
       it 'DONT Display print, when question_path', type: :view do
         render partial: 'shared/breadcrumb.haml', locals: {context: context_question}
         expect(rendered).not_to have_css(selector_under_test)
       end
     end
-
-    # it 'Display breadcrumb for question when a question is displayed' do
-    #   all_question_path = RoutesList.asked_questions.values.map {|v| v[0...-10]}
-    #   all_question_path.each do |question_path|
-    #     visit question_path
-    #     expect(page).to have_css('.c-breadcrumb-current--question')
-    #   end
-    # end
-
-    # it 'Display breadcrumb for results when results are displayed' do
-    #   name_of_aid = 'harki-test'
-    #   asker = Asker.new(v_harki: 'oui')
-    #   create(:aid, :aid_harki, name: name_of_aid)
-
-    #   base64_str = ConvertAskerInBase64Service.new.into_base64(asker)
-    #   visit(aides_path + '?for_id=' + base64_str)
-    #   expect(page).to have_css('.c-breadcrumb-current--results')
-    #   expect(page).to have_css('.c-breadcrumb .js-printer')
-    # end
-
-    # it 'Display breadcrumb for detail when detail is displayed', focus: true do
-    #   name_of_aid = 'harki-test'
-    #   asker = Asker.new(v_harki: 'oui')
-    #   create(:aid, :aid_harki, name: name_of_aid)
-
-    #   base64_str = ConvertAskerInBase64Service.new.into_base64(asker)
-    #   visit final_detail_path (base64_str)
-    #   expect(page).to have_css('.c-breadcrumb-current--detail')
-    #   expect(page).to have_css('.c-breadcrumb .js-printer')
-    # end
 
     def context_detail_with_forid
       build_req(detail_path('pmsmp'), "any")
