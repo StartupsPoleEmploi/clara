59d8f3cfeeee86bcd566efc28763e2f4c93f1df6
David Boureau <bdavidxyz@gmail.com>
2018-02-23 11:58:10 +0100

    wip

--- app/view_objects/breadcrumb.rb
+++ app/view_objects/breadcrumb.rb
@@ -3,6 +3,7 @@ class Breadcrumb < ViewObject
   def after_init(args)
     locals = hash_for(args)
     @where = string_for(locals[:where])
+    @context = locals[:context] if locals[:context]
   end
 
   def display_form?
@@ -10,6 +11,9 @@ class Breadcrumb < ViewObject
   end
 
   def display_result?
+    p '- - - - - - - - - - - - - - @context- - - - - - - - - - - - - - - -' 
+    p @context.request.path.inspect
+    p ''
     @context.request.path == aides_path && @context.params[:for_id]
   end
 
--- spec/views/breadcrumb_spec.rb
+++ spec/views/breadcrumb_spec.rb
@@ -8,6 +8,11 @@ feature 'Breadcrumb partial' do
     #   expect(rendered).to have_css('.c-breadcrumb')
     # end
 
+    scenario 'Display results, when /aides?for_id=', type: :view do
+      render partial: 'shared/breadcrumb.haml', locals: { }
+      expect(rendered).to have_css('.c-breadcrumb-current--results')
+    end
+
     # scenario 'Display breadcrumb for question when a question is displayed' do
     #   all_question_path = RoutesList.get_questions.values.map {|v| v[0...-10]}
     #   all_question_path.each do |question_path|
@@ -16,16 +21,16 @@ feature 'Breadcrumb partial' do
     #   end
     # end
 
-    scenario 'Display breadcrumb for results when results are displayed' do
-      name_of_aid = 'harki-test'
-      asker = Asker.new(v_harki: 'oui')
-      create(:aid, :aid_harki, name: name_of_aid)
+    # scenario 'Display breadcrumb for results when results are displayed' do
+    #   name_of_aid = 'harki-test'
+    #   asker = Asker.new(v_harki: 'oui')
+    #   create(:aid, :aid_harki, name: name_of_aid)
 
-      base64_str = ConvertAskerInBase64Service.new.into_base64(asker)
-      visit(aides_path + '?for_id=' + base64_str)
-      expect(page).to have_css('.c-breadcrumb-current--results')
-      expect(page).to have_css('.c-breadcrumb .js-printer')
-    end
+    #   base64_str = ConvertAskerInBase64Service.new.into_base64(asker)
+    #   visit(aides_path + '?for_id=' + base64_str)
+    #   expect(page).to have_css('.c-breadcrumb-current--results')
+    #   expect(page).to have_css('.c-breadcrumb .js-printer')
+    # end
 
     # scenario 'Display breadcrumb for detail when detail is displayed', focus: true do
     #   name_of_aid = 'harki-test'
