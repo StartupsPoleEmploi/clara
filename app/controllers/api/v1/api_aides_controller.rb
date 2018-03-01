2be02a038f0c4c3e43ad325ccaa5f76c26490021
David Boureau <bdavidxyz@gmail.com>
2018-03-01 17:55:44 +0100

    extract params

--- app/controllers/api/v1/api_aides_controller.rb
+++ app/controllers/api/v1/api_aides_controller.rb
@@ -9,12 +9,19 @@ module Api
           p '- - - - - - - - - - - - - - current_user- - - - - - - - - - - - - - - -' 
           p current_user.inspect
           p params[:v_duree_d_inscription]
+          a = Asker.new(asker_params)
+          p a.inspect
           p ''
 
         else
           p ' - - - - - - - -no current_user'
         end
       end
+
+      def asker_params
+        params.permit(:v_handicap, :v_harki, :v_detenu, :v_protection_internationale, :v_diplome, :v_category, :v_duree_d_inscription, :v_allocation_value_min, :v_allocation_type, :v_location_label, :v_qpv, :v_zrr, :v_age, :v_location_citycode, :v_location_country, :v_location_label, :v_location_route, :v_location_state, :v_location_street_number, :v_location_zipcode).to_h
+      end
+
 
     end
   end
