8976e3e9e7a2f2bdac93eacf6495215dffae25ba
David Boureau <bdavidxyz@gmail.com>
2018-03-01 14:54:31 +0100

    dashboard ok

--- db/schema.rb
+++ db/schema.rb
@@ -102,6 +102,8 @@ ActiveRecord::Schema.define(version: 20180301132059) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
