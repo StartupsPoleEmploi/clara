cc8bf61e2fa6d46a386dcbd3b1edb102c4da2a73
maxdeval <maxime.devallez@pole-emploi.fr>
2018-03-02 14:47:17 +0100

    trouver checkbox

--- app/assets/javascripts/other_component.js
+++ app/assets/javascripts/other_component.js
@@ -1,7 +1,20 @@
 _.set(window, 'clara.other_component',
 
-	function other_component(){
+	function other_component(root_element){
+
+    function $d(selector) {
+      return $(root_element + ' ' + selector);
+    }
+
+    $(root_element + ' input#val_harki').length && 
+    $(root_element + ' input#val_detenu').length && 
+    $(root_element + ' input#val_pi').length && 
+    $(root_element + ' input#val_handicap').length && 
+    $(root_element + ' input#none').length  
+
+      
     return false;
   }
-	
+
+
 );
--- app/assets/javascripts/other_questions.js
+++ app/assets/javascripts/other_questions.js
@@ -1,7 +1,27 @@
 $(document).on('ready turbolinks:load', function() {
   if ($( 'body' ).hasClass('other_questions', 'new' )) {
 
-    clara.other_component();
-    
+   $( 'input#none' ).click(function() {
+      $('input#val_harki').prop('checked', false);
+      $('input#val_detenu').prop('checked', false);
+      $('input#val_pi').prop('checked', false);
+      $('input#val_handicap').prop('checked', false);
+    });   
+
+    $( 'input#val_harki' ).click(function() {
+      $('input#none').prop('checked', false);
+    });   
+
+    $( 'input#val_detenu' ).click(function() {
+      $('input#none').prop('checked', false);
+    });   
+
+    $( 'input#val_pi' ).click(function() {
+      $('input#none').prop('checked', false);
+    });   
+
+    $( 'input#val_handicap' ).click(function() {
+      $('input#none').prop('checked', false);
+    });      
   }
 });
