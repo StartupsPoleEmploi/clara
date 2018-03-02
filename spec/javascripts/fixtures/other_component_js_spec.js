46bbae3bfc0d8ca14068486c949c9519c9d1f38c
maxdeval <maxime.devallez@pole-emploi.fr>
2018-03-02 11:39:22 +0100

    failure test

--- spec/javascripts/fixtures/other_component_js_spec.js
+++ spec/javascripts/fixtures/other_component_js_spec.js
@@ -1,8 +1,8 @@
 describe('other_component_spec.js', function() {
 
-it('Needs other_component_spec to be mapped to global clara.other_component', function() {
+  it('Needs other_component_spec to be mapped to global clara.other_component', function() {
     expect(clara).toBeDefined();
     expect(clara.other_component).toBeDefined();
   });
 
-};
+});
