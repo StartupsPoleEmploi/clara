d30ad3900dfb697704f1097786711f7ec9d73def
maxdeval <maxime.devallez@pole-emploi.fr>
2018-03-02 15:22:07 +0100

    fail test bad input

--- spec/javascripts/fixtures/other_component_js_spec.js
+++ spec/javascripts/fixtures/other_component_js_spec.js
@@ -25,6 +25,7 @@ describe('other_component_spec.js', function() {
       expect(clara.other_component("#valid-root")).toEqual(true);  
     });
     it('If bad input return false', function() {
+      $('#val_harki').attr('id', 'azerty')
       expect(clara.other_component("#valid-root")).toEqual(true);  
     });
   });
