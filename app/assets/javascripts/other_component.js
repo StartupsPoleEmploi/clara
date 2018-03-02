437965b90c720e4c5c82ae61b2dd273063aa2fb1
maxdeval <maxime.devallez@pole-emploi.fr>
2018-03-02 11:47:24 +0100

    suceed test

--- app/assets/javascripts/other_component.js
+++ app/assets/javascripts/other_component.js
@@ -1,28 +1,6 @@
 _.set(window, 'clara.other_component',
 
-	function other_component(){
-		    $( 'input#none' ).click(function() {
-      $('input#val_harki').prop('checked', false);
-      $('input#val_detenu').prop('checked', false);
-      $('input#val_pi').prop('checked', false);
-      $('input#val_handicap').prop('checked', false);
-    });   
-
-    $( 'input#val_harki' ).click(function() {
-      $('input#none').prop('checked', false);
-    });   
-
-    $( 'input#val_detenu' ).click(function() {
-      $('input#none').prop('checked', false);
-    });   
-
-    $( 'input#val_pi' ).click(function() {
-      $('input#none').prop('checked', false);
-    });   
-
-    $( 'input#val_handicap' ).click(function() {
-      $('input#none').prop('checked', false);
-    });  
-
-  };
-	);
+	function other_component(){		
+  }
+	
+);
--- spec/javascripts/fixtures/other_component_js_spec.js
+++ spec/javascripts/fixtures/other_component_js_spec.js
@@ -1,3 +1,8 @@
+//= require lodash/lodash
+//= require lodash/lodash_extension
+//= require jquery
+//= require other_component
+
 describe('other_component_spec.js', function() {
 
   it('Needs other_component_spec to be mapped to global clara.other_component', function() {
