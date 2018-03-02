3cf8ee65ab7ebf6d4f6e7ed0bbfb141f740abe98
David Boureau <bdavidxyz@gmail.com>
2018-03-02 08:40:17 +0100

    rspec correctly made

--- spec/requests/api_aides_controller_spec.rb
+++ spec/requests/api_aides_controller_spec.rb
@@ -10,10 +10,32 @@ describe Api::V1::ApiAidesController, type: :request do
   end
 
   describe 'Nominal' do
-    it 'Returns all eligible, uncertain, and ineligible aids' do
-      get '/api/v1/aides', headers: authenticated_header
-      json = JSON.parse(response.body)
-      expect(response).to be_success
+    json_returned = nil
+    response_returned = nil
+    before do
+      if !json_returned
+        create(:aid, :aid_harki, name: "aide harki")
+        create(:aid, :aid_not_harki, name: "aide not_harki")
+        create(:aid, :aid_adult_and_harki, name: "aide aid_adult_and_harki")
+        get '/api/v1/aides?v_harki=oui', headers: authenticated_header
+        json_returned = JSON.parse(response.body)
+        response_returned = response
+      end
+    end
+    it 'Returns a successful answer' do
+      expect(response_returned).to be_success
+    end
+    it 'Returns all eligible aids' do
+      expect(json_returned["all_eligible"].size).to eq 1
+      expect(json_returned["all_eligible"][0]["name"]).to eq 'aide harki'
+    end
+    it 'Returns all ineligible aids' do
+      expect(json_returned["all_ineligible"].size).to eq 1
+      expect(json_returned["all_ineligible"][0]["name"]).to eq 'aide not_harki'
+    end
+    it 'Returns all uncertain aids' do
+      expect(json_returned["all_uncertain"].size).to eq 1
+      expect(json_returned["all_uncertain"][0]["name"]).to eq 'aide aid_adult_and_harki'
     end
   end
 
