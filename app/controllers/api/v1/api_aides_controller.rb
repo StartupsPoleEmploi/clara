5e523f345751f258919ac98cd99c02d3054623aa
David Boureau <bdavidxyz@gmail.com>
2018-03-02 09:02:57 +0100

    use a service instead of code inside controller

--- app/controllers/api/v1/api_aides_controller.rb
+++ app/controllers/api/v1/api_aides_controller.rb
@@ -7,34 +7,8 @@ module Api
       def index
         if current_user
           asker = Asker.new(asker_params)
-          result = SerializeResultsService.new(asker).go
-          result.delete :asker
-          result[:all_eligible] = result.delete :flat_all_eligible
-          result[:all_uncertain] = result.delete :flat_all_uncertain
-          result[:all_ineligible] = result.delete :flat_all_ineligible
-
-          format_bunch_of_eligies(result[:all_eligible])
-          format_bunch_of_eligies(result[:all_uncertain])
-          format_bunch_of_eligies(result[:all_ineligible])
-
-          render json: result.to_json
-        end
-      end
-
-      def format_bunch_of_eligies(hash_of_eligies)
-        hash_of_eligies.each do |e|  
-          e.delete "contract_type_icon"
-          e.delete "contract_type_id"
-          e.delete "how_much"
-          e.delete "how_and_when"
-          e.delete "limitations"
-          e.delete "additionnal_conditions"
-          e.delete "what"
-          e.delete "updated_at"
-          e.delete "created_at"
-          e.delete "archived_at"
-          e.delete "last_update"
-          e.delete "contract_type_order"
+          result = SerializeResultsService.new(asker).jsonify
+          render json: result
         end
       end
 
