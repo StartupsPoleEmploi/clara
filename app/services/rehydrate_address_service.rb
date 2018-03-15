de1d869e05b5a5ec287c1a0ee72fbad16c841872
David Boureau <bdavidxyz@gmail.com>
2018-03-15 17:09:13 +0100

    working rehydratation

--- app/services/rehydrate_address_service.rb
+++ app/services/rehydrate_address_service.rb
@@ -8,18 +8,10 @@ class RehydrateAddressService
     p zipcode_and_cityname.inspect
     p ''
 
-    if zipcode_and_cityname.is_a?(String) && zipcode_and_cityname.include?("erreur_") 
-      return asker
+    if zipcode_and_cityname.is_a?(Array)
+      asker.v_location_zipcode  = zipcode_and_cityname[0]
+      asker.v_location_city  = zipcode_and_cityname[1]
     end
-    
-    # asker.v_location_state         = address.state
-    # asker.v_location_label         = address.label
-    # asker.v_location_route         = address.route
-    # asker.v_location_city          = address.city
-    # asker.v_location_country       = address.country
-    # asker.v_location_zipcode       = address.zipcode
-    # asker.v_location_citycode      = address.citycode
-    # asker.v_location_street_number = address.street_number
-    # asker.v_location_state         = address.state
+    asker
   end
 end
--- spec/services/rehydrate_address_service_spec.rb
+++ spec/services/rehydrate_address_service_spec.rb
@@ -7,9 +7,19 @@ describe RehydrateAddressService do
       output = RehydrateAddressService.new.from_citycode!(Date.new)
       expect(output).to eq(Asker.new)
     end
-    it 'Rehydrate address from citycode' do
-      output = RehydrateAddressService.new.from_citycode!(Asker.new(v_location_citycode: "59035"))
-      expect(output).to eq(Asker.new(v_location_citycode: "59035"))
+    describe 'Rehydrate address from citycode' do
+      before do
+        ban_layer = instance_double("BanService")
+        allow(ban_layer).to receive(:get_zipcode_and_cityname).and_return(["59440", "Avesnelles"])
+        BanService.set_instance(ban_layer)
+      end      
+      after do
+        BanService.set_instance(nil)     
+      end      
+      it 'Nominal' do
+        output = RehydrateAddressService.new.from_citycode!(Asker.new(v_location_citycode: "59035"))
+        expect(output).to eq(Asker.new(v_location_citycode: "59035", v_location_city:"Avesnelles", v_location_zipcode: "59440"))
+      end
     end
   end
 
