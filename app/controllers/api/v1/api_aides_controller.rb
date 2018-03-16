4bf217710bd01eb594859ee2874441f45f319f38
David Boureau <bdavidxyz@gmail.com>
2018-03-16 11:41:22 +0100

    first working version

--- app/controllers/api/v1/api_aides_controller.rb
+++ app/controllers/api/v1/api_aides_controller.rb
@@ -4,6 +4,10 @@ module Api
 
       before_action :authenticate_user
 
+      def detail
+        render json: (Aid.find_by!(slug: slug_param)).attributes
+      end
+
       def eligible
         render json: eligible_aids_for(processed_asker)        
       end
@@ -16,14 +20,18 @@ module Api
         render json: uncertain_aids_for(processed_asker)        
       end
 
-      def english_asker_attr
+      def slug_param
+        (params.permit(:aid_slug).to_h)[:aid_slug]
+      end
+
+      def english_asker_params
         params.permit(:disabled, :harki, :ex_invict, :international_protection, :diploma, :category, :inscription_period, :monthly_allocation_value, :allocation_type, :age, :location_street_number, :location_route, :location_citycode).to_h
       end
 
       private
 
       def processed_asker
-        asker = TranslateAskerService.new(english_asker_attr).to_french
+        asker = TranslateAskerService.new(english_asker_params).to_french
         RehydrateAddressService.get_instance.from_citycode!(asker)
       end
 
