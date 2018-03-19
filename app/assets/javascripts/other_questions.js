01088ea428f9ab4d8021fe52926b448f1e3792d0
maxdeval <maxime.devallez@pole-emploi.fr>
2018-03-19 11:43:09 +0100

    call other_component in other_question

--- app/assets/javascripts/other_questions.js
+++ app/assets/javascripts/other_questions.js
@@ -1,30 +1,7 @@
 $(document).on('ready turbolinks:load', function() {
   if ($('body').hasClass('other_questions', 'new')) {
-    /*  
-        To modify to call other_component
-        clara.other_component();
-    */
-    $('input#none').click(function() {
-      $('input#val_harki').prop('checked', false);
-      $('input#val_detenu').prop('checked', false);
-      $('input#val_pi').prop('checked', false);
-      $('input#val_handicap').prop('checked', false);
-    });
-
-    $('input#val_harki').click(function() {
-      $('input#none').prop('checked', false);
-    });
-
-    $('input#val_detenu').click(function() {
-      $('input#none').prop('checked', false);
-    });
-
-    $('input#val_pi').click(function() {
-      $('input#none').prop('checked', false);
-    });
-
-    $('input#val_handicap').click(function() {
-      $('input#none').prop('checked', false);
-    });
+      
+    clara.other_component('c.fieldset');
+    
   }
 });
