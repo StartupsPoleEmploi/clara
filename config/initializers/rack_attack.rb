c2ef79c4931fcf87f2f527afeab353cace0c8786
David Boureau <bdavidxyz@gmail.com>
2018-03-21 15:01:24 +0100

    with test that works

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -13,9 +13,14 @@ class Rack::Attack
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', limit: 3, period: 1.seconds) do |req|
-    req.ip unless req.path.start_with?('/assets')
-  end
-
+  # if 
+    # p "eeeeeuh"
+    # p ENV["THROTTLE_DURING_TEST"]
+    throttle('req/ip', limit: 3, period: 1.seconds) do |req|
+      p "should_throttle..."
+      p ENV["THROTTLE_DURING_TEST"]
+      req.ip if (!Rails.env.test?) || (Rails.env.test? && ENV["THROTTLE_DURING_TEST"])
+    end
+  # end
 
 end
