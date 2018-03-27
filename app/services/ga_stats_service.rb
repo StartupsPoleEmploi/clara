2bef425a108c599fa655c5c72877fd026ad31c45
David Boureau <bdavidxyz@gmail.com>
2018-03-27 14:06:47 +0200

    having stats working

--- app/services/ga_stats_service.rb
+++ app/services/ga_stats_service.rb
@@ -4,7 +4,7 @@ class GaStatsService
   def number_of_sessions
     res = 0
     begin
-      res = Stats.first.ga[0]["data"]["totals"][0]["values"][0].to_i
+      res = Stat.first.ga[0]["data"]["totals"][0]["values"][0].to_i
     rescue Exception => e
       res = 0
     end
