19dfba0d0ac7b7615e203f20994f54c65b0b73d1
maxdeval <maxime.devallez@pole-emploi.fr>
2018-04-03 10:21:19 +0200

    add variable_spec tests for variable

---
+++ spec/models/variable_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe Variable, type: :model do
+
+  describe 'database columns' do
+    it { is_expected.to have_db_column(:id) }
+    it { is_expected.to have_db_column(:name) }
+    it { is_expected.to have_db_column(:variable_type) }
+    it { is_expected.to have_db_column(:created_at) }
+    it { is_expected.to have_db_column(:updated_at) }
+    it { is_expected.to have_db_column(:description) }
+  end
+  describe 'association' do
+    it { is_expected. to have_many(:rule) }
+  end
+  describe 'validates' do
+    context 'variable_type' do
+      it { is_expected.not_to allow_value('').for(:variable_type) }
+      it { is_expected.to allow_value('ma variable_type').for(:variable_type) }
+    end
+  end
+  describe 'enum' do
+    it 'variable_type should be an integer or string' do
+      create(:variable, variable_type: 'string')
+      create(:variable, variable_type: 4)
+      expect(variable_type.is_a?(Integer)).to eq(true)
+      expect(variable_type.is_a?(String)).to eq(true)
+    end
+  end
+end
+
