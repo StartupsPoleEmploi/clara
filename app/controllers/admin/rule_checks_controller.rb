3bd74f2bbce9966c5883ec1f5834c8eafbbb7c72
David Boureau <bdavidxyz@gmail.com>
2018-04-05 13:53:15 +0200

    ok but not unit-tested

--- app/controllers/admin/rule_checks_controller.rb
+++ app/controllers/admin/rule_checks_controller.rb
@@ -5,9 +5,9 @@ module Admin
       all_rules = Rule.all
       @all_rules_composition = []
       all_rules.each do |one_rule|
-        typecheck = RuleCheckService.new.check_type(one_rule)
-        @all_rules_composition << { type: '2-type', name: one_rule.name, result: typecheck, link: edit_admin_rule_path(one_rule.id) }
-        typecheck = RuleCheckService.new.check_custom_rule(one_rule, @all_rules_composition)
+      typecheck = RuleCheckService.new.check_type(one_rule)
+      @all_rules_composition << { type: '2-type', name: one_rule.name, result: typecheck, link: edit_admin_rule_path(one_rule.id) }
+      typecheck = RuleCheckService.new.check_custom_rule(one_rule, @all_rules_composition)
       end
       @all_rules_composition = @all_rules_composition.sort_by { |hsh| hsh[:type] }
     end
--- app/services/rule_check_service.rb
+++ app/services/rule_check_service.rb
@@ -6,11 +6,11 @@ class RuleCheckService
     if rule.variable.present?
       if rule.variable.variable_type == 'integer'
         state='ok' if !!( rule.value_eligible.match /^(\d)+$/ )
-      elsif rule.variable.variable_type == 'string'
+      elsif rule.variable.variable_type == 'string' && rule.operator_type == "starts_with"
+        state = 'ok' if rule.value_eligible != ''
+      elsif rule.variable.variable_type == 'string' && (rule.operator_type == "eq" || rule.operator_type == "not_equal")
         array_of_possibilities = rule.variable.description.split(',').map {|e| e.strip}
-        if array_of_possibilities.include?(rule.value_eligible)
-          state='ok'
-        end
+        state = 'ok' if array_of_possibilities.include?(rule.value_eligible)
       else
         state='error'
       end
