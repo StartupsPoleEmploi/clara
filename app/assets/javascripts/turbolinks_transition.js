bb01b991c11bebf812bed100bc948d9b99fc992f
David Boureau <bdavidxyz@gmail.com>
2018-04-19 15:54:45 +0200

    removed button-next.js

--- app/assets/javascripts/turbolinks_transition.js
+++ app/assets/javascripts/turbolinks_transition.js
@@ -1,8 +1,21 @@
 
-// $(document).on('turbolinks:request-start', function() {
-//   $('.c-turbolinks-transition').removeClass('is-hidden');
-// });
+$(document).on('turbolinks:request-start', function() {
+  $('.c-turbolinks-transition').removeClass('is-hidden');
+});
 
-$(document).on('turbolinks:request-end turbolinks:before-cache', function() {
+$(document).on('turbolinks:request-end', function() {
   $('.c-turbolinks-transition').addClass('is-hidden');
+});
+
+
+$(document).on('turbolinks:request-start', function() {
+  console.log("turbolinks:request-start")
+});
+
+$(document).on('turbolinks:request-end', function() {
+  console.log("turbolinks:request-end")
+});
+
+$(document).on('turbolinks:before-cache', function() {
+  console.log("turbolinks:before-cache")
 });
