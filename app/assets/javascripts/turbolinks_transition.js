45404269f118f9a8906f642a4372514d88e13d4f
David Boureau <bdavidxyz@gmail.com>
2018-04-19 15:35:57 +0200

    [721] turbolinks reset

--- app/assets/javascripts/turbolinks_transition.js
+++ app/assets/javascripts/turbolinks_transition.js
@@ -1,7 +1,7 @@
 
-$(document).on('turbolinks:request-start', function() {
-  $('.c-turbolinks-transition').removeClass('is-hidden');
-});
+// $(document).on('turbolinks:request-start', function() {
+//   $('.c-turbolinks-transition').removeClass('is-hidden');
+// });
 
 $(document).on('turbolinks:request-end turbolinks:before-cache', function() {
   $('.c-turbolinks-transition').addClass('is-hidden');
