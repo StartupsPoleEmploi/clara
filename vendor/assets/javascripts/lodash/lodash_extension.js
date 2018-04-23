e67dcdd23347ddf0703649d2c1b67a3cafefd320
David Boureau <bdavidxyz@gmail.com>
2018-04-23 20:55:18 +0200

    done, added better console log

--- vendor/assets/javascripts/lodash/lodash_extension.js
+++ vendor/assets/javascripts/lodash/lodash_extension.js
@@ -6,6 +6,29 @@ _.mixin({
     });
   },
 
+  dateFr: function() {
+    // les noms de jours / mois
+    var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+    var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
+    // on recupere la date
+    var date = new Date();
+    // on construit le message
+    var message = jours[date.getDay()] + " "; // nom du jour
+    message += date.getDate() + " "; // numero du jour
+    message += mois[date.getMonth()] + " "; // mois
+    message += date.getFullYear();
+    return message;
+  },
+
+  hourFr : function() {
+     var date = new Date();
+     var heure = date.getHours();
+     var minutes = date.getMinutes();
+     if(minutes < 10)
+          minutes = "0" + minutes;
+     return heure + "h" + minutes;
+  },
+
   // from https://davidwalsh.name/query-string-javascript
   getUrlParameter: function(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
