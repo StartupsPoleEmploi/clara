fbaebe228ba9fd1b475d88a3385dccfe3d2d8e5a
David Boureau <bdavidxyz@gmail.com>
2018-04-24 10:48:58 +0200

    better code

--- app/assets/javascripts/google_analytics_push.js
+++ app/assets/javascripts/google_analytics_push.js
@@ -1,6 +1,6 @@
 $(document).on('ready turbolinks:load', function(event) {
   if (typeof ga === "function") {
-    ga("set", "location", event.originalEvent.data.url)
+    ga("set", "location", _.get(event, "originalEvent.data.url"));
     return ga("send", "pageview");
   }
 });
