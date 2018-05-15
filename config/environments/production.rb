a88a7693aab083b1a081d985c29d8f2d34436dcf
David Boureau <bdavidxyz@gmail.com>
2018-05-15 14:59:15 +0200

    744ter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.secret_key_base = ENV["ARA_SECRET_KEY_BASE"]
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
