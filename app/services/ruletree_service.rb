d1022f1caf82167a19c17d41ab3f5db2efc67136
David Boureau <bdavidxyz@gmail.com>
2018-05-22 15:53:04 +0200

    one green test

--- app/services/ruletree_service.rb
+++ app/services/ruletree_service.rb
@@ -15,10 +15,23 @@ class RuletreeService
     @@the_double.nil? ? RuletreeService.new : @@the_double
   end
 
+  def initialize
+    all_rules = CacheService.get_instance.read("all_rules")
+    begin
+      JSON.parse(all_rules)
+    rescue Exception => e
+      all_rules = Rule.all.to_json(:include => :slave_rules)
+      CacheService.get_instance.write("all_rules", all_rules)
+    ensure
+      @all_rules = JSON.parse(all_rules)
+    end
+  end
+
   def resolve(rule_id)
   end
 
   def _all_rules
+    @all_rules
   end
 
 end
