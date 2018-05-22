85c1b56004d7f0a0d4a68ffffdc21061c064148a
David Boureau <bdavidxyz@gmail.com>
2018-05-22 16:01:09 +0200

    red test, wip

--- spec/services/ruletree_service_spec.rb
+++ spec/services/ruletree_service_spec.rb
@@ -13,16 +13,14 @@ describe RuletreeService do
     r_adult_and_spectacle = create(:rule, :be_an_adult_and_a_spectacles, name: "r_adult_and_spectacle_rts")
     r_adult_or_spectacle = create(:rule, :be_an_adult_or_a_spectacles, name: "r_adult_or_spectacle_rts")
 
-
-
   end
 
   describe 'instantiation' do
     
-    describe 'Cache Read' do
+    describe 'Cache Read, already something inside the database' do
       before do
         cache_layer = instance_double("CacheService")
-        allow(cache_layer).to receive(:read).and_return('[]')
+        allow(cache_layer).to receive(:read).and_return('[{}]')
         allow(cache_layer).to receive(:write).and_return(nil)
         CacheService.set_instance(cache_layer)        
       end
@@ -33,60 +31,51 @@ describe RuletreeService do
         # given
 
         # when
-        result = RuletreeService.get_instance
+        RuletreeService.get_instance
         # then
         expect(cache_layer).to have_received(:read).with("all_rules")
       end
-      # it 'Must NOT write anything to cache' do
-      #   # given
+      it 'Must NOT write anything to cache' do
+        # given
 
-      #   # when
-      #   result = RuletreeService.get_instance.go(the_asker)
-      #   # then
-      #   expect(cache_layer).not_to have_received(:write)
-      # end
-      # it 'Must fetch results from cache, if any' do
-      #   # given
-      #   # when
-      #   result = RuletreeService.get_instance.go(the_asker)
-      #   # then
-      #   expect(result.size).to eq(2)
-      # end
+        # when
+        RuletreeService.get_instance
+        # then
+        expect(cache_layer).not_to have_received(:write)
+      end
+      it 'Must fetch results from cache, if any' do
+        # given
+        # when
+        sut = RuletreeService.get_instance._all_rules
+        # then
+        expect(sut.size).to eq(1)
+      end
     end
-    # describe 'Cache Write' do
-    #   before do
-    #     cache_layer = instance_double("CacheService")
-    #     allow(cache_layer).to receive(:read).and_return(nil)
-    #     allow(cache_layer).to receive(:write).and_return(nil)
-    #     CacheService.set_instance(cache_layer)        
-    #   end
-    #   after do
-    #     CacheService.set_instance(nil)        
-    #   end
-    #   it 'Must have read key "all_rules" from cache' do
-    #     # given
-
-    #     # when
-    #     result = RuletreeService.get_instance.go(the_asker)
-    #     # then
-    #     expect(cache_layer).to have_received(:read).with("all_rules")
-    #   end
-    #   it 'Must WRITE the list to the cache' do
-    #     # given
-
-    #     # when
-    #     result = RuletreeService.get_instance.go(the_asker)
-    #     # then
-    #     expect(cache_layer).to have_received(:write).with("all_rules", anything)
-    #   end
-    #   it 'Must fetch results from DB' do
-    #     # given
-    #     # when
-    #     result = RuletreeService.get_instance.go(the_asker)
-    #     # then
-    #     expect(result.size).to eq(6)
-    #   end
-    # end
+    describe 'Cache Write, nothing inside the database' do
+      before do
+        cache_layer = instance_double("CacheService")
+        allow(cache_layer).to receive(:read).and_return(nil)
+        allow(cache_layer).to receive(:write).and_return(nil)
+        CacheService.set_instance(cache_layer)        
+      end
+      after do
+        CacheService.set_instance(nil)        
+      end
+      it 'Must WRITE the list to the cache' do
+        # given
+        # when
+        RuletreeService.get_instance
+        # then
+        expect(cache_layer).to have_received(:write).with("all_rules", anything)
+      end
+      it 'Must fetch results from DB' do
+        # given
+        # when
+        sut = RuletreeService.get_instance._all_rules
+        # then
+        expect(sut.size).to eq(4)
+      end
+    end
 
   end
 
