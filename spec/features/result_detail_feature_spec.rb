3c0395c25c62633124be3e46b00d8170ef96beca
David Boureau <bdavidxyz@gmail.com>
2018-05-23 21:42:56 +0200

    one more green test

--- spec/features/result_detail_feature_spec.rb
+++ spec/features/result_detail_feature_spec.rb
@@ -43,7 +43,6 @@ feature 'detail of a result page' do
       enable_cache_service
     end
     scenario 'Display all details correctly' do
-      save_and_open_page
       _display_resume_of_situation(true)
       _display_all_details_correctly
     end
