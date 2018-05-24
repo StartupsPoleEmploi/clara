f7d44b771c770df131c40c85f04e4de9c74780e2
David Boureau <bdavidxyz@gmail.com>
2018-05-24 11:22:49 +0200

    wip

--- app/services/aid_calculation_service.rb
+++ app/services/aid_calculation_service.rb
@@ -21,13 +21,26 @@ class AidCalculationService
     p ''
     aids_as_hash = AidtreeService.get_instance.go(asker)
     p '- - - - - - - - - - - - - - aids_as_hash- - - - - - - - - - - - - - - -' 
-    pp aids_as_hash.inspect
+    pp aids_as_hash
     p ''
 
+    @calculated_aids_as_hash =  aids_as_hash.map do |aid_as_hash|  
+      eligibility = RuletreeService.get_instance.resolve(aid_as_hash["rule_id"], asker.attributes)
+      aid_as_hash["eligibility"] = eligibility
+      aid_as_hash
+    end
+    p '- - - - - - - - - - - - - - calculated_aids_as_hash- - - - - - - - - - - - - - - -' 
+    pp @calculated_aids_as_hash
+    p ''
+
+    # aids_as_hash.each do |aid_as_hash|  
+    #   eligibility = RuletreeService.get_instance.resolve(aid_as_hash["rule_id"], asker.attributes)
+    #   aid_as_hash["eligibility"] = eligibility
+    # end
   end
 
   def _all_aids
-    
+    @calculated_aids_as_hash
   end
 
 end
