79a677c994582ae0de0dde7c90d1f9e18e8f8f13
David Boureau <bdavidxyz@gmail.com>
2018-05-24 11:44:24 +0200

    wuo

--- app/services/serialize_results_service.rb
+++ app/services/serialize_results_service.rb
@@ -17,35 +17,39 @@ class SerializeResultsService
   end
 
   def go(asker)
+    calculator = AidCalculationService.get_instance(asker)
     res = {
-     flat_all_eligible: displayable_hash(all_eligible(asker)),
-     flat_all_uncertain: displayable_hash(all_uncertain(asker)),
-     flat_all_ineligible: displayable_hash(all_ineligible(asker)),
+     flat_all_eligible: calculator.all_eligible,
+     flat_all_uncertain: calculator.all_uncertain,
+     flat_all_ineligible: calculator.all_ineligible,
      asker: asker.attributes
     }
     res
   end
 
   def jsonify_eligible(asker)
+    calculator = AidCalculationService.get_instance(asker)
     result = {
       asker: asker.attributes,
-      aids: whitelist(displayable_hash(all_eligible(asker)))
+      aids: whitelist(calculator.all_eligible)
     }
     result.to_json
   end
 
   def jsonify_ineligible(asker)
+    calculator = AidCalculationService.get_instance(asker)
     result = {
       asker: asker.attributes,
-      aids: whitelist(displayable_hash(all_ineligible(asker)))
+      aids: whitelist(calculator.all_ineligible)
     }
     result.to_json
   end
 
   def jsonify_uncertain(asker)
+    calculator = AidCalculationService.get_instance(asker)
     result = {
       asker: asker.attributes,
-      aids: whitelist(displayable_hash(all_uncertain(asker)))
+      aids: whitelist(calculator.all_uncertain)
     }
     result.to_json
   end
@@ -54,20 +58,20 @@ private
   def whitelist(aids)
     aids.map {|aid| WhitelistAidService.new.for_aid_in_list(aid)}
   end
-  def displayable_hash(aids)
-    ResultService.new.convert_to_displayable_hash(aids)
-  end
-  def all_eligible(asker)
-    # AidService.all_eligible(asker)
-    {}
-  end
-  def all_ineligible(asker)
-    # AidService.all_ineligible(asker)
-    {}
-  end
-  def all_uncertain(asker)
-    # AidService.all_uncertain(asker)
-    {}
-  end
+  # def displayable_hash(aids)
+  #   ResultService.new.convert_to_displayable_hash(aids)
+  # end
+  # def all_eligible(asker)
+  #   # AidService.all_eligible(asker)
+  #   {}
+  # end
+  # def all_ineligible(asker)
+  #   # AidService.all_ineligible(asker)
+  #   {}
+  # end
+  # def all_uncertain(asker)
+  #   # AidService.all_uncertain(asker)
+  #   {}
+  # end
 
 end
