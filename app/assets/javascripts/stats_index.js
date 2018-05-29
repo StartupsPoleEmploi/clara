c55c4e06ee6066b2f36546b498a422b0d3bf8544
David Boureau <bdavidxyz@gmail.com>
2018-05-29 16:36:51 +0200

    stronger jes

--- app/assets/javascripts/stats_index.js
+++ app/assets/javascripts/stats_index.js
@@ -6,13 +6,13 @@ $(document).on('ready turbolinks:load', function() {
     var json_data = JSON.parse($('.ct-gape').attr('data-loaded'))["json_data"]
     var total_nb = _.chain(json_data)
                       .filter(function (e) {return e["Segment"] === "Tous les utilisateurs"}) 
-                      .map(function (e) {return _.toNumber(e["Sessions"].replace(/\s/g,''))})
+                      .map(function (e) {return _.toNumber(e["Sessions"].match(/\d/g).join(""))})
                       .sum()
                       .value()
     
     var advisor_nb = _.chain(json_data)
                         .filter(function (e) {return e["Segment"] === "Conseillers PE"}) 
-                        .map(function (e) {return _.toNumber(e["Sessions"].replace(/\s/g,''))})
+                        .map(function (e) {return _.toNumber(e["Sessions"].match(/\d/g).join(""))})
                         .sum()
                         .value()
     
@@ -46,7 +46,7 @@ $(document).on('ready turbolinks:load', function() {
     });
 
     var week_board = _.map(grouped_board, function(v, k) {
-      return _.sum(_.map(v, function(m) {return _.toNumber(m["Sessions"].replace(/\s/g,''))}))
+      return _.sum(_.map(v, function(m) {return _.toNumber(m["Sessions"].match(/\d/g).join(""))}))
     });
 
     var nice_keys = _.map(_.keys(grouped_board), function(e){var splitted = _.split(e, " "); return splitted[1] + " " + splitted[2] + " " + splitted[3];})
