2fdd7c3c77d6699f4e9f0e0ad6c730eca4c016d6
David Boureau <bdavidxyz@gmail.com>
2018-05-29 16:01:53 +0200

    wip

--- app/assets/javascripts/stats_index.js
+++ app/assets/javascripts/stats_index.js
@@ -3,42 +3,58 @@ $(document).on('ready turbolinks:load', function() {
     /**
     * Load from PE / not from PE
     */
-    var all_pe = _.get(JSON.parse($('.ct-gape').attr('data-loaded')), '[0].data.rows')
-    // var pe_discriminator_function = function(e){ return _.toBoolean(_.get(e, 'dimensions[0]'))}
-    // var inside_pe = _.filter(all_pe, pe_discriminator_function)
-    // var outside_pe = _.reject(all_pe, pe_discriminator_function)
-    // var advisor_nb = _.toInteger(_.get(inside_pe, '[0].metrics[0].values[0]'))
-    // var asker_nb = _.toInteger(_.get(outside_pe, '[0].metrics[0].values[0]'))
-    // var total_nb = advisor_nb + asker_nb
-    // new Chartist.Bar(
-    //   '.ct-gape',
-    //   {
-    //     series: [{ name: 'Conseillers ' + _.toPercentage(advisor_nb, total_nb), data: [advisor_nb] }, { name: 'Demandeurs ' + _.toPercentage(asker_nb, total_nb), data: [asker_nb] }]
-    //   },
-    //   {
-    //     seriesBarDistance: 10,
-    //     plugins: [Chartist.plugins.legend()],
-    //     reverseData: true,
-    //     horizontalBars: true,
-    //     seriesBarDistance: 30,
-    //     axisY: {
-    //       offset: 70
-    //     }
-    //   }
-    // );
+    var json_data = JSON.parse($('.ct-gape').attr('data-loaded'))["json_data"]
+    var total_nb = _.chain(json_data)
+                      .filter(function (e) {return e["Segment"] === "Tous les utilisateurs"}) 
+                      .map(function (e) {return _.toNumber(e["Sessions"])})
+                      .sum()
+                      .value()
+    
+    var advisor_nb = _.chain(json_data)
+                        .filter(function (e) {return e["Segment"] === "Conseillers PE"}) 
+                        .map(function (e) {return _.toNumber(e["Sessions"])})
+                        .sum()
+                        .value()
+    
+    var asker_nb = total_nb - advisor_nb
+
+    new Chartist.Bar(
+      '.ct-gape',
+      {
+        series: [{ name: 'Conseillers ' + _.toPercentage(advisor_nb, total_nb), data: [advisor_nb] }, { name: 'Demandeurs ' + _.toPercentage(asker_nb, total_nb), data: [asker_nb] }]
+      },
+      {
+        seriesBarDistance: 10,
+        plugins: [Chartist.plugins.legend()],
+        reverseData: true,
+        horizontalBars: true,
+        seriesBarDistance: 30,
+        axisY: {
+          offset: 70
+        }
+      }
+    );
 
 
     /**
     * Load sessions
     */
-    // var board = _.get(JSON.parse($('.ct-gabarline').attr('data-loaded')), '[0].data.rows');
-    // var grouped_board = _.groupBy(board, function(e) {
-    //   return moment(_.get(e, 'dimensions[0]')).startOf('isoWeek');
-    // });
+    var json_data = JSON.parse($('.ct-gabarline').attr('data-loaded'))["json_data"]
+
+    var grouped_board = _.groupBy(json_data, function(e) {
+      return moment(e["Index des jours"], "DD/MM/YYYY").startOf('isoWeek');
+    });
+
+    var week_board = _.map(grouped_board, function(v, k) {
+      return _.sum(_.map(v, function(m) {return _.toNumber(m["Sessions"])}))
+    });
+
+    var nice_keys = 
+
     // var week_board = _.map(grouped_board, function(v, k) {
     //   return _.sum(
     //     _.map(v, function(m) {
-    //       return _.toInteger(_.get(m, 'metrics[0].values[0]'));
+    //       return _.trim()oInteger(_.get(m, 'metrics[0].values[0]'));
     //     })
     //   );
     // });
