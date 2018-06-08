caa98387941923a7fc3b530c327744d87f13a3b6
David Boureau <bdavidxyz@gmail.com>
2018-06-08 16:32:19 +0200

    propage sub context successfully !!!

--- app/assets/javascripts/result_default.js
+++ app/assets/javascripts/result_default.js
@@ -12,13 +12,32 @@ $(document).on('ready turbolinks:load', function() {
     }
   
 
+    function AidPerContractViewModel(name, isOpened) {
+      var self = this;
+      self.name = name;
+      self.isOpened = ko.observable(isOpened);
+    }
+
+    function AidsPerContractViewModel(name, isOpened) {
+      var self = this;
+      self.isOpened = ko.observable(isOpened);
+      self.name = name;
+      self.toggleOpen = function() {self.isOpened(!self.isOpened())}
+      var aid_per_contract_array = _.map([], function(e) {
+        return new AidPerContractViewModel(e.name, false)
+      });
+      
+      self.o_aids_per_contract = ko.observableArray(aid_per_contract_array);
+    }
 
     function AppViewModel() {
       var self = this;
+    
       var filter_array = _.map(gon.loaded.flat_all_filter, function(e) {
         return new FilterViewModel(e.id, e.name, e.description, false)
       });
       self.o_filters = ko.observableArray(filter_array);
+      self.o_eligibles = ko.observable(new AidsPerContractViewModel('eligible', false))
     }
     
     var appViewModel = new AppViewModel();
