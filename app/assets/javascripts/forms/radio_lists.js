4a7b904d7738b310c2b0e10da334cc2399378c14
David Boureau <bdavidxyz@gmail.com>
2018-06-23 14:58:41 +0200

    wip

--- app/assets/javascripts/forms/radio_lists.js
+++ app/assets/javascripts/forms/radio_lists.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-  $('.c-radiolist__element').keypress(function(e){
+  $('.js-enterspace').keypress(function(e){
     var theCode = (e.keyCode ? e.keyCode : e.which);
     if(theCode === 13 || theCode === 32){
       $($(this).find('input')).trigger('click');
