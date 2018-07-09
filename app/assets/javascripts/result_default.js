00f154ff8a7180fd524db7b66bbb9458a3c8ad89
David Boureau <bdavidxyz@gmail.com>
2018-07-09 23:21:48 +0200

    initial_eligy

--- app/assets/javascripts/result_default.js
+++ app/assets/javascripts/result_default.js
@@ -22,47 +22,41 @@ $(document).on('turbolinks:load', function () {
       return $('#' + eligy + ' .c-resultcard[data-cslug="'+contract_name+'"]' + ' .c-resultaid[data-aslug="'+aid_name+'"] .c-resultfilter').map(function(){return $(this).data()["name"]}).get();
     }
 
-    window.initial_eligibles_aids_per_contract = _.map(
-      collect_aids_per_contract('o_eligibles'), 
-      function(contract_name){
-        return {
-          name: contract_name, 
-          is_collapsed: false,
-          aids: _.map(
-            collect_aids('o_eligibles', contract_name), 
-            function(aid_name) {
-              return {
-                name: aid_name,
-                is_collapsed: false,
-                filters: _.map(
-                  collect_filters('o_eligibles', contract_name, aid_name),
-                  function(filter_name) {
-                    return {
-                      name: filter_name,
-                      is_collapsed: false,
+    window.initial_eligy = function(eligy) {
+      return _.map(
+        collect_aids_per_contract(eligy), 
+        function(contract_name){
+          return {
+            name: contract_name, 
+            is_collapsed: false,
+            aids: _.map(
+              collect_aids(eligy, contract_name), 
+              function(aid_name) {
+                return {
+                  name: aid_name,
+                  is_collapsed: false,
+                  filters: _.map(
+                    collect_filters(eligy, contract_name, aid_name),
+                    function(filter_name) {
+                      return {
+                        name: filter_name,
+                        is_collapsed: false,
+                      }
                     }
-                  }
-                )
+                  )
+                }
               }
-            }
-          )
+            )
 
+          }
         }
-      }
-    );
+      );
+    }
+
     window.initial_state = {
-      eligibles_zone: {
-        is_collapsed: false,
-        aids_per_contract: initial_eligibles_aids_per_contract
-      },
-      ineligibles_zone: {
-        is_collapsed: false,
-        aids_per_contract: _.map($('#o_ineligibles .c-resultcard').map(function(){return $(this).data()["cslug"]}).get(), function(e){return {name:e, is_collapsed:false}})
-      },
-      uncertains_zone: {
-        is_collapsed: false,
-        aids_per_contract: _.map($('#o_uncertains .c-resultcard').map(function(){return $(this).data()["cslug"]}).get(), function(e){return {name:e, is_collapsed:false}})
-      },
+      eligibles_zone: initial_eligy('o_eligibles'),
+      ineligibles_zone: initial_eligy('o_ineligibles'),
+      uncertains_zone: initial_eligy('o_uncertains'),
       filters_zone: {
         is_collapsed: false,
         filters: _.map($('#o_all_filters .c-resultfiltering').map(function(){return $(this).data()["name"]}).get(), function(e){return {name: e, is_checked: false}})
