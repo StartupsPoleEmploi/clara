1eb8b60e322e2dc90a62a4a03d28e6aa75682be4
David Boureau <bdavidxyz@gmail.com>
2018-07-09 17:57:01 +0200

    collect filters ok

--- app/assets/javascripts/result_default.js
+++ app/assets/javascripts/result_default.js
@@ -12,10 +12,16 @@ $(document).on('turbolinks:load', function () {
     *
     *
     **/
+    window.collect_aids_per_contract = function(){
+      return $('#o_eligibles .c-resultcard').map(function(){return $(this).data()["cslug"]}).get();
+    }
+    window.collect_filters = function(eligy, contract_name, aid_name){
+      return $('#' + eligy + ' .c-resultcard[data-cslug="'+contract_name+'"]' + ' .c-resultaid[data-aslug="'+aid_name+'"] .c-resultfilter').map(function(){return $(this).data()["name"]}).get();
+    }
     window.initial_state = {
       eligibles_zone: {
         is_collapsed: false,
-        aids_per_contract: _.map($('#o_eligibles .c-resultcard').map(function(){return $(this).data()["cslug"]}).get(), function(e){return {name:e, is_collapsed:false}})
+        aids_per_contract: _.map($('#o_eligibles .c-resultcard').map(function(){return $(this).data()["cslug"]}).get(), function(e){return {name:e, is_collapsed:false, aids: []}})
       },
       ineligibles_zone: {
         is_collapsed: false,
@@ -28,7 +34,7 @@ $(document).on('turbolinks:load', function () {
       filters_zone: {
         is_collapsed: false,
         filters: _.map($('#o_all_filters .c-resultfiltering').map(function(){return $(this).data()["name"]}).get(), function(e){return {name: e, is_checked: false}})
-      }
+      },
       recap_zone: {
         is_collapsed: false
       }
