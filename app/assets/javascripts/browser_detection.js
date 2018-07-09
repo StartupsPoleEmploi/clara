73e59d40296b14d71f967c328ff3b3044cae51bd
David Boureau <bdavidxyz@gmail.com>
2018-07-09 10:22:17 +0200

    template modified

--- app/assets/javascripts/browser_detection.js
+++ app/assets/javascripts/browser_detection.js
@@ -1,10 +1,10 @@
-$(document).on('ready', function () {
+$(document).on('ready turbolinks:load', function () {
   var template_to_add = 
-  "<div class='.c-browser-deprecated u-padding' style='background-color: orange;'>" +
-    "<div class='.c-browser-deprecated__line'>" +
+  "<div class='c-browser-deprecated u-padding' style='background-color: orange;'>" +
+    "<div class='c-browser-deprecated__line'>" +
       "⚠ Certains contenus ne seront pas correctement affichés car votre navigateur est obsolète." +
     "</div>" +
-    "<div class='.c-browser-deprecated__line'>" +
+    "<div class='c-browser-deprecated__line'>" +
       "<a href='https://whatbrowser.org'>Ce site</a> peut vous aider à mettre à jour votre navigateur." +
     "</div>" +
   "</div>";
