d38c463ec2f76b082e3765ab9e7ea34dade10fe8
David Boureau <bdavidxyz@gmail.com>
2018-07-10 09:39:03 +0200

    firing action

--- app/assets/javascripts/result_default.js
+++ app/assets/javascripts/result_default.js
@@ -12,17 +12,17 @@ $(document).on('turbolinks:load', function () {
     *
     *
     **/
-    window.collect_aids_per_contract = function(eligy){
+    var collect_aids_per_contract = function(eligy){
       return $('#' + eligy + ' .c-resultcard').map(function(){return $(this).data()["cslug"]}).get();
     }
-    window.collect_aids = function(eligy, contract_name){
+    var collect_aids = function(eligy, contract_name){
       return $('#' + eligy + ' .c-resultcard[data-cslug="'+contract_name+'"]' + ' .c-resultaid').map(function(){return $(this).data()["aslug"]}).get();
     }
-    window.collect_filters = function(eligy, contract_name, aid_name){
+    var collect_filters = function(eligy, contract_name, aid_name){
       return $('#' + eligy + ' .c-resultcard[data-cslug="'+contract_name+'"]' + ' .c-resultaid[data-aslug="'+aid_name+'"] .c-resultfilter').map(function(){return $(this).data()["name"]}).get();
     }
 
-    window.initial_eligy = function(eligy) {
+    var initial_eligy = function(eligy) {
       return _.map(
         collect_aids_per_contract(eligy), 
         function(contract_name){
@@ -40,23 +40,29 @@ $(document).on('turbolinks:load', function () {
                     function(filter_name) {
                       return {
                         name: filter_name,
-                        is_collapsed: false,
+                        is_shown: false,
                       }
                     }
                   )
                 }
               }
             )
-
           }
         }
       );
     }
 
     window.initial_state = {
-      eligibles_zone: initial_eligy('o_eligibles'),
-      ineligibles_zone: initial_eligy('o_ineligibles'),
-      uncertains_zone: initial_eligy('o_uncertains'),
+      eligibles_zone: {
+        eligibles: initial_eligy('o_eligibles')
+      },
+      uncertains_zone: {
+        uncertains: initial_eligy('o_uncertains')
+      },
+      ineligibles_zone: {
+        is_collapsed: true,
+        ineligibles: initial_eligy('o_ineligibles')
+      },
       filters_zone: {
         is_collapsed: false,
         filters: _.map($('#o_all_filters .c-resultfiltering').map(function(){return $(this).data()["name"]}).get(), function(e){return {name: e, is_checked: false}})
@@ -81,7 +87,15 @@ $(document).on('turbolinks:load', function () {
       if (state === undefined) {
         return initial_state;
       }
-      var newState = _.assign({}, state);
+
+      // Works better than _.assign or Object.assign
+      var newState = JSON.parse(JSON.stringify(state));
+
+      if (action.type === 'TOGGLE_FILTERS_ZONE') {
+        _.set(newState, 'filters_zone.is_collapsed', !_.get(newState, 'filters_zone.is_collapsed'));
+      }
+
+
       return newState;
     }
 
@@ -96,9 +110,23 @@ $(document).on('turbolinks:load', function () {
     *
     *
     **/
-    var main_store = Redux.createStore(main_reducer, initial_state);
+    window.main_store = Redux.createStore(main_reducer, initial_state);
 
-    main_store.dispatch({ type: 'init' });
+    /**
+    *
+    *
+    *
+    *         DISPATCHERS 
+    *
+    *
+    *
+    *
+    **/
+    $('.js-filters-zone').on('click', function(){main_store.dispatch({type: 'TOGGLE_FILTERS_ZONE'})})
+
+    main_store.dispatch({ type: 'INIT' });
+    
+
 
   }
 });
