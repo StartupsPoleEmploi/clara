e0b1b3634551571198228011923c07d7a72cb497
David Boureau <bdavidxyz@gmail.com>
2018-07-12 11:21:18 +0200

    print ok

--- app/assets/javascripts/print.js
+++ app/assets/javascripts/print.js
@@ -1,6 +1,6 @@
 $(document).on('ready turbolinks:load', function () {
 
-  $('.c-breadcrumb-print').click(function(e) {
+  $('.c-breadcrumb-printer__item').click(function(e) {
     if (typeof ga === "function") {
       ga('send', 'event', 'results', 'print', document.location.pathname);
     }
