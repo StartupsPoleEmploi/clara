bc75518a80f09c6790c9ef7075980f27241594b2
David Boureau <bdavidxyz@gmail.com>
2018-07-12 14:44:03 +0200

    print ok

--- app/assets/javascripts/print.js
+++ app/assets/javascripts/print.js
@@ -4,6 +4,9 @@ $(document).on('ready turbolinks:load', function () {
     if (typeof ga === "function") {
       ga('send', 'event', 'results', 'print', document.location.pathname);
     }
+    window.main_store.dispatch({type:'FOLD_ELIGY', eligy_name: "eligibles"});
+    window.main_store.dispatch({type:'FOLD_ELIGY', eligy_name: "ineligibles"});
+    window.main_store.dispatch({type:'FOLD_ELIGY', eligy_name: "uncertains"});
     window.print();
   });
 
