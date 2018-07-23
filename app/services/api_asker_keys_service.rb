6a30997453e19a6c1f5d938e893a8842e059eaaa
David Boureau <bdavidxyz@gmail.com>
2018-07-23 15:32:42 +0200

    reverse hash

--- app/services/api_asker_keys_service.rb
+++ app/services/api_asker_keys_service.rb
@@ -4,18 +4,19 @@ class ApiAskerKeysService
 
   def asker_hash
     {
-      v_spectacle: ""
-      v_handicap: ""
-      v_diplome: ""                
-      v_category: ""     
-      v_duree_d_inscription: ""
-      v_allocation_type: ""    
-      v_allocation_value_min: ""
-      v_age: ""       
-      v_location_citycode: ""
+      v_spectacle: "spectacle",
+      v_handicap: "disabled",
+      v_diplome: "diploma",
+      v_category: "category",
+      v_duree_d_inscription: "inscription_period",
+      v_allocation_type: "allocation_type",
+      v_allocation_value_min: "monthly_allocation_value",
+      v_age: "age",
+      v_location_citycode: "location_citycode"
     }
-   
   end
   
-
+  def reverse_hash
+    asker_hash.invert
+  end
 end
