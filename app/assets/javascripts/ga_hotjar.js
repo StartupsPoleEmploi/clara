2f50a63544a6a224dbc1ed767e368f04a8a0197b
David Boureau <bdavidxyz@gmail.com>
2018-07-23 09:48:11 +0200

    done

--- app/assets/javascripts/ga_hotjar.js
+++ app/assets/javascripts/ga_hotjar.js
@@ -11,10 +11,12 @@ $(document).on('ready', function () {
     $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
       var current_ip = _.get(data, 'ip');
       var pe_ips = _.split(window.clara.env.ARA_URL_PE, ",");
+      console.log(pe_ips)
+      console.log(current_ip)
       if (_.includes(pe_ips, current_ip)) {
         console.log("Bienvenue chez Pôle Emploi");
         //load hotjar
-        (function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:window.clara.env.ARA_HOTJAR_ID,hjsv:5}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+        (function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:window.clara.hotjar_id,hjsv:5}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
         ga('set', 'dimension1', 'true');
       } else {
         console.log("Bienvenue");
