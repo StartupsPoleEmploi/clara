54d561836fdde3cb2f6a310e50a11484c3683931
David Boureau <bdavidxyz@gmail.com>
2018-07-25 15:25:49 +0200

    ZrrService cleaned

--- app/services/zrr_service.rb
+++ app/services/zrr_service.rb
@@ -8,22 +8,6 @@ class ZrrService
     
     ActivatedModelsService.get_instance.zrr?(citycode.to_s) ? "oui" : "non"
 
-    # if (citycode && citycode.is_a?(String) && !citycode.blank?)
-    #   escaped_address = URI.escape(@base_url + "/#{citycode}") 
-    #   uri = URI.parse(escaped_address)
-    #   response = HttpService.get_instance.get(uri)
-    #   if !response.blank? && response.include?("timeout")
-    #     return 'service_indisponible'
-    #   elsif !response.blank? && response.include?("{\"id\":\"#{citycode}\"}")
-    #     return 'en_zrr'
-    #   elsif !response.blank? && !response.include?("id")
-    #     return 'hors_zrr'
-    #   else
-    #     return 'erreur_communication'
-    #   end
-    # else
-    #   return 'erreur_technique'
-    # end
   end
   
 end
