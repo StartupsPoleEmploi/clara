2acad077d0cad3498fe935118762eb24db8d9f02
maxdeval <maxime.devallez@pole-emploi.fr>
2018-07-25 15:28:46 +0200

    green test

--- spec/services/age_service_spec.rb
+++ spec/services/age_service_spec.rb
@@ -4,10 +4,13 @@ describe AgeService do
   describe '.new_and_download' do
     it 'Returns a new age form with age filled from given asker' do
       #given
+      asker = Asker.new
+      asker.v_age = 'a_age'
       #when
+      result = AgeService.new.new_and_download(asker)
       #then
-      expect(result.class.to_i).to eq('AgeForm')
-      expect(result.type).to eq(16)
+      expect(result.class.to_s).to eq('AgeForm')
+      expect(result.number_of_years).to eq('a_age')
     end
   end
 end
