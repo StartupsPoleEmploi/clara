77ae8b746da55ce77ad534d82864a71a99e81366
David Boureau <bdavidxyz@gmail.com>
2018-07-26 15:52:32 +0200

    cleaned

--- app/services/zrr_service.rb
+++ app/services/zrr_service.rb
@@ -5,12 +5,14 @@ require 'json'
 class ZrrService
 
   def zrr?(citycode) 
-    full_url = "https://bdavidxyz.github.io/zrrs/"
-    escaped_address = URI.escape(full_url) 
-    uri = URI.parse(escaped_address)
-    response = HttpService.get_instance.get(uri)
-
-    response && response.include?('citycode') ? "oui" : "non"
+    zrrs = Rails.cache.fetch('zrrs') {
+      full_url = "https://bdavidxyz.github.io/zrrs/"
+      escaped_address = URI.escape(full_url) 
+      uri = URI.parse(escaped_address)
+      response = HttpService.get_instance.get(uri)
+      response.to_s
+    }
+    zrrs && zrrs.include?(citycode) ? "oui" : "non"
   end
   
 end
