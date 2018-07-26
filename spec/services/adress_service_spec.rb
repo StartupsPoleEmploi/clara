1bf38129ec03f25919ba9e9973dac7b9422e5495
maxdeval <maxime.devallez@pole-emploi.fr>
2018-07-26 10:41:04 +0200

    red test

--- spec/services/adress_service_spec.rb
+++ spec/services/adress_service_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
-describe AdressService do
-
+describe AddressService do
+  describe '.upload' do
+    it 'should inject an adress into asker' do
+      #given
+      #when
+      #then
+      expect(asker.v_location_label).to eq('')
+      expect(asker.v_location_route).to eq('')
+      expect(asker.v_location_city).to eq('')
+      expect(asker.v_location_zipcode).to eq('')
+      expect(asker.v_location_citycode).to eq('')
+      expect(asker.v_location_street_number).to eq('')
+      expect(asker.v_location_state).to eq('')
+    end
+  end
 
 end
 
