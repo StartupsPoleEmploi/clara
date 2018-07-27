622e71f34e2514b506be99a1fcaf74d0e524a546
David Boureau <bdavidxyz@gmail.com>
2018-07-27 10:37:53 +0200

    no more user

--- app/services/activated_models_service.rb
+++ app/services/activated_models_service.rb
@@ -7,11 +7,11 @@ class ActivatedModelsService
 
       all_activated_aids_json = Aid.activated.to_json(:only => [ :id, :name, :slug, :short_description, :rule_id, :contract_type_id ], :include => {filters: {only:[:id, :slug]}})
       all_filters_json = Filter.all.to_json(:only => [ :id, :slug ])
-      all_contracts_json = ContractType.all.to_json(:only => [ :id ])
+      all_contracts_json = ContractType.all.to_json(:only => [ :id, :slug ])
       all_variables_json = Variable.all.to_json(:only => [ :id, :name, :variable_type, :description ])
-      r_all = JSON.parse(Rule.all.to_json(:only => [ :id, :value_eligible, :operator_type, :composition_type, :variable_id, :value_ineligible ], :include => {slave_rules: {only:[:id]}}))
+      r_all = JSON.parse(Rule.all.to_json(:only => [ :id, :name, :value_eligible, :operator_type, :composition_type, :variable_id, :value_ineligible ], :include => {slave_rules: {only:[:id, :name]}}))
       all_rules = r_all.map{|h| HashService.new.recursive_compact(h)}
-      all_users_json = User.all.to_json(:only => [ :email ])
+      all_users_json = User.all.to_json(:only => [ :id, :email ])
       activated_models = {}
       activated_models["all_activated_aids"] = JSON.parse(all_activated_aids_json)
       activated_models["all_filters"]        = JSON.parse(all_filters_json)
