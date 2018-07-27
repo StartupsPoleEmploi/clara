ea80f7509bcf706da7a56f8e03fb94aae29917ad
David Boureau <bdavidxyz@gmail.com>
2018-07-27 11:36:56 +0200

    removed track call

--- app/services/activated_models_service.rb
+++ app/services/activated_models_service.rb
@@ -1,31 +1,9 @@
 class ActivatedModelsService
   include Singleton
-  
 
-  def initialize
-    @all_activated_models = Rails.cache.fetch("activated_models") do
-
-      all_activated_aids_json = Aid.activated.to_json(:only => [ :id, :name, :slug, :short_description, :rule_id, :contract_type_id ], :include => {filters: {only:[:id, :slug]}})
-      all_filters_json = Filter.all.to_json(:only => [ :id, :slug ])
-      all_contracts_json = ContractType.all.to_json(:only => [ :id, :slug ])
-      all_variables_json = Variable.all.to_json(:only => [ :id, :name, :variable_type, :description ])
-      r_all = JSON.parse(Rule.all.to_json(:only => [ :id, :name, :value_eligible, :operator_type, :composition_type, :variable_id, :value_ineligible ], :include => {slave_rules: {only:[:id, :name]}}))
-      all_rules = r_all.map{|h| HashService.new.recursive_compact(h)}
-      all_users_json = User.all.to_json(:only => [ :id, :email ])
-      activated_models = {}
-      activated_models["all_activated_aids"] = JSON.parse(all_activated_aids_json)
-      activated_models["all_filters"]        = JSON.parse(all_filters_json)
-      activated_models["all_contracts"]      = JSON.parse(all_contracts_json)
-      activated_models["all_rules"]          = all_rules
-      activated_models["all_variables"]      = JSON.parse(all_variables_json)
-      activated_models["all_users"]          = JSON.parse(all_users_json)
-      # p '- - - - - - - - - - - - - - activated_models- - - - - - - - - - - - - - - -' 
-      # pp activated_models
-      # p ''
-      activated_models
-    end
+  def write(val)
+    @all_activated_models = val
   end
-
   def read
     @all_activated_models
   end
@@ -48,70 +26,6 @@ class ActivatedModelsService
 
   def variables
     @all_activated_models["all_variables"]
-  end
-
-  def users
-    @all_activated_models["all_users"]
-  end
-
-  def _clean_all_activated_aids(aids)
-    aids.each do |aid|  
-      aid.delete("created_at")
-      aid.delete("updated_at")
-      aid.delete("what")
-      aid.delete("short_description")
-      aid.delete("how_much")
-      aid.delete("additionnal_conditions")
-      aid.delete("limitations")
-      aid.delete("how_and_when")
-      aid["filters"].map! do |filter|
-        filter.select {|k,v| k == "id" }
-      end
-    end
-    aids
-  end
-
-  def _clean_all_contracts(contracts)
-    contracts.each do |contract|  
-      contract.delete("created_at")
-      contract.delete("updated_at")
-    end
-    contracts
-  end
-
-  def _clean_all_filters(filters)
-    filters.each do |filter|  
-      filter.delete("created_at")
-      filter.delete("updated_at")
-    end
-    filters
-  end
-
-  def _clean_all_users(users)
-    users.each do |user|  
-      user.delete("created_at")
-      user.delete("updated_at")
-    end
-    users
-  end
-
-  def _clean_all_rules(rules)
-    rules.each do |rule|  
-      rule.delete("created_at")
-      rule.delete("updated_at")
-      rule["slave_rules"].map! do |slave_rule|
-        slave_rule.select {|k,v| k == "id" }
-      end
-    end
-    rules
-  end
-
-  def _clean_all_variables(variables)
-    variables.each do |variable|  
-      variable.delete("created_at")
-      variable.delete("updated_at")
-    end
-    variables
   end
 
 end
--- app/services/zrr_service.rb
+++ app/services/zrr_service.rb
@@ -8,14 +8,18 @@ class ZrrService
     str_val = citycode.to_s
     five_digits_only = /\A\d{5}\z/
     has_5_digits = !!str_val.match(five_digits_only)
-    zrrs = Rails.cache.fetch('zrrs') {
-      full_url = "https://bdavidxyz.github.io/zrrs/"
-      escaped_address = URI.escape(full_url) 
-      uri = URI.parse(escaped_address)
-      response = HttpService.get_instance.get(uri)
-      response.to_s
-    }
-    has_5_digits && zrrs && zrrs.include?(citycode) ? "oui" : "non"
+    return false unless has_5_digits
+    # full_url = "https://qpvzrr.herokuapp.com/zrr/zrrs/#{citycode}"
+    # escaped_address = URI.escape(full_url) 
+    # uri = URI.parse(escaped_address)
+    # response = HttpService.get_instance.get(uri)
+    # zrrs = response.to_s
+    # zrrs && zrrs.include?(citycode) ? "oui" : "non"
+    zrrs = Rails.cache.fetch("zrrs") do
+      Zrr.first.value  
+    end
+    zrrs && zrrs.include?(citycode) ? "oui" : "non"
   end
   
+
 end
