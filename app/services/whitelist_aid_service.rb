8a6c2d05e95e92c49f52f1e6e63f2869db4e5a10
David Boureau <bdavidxyz@gmail.com>
2018-07-28 10:56:39 +0200

    correct filters in aid list

--- app/services/whitelist_aid_service.rb
+++ app/services/whitelist_aid_service.rb
@@ -13,18 +13,19 @@ class WhitelistAidService
   end
   
   def for_aid_in_list(aid_hash)
+    aids = aid_hash.deep_dup
     a = ActivatedModelsService.instance
-    return {} unless aid_hash.is_a?(Hash)
-    aid_hash["contract_type"] = a.contract_types.find{|c| c["id"] == aid_hash["contract_type_id"] }["slug"] if aid_hash["contract_type_id"].is_a?(Integer) 
+    return {} unless aids.is_a?(Hash)
+    aids["contract_type"] = a.contract_types.find{|c| c["id"] == aids["contract_type_id"] }["slug"] if aids["contract_type_id"].is_a?(Integer) 
     #  WEIRD BUG BELOW
-    # aid_hash["filters"].map!{|f| a.filters.find{|c| p c;p f; c["id"] == f["id"]}["slug"]} if aid_hash["filters"].is_a?(Array) && !aid_hash["filters"].empty?
+    aids["filters"].map!{|f| a.filters.find{|c| c["id"] == f["id"]}["slug"]} if aids["filters"].is_a?(Array) && !aids["filters"].empty?
     # pp '-------------------------------------------------------'
-    # pp aid_hash["filters"]
-    # aid_hash["filters"].map!{|f| f["slug"]}
-    # pp aid_hash["filters"]
+    # pp aids["filters"]
+    # aids["filters"].map!{|f| f["slug"]}
+    # pp aids["filters"]
     wanted_keys = %w[name slug short_description contract_type filters]
 
-    return aid_hash.select { |key, _| wanted_keys.include? key }
+    return aids.select { |key, _| wanted_keys.include? key }
   end
   
 end
