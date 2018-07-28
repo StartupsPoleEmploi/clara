58842817f07c40e670047f0fc31a2dbe3903fdb6
David Boureau <bdavidxyz@gmail.com>
2018-07-28 10:51:45 +0200

    better

--- app/services/hash_service.rb
+++ app/services/hash_service.rb
@@ -28,16 +28,13 @@ class HashService
 
   # Based on https://stackoverflow.com/a/35785227/2595513
   def reject_keys_that_starts_with! x, val, count=0
-    has_been_rejected = false
-    x_size_before = x.respond_to?(:size) ? x.size : 0
-    if x.is_a? Hash
-      has_been_rejected = !!x.reject!{|k,v| k.to_s.start_with?(val)}
-    end
-    x_size_after = x.respond_to?(:size) ? x.size : 0
-    count += x_size_before - x_size_after if has_been_rejected
     if x.is_a?(Array)
       x.each{|e| count = reject_keys_that_starts_with! e, val, count}  
     elsif x.is_a?(Hash)
+      x_size_before = x.respond_to?(:size) ? x.size : 0
+      !!x.reject!{|k,v| k.to_s.start_with?(val)}
+      x_size_after = x.respond_to?(:size) ? x.size : 0
+      count += x_size_before - x_size_after
       x.each do |k,v| 
         if x[k].is_a? Hash
           x[k].each{|m,n| count = reject_keys_that_starts_with! x[k], val, count}
