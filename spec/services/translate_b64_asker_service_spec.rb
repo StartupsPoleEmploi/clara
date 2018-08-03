7562b60c66e37980c2d30b63142049271a91a90f
David Boureau <bdavidxyz@gmail.com>
2018-08-03 12:02:26 +0200

    wip

--- spec/services/translate_b64_asker_service_spec.rb
+++ spec/services/translate_b64_asker_service_spec.rb
@@ -6,10 +6,19 @@ describe TranslateB64AskerService do
     #given
     asker = create(:asker, :full_user_input)
     #when
-    result = TranslateB64AskerService.new.into_b64(asker)
+    sut = TranslateB64AskerService.new.into_b64(asker)
     #then
-    expect(result).to eq("MzUsMSxvLDMsbyxwLDc5MzUxLDM0MCxu")
-    # expect(Base64.urlsafe_decode64(asker_64))
+    expect(sut).to eq("MzUsMSxvLDMsbyxwLDc5MzUxLDM0MCxu")
+  end
+
+  it 'The generated string must be an list properties once decoded' do
+    #given
+    asker = create(:asker, :full_user_input)
+    #when
+    str = TranslateB64AskerService.new.into_b64(asker)
+    sut = Base64.urlsafe_decode64(str)
+    #then
+    expect(sut).to eq("35,1,o,3,o,p,79351,340,n")
   end
 
 end
