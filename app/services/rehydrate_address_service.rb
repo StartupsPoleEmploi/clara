f0811c9d1240ac27ab011efc80b78d8988461874
David Boureau <bdavidxyz@gmail.com>
2018-08-03 14:21:49 +0200

    rehydrate zipcode and cityname

--- app/services/rehydrate_address_service.rb
+++ app/services/rehydrate_address_service.rb
@@ -3,8 +3,9 @@ class RehydrateAddressService
   def from_citycode!(asker)
     return asker unless asker.is_a?(Asker) && asker.v_location_citycode != nil
     CalculateAskerService.new(asker).calculate_zrr!
-    BanService.get_instance.get_zipcode_and_cityname(asker)
-    asker
+    res = BanService.get_instance.get_zipcode_and_cityname(asker)
+    asker.v_location_zipcode = res.is_a?(Array) ? res[0] : nil
+    asker.v_location_city = res.is_a?(Array) ? res[1] : nil
   end
 
 end
