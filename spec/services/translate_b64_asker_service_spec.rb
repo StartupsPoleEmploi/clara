f835b4323fbaa79f9cd21b80f67834a358b145b3
David Boureau <bdavidxyz@gmail.com>
2018-08-03 12:10:20 +0200

    tests ok

--- spec/services/translate_b64_asker_service_spec.rb
+++ spec/services/translate_b64_asker_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe TranslateB64AskerService do
 
-  it 'Should translate an inputed-asker into a non-user-friendly string' do
+  it '.into_b64 Should translate an inputed-asker into a non-user-friendly string' do
     #given
     asker = create(:asker, :full_user_input)
     #when
@@ -11,14 +11,38 @@ describe TranslateB64AskerService do
     expect(sut).to eq("MzUsMSxvLDMsbyxwLDc5MzUxLDM0MCxu")
   end
 
-  it 'The generated string must be an list properties once decoded' do
+  it '.into_b64 The generated string must be an list properties once decoded' do
     #given
     asker = create(:asker, :full_user_input)
-    #when
     str = TranslateB64AskerService.new.into_b64(asker)
+    #when
     sut = Base64.urlsafe_decode64(str)
     #then
     expect(sut).to eq("35,1,o,3,o,p,79351,340,n")
+  end
+
+  it '.from_b64 Should return an asker' do
+    #given
+    #when
+    sut = TranslateB64AskerService.new.from_b64("MjYsNCxvLDMsbyxtLDgyNzEyLDIwMDMsbg==")
+    #then
+    expect(sut.is_a?(Asker)).to eq(true)
+  end
+
+  it '.from_b64 asker properties must be filled' do
+    #given
+    #when
+    sut = TranslateB64AskerService.new.from_b64("MjYsNCxvLDMsbyxtLDgyNzEyLDIwMDMsbg==")
+    #then
+    expect(sut.v_handicap)            .to eq("oui")
+    expect(sut.v_spectacle)           .to eq("non")
+    expect(sut.v_diplome)             .to eq("niveau_3")
+    expect(sut.v_category)            .to eq("autres_cat")
+    expect(sut.v_duree_d_inscription) .to eq("moins_d_un_an")
+    expect(sut.v_allocation_value_min).to eq("2003")
+    expect(sut.v_allocation_type)     .to eq("RSA")
+    expect(sut.v_age)                 .to eq("26")
+    expect(sut.v_location_citycode)   .to eq("82712")
   end
 
 end
