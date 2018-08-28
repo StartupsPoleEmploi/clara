e1afca0363b8b6919584289c6b7df4bb7f899252
maxdeval <maxime.devallez@pole-emploi.fr>
2018-08-28 15:00:03 +0200

    green test

--- spec/services/hash_service_spec.rb
+++ spec/services/hash_service_spec.rb
@@ -4,11 +4,21 @@ describe HashService do
   describe 'recursive_compact' do
     it 'should remove all empty elements from a hash' do
       #given
-      hash_or_array = { "Jane Doe" => 2 , "Jim Doe" => nil }
+      myHash = { "Jane Doe" => 2 , "Jim Doe" => nil }
       #when
-      result = HashService.new.recursive_compact(hash_or_array)
+      result = HashService.new.recursive_compact(myHash)
       #then
       expect(result).to eq({ "Jane Doe" => 2 })
+    end
+  end
+  describe 'reject_ids!' do
+    it 'should remove id and _id ' do
+      #given
+      x = {"my_id" => 1, "id" => 2, "anything else" => 3}
+      #when
+      result = HashService.new.reject_ids!(x)
+      #then
+      expect(result).to eq({"anything else" => 3})
     end
   end
 end
