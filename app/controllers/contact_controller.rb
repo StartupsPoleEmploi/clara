fad93e20df0568a4e4f1f0dd22d2751edbd08b7e
David Boureau <bdavidxyz@gmail.com>
2018-09-04 15:14:33 +0200

    contact_controller up

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,13 +2,23 @@ class ContactController < ApplicationController
   
 
   def index
+    @contact = ContactForm.new
   end
 
   def create
+    @contact = AddressForm.new(allowed_params)
   end
   
   def allowed_params
-    params.require(:contact_form).permit(:blabla).to_h
+    params.require(:contact_form).permit(
+     :first_name,
+     :last_name,
+     :email,
+     :region,
+     :youare,
+     :askfor,
+     :question,
+    ).to_h
   end
 
 end
