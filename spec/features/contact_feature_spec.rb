f0770a8a7d0114de51b42ebc3e38635c48e37429
David Boureau <bdavidxyz@gmail.com>
2018-09-07 14:46:07 +0200

    nominal scenario under test

--- spec/features/contact_feature_spec.rb
+++ spec/features/contact_feature_spec.rb
@@ -22,6 +22,19 @@ feature 'Contact' do
     expect(page).to have_css('.c-contact-card')
   end
 
+  scenario 'Nominal' do 
+    # given
+    visit contact_index_path
+    # when
+    find('#first_name').set('Francis')
+    find('#last_name').set('Drake')
+    find('#email').set('francis@drake.com')
+    find("#region").select("Bretagne")
+    find('#send_message').click
+    # then
+    save_and_open_page
+  end
+
   scenario 'After a successful attempt, cannot send sth again'
   scenario 'After a successful attempt, back button means back to welcome page'
   scenario 'If Honeypot if filled, the form fails to be validated'
