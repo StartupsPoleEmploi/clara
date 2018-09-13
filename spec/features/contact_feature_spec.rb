0c94dc089fdadb994242a192df9880286435c4c2
David Boureau <bdavidxyz@gmail.com>
2018-09-13 14:46:33 +0200

    specs green again

--- spec/features/contact_feature_spec.rb
+++ spec/features/contact_feature_spec.rb
@@ -197,7 +197,7 @@ feature 'Contact' do
     find('#email').set('francis@drake.com')
     find("#region").select("Bretagne")
     find("#youare").select("Un particulier")
-    find("#askfor").select("Apporter une information pour modifier un contenu")
+    choose("Apporter une information pour modifier un contenu")
     find("#question").set("Mais pourquoi une question ?")
     expect(page).not_to have_css('.is-error')
     expect(page).not_to have_css('ul.c-contact-errors-list li')
