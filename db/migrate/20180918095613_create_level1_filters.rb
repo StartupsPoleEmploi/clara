1bffd53135d8ac10bf5b73ecafa34a1cd1875015
David Boureau <bdavidxyz@gmail.com>
2018-09-18 11:58:13 +0200

    migration ok

--- db/migrate/20180918095613_create_level1_filters.rb
+++ db/migrate/20180918095613_create_level1_filters.rb
@@ -1,7 +1,7 @@
 class CreateLevel1Filters < ActiveRecord::Migration[5.2]
   def change
     create_table :level1_filters do |t|
-      t.string :name
+      t.string :name, unique: true
 
       t.timestamps
     end
