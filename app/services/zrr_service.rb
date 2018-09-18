deeb741819afc26fa39c28c11117e48eece38cc4
David Boureau <bdavidxyz@gmail.com>
2018-09-18 16:06:56 +0200

    simplified

--- app/services/zrr_service.rb
+++ app/services/zrr_service.rb
@@ -5,8 +5,6 @@ require 'json'
 class ZrrService
 
   def zrr?(citycode)
-    false_value = "non" 
-    true_value = "oui" 
     str_val = citycode.to_s
     five_digits_only = /\A\d{5}\z/
     has_5_digits = !!str_val.match(five_digits_only)
@@ -14,7 +12,7 @@ class ZrrService
     zrrs = Rails.cache.fetch("zrrs") do
       Zrr.first ? Zrr.first.value  : ""
     end
-    zrrs && zrrs.include?(citycode) ? true_value : false_value
+    zrrs && zrrs.include?(citycode) ? "oui" : "non"
   end
   
 
