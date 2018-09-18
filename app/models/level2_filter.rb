3d4b38766a4eefb16f87eb3681bcd336cd9b31ab
David Boureau <bdavidxyz@gmail.com>
2018-09-18 14:43:00 +0200

    trying level2 hasmany level3

--- app/models/level2_filter.rb
+++ app/models/level2_filter.rb
@@ -3,6 +3,7 @@ class Level2Filter < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
+  has_many :level3_filters
   friendly_id :name, use: :slugged
 
   def should_generate_new_friendly_id?
