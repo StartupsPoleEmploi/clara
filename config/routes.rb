87772d2aa7d63279b95556749f59fc8e1c13c8c1
David Boureau <bdavidxyz@gmail.com>
2018-09-18 12:22:18 +0200

    level3_filters ok

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
     resources :users
     resources :level1_filters
     resources :level2_filters
+    resources :level3_filters
     resources :filters
     resources :aids
     resources :rules do 
