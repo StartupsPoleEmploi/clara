0a17dd36ea9e36b8e5ab82f1cc65858272a73b25
David Boureau <bdavidxyz@gmail.com>
2018-09-18 15:24:44 +0200

    level3 with aids

--- app/models/level3_filter.rb
+++ app/models/level3_filter.rb
@@ -3,6 +3,8 @@ class Level3Filter < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
+  has_and_belongs_to_many :aids
+  
   friendly_id :name, use: :slugged
 
   def should_generate_new_friendly_id?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_122728) do
+ActiveRecord::Schema.define(version: 2018_09_18_130700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 2018_09_18_122728) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.bigint "level2_filter_id"
+    t.boolean "confidentiality", default: true
     t.index ["level2_filter_id"], name: "index_level3_filters_on_level2_filter_id"
     t.index ["slug"], name: "index_level3_filters_on_slug", unique: true
   end
