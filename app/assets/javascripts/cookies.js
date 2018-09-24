1c62d3e5542bce67253ad9d0fa8325057acf634b
David Boureau <bdavidxyz@gmail.com>
2018-09-24 10:54:59 +0200

    removed document ready

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -1,4 +1,4 @@
-$(document).on('ready turbolinks:load', function () {
+$(document).on('turbolinks:load', function () {
   if ($('body').hasClass('cookies', 'edit')) {
 
     console.log('cookies editing...')
