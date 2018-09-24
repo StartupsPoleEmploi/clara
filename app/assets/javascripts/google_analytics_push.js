c89ea5750ee498fd33a0239cbfe0e34ec559693b
David Boureau <bdavidxyz@gmail.com>
2018-09-24 12:17:09 +0200

    cleaned

--- app/assets/javascripts/google_analytics_push.js
+++ app/assets/javascripts/google_analytics_push.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function () {
-  if (typeof ga === "function" && !_.get(window, 'gon.disable_analytics')) {
+  if (typeof ga === "function") {
     ga("set", "location", location.pathname + location.search);
     return ga("send", "pageview");
   }
