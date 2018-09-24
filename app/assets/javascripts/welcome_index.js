ebe40bee3668f38874a66dbfcd4754eef2355392
maxdeval <maxime.devallez@pole-emploi.fr>
2018-09-24 16:49:58 +0200

    trying to create storage for welcome index

--- app/assets/javascripts/welcome_index.js
+++ app/assets/javascripts/welcome_index.js
@@ -1,5 +1,6 @@
 $(document).on('ready turbolinks:load', function () {
   if ($('body').hasClass('welcome', 'index')) {
+    var cookiesBannerStorage = store.set("ok_for_all_cookies", "true")
     if (localStorage) {
       localStorage.clear();
     }
