fc4525e66679d25edf7007f122fed9cdbb98a48b
David Boureau <bdavidxyz@gmail.com>
2018-09-24 11:52:10 +0200

    better logging

--- app/assets/javascripts/google_analytics_push.js
+++ app/assets/javascripts/google_analytics_push.js
@@ -1,6 +1,4 @@
 $(document).on('turbolinks:load', function () {
-  console.log("!_.get(window, 'gon.disable_analytics')")
-  console.log(!_.get(window, 'gon.disable_analytics'))
   if (typeof ga === "function" && !_.get(window, 'gon.disable_analytics')) {
     ga("set", "location", location.pathname + location.search);
     return ga("send", "pageview");
--- app/assets/javascripts/remove_ga.js
+++ app/assets/javascripts/remove_ga.js
@@ -1,4 +1,6 @@
 $(document).on('turbolinks:load', function () {
+console.log('remove GA');
+
   var disable_analytics = _.get(window, 'gon.disable_analytics');
   if (disable_analytics === true) {
     $( "script[src*='analytics.com']" ).remove();    
