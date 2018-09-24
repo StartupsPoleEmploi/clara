b097cd82665e9bd5611341123ef08c652a891716
David Boureau <bdavidxyz@gmail.com>
2018-09-24 11:26:03 +0200

    one first working version of disabling analytics

--- app/assets/javascripts/google_analytics_push.js
+++ app/assets/javascripts/google_analytics_push.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function () {
-  if (typeof ga === "function") {
+  if (typeof ga === "function" && !_.get(window, 'gon.disable_analytics')) {
     ga("set", "location", location.pathname + location.search);
     return ga("send", "pageview");
   }
