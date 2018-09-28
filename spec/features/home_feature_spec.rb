5433395c99df100d076ba5e0a0f0ff885ceedd7e
maxdeval <maxime.devallez@pole-emploi.fr>
2018-09-28 16:04:05 +0200

    red test

--- spec/features/home_feature_spec.rb
+++ spec/features/home_feature_spec.rb
@@ -30,4 +30,7 @@ feature 'HomeSpec' do
     expect(current_path).to eq new_inscription_question_path
   end
 
+  scenario 'Meta balise should not have blank content' do
+    expect().to eq("neOTrE-YKZ9LbgLlaX8UkYN6MJTPlWpeotPQqbrJ19Q" && "TNQnOQs84jCeQUgbEPOAV9gE4LXDUQaG5ypdnlRz2E8" && "eZYLdUFSc9uBqciZ7fcOxXybmBPxcPME-N9NCRvFn8w" && "f_xo9InTg_SGabP-vnvDjrslUaixMcuxCaGidnXJT9s" && "rjlrEGuLOWAOnmomQMmM5epqCRA3dZY4p1oMDbfCxiY" && "YOm5sNGxbHWmN2Rf3Udg6v29ca5YjM3RNRz8TjWA6Pw" && "0jDmSXtIkrU_nDcXoExkAHbG0TmRlGrx7LdJoX9ktWg")
+  end
 end
