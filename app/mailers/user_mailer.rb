c1bdcdbc92219f739bca13a4e7853b2c2ad045e7
David Boureau <bdavidxyz@gmail.com>
2018-10-01 11:40:25 +0200

    trying reply-to

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @contact = params[:contact] 
     @origin = params[:origin] 
-    mail(from: @contact.email, to: ENV['ARA_EMAIL_DESTINATION'], subject: "Demande de contact via le site clara")
+    mail(from: ENV['ARA_EMAIL_USER'], reply_to: @contact.email, to: ENV['ARA_EMAIL_DESTINATION'], subject: "Demande de contact via le site clara")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   
   namespace :api do
     namespace :v1 do
