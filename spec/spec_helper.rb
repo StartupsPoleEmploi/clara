81855e053a66ad8000282258bd3b24ddfa6363ed
David Boureau <bdavidxyz@gmail.com>
2018-10-02 15:33:06 +0200

    try custom matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,3 +24,18 @@ RSpec.configure do |config|
 
 end
 
+# See https://stackoverflow.com/a/18266402/2595513
+RSpec::Matchers.define :have_meta do |name, expected|
+  match do |actual|
+    has_css?("meta[name='#{name}'][content='#{expected}']")
+  end
+
+  failure_message_for_should do |actual|
+    actual = first("meta[name='#{name}']")
+    if actual
+      "expected that meta #{name} would have content='#{expected}' but was '#{actual[:content]}'"
+    else
+      "expected that meta #{name} would exist with content='#{expected}'"
+    end
+  end
+end
