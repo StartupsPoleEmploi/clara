d57148b049427e7cc9110f6a5163b374a0490280
David Boureau <bdavidxyz@gmail.com>
2018-10-02 15:55:06 +0200

    removed app/views/layouts/error_layout.html.haml, buggy application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,10 @@ module ApplicationHelper
     content_for :title_data, text.to_s
   end
 
+  def description_data(text)
+    content_for :description_data, text.to_s
+  end
+
   def view_object(name, args = {})
     class_name = name.to_s.titleize.split(" ").join("")
     class_name.constantize.new(self, args)
