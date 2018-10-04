9f90295402317c925f9a2f4b5a6ada5e7f4af289
David Boureau <bdavidxyz@gmail.com>
2018-10-04 15:08:01 +0200

    modified

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,20 +23,3 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
 end
- 
-# See https://stackoverflow.com/a/18266402/2595513
-# See https://github.com/spree/spree/blob/master/core/lib/spree/testing_support/capybara_ext.rb#L148-L161
-RSpec::Matchers.define :have_meta do |name, expected|
-  match do |_actual|
-    has_css?("meta[name='#{name}'][content='#{expected}']", visible: false)
-  end
-
-  failure_message do |actual|
-    actual = first("meta[name='#{name}']")
-    if actual
-      "expected that meta #{name} would have content='#{expected}' but was '#{actual[:content]}'"
-    else
-      "expected that meta #{name} would exist with content='#{expected}'"
-    end
-  end
-end
