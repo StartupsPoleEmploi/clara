2da7d6ee839ddc3e441072e7597683e27c3d04cd
David Boureau <bdavidxyz@gmail.com>
2018-10-04 11:29:57 +0200

    first working version!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,12 @@ module ApplicationHelper
     # quick hack to shorten the description
     local_text = text.to_s
     if request && request.respond_to?("path") && request.path.include?("/aides/detail")
-      local_text = text.to_s.split("(")[0].to_s.split(".")[0].to_s.split(":")[0].to_s
+      plain_text = Nokogiri::HTML(CGI::unescapeHTML(text)).text.to_s
+      escaped_text = plain_text.split("(")[0].to_s.split(".")[0].to_s.split(":")[0].to_s
+      if escaped_text.size > 155
+        escaped_text = escaped_text.split(",")[0].to_s
+      end
+      local_text = escaped_text
     end
     content_for :description_data, local_text
   end
