6b2bf2f6dd2bf6f8b6c0bf10f59babaf0515238f
David Boureau <bdavidxyz@gmail.com>
2018-10-04 11:37:03 +0200

    cleaned

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,9 +10,6 @@ module ApplicationHelper
     if request && request.respond_to?("path") && request.path.include?("/aides/detail")
       plain_text = Nokogiri::HTML(CGI::unescapeHTML(text)).text.to_s
       escaped_text = plain_text.split("(")[0].to_s.split(".")[0].to_s.split(":")[0].to_s
-      p '- - - - - - - - - - - - - - escaped_text- - - - - - - - - - - - - - - -' 
-      pp escaped_text
-      p ''
       if escaped_text.size > 155
         escaped_text = escaped_text.split(",")[0].to_s
       end
