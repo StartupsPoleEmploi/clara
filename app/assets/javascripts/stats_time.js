53c14f31c7486c04a32b0c4d627562ec1d09ae49
David Boureau <bdavidxyz@gmail.com>
2018-10-05 14:36:13 +0200

    stats time works

--- app/assets/javascripts/stats_time.js
+++ app/assets/javascripts/stats_time.js
@@ -1,23 +1,40 @@
+_.set(window, 'clara.draw_stats_time', function() {
+  var loaded = JSON.parse($('.ct-savedtime').attr('data-loaded'));
+
+  new Chartist.Bar('.ct-savedtime', {
+    labels: loaded["ordered_label"],
+    series: [loaded["ordered_serie"]]
+  }, {
+    height: 300,
+    showArea: true,
+    axisX: {
+      // On the x-axis start means top and end means bottom
+      position: 'start'
+    },
+    axisY: {
+      // On the y-axis start means left and end means right
+      position: 'end'
+    }
+  });
+});
+
+
+
 $(document).on('ready', function () {
   if ($('body').hasClasses('stats', 'time')) {
 
-    var loaded = JSON.parse($('.ct-savedtime').attr('data-loaded'));
-
-    new Chartist.Bar('.ct-savedtime', {
-      labels: loaded["ordered_label"],
-      series: [loaded["ordered_serie"]]
-    }, {
-      height: 300,
-      showArea: true,
-      axisX: {
-        // On the x-axis start means top and end means bottom
-        position: 'start'
-      },
-      axisY: {
-        // On the y-axis start means left and end means right
-        position: 'end'
-      }
-    });
+    if (_.get(window, 'Chartist')) {
+      clara.draw_stats_time();
+    } else {
+      $.getScript( "https://cdnjs.cloudflare.com/ajax/libs/chartist/0.11.0/chartist.min.js", function( data, textStatus, jqxhr ) {
+        $.getScript( "https://cdnjs.cloudflare.com/ajax/libs/chartist-plugin-legend/0.6.2/chartist-plugin-legend.min.js", function( data, textStatus, jqxhr ) {
+          $.getScript( "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js", function( data, textStatus, jqxhr ) {
+            console.log("Chartist, plugin, and momentjs loaded ")
+            clara.draw_stats_time();
+          });
+        });
+      });
+    }
 
   }
 });
