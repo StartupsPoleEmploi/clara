04bd5dccab3cd499d8209f4930240886476b70d3
David Boureau <bdavidxyz@gmail.com>
2018-10-05 15:37:32 +0200

    added 500

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,3 @@ Rails.application.config.assets.version = '1.1'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( custom_administrate.css print.css)
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-
