f60bd396cb3663d71959ed7500773cd715a48db0
David Boureau <bdavidxyz@gmail.com>
2018-10-08 16:46:24 +0200

    greatly simplified version

--- app/assets/javascripts/load_js_for_page.js
+++ app/assets/javascripts/load_js_for_page.js
@@ -15,29 +15,11 @@
 // DEBUG purpose : in the chrome console, >$ $(document).trigger( "ready")
 // $(document).on("ready", function(){console.log("I'm ready");});
 
-function load_js_for_page(array_of_selectors, a_function) {
-  $(document).on("ready turbolinks:load", max_one_call(a_function, array_of_selectors));
-}
-
-var max_one_call = makeSelfDestructingEventCallback(1);
-
-function makeSelfDestructingEventCallback(maxExecutions) {
-  return function(callback, selectors) {
-    var count = 0;
-    return function(event) {
-      if ($( 'body' ).hasClass(_.join(selectors, ' '))) {
-        if (count++ >= maxExecutions){
-          console.log ('turning off')
-          $(this).off(event)
-          return;
-        }
-        console.log ('execute')
-        //pass any normal arguments down to the wrapped callback
-        return callback.apply(this, arguments);
-      } else {
-        count = 0;
-      }
+function load_js_for_page(selectors, a_function) {
+  $(document).on("ready turbolinks:load", function() {
+    if (_.isEmpty(selectors) || _.every(selectors, function(sel){return $('body').hasClass(sel)})) {
+      a_function();
     }
-  }
-  
+  });
 }
+
