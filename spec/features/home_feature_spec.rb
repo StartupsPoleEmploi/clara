b062487f873af27bda7d303167b44249296344d1
maxdeval <maxime.devallez@pole-emploi.fr>
2018-10-08 17:03:12 +0200

    green for all users meta google search

--- spec/features/home_feature_spec.rb
+++ spec/features/home_feature_spec.rb
@@ -35,4 +35,34 @@ feature 'HomeSpec' do
     expect(page).to have_css 'meta[name="google-site-verification"][content="neOTrE-YKZ9LbgLlaX8UkYN6MJTPlWpeotPQqbrJ19Q"]', :visible => false
   end
 
+  scenario 'Home should a meta google search controler for Mat' do
+    visit root_path 
+    expect(page).to have_css 'meta[name="google-site-verification"][content="TNQnOQs84jCeQUgbEPOAV9gE4LXDUQaG5ypdnlRz2E8"]', :visible => false
+  end
+
+  scenario 'Home should a meta google search controler for Beg' do
+    visit root_path 
+    expect(page).to have_css 'meta[name="google-site-verification"][content="eZYLdUFSc9uBqciZ7fcOxXybmBPxcPME-N9NCRvFn8w"]', :visible => false
+  end
+
+  scenario 'Home should a meta google search controler for Maxd' do
+    visit root_path 
+    expect(page).to have_css 'meta[name="google-site-verification"][content="f_xo9InTg_SGabP-vnvDjrslUaixMcuxCaGidnXJT9s"]', :visible => false
+  end
+
+  scenario 'Home should a meta google search controler for Davb' do
+    visit root_path 
+    expect(page).to have_css 'meta[name="google-site-verification"][content="rjlrEGuLOWAOnmomQMmM5epqCRA3dZY4p1oMDbfCxiY"]', :visible => false
+  end
+
+  scenario 'Home should a meta google search controler for Acm' do
+    visit root_path 
+    expect(page).to have_css 'meta[name="google-site-verification"][content="YOm5sNGxbHWmN2Rf3Udg6v29ca5YjM3RNRz8TjWA6Pw"]', :visible => false
+  end
+
+    scenario 'Home should a meta google search controler for Mar' do
+    visit root_path 
+    expect(page).to have_css 'meta[name="google-site-verification"][content="0jDmSXtIkrU_nDcXoExkAHbG0TmRlGrx7LdJoX9ktWg"]', :visible => false
+  end
+
 end
