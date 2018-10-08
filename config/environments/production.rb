5499036f2c2007f0f819f335515ffd9aa353fcce
maxdeval <maxime.devallez@pole-emploi.fr>
2018-10-08 12:17:55 +0200

    changed address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@clara.pole-emploi.fr'}
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'in-v3.mailjet.com',
     port: 587,
     user_name: ENV['ARA_EMAIL_USER'],
     password: ENV['ARA_EMAIL_PWD'],
