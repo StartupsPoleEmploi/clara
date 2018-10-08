8b87fe5824d74af84def64d3e633979821a4d06f
David Boureau <bdavidxyz@gmail.com>
2018-10-08 12:31:41 +0200

    debugging session

--- app/assets/javascripts/load_js_for_page.js
+++ app/assets/javascripts/load_js_for_page.js
@@ -20,12 +20,13 @@ function load_js_for_page(array_of_selectors, a_function) {
 function make_self_destructing_event_callback(maxExecutions, callback, selectors) {
 
   if (_.every(selectors, function(sel){ return $('body').hasClass(sel);})) {
-    console.log("try to trigger loading for " + selectors)
+    // console.log("try to trigger loading for " + selectors)
     var count = 0;
     return function(event) {
-      console.log("event is " + event.type);
+      // console.log(selectors)
+      if (_.size(selectors) > 0) console.log("event is " + event.type + " with count " + count + " with selectors " + selectors);
       if (count++ >= maxExecutions){
-        console.log("turned off event for " + event.type + " " + selectors);
+        if (_.size(selectors) > 0) console.log("turned off event for " + event.type + " " + selectors);
         $(this).off(event)
         return;
       }
