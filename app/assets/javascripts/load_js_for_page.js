2b479af4ed68a0164731a1dd1ec618303a2f5b56
David Boureau <bdavidxyz@gmail.com>
2018-10-08 16:22:06 +0200

    woooooorks !

--- app/assets/javascripts/load_js_for_page.js
+++ app/assets/javascripts/load_js_for_page.js
@@ -12,55 +12,30 @@
 *
 */
 $(document).on("ready", function(){console.log("I'm ready");});
+
 function load_js_for_page(array_of_selectors, a_function) {
-  $(document).on("ready turbolinks:load", 
-    (function(){
-      return function(event) {
-        var that = this;
-        var count = 0;
-        console.log('count is ' + count)
-        if ($( 'body' ).hasClass(_.join(array_of_selectors, ' '))) {
-          if (count++ >= 1){
-            console.log("refused to continue, count is " + count)
-            return;
-          }
-          console.log('calling a_function')
-          return a_function();
-        }
-      }
-    })()
-  );
+  $(document).on("ready turbolinks:load", max_one_call(a_function, array_of_selectors));
 }
 
-function make_self_destructing_event_callback(maxExecutions, callback, selectors) {
+var max_one_call = makeSelfDestructingEventCallback(1);
 
-  var debug = (_.size(selectors) > 0);
-  // if (debug) console.log("enter the arena")
-  // if (_.every(selectors, function(sel){ return $('body').hasClass(sel);})) {
-    // if (debug) console.log($('body').attr("class").split(' '));
+function makeSelfDestructingEventCallback(maxExecutions) {
+  return function(callback, selectors) {
     var count = 0;
     return function(event) {
-      // console.log(selectors)
-      // if (debug) console.log("within closure " + $('body').attr("class").split(' '))
-      var current_selectors = $('body').attr("class").split(' ');
-      var is_page_corresponding_to_selectors = _.isEmpty(_.difference(selectors, current_selectors))
-      // if (debug) console.log("is_page_corresponding_to_selectors " + is_page_corresponding_to_selectors)
-      // if (debug) console.log("event is " + event.type + " with count " + count + " with selectors " + selectors);
-      if (!is_page_corresponding_to_selectors) {
-        if (debug) console.log("resetted event for " + event.type + " " + selectors + " for " + $('body').attr("class").split(' '));
+      if ($( 'body' ).hasClass(_.join(selectors, ' '))) {
+        if (count++ >= maxExecutions){
+          console.log ('turning off')
+          $(this).off(event)
+          return;
+        }
+        console.log ('execute')
+        //pass any normal arguments down to the wrapped callback
+        return callback.apply(this, arguments);
+      } else {
         count = 0;
       }
-      else if (count++ >= maxExecutions){
-        if (debug) console.log("turned off event for " + event.type + " " + selectors + " for " + $('body').attr("class").split(' '));
-        $(this).off(event)
-        return;
-      }
-
-
-      //pass any normal arguments down to the wrapped callback
-      return callback.apply(this, arguments);
     }
-
-  // }
-
+  }
+  
 }
