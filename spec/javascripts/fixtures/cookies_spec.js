289e9ca1e5e7af639c884b065cf94d8beb8b3051
maxdeval <maxime.devallez@pole-emploi.fr>
2018-10-10 16:13:47 +0200

    green test

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -8,10 +8,12 @@ describe('cookies.js', function() {
     beforeEach(function() {
       $('body').append('<button id="authorize_all">Tout autoriser</button>')
       $('body').append('<button id="forbid_all">Tout autoriser</button>')
+      $('body').append('<button id="authorize_statistic">Tout autoriser</button>')
     });
     afterEach(function() {
       $('#authorize_all').remove()
       $('#forbid_all').remove()
+      $('#authorize_statistic').remove()
     })
 
     it('Needs to be mapped to global clara', function() {
