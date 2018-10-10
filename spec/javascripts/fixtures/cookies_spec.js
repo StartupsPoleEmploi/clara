911e0f3dd0aa7f0a75e5eb50b3be755aead8ab27
maxdeval <maxime.devallez@pole-emploi.fr>
2018-10-10 16:08:53 +0200

    better organization

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -4,17 +4,21 @@
 //= require cookies
 
 describe('cookies.js', function() {
+  describe('There are is authorize button', function(){
+    beforeEach(function() {
+      $('body').append('<button id="authorize_all">Tout autoriser</button>')
+    });
+    afterEach(function() {
+      $('#authorize_all').remove()
+    })
 
-  beforeEach(function() {
-    $('body').append('<button id="authorize_all">Tout autoriser</button>')
-  })
-  
-  it('Needs to be mapped to global clara', function() {
-    expect(clara).toBeDefined();
-  });
+    it('Needs to be mapped to global clara', function() {
+      expect(clara).toBeDefined();
+    });
 
-  it('Needs to have an authorize_all button',function(){
-    expect($('#authorize_all').length).toEqual(1);
+    it('Needs to have an authorize_all button',function(){
+      expect($('#authorize_all').length).toEqual(1);
+    });
   });
 
 });
