8fac8de0b9ab4484ea176d0ae02052367bba7b98
maxdeval <maxime.devallez@pole-emploi.fr>
2018-10-10 15:58:18 +0200

    red test verify button

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -4,8 +4,13 @@
 //= require cookies
 
 describe('cookies.js', function() {
+
   it('Needs to be mapped to global clara', function() {
     expect(clara).toBeDefined();
   });
+
+  it('Needs to have a authorize_all button',function(){
+    expect($('#authorize_all').length).toEqual(1)
+  })
 
 });
