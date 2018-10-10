f240368a7349f41eb1aec7e96a347e164151f999
maxdeval <maxime.devallez@pole-emploi.fr>
2018-10-10 15:24:16 +0200

    clara is mapped, green

---
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -0,0 +1,11 @@
+//= require lodash/lodash
+//= require lodash/lodash_extension
+//= require jquery
+//= require cookies
+
+describe('cookies.js', function() {
+  it('Needs to be mapped to global clara', function() {
+    expect(clara).toBeDefined();
+  });
+
+});
