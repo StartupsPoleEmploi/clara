d6a22408c0b0bc9be7c85a5aa990e83f7a4990e6
David Boureau <bdavidxyz@gmail.com>
2018-10-19 08:34:23 +0200

    wip

--- app/assets/javascripts/google_analytics_push.js
+++ app/assets/javascripts/google_analytics_push.js
@@ -3,4 +3,4 @@ load_js_for_page([], function(){
     ga("set", "location", location.pathname + location.search);
     return ga("send", "pageview");
   }
-});
+}, "ga_push");
--- app/assets/javascripts/load_js_for_page.js
+++ app/assets/javascripts/load_js_for_page.js
@@ -12,11 +12,47 @@
 *
 */
 
-function load_js_for_page(selectors, a_function) {
-  $(document).on("ready turbolinks:load", function(event) {
-    if (_.isEmpty(selectors) || _.every(selectors, function(sel){return $('body').hasClass(sel)})) {
-      a_function();
+function load_js_for_page(selectors, a_function, optional_id) {
+
+  var should_apply_on_all_pages = function(){
+    return _.isEmpty(selectors);
+  };
+
+  var should_apply_on_selected_page = function(){ 
+    var res = false;
+    if(!_.isEmpty(selectors)){
+      res = _.every(selectors, function(sel){return $('body').hasClass(sel)});
     }
+    return res;
+  };
+
+  var func = _.cond([
+    [should_apply_on_all_pages, callback],
+    [should_apply_on_selected_page, callback]
+  ]);
+
+
+  
+  $(document).on("ready turbolinks:load", function(event) {
+    // if (_.isEmpty(selectors) || _.every(selectors, function(sel){return $('body').hasClass(sel)})) {
+      var local_id = _.isEmpty(selectors) ? optional_id : selectors.toString();
+      console.log("function " + local_id + " is called with event " + event.type);
+      a_function();
+    // }
   });
 }
 
+// var func = _.cond([
+//   [_.matches({ 'a': 1 }),           _.constant('matches A')],
+//   [_.conforms({ 'b': _.isNumber }), _.constant('matches B')],
+//   [_.stubTrue,                      _.constant('no match')]
+// ]);
+ 
+// func({ 'a': 1, 'b': 2 });
+// // => 'matches A'
+ 
+// func({ 'a': 0, 'b': 1 });
+// // => 'matches B'
+ 
+// func({ 'a': '1', 'b': '2' });
+// // => 'no match'
