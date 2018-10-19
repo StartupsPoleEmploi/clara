178ad26918670034e3282ccba2b16f0bbd902020
David Boureau <bdavidxyz@gmail.com>
2018-10-19 18:16:02 +0200

    done

--- app/assets/javascripts/google_analytics_push.js
+++ app/assets/javascripts/google_analytics_push.js
@@ -2,7 +2,12 @@ load_js_for_page([], function(){
   if (typeof ga === "function") {
     ga("set", "location", location.pathname + location.search);
     
-    // ga('send', 'event', 'results', 'print', document.location.pathname);
+    var es5supportNumber = 42;
+    if (_.get(window, 'isES5Supported') === false) es5supportNumber = 0;
+    if (_.get(window, 'isES5Supported') === true) es5supportNumber = 1;
+
+    ga('send', 'event', 'material', 'flexbox', _.get(window, '_DD'));
+    ga('send', 'event', 'material', 'es5', es5supportNumber);
 
     return ga("send", "pageview");
   }
