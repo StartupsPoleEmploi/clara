5cffdcca5dcb58b87fe81bf0ade7e07f7207cb82
David Boureau <bdavidxyz@gmail.com>
2018-10-19 16:13:24 +0200

    done

---
+++ app/assets/javascripts/grade_questions.js
@@ -0,0 +1,3 @@
+load_js_for_page(["grade_questions", "new"], function() {
+  $("input:radio:first").focus();
+});
---
+++ app/assets/javascripts/inscription_questions.js
@@ -0,0 +1,3 @@
+load_js_for_page(["inscription_questions", "new"], function() {
+  $("input:radio:first").focus();
+});
