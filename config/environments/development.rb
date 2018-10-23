51e3b10a58724a6839aa154638c3b492e32fd5a0
David Boureau <bdavidxyz@gmail.com>
2018-10-23 14:47:33 +0200

    prod and dev ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,19 +69,8 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.delivery_method = :mailjet
-  # config.action_mailer.delivery_method = :letter_opener_web
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_options = {from: 'no-reply@clara.pole-emploi.fr'}
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: 587,
-  #   user_name: ENV['ARA_EMAIL_USER'],
-  #   password: ENV['ARA_EMAIL_PWD'],
-  #   authentication: 'plain',
-  #   enable_starttls_auto: true
-  # }
+  # config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :letter_opener_web
 
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,15 +94,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@clara.pole-emploi.fr'}
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV['ARA_EMAIL_USER'],
-    password: ENV['ARA_EMAIL_PWD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :mailjet
+
 end
