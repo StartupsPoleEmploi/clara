707763aa646a2603af293e309ea4dfa4d798a740
David Boureau <bdavidxyz@gmail.com>
2018-10-23 14:31:17 +0200

    mail() method configured

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,12 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @contact = params[:contact] 
     @origin = params[:origin] 
-    mail(from: ENV['ARA_EMAIL_USER'], reply_to: @contact.email, to: ENV['ARA_EMAIL_DESTINATION'], subject: "Demande de contact via le site clara")
+    # mail(from: ENV['ARA_EMAIL_USER'], reply_to: @contact.email, to: ENV['ARA_EMAIL_DESTINATION'], subject: "Demande de contact via le site clara")
+    mail(
+      to: ENV['ARA_EMAIL_DESTINATION'],
+      bcc: ENV['ARA_EMAIL_BCC'],
+      delivery_method_options: { api_key: ENV['ARA_EMAIL_API_KEY'], secret_key: ENV['ARA_EMAIL_SECRET_KEY'] }
+    )
   end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,8 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :mailjet
+  # config.action_mailer.delivery_method = :letter_opener_web
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_options = {from: 'no-reply@clara.pole-emploi.fr'}
