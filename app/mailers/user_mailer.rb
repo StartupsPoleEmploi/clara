6d4793af1b70ba920484110f580cdb93d811a2e1
David Boureau <bdavidxyz@gmail.com>
2018-10-23 14:58:47 +0200

    BCC to CC

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     mail(
       from: ENV['ARA_EMAIL_FROM'],
       to: ENV['ARA_EMAIL_DESTINATION'],
-      cc: ENV['ARA_EMAIL_BCC'],
+      cc: ENV['ARA_EMAIL_CC'],
       delivery_method_options: { api_key: ENV['ARA_EMAIL_API_KEY'], secret_key: ENV['ARA_EMAIL_SECRET_KEY'] }
     )
   end
