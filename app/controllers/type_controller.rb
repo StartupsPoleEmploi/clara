f8248523aee25a6049d370568df50f37cf5434c7
David Boureau <bdavidxyz@gmail.com>
2018-10-23 14:19:39 +0200

    cleaned after review

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -4,9 +4,6 @@ class TypeController < ApplicationController
     activated = ActivatedModelsService.instance
     contract = activated.contracts.detect{ |contract| contract["slug"] ==  params[:id] }
     aids_of_contract = activated.aids.find_all{ |aid| aid["contract_type_id"] == contract["id"] }
-    # p '- - - - - - - - - - - - - - contract- - - - - - - - - - - - - - - -' 
-    # pp contract
-    # p ''
     hydrate_view(
       contract:  contract,
       aids: aids_of_contract
