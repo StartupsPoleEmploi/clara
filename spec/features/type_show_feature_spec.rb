49802c0660f0ce6515a43cd4666fd8d0e8a2f316
David Boureau <bdavidxyz@gmail.com>
2018-10-23 11:42:10 +0200

    added 2 tests

--- spec/features/type_show_feature_spec.rb
+++ spec/features/type_show_feature_spec.rb
@@ -6,8 +6,8 @@ feature 'A show type page' do
     before do
       if !seen
         @contract_type = create(:contract_type, :contract_type_amob)
-        create(:aid, :aid_spectacle, name: "aid_spectacle_1", contract_type: @contract_type)
-        create(:aid, :aid_not_spectacle, name: "aid_not_spectacle_1", contract_type: @contract_type)
+        create(:aid, :aid_spectacle, name: "aid_spectacle_1", contract_type: @contract_type, ordre_affichage: 12)
+        create(:aid, :aid_not_spectacle, name: "aid_not_spectacle_1", contract_type: @contract_type, ordre_affichage: 7)
         visit type_path(@contract_type.slug)
         seen = Nokogiri::HTML(page.html)
       end
@@ -21,12 +21,12 @@ feature 'A show type page' do
     it 'Should have 2 aids' do
       should_have seen, 2, ".c-result-aid"
     end
-    # it "Title of first active aid must be aid_not_spectacle_1" do
-    #   should_have seen, "1st", ".c-result-aid__title", :with_text, "aid_not_spectacle_1"
-    # end
-    # it "Title of second active aid must be aid_spectacle_1" do
-    #   should_have seen, "2nd", ".c-result-aid__title", :with_text, "aid_spectacle_1"
-    # end
+    it "Title of first active aid must be aid_not_spectacle_1" do
+      should_have seen, "1st", ".c-result-aid__title", :with_text, "aid_not_spectacle_1"
+    end
+    it "Title of second active aid must be aid_spectacle_1" do
+      should_have seen, "2nd", ".c-result-aid__title", :with_text, "aid_spectacle_1"
+    end
     it "Call to action with text \"Je commence\" must be present" do
       should_have seen, 1, ".c-detail-cta", :with_text, "Je commence"
     end
