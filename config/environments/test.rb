e0af7ac4ae2e328972f04f5f08ad250f94f43c66
David Boureau <bdavidxyz@gmail.com>
2018-10-23 15:01:08 +0200

    commented test.rb section

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,7 +39,16 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
+  # config.action_mailer.raise_delivery_errors = true 
+  # config.action_mailer.default_options = {from: 'no-reply@clara.pole-emploi.fr'}  
+  # config.action_mailer.smtp_settings = {  
+  #   address: 'smtp.gmail.com',  
+  #   port: 587,  
+  #   user_name: ENV['ARA_EMAIL_USER'], 
+  #   password: ENV['ARA_EMAIL_PWD'], 
+  #   authentication: 'plain',  
+  #   enable_starttls_auto: true  
+  # }
   
 
   # Print deprecation notices to the stderr.
