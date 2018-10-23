d2f30c6224fdcb6ed871d2f9e903cd5d124be459
David Boureau <bdavidxyz@gmail.com>
2018-10-23 14:20:00 +0200

    clenaed 2

--- app/view_objects/result_line.rb
+++ app/view_objects/result_line.rb
@@ -3,22 +3,8 @@ class ResultLine < ViewObject
 
   def after_init(args)
     locals = hash_for(args)
-    # p '- - - - - - - - - - - - - - locals- - - - - - - - - - - - - - - -' 
-    # pp locals
-    # p ''
     @aids =  array_of_hash_for(locals[:aids])
-    # p '- - - - - - - - - - - - - - @aids- - - - - - - - - - - - - - - -' 
-    # pp @aids
-    # p ''
     @contract = hash_for(locals[:contract])
-    # p '- - - - - - - - - - - - - - @contract- - - - - - - - - - - - - - - -' 
-    # pp @contract
-    # p ''
-    # @clazz =         string_for(locals[:clazz])
-  end
-
-  def has_line
-    # !@line.blank?
   end
   
   def clazz
@@ -30,14 +16,6 @@ class ResultLine < ViewObject
 
   def ordered_aids
     @aids.sort_by { |aid| aid[:ordre_affichage] }
-  end
-
-  def aids_size
-    # @aids.size
-  end
-
-  def business_id
-    # @contract_type[:business_id]
   end
 
   def _safe_biz_id
