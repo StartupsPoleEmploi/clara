5409f3f5714fa3e395ed584bd959060ba698a2c8
David Boureau <bdavidxyz@gmail.com>
2018-10-24 15:09:12 +0200

    rough controller

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -1,13 +1,41 @@
 class TypeController < ApplicationController
 
   def show
-    activated = ActivatedModelsService.instance
-    contract = activated.contracts.detect{ |contract| contract["slug"] ==  params[:id] }
-    aids_of_contract = activated.aids.find_all{ |aid| aid["contract_type_id"] == contract["id"] }
-    hydrate_view(
-      contract:  contract,
-      aids: aids_of_contract
-    )
+    if have_active_asker?
+      existing = pull_existing_from_cache
+      if (existing)
+        instantiate_asker(existing)
+      else
+        pull_asker_from_query_param
+        augment_asker
+        cacheable = create_cacheable_results_from_asker
+        write_to_cache(cacheable)
+      end
+    else
+      activated = ActivatedModelsService.instance
+      contract = activated.contracts.detect{ |contract| contract["slug"] ==  params[:id] }
+      aids_of_contract = activated.aids.find_all{ |aid| aid["contract_type_id"] == contract["id"] }
+      hydrate_view(
+        contract:  contract,
+        aids: aids_of_contract
+      )
+    end
+
   end
 
+  def have_active_asker?
+    !!params[:for_id]
+  end
+
+  def instantiate_asker(existing)
+    @asker = Asker.new(existing[:asker])
+  end
+
+  def pull_existing_from_cache
+    Rails.cache.read(params[:for_id]) if params[:for_id] != "random"
+  end
+
+  def write_to_cache(cacheable)
+    Rails.cache.write(params[:for_id], cacheable, {expires_in: 10.minutes})
+  end
 end
