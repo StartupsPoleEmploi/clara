b379c87d96732f37743b535590bb9c0e24dcbb72
David Boureau <bdavidxyz@gmail.com>
2018-11-13 11:35:26 +0100

    all green tests

--- spec/services/serialize_results_service_spec.rb
+++ spec/services/serialize_results_service_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 describe SerializeResultsService do
 
+  def hash_for(a_filter)
+    {"id"=>a_filter.id, "slug"=>a_filter.slug}
+  end
+
   describe '._filter' do
     let(:se_divertir) {create(:filter, :name => "se divertir")}
     let(:se_deplacer) {create(:filter, :name => "se deplacer")}
+    let(:se_reprendre) {create(:filter, :name => "se reprendre")}
+
     let(:addiction) {create(:level3_filter, :name => "addiction")}
     let(:argent) {create(:level3_filter, :name => "argent")}
+    let(:attente) {create(:level3_filter, :name => "attente")}
+
     let(:elies) {
       [
         {"id"=>8,
@@ -16,7 +24,7 @@ describe SerializeResultsService do
           "ordre_affichage"=>1,
           "contract_type_id"=>4,
           "filters"=>[],
-          "level3_filters"=>[{"id"=>addiction.id, "slug"=>addiction.slug}, {"id"=>argent.id, "slug"=>argent.slug}],
+          "level3_filters"=>[hash_for(attente), hash_for(argent)],
           "eligibility"=>"ineligible"},
         {"id"=>9,
           "name"=>"Aide Adulte ou Spectacle",
@@ -24,7 +32,7 @@ describe SerializeResultsService do
           "short_description"=>"aide pour majeurs ou intermittent du spectacle",
           "ordre_affichage"=>0,
           "contract_type_id"=>3,
-          "filters"=>[{"id"=>se_deplacer.id, "slug"=>se_deplacer.slug}, {"id"=>se_divertir.id, "slug"=>se_divertir.slug}],
+          "filters"=>[hash_for(se_deplacer), hash_for(se_divertir)],
           "level3_filters"=>[],
           "eligibility"=>"eligible"},
        {"id"=>10,
@@ -33,7 +41,7 @@ describe SerializeResultsService do
           "short_description"=>"aide réservée aux plus de 18 ans",
           "ordre_affichage"=>0,
           "contract_type_id"=>1,
-          "filters"=>[{"id"=>se_deplacer.id, "slug"=>se_deplacer.slug}],
+          "filters"=>[hash_for(se_deplacer)],
           "level3_filters"=>[],
           "eligibility"=>"uncertain"},
        {"id"=>11,
@@ -42,8 +50,8 @@ describe SerializeResultsService do
           "short_description"=>"Un disposition d'insertion",
           "ordre_affichage"=>12,
           "contract_type_id"=>2,
-          "filters"=>[{"id"=>se_deplacer.id, "slug"=>se_deplacer.slug}],
-          "level3_filters"=>[{"id"=>addiction.id, "slug"=>addiction.slug}, {"id"=>argent.id, "slug"=>argent.slug}],
+          "filters"=>[hash_for(se_reprendre)],
+          "level3_filters"=>[hash_for(addiction), hash_for(argent)],
           "eligibility"=>"uncertain"}
       ]
     }
@@ -56,17 +64,18 @@ describe SerializeResultsService do
 
       res = SerializeResultsService.get_instance._filter(elies, simple_filters, level3_filters)
       expect(res.size).to eq(1)
+      expect(res[0]["id"]).to eq(9)
     end
     it "Is able to filter according to level3 filter only" do
       simple_filters = nil
-      level3_filters = "attente,argent"
+      level3_filters = "attente"
 
       res = SerializeResultsService.get_instance._filter(elies, simple_filters, level3_filters)
-      expect(res.size).to eq(2)
+      expect(res.size).to eq(1)
     end
     it 'Is able to filter according to both level3 and simple filter' do
-      simple_filters = "se-regarder,se-deplacer"
-      level3_filters = "argent,attente"
+      simple_filters = "se-reprendre,se-deplacer"
+      level3_filters = "argent,addiction"
 
       res = SerializeResultsService.get_instance._filter(elies, simple_filters, level3_filters)
       expect(res.size).to eq(1)      
