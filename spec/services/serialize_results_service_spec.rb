2309b80a487b6ca3e92ada6f1d653e2b7b41b63b
David Boureau <bdavidxyz@gmail.com>
2018-11-13 11:54:34 +0100

    works!

--- spec/services/serialize_results_service_spec.rb
+++ spec/services/serialize_results_service_spec.rb
@@ -6,6 +6,19 @@ describe SerializeResultsService do
     {"id"=>a_filter.id, "slug"=>a_filter.slug}
   end
 
+  def elies_factory(an_id, simple_filters, level3_filters)
+    {"id"=>an_id,
+      "name"              => "Aide Number #{an_id}",
+      "slug"              => "aide-number-#{an_id}",
+      "short_description" => "aide générée automatiquement, numéro #{an_id}",
+      "ordre_affichage"   => [1,2,3,4].sample,
+      "contract_type_id"  => [1,2,3,4].sample,
+      "filters"           => simple_filters.map { |e| {"id"=>e.id, "slug"=>e.slug} },
+      "level3_filters"    => level3_filters.map { |e| {"id"=>e.id, "slug"=>e.slug} },
+      "eligibility"       => ["eligible", "ineligible", "uncertain"].sample
+    }
+  end
+
   describe '._filter' do
     let(:se_divertir) {create(:filter, :name => "se divertir")}
     let(:se_deplacer) {create(:filter, :name => "se deplacer")}
@@ -15,70 +28,40 @@ describe SerializeResultsService do
     let(:argent) {create(:level3_filter, :name => "argent")}
     let(:attente) {create(:level3_filter, :name => "attente")}
 
-    let(:elies) {
-      [
-        {"id"=>8,
-          "name"=>"Aide Amob",
-          "slug"=>"aide-amob",
-          "short_description"=>"aide pour majeurs ou intermittent du spectacle",
-          "ordre_affichage"=>1,
-          "contract_type_id"=>4,
-          "filters"=>[],
-          "level3_filters"=>[hash_for(attente), hash_for(argent)],
-          "eligibility"=>"ineligible"},
-        {"id"=>9,
-          "name"=>"Aide Adulte ou Spectacle",
-          "slug"=>"aide-adulte-ou-spectacle",
-          "short_description"=>"aide pour majeurs ou intermittent du spectacle",
-          "ordre_affichage"=>0,
-          "contract_type_id"=>3,
-          "filters"=>[hash_for(se_deplacer), hash_for(se_divertir)],
-          "level3_filters"=>[],
-          "eligibility"=>"eligible"},
-       {"id"=>10,
-          "name"=>"Aide Adulte",
-          "slug"=>"aide-adulte",
-          "short_description"=>"aide réservée aux plus de 18 ans",
-          "ordre_affichage"=>0,
-          "contract_type_id"=>1,
-          "filters"=>[hash_for(se_deplacer)],
-          "level3_filters"=>[],
-          "eligibility"=>"uncertain"},
-       {"id"=>11,
-          "name"=>"Dispositif insertion",
-          "slug"=>"disposition-insertion",
-          "short_description"=>"Un disposition d'insertion",
-          "ordre_affichage"=>12,
-          "contract_type_id"=>2,
-          "filters"=>[hash_for(se_reprendre)],
-          "level3_filters"=>[hash_for(addiction), hash_for(argent)],
-          "eligibility"=>"uncertain"}
-      ]
-    }
-    before do
-      create :rule, :be_an_adult, name: 'an_adult'
-    end
+
     it 'Is able to filter according to simple filter only' do
+      elies = []
+              .push(elies_factory(42, [se_divertir, se_deplacer], []))
+              .push(elies_factory(43, [], []))
       simple_filters = "se-regarder,se-divertir"
       level3_filters = nil
 
       res = SerializeResultsService.get_instance._filter(elies, simple_filters, level3_filters)
       expect(res.size).to eq(1)
-      expect(res[0]["id"]).to eq(9)
+      expect(res[0]["id"]).to eq(42)
     end
     it "Is able to filter according to level3 filter only" do
+      elies = []
+              .push(elies_factory(42, [se_divertir, se_deplacer], []))
+              .push(elies_factory(43, [], [argent]))
       simple_filters = nil
-      level3_filters = "attente"
-
-      res = SerializeResultsService.get_instance._filter(elies, simple_filters, level3_filters)
-      expect(res.size).to eq(1)
-    end
-    it 'Is able to filter according to both level3 and simple filter' do
-      simple_filters = "se-reprendre,se-deplacer"
       level3_filters = "argent,addiction"
 
       res = SerializeResultsService.get_instance._filter(elies, simple_filters, level3_filters)
-      expect(res.size).to eq(1)      
+      expect(res.size).to eq(1)
+      expect(res[0]["id"]).to eq(43)
+    end
+    it 'Is able to filter according to both level3 and simple filter' do
+      elies = []
+              .push(elies_factory(42, [se_divertir, se_deplacer], [addiction]))
+              .push(elies_factory(43, [se_divertir], [argent]))
+              .push(elies_factory(44, [se_deplacer], [argent]))
+      simple_filters = "se-divertir,se-regarder"
+      level3_filters = "argent"
+
+      res = SerializeResultsService.get_instance._filter(elies, simple_filters, level3_filters)
+      expect(res.size).to eq(1)
+      expect(res[0]["id"]).to eq(43)     
     end
     it 'Returns empty eligies if input are not properly set'
   end
