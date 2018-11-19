fbb4f58cf8e0deb1bdb4d97b811691b0b0d1bf57
David Boureau <bdavidxyz@gmail.com>
2018-11-19 11:47:58 +0100

    spec uptodate

--- spec/features/cookie_feature_spec.rb
+++ spec/features/cookie_feature_spec.rb
@@ -7,14 +7,23 @@ feature 'CookieSpec' do
     visit root_path
     expect(page).to have_selector("#hj-script", visible: false)
   end
+
   scenario 'When visiting the home page, 2 analytics scripts are included by default' do
     visit root_path
     expect(page).to have_selector("#ga-script", visible: false)
     expect(page).to have_selector("#ga-create-script", visible: false)
   end
-  scenario 'When visiting RGPD page, all cookies are accepted by user' do
+  
+  scenario 'When visiting RGPD page, statistics are enabled by default' do
     visit edit_cooky_path("preference")
-    #
+    disable_statistics_checkbox = "input#input_stat"
+    expect(page.find(disable_statistics_checkbox)).not_to be_checked
+  end
+
+  scenario 'When visiting RGPD page, navigation is enabled by default' do
+    visit edit_cooky_path("preference")
+    disable_navigation_checkbox = "input#input_nav"
+    expect(page.find(disable_navigation_checkbox)).not_to be_checked
   end
 
   scenario 'User can refuse analytics' do
