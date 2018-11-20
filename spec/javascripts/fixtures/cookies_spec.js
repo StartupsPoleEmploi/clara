b384d16b41386542c4cf3742399fb81020fc0a07
Maxime Devallez <maxdeval02@gmail.com>
2018-11-20 15:29:11 +0100

    red tests

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -4,5 +4,14 @@
 //= require cookies
 
 describe('Cookies.js', function() {
-  
+  it('Should have clara mapped', function() {
+    expect(clara).toBeDefined();
+  });
+  it("Should check all checkbox when you click on 'Tout Autoriser'", function() {
+    // given
+    // when
+    // then
+    expect($("#authorize_statistic").is(":checked")).toBe(true)
+    expect($("#authorize_navigation").is(":checked")).toBe(true)
+  });
 });
