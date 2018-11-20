ecd06f5d03dcf5c1a999d9232120d47a74ec4a7d
Maxime Devallez <maxdeval02@gmail.com>
2018-11-20 15:51:58 +0100

    still red

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -10,8 +10,11 @@ describe('cookies.js', function() {
   it("Should check all checkbox when you click on 'Tout Autoriser'", function() {
     // given
     $('body').append('<input id="authorize_all" name="control_all" type="radio" value="authorize_all"></input>');
+    $('body').append('<input id="forbid_all" name="control_all" type="radio" value="forbid_all"></input>');
     $('body').append('<input id="authorize_statistic" name="statistic" type="radio" value="authorize_statistic"></input>');
     $('body').append('<input id="forbid_statistic" name="statistic" type="radio" value="forbid_statistic"></input>');
+    $('body').append('<input id="authorize_navigation" name="statistic" type="radio" value="authorize_navigation"></input>');
+    $('body').append('<input id="forbid_navigation" name="statistic" type="radio" value="forbid_navigation"></input>');
     // when
     $("#authorize_all").click()
     // then
