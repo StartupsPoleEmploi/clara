8ace86fa62d49cb173e7c3a2383d4e4bbf0d026e
David Boureau <bdavidxyz@gmail.com>
2018-11-20 15:24:50 +0100

    cleaned

--- app/controllers/admin/aids_controller.rb
+++ app/controllers/admin/aids_controller.rb
@@ -11,14 +11,8 @@ module Admin
     
     def find_filters 
       of_aids = find_filters_params["ids"].map { |e| e.to_i  }
-      p '- - - - - - - - - - - - - - find_filters- - - - - - - - - - - - - - - -' 
-      pp of_aids
-      p ''
       activated = ActivatedModelsService.instance
       selected_aids = activated.aids.select { |aid| of_aids.include?(aid["id"])  }
-      p '- - - - - - - - - - - - - - selected_aids- - - - - - - - - - - - - - - -' 
-      pp selected_aids
-      p ''
       raw_aids = selected_aids.map{|aid| aid.select{|x| x == "id" || x.end_with?("filters")} }
       render json: {
         status: "ok",
