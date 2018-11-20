48e3a25e618300bd9bde1db7e70e2079e8804c15
David Boureau <bdavidxyz@gmail.com>
2018-11-20 15:59:55 +0100

    cleaned

--- app/assets/javascripts/administrate/aids_collection.js
+++ app/assets/javascripts/administrate/aids_collection.js
@@ -51,38 +51,10 @@ $( document ).ready(function() {
   }
 
   if (window.location.pathname === "/admin/aids") {
-    // console.log('You are now on admin aids collection');
-
-    var need_filters_position = -1;
-    var clara_filters_position = -1;
-    var custom_filters_position = -1;
-    var id_position = -1;
-    $('th[role="columnheader"]').each(function(current_index, elt) {
-      function endsWith(str, suffix) {
-        return str.indexOf(suffix, str.length - suffix.length) !== -1;
-      }
-      if (endsWith($(elt).find('a').attr('href'), "=custom_filters")) {
-        custom_filters_position = current_index;
-      }
-      if (endsWith($(elt).find('a').attr('href'), "=need_filters")) {
-        need_filters_position = current_index;
-      }
-      if (endsWith($(elt).find('a').attr('href'), "=filters")) {
-        clara_filters_position = current_index;
-      }
-      if (endsWith($(elt).find('a').attr('href'), "=id")) {
-        id_position = current_index;
-      }
-    });
-    console.log('need_filters_position ' + need_filters_position)
-    console.log('clara_filters_position ' + clara_filters_position)
-    console.log('custom_filters_position ' + custom_filters_position)
-    console.log('id_position ' + id_position)
-
 
     var displayed_ids = []
     $('.js-table-row').each(function(i,e) {
-      var id_as_string = $(e).find("td:eq(" + id_position + ")").text().trim();
+      var id_as_string = $(e).find("td:eq(" + find_col_nb_for("id") + ")").text().trim();
 
       displayed_ids.push(parseInt(id_as_string, 10));
 
