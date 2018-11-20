33dee1570ea590c37a4ffb366c76da9d7b4be7a8
Maxime Devallez <maxdeval02@gmail.com>
2018-11-20 15:00:36 +0100

    cleaned

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -25,33 +25,32 @@ load_js_for_page(["cookies", "edit"], function() {
     $("#input_stat").prop("checked", true);
   });
 
-  $("#forbid_statistic").click(function() {
-    $('input[name=control_all]').prop('checked',false);
-  });
-
-  $("#forbid_navigation").click(function() {
-    $('input[name=control_all]').prop('checked',false);
-  });
 
   $("#authorize_navigation").click(function() {
+    $("#input_nav").prop("checked", false);
     if($("#authorize_statistic").is(":checked")) {
       $("#authorize_all").prop("checked", true);
     };
   });
 
   $("#authorize_statistic").click(function() {
+    $("#input_stat").prop("checked", false);
     if($("#authorize_navigation").is(":checked")) {
       $("#authorize_all").prop("checked", true);
     };
   });
 
   $("#forbid_navigation").click(function() {
+    $('input[name=control_all]').prop('checked',false);
+    $("#input_nav").prop("checked", true);
     if($("#forbid_statistic").is(":checked")) {
       $("#forbid_all").prop("checked", true);
     };
   });
 
   $("#forbid_statistic").click(function() {
+    $('input[name=control_all]').prop('checked',false);
+    $("#input_stat").prop("checked", true);
     if($("#forbid_navigation").is(":checked")) {
       $("#forbid_all").prop("checked", true);
     };
