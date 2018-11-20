666b8c9ea9a270373466c05f041d43cff007f123
Maxime Devallez <maxdeval02@gmail.com>
2018-11-20 15:32:49 +0100

    still red

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -10,6 +10,9 @@ describe('Cookies.js', function() {
   it("Should check all checkbox when you click on 'Tout Autoriser'", function() {
     // given
     // when
+    $("#authorize_all").click(function(){
+      $("#authorize_all").prop("checked", true) 
+    }
     // then
     expect($("#authorize_statistic").is(":checked")).toBe(true)
     expect($("#authorize_navigation").is(":checked")).toBe(true)
