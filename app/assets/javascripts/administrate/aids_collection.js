52618b3eb3f7df0c4e0e50c45e44dcefd2d5dc65
David Boureau <bdavidxyz@gmail.com>
2018-11-20 15:51:33 +0100

    wip

--- app/assets/javascripts/administrate/aids_collection.js
+++ app/assets/javascripts/administrate/aids_collection.js
@@ -1,7 +1,49 @@
 $( document ).ready(function() {
 
+  function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+  }
+
   function treat_successfully_retrieved_filters(aids) {
-    console.log("aids are " + JSON.stringify(aids[0]))
+    _.each(aids, function(aid) {
+      // var $row = find_row_whose_id_is(aid["id"]);
+      // var $cell = find_cell_for($row, "filters");
+    });
+    console.log("aids are " + JSON.stringify(aids[0]));
+    var darow = find_row_whose_id_is(42);
+    console.log(darow);
+    var dacell = find_cell_for(darow, "filters");
+    console.log(dacell);
+  }
+
+  function find_cell_for(jq_row, filter_column_name) {
+    var col_nb = find_col_nb_for(filter_column_name);
+    console.log(col_nb);
+    $(jq_row).find("td:eq(" + col_nb + ")").text("blabla")
+  }
+
+  function find_row_whose_id_is(id_value) {
+    var id_col_nb = find_col_nb_for("id");
+    console.log(id_col_nb)
+    var id_value_as_str = id_value.toString();
+    console.log(id_value_as_str)
+    return $('.js-table-row').filter(function(i, e) {
+      var id_as_string = $(e).find("td:eq(" + id_col_nb + ")").text().trim();
+      // console.log(id_as_string)
+      // console.log(id_value_as_str)
+      // console.log("")
+      return id_value_as_str === id_as_string;
+    });
+  }
+
+  function find_col_nb_for(stuff) {
+    var id_col_nb = -1;
+    $('th[role="columnheader"]').each(function(current_index, elt) {
+      if (endsWith($(elt).find('a').attr('href'), "="+stuff)) {
+        id_col_nb = current_index;
+      }
+    });
+    return id_col_nb;
   }
 
   if (window.location.pathname === "/admin/aids") {
