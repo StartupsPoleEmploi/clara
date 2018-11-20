9d019d404af8a5990e7f75e6a9ada13ecc09c058
Maxime Devallez <maxdeval02@gmail.com>
2018-11-20 10:38:28 +0100

    removed redux

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -1,9 +1,8 @@
 load_js_for_page(["cookies", "edit"], function() {
 
-  /**
-  *         INITIAL STATE 
-  **/
-  var STATE_KEY = 'cookies_preference';
+  $("input:radio:first").focus();
+
+/*  var STATE_KEY = 'cookies_preference';
 
   var initial_state = {
     disable_statistic: false,
@@ -26,16 +25,14 @@ load_js_for_page(["cookies", "edit"], function() {
     $(id).removeClass("is-disabled");
   }
 
-  /**
-  *         MAIN REDUCER 
-  **/
+
   var main_reducer = function(state, action) {
 
     if (state === undefined) {
       return default_state();
     }
 
-    // Works better than _.assign or Object.assign
+
     var newState = JSON.parse(JSON.stringify(state));
 
     if (action.type === 'AUTHORIZE_STATISTIC') {
@@ -62,14 +59,10 @@ load_js_for_page(["cookies", "edit"], function() {
     return newState;
   };
 
-  /**
-  *         MAIN STORE 
-  **/
+
   var main_store = Redux.createStore(main_reducer, default_state());
 
-  /**
-  *         DISPATCHERS 
-  **/
+
   $('#authorize_statistic').click(function(){
     var that = this;
     main_store.dispatch({type: 'AUTHORIZE_STATISTIC'});
@@ -97,14 +90,10 @@ load_js_for_page(["cookies", "edit"], function() {
     main_store.dispatch({type: 'FORBID_ALL'});
   });
 
-  /**
-  *         SUBSCRIBERS
-  **/
   main_store.subscribe(function() {
 
     var state = main_store.getState();
 
-    // state of simple buttons
     if (state.disable_statistic === true) {
       disable_button("#authorize_statistic");
       enable_button("#forbid_statistic");
@@ -121,7 +110,7 @@ load_js_for_page(["cookies", "edit"], function() {
       disable_button("#forbid_navigation");
     }
 
-    // state aggregated buttons
+    
     if (_.every(state)) {
       enable_button("#forbid_all");
       disable_button("#authorize_all");        
@@ -130,18 +119,18 @@ load_js_for_page(["cookies", "edit"], function() {
       disable_button("#forbid_all");
       enable_button("#authorize_all");        
     } 
-    else { // some
+    else { 
       disable_button("#authorize_all");
       disable_button("#forbid_all");        
     } 
 
-    // state of form
+    
     $("#input_nav").prop( "checked", state.disable_navigation );
     $("#input_stat").prop( "checked", state.disable_statistic );
   }); 
 
-  // fire initial state
-  main_store.dispatch({type: 'INIT'})
+  
+  main_store.dispatch({type: 'INIT'})*/
 
 
 });
