8bf45df7628a881e6a0d1dd1c671284b5b4b9ba8
Maxime Devallez <maxdeval02@gmail.com>
2018-11-20 15:42:46 +0100

    still red

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -3,16 +3,19 @@
 //= require jquery
 //= require cookies
 
-describe('Cookies.js', function() {
+describe('cookies.js', function() {
   it('Should have clara mapped', function() {
     expect(clara).toBeDefined();
   });
   it("Should check all checkbox when you click on 'Tout Autoriser'", function() {
     // given
+    $('body').append('<input id="authorize_all" name="control_all" type="radio" value="authorize_all"></input>');
+    $('body').append('<input id="authorize_statistic" name="statistic" type="radio" value="authorize_statistic"></input>');
+    $('body').append('<input id="forbid_statistic" name="statistic" type="radio" value="forbid_statistic"></input>');
     // when
-    $("#authorize_all").click(function(){
+/*    $("#authorize_all").click(function(){
       $("#authorize_all").prop("checked", true) 
-    }
+    }*/
     // then
     expect($("#authorize_statistic").is(":checked")).toBe(true)
     expect($("#authorize_navigation").is(":checked")).toBe(true)
