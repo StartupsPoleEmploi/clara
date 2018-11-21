8a9f33c334f8351280353311ee6da2bf59c4244b
Maxime Devallez <maxdeval02@gmail.com>
2018-11-21 11:12:08 +0100

    added beforeEach and afterEach

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -6,15 +6,7 @@
 
 describe('cookies.js', function() {
 
-  function setUpHTMLFixture() {
-          $('body').append(' <input id="authorize_all" name="control_all" type="radio" value="authorize_all"></input>           \
-                                      <input id="forbid_all" name="control_all" type="radio" value="forbid_all"></input>          \
-                                      <input id="authorize_statistic" name="statistic" type="radio" value="authorize_statistic"></input>                                             \
-                                      <input id="forbid_statistic" name="statistic" type="radio" value="forbid_statistic"></input>              \
-                                      <input id="authorize_navigation" name="navigation" type="radio" value="authorize_navigation"></input>  \
-                                      <input id="forbid_navigation" name="navigation" type="radio" value="forbid_navigation"></input>                         \
-                                  ');
-  }
+
 
   
   it('Should have clara mapped', function() {
@@ -23,8 +15,23 @@ describe('cookies.js', function() {
   });
 
   beforeEach(function() { 
-    setUpHTMLFixture();
+    $('body').append(' <input id="authorize_all" name="control_all" type="radio" value="authorize_all"></input>           \
+                            <input id="forbid_all" name="control_all" type="radio" value="forbid_all"></input>          \
+                            <input id="authorize_statistic" name="statistic" type="radio" value="authorize_statistic"></input>                                             \
+                            <input id="forbid_statistic" name="statistic" type="radio" value="forbid_statistic"></input>              \
+                            <input id="authorize_navigation" name="navigation" type="radio" value="authorize_navigation"></input>  \
+                            <input id="forbid_navigation" name="navigation" type="radio" value="forbid_navigation"></input>                         \
+                        ');
   });
+
+  afterEach(function() {
+    $("#authorize_all").remove();
+    $("#forbid_all").remove();
+    $("#authorize_statistic").remove();
+    $("#authorize_navigation").remove();
+    $("#forbid_navigation").remove();
+    $("#forbid_statistic").remove();
+  })
 
   it("Should check all checkbox when you click on 'Tout Autoriser'", function() {
     // given
