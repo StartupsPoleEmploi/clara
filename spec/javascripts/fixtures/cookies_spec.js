aa5c7a49679c41d57497cc31476e643fdf060979
Maxime Devallez <maxdeval02@gmail.com>
2018-11-21 13:51:13 +0100

    still red

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -41,52 +41,52 @@ describe('cookies.js', function() {
     
     it("Arriving on the page authorize_navigation should be checked", function() {
       // given
-      $("#authorize_all").prop("checked", true)
+      $("#authorize_all").prop("checked", true);
       // when
       clara.cookies.init_cookies_preferences();
       // then
-      expect($("#authorize_navigation").is(":checked")).toBe(true)
+      expect($("#authorize_navigation").is(":checked")).toBe(true);
     });
 
     it("Arriving on the page authorize_statistic should be checked", function() {
       // given
-      $("#authorize_all").prop("checked", true)
+      $("#authorize_all").prop("checked", true);
       // when
       clara.cookies.init_cookies_preferences();
       // then
-      expect($("#authorize_statistic").is(":checked")).toBe(true)
+      expect($("#authorize_statistic").is(":checked")).toBe(true);
     });
     it("Arriving on the page forbid_statistic should be unchecked", function() {
       // given
-      $("#authorize_all").prop("checked", true)
+      $("#authorize_all").prop("checked", true);
       // when
       clara.cookies.init_cookies_preferences();
       // then
-      expect($("#forbid_statistic").is(":checked")).toBe(false)
+      expect($("#forbid_statistic").is(":checked")).toBe(false);
     });
     it("Arriving on the page forbid_navigation should be unchecked", function() {
       // given
-      $("#authorize_all").prop("checked", true)
+      $("#authorize_all").prop("checked", true);
       // when
       clara.cookies.init_cookies_preferences();
       // then
-      expect($("#forbid_navigation").is(":checked")).toBe(false)
+      expect($("#forbid_navigation").is(":checked")).toBe(false);
     });
     it("Arriving on the page input_nav should be unchecked", function() {
       // given
-      $("#authorize_all").prop("checked", true)
+      $("#authorize_all").prop("checked", true);
       // when
       clara.cookies.init_cookies_preferences();
       // then
-      expect($("#input_nav").is(":checked")).toBe(false)
+      expect($("#input_nav").is(":checked")).toBe(false);
     });
     it("Arriving on the page input_stat should be unchecked", function() {
       // given
-      $("#authorize_all").prop("checked", true)
+      $("#authorize_all").prop("checked", true);
       // when
       clara.cookies.init_cookies_preferences();
       // then
-      expect($("#input_stat").is(":checked")).toBe(false)
+      expect($("#input_stat").is(":checked")).toBe(false);
     });
   });
 
@@ -94,8 +94,9 @@ describe('cookies.js', function() {
     it('Should check authorize_statistic when you click on authorize_all', function() {
       //given
       //when
+      clara.cookies.authorize_all();
       //then
-      expect($("#authorize_statistic").is(":checked")).toBe(true)
+      expect($("#authorize_statistic").is(":checked")).toBe(true);
     });
   });
 
