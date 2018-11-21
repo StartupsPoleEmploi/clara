cb04e9028c6309531c95eac077d42ed047897eb5
Maxime Devallez <maxdeval02@gmail.com>
2018-11-21 11:48:43 +0100

    added checkboxes in spec dom

--- spec/javascripts/fixtures/cookies_spec.js
+++ spec/javascripts/fixtures/cookies_spec.js
@@ -14,34 +14,29 @@ describe('cookies.js', function() {
     expect(clara.cookies).toBeDefined();
   });
 
-/*  beforeEach(function() { 
+  beforeEach(function() { 
     $('body').append(' <input id="authorize_all" name="control_all" type="radio" value="authorize_all"></input>           \
                             <input id="forbid_all" name="control_all" type="radio" value="forbid_all"></input>          \
                             <input id="authorize_statistic" name="statistic" type="radio" value="authorize_statistic"></input>                                             \
                             <input id="forbid_statistic" name="statistic" type="radio" value="forbid_statistic"></input>              \
                             <input id="authorize_navigation" name="navigation" type="radio" value="authorize_navigation"></input>  \
                             <input id="forbid_navigation" name="navigation" type="radio" value="forbid_navigation"></input>                         \
+                            <input id="input_nav" type="checkbox" value="input_nav"></input>                         \
+                            <input id="input_stat" type="checkbox" value="input_stat"></input>                         \
                         ');
-  });*/
+  });
 
-/*  afterEach(function() {
+  afterEach(function() {
     $("#authorize_all").remove();
     $("#forbid_all").remove();
     $("#authorize_statistic").remove();
     $("#authorize_navigation").remove();
     $("#forbid_navigation").remove();
     $("#forbid_statistic").remove();
-  })*/
+  })
 
   it("Should check all checkbox when you click on 'Tout Autoriser'", function() {
     // given
-    $('body').append(' <input id="authorize_all" name="control_all" type="radio" value="authorize_all"></input>           \
-                            <input id="forbid_all" name="control_all" type="radio" value="forbid_all"></input>          \
-                            <input id="authorize_statistic" name="statistic" type="radio" value="authorize_statistic"></input>                                             \
-                            <input id="forbid_statistic" name="statistic" type="radio" value="forbid_statistic"></input>              \
-                            <input id="authorize_navigation" name="navigation" type="radio" value="authorize_navigation"></input>  \
-                            <input id="forbid_navigation" name="navigation" type="radio" value="forbid_navigation"></input>                         \
-                        ');
     // when
     clara.cookies.authorize_all();
     // then
