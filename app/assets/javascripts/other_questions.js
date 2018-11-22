6a4cbe5d19ffada9bafe02119d3d304a9d068a75
Maxime Devallez <maxdeval02@gmail.com>
2018-11-22 12:07:17 +0100

    other questions

--- app/assets/javascripts/other_questions.js
+++ app/assets/javascripts/other_questions.js
@@ -1,23 +1,31 @@
 load_js_for_page(["other_questions", "new"], function() {
 
-    $("input:checkbox:first").focus();
+  $("input:checkbox:first").focus();
 
-    $( 'input#none' ).click(function() {
-      $('input#val_spectacle').prop('checked', false);
-      $('input#val_handicap').prop('checked', false);
-      $('input#val_cadre').prop('checked', false);
-    });   
+  $( 'input#none' ).click(function() {
+    $('input#val_spectacle').prop('checked', false);
+    $('input#val_handicap').prop('checked', false);
+    $('input#val_cadre').prop('checked', false);
+  });   
 
-    $( 'input#val_spectacle' ).click(function() {
-      $('input#none').prop('checked', false);
-    });   
+  $( 'input#val_spectacle' ).click(function() {
+    $('input#none').prop('checked', false);
+  });   
 
-    $( 'input#val_handicap' ).click(function() {
-      $('input#none').prop('checked', false);
-    });
-    
-    $( 'input#val_cadre' ).click(function() {
-      $('input#none').prop('checked', false);
-    });
+  $( 'input#val_handicap' ).click(function() {
+    $('input#none').prop('checked', false);
+  });
+  
+  $( 'input#val_cadre' ).click(function() {
+    $('input#none').prop('checked', false);
+  });
+
+  var roboto = new FontFaceObserver('Roboto');
+
+  roboto.load().then(function () {
+    document.body.className += " fonts-loaded";
+  });
+  
+
     
 });
