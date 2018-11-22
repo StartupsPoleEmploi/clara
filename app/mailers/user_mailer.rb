426d687528b6fa75130955f8784bc377bcceabc4
Maxime Devallez <maxdeval02@gmail.com>
2018-11-22 11:43:46 +0100

    cleaned

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,8 +6,8 @@ class UserMailer < ApplicationMailer
     mail(
       subject: "Demande de contact via le site Clara",
       from: ENV['ARA_EMAIL_FROM'],
-      to: ENV['ARA_EMAIL_BCC'],
-
+      to: ENV['ARA_EMAIL_DESTINATION'],
+      cc: ENV['ARA_EMAIL_CC'],
       reply_to: @contact.email,
       delivery_method_options: { api_key: ENV['ARA_EMAIL_API_KEY'], secret_key: ENV['ARA_EMAIL_SECRET_KEY'] }
     )
