9d70131d7b290e347fd80ff1c11401277bd610cd
Maxime Devallez <maxdeval02@gmail.com>
2018-11-22 10:03:21 +0100

    good on inscription_questions

--- app/assets/javascripts/inscription_questions.js
+++ app/assets/javascripts/inscription_questions.js
@@ -1,5 +1,24 @@
 load_js_for_page(["inscription_questions", "new"], function() {
 
   $("input:radio:first").focus();
+
+  var roboto400 = new FontFaceObserver("Roboto", {
+  weight: 400
+  });
+  var roboto500 = new FontFaceObserver("Roboto", {
+    weight: 500
+  });
+  var roboto700 = new FontFaceObserver("Roboto", {
+    weight: 700
+  });
+
+  Promise.all([
+    roboto400.load(),
+    roboto500.load(),
+    roboto700.load()
+  ]).then(function() {
+    document.documentElement.className += " fonts-loaded";
+    console.log("Roboto font is available")
+  });
   
 });
