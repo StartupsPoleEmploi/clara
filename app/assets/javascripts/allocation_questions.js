d1cc443a0661ea20c9a44f02dc4a502c57f88b5b
Maxime Devallez <maxdeval02@gmail.com>
2018-11-22 12:01:55 +0100

    allocation_question

--- app/assets/javascripts/allocation_questions.js
+++ app/assets/javascripts/allocation_questions.js
@@ -1,5 +1,12 @@
 load_js_for_page(["allocation_questions", "new"], function() {
 
   $("input:radio:first").focus();
+
+  var roboto = new FontFaceObserver('Roboto');
+
+  roboto.load().then(function () {
+    document.body.className += " fonts-loaded";
+    console.log("Police roboto est prête")
+  });
   
 });
--- app/assets/javascripts/inscription_questions.js
+++ app/assets/javascripts/inscription_questions.js
@@ -2,7 +2,7 @@ load_js_for_page(["inscription_questions", "new"], function() {
 
   $("input:radio:first").focus();
 
-  var roboto400 = new FontFaceObserver('Roboto');
+  var roboto = new FontFaceObserver('Roboto');
 
   roboto.load().then(function () {
     document.body.className += " fonts-loaded";
