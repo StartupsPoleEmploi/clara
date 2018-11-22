522012bd49da65a08e493e5a0b531676fc91b1cf
David Boureau <bdavidxyz@gmail.com>
2018-11-22 17:03:20 +0100

    [1040] missing test U

--- config/initializers/init_version.rb
+++ config/initializers/init_version.rb
@@ -1,6 +1,6 @@
 major = ENV["MAJOR_VERSION"] || 16
 minor = ENV["MINOR_VERSION"] || 6
-build = ENV["BUILD_VERSION"] || 3
+build = ENV["BUILD_VERSION"] || 4
 
 
 ARA_VERSION = "#{[major.to_s, minor.to_s, build.to_s].join('.')}"
