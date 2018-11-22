2a8c8769c125c6e3e9b349efd5e6840b83491267
Maxime Devallez <maxdeval02@gmail.com>
2018-11-22 12:05:17 +0100

    grade question

--- app/assets/javascripts/grade_questions.js
+++ app/assets/javascripts/grade_questions.js
@@ -1,5 +1,11 @@
 load_js_for_page(["grade_questions", "new"], function() {
 
   $("input:radio:first").focus();
+
+  var roboto = new FontFaceObserver('Roboto');
+
+  roboto.load().then(function () {
+    document.body.className += " fonts-loaded";
+  });
   
 });
