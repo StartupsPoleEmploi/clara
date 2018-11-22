84fd30cf0152f5992e38c6d1816bcf507fc2ce11
David Boureau <bdavidxyz@gmail.com>
2018-11-22 15:09:31 +0100

    wip

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -1,78 +1,26 @@
 _.set(window, 'clara.cookies', {
-    init_cookies_preferences: function() {
-      if($("#authorize_all").is(":checked")) {
-        $("#authorize_statistic").prop("checked", true);
-        $("#authorize_navigation").prop("checked", true);
-        $("#input_nav").prop("checked", false);
-        $("#input_stat").prop("checked", false);
-      };
-    },
     authorize_all: function() {
       $("#authorize_all").click(function() {
         $("#authorize_statistic").prop("checked", true);
         $("#authorize_navigation").prop("checked", true);
-        $("#input_nav").prop("checked", false);
-        $("#input_stat").prop("checked", false);
+        // $("#input_nav").prop("checked", false);
+        // $("#input_stat").prop("checked", false);
       });
     },
     forbid_all: function() {
       $("#forbid_all").click(function() {
         $("#forbid_statistic").prop("checked", true);
         $("#forbid_navigation").prop("checked", true);
-        $("#input_nav").prop("checked", true);
-        $("#input_stat").prop("checked", true);
       });
     },
-    authorize_navigation: function () {
-      $("#authorize_navigation").click(function() {
-        $('input[name=control_all]').prop('checked',false);
-        $("#input_nav").prop("checked", false);
-        if($("#authorize_statistic").is(":checked")) {
-          $("#authorize_all").prop("checked", true);
-        };
-      });
-    },
-    authorize_statistic: function() {
-      $("#authorize_statistic").click(function() {
-        $('input[name=control_all]').prop('checked',false);
-        $("#input_stat").prop("checked", false);
-        if($("#authorize_navigation").is(":checked")) {
-          $("#authorize_all").prop("checked", true);
-        };
-      });
-    },
-    forbid_navigation: function () {
-      $("#forbid_navigation").click(function() {
-        $('input[name=control_all]').prop('checked',false);
-        $("#input_nav").prop("checked", true);
-        if($("#forbid_statistic").is(":checked")) {
-          $("#forbid_all").prop("checked", true);
-        };
-      });
-    },
-    forbid_statistic: function () {
-      $("#forbid_statistic").click(function() {
-        $('input[name=control_all]').prop('checked',false);
-        $("#input_stat").prop("checked", true);
-        if($("#forbid_navigation").is(":checked")) {
-          $("#forbid_all").prop("checked", true);
-        };
-      });
-    }
 });
 
 clara.load_js(function only_if(){return $("body").hasClasses("cookies", "edit")}, function() {
 
 
   $("input:radio:first").focus();
-  $("#authorize_all").prop("checked", true);
-  clara.cookies.init_cookies_preferences();
   clara.cookies.authorize_all();
   clara.cookies.forbid_all();
-  clara.cookies.authorize_navigation();
-  clara.cookies.authorize_statistic();
-  clara.cookies.forbid_navigation();
-  clara.cookies.forbid_statistic();
 
 });
   
