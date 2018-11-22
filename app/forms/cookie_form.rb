de1b0f02671e5d2090f1f450892b4dc07f0e53b4
Maxime Devallez <maxdeval02@gmail.com>
2018-11-22 16:37:13 +0100

    found mistake

--- app/forms/cookie_form.rb
+++ app/forms/cookie_form.rb
@@ -1,6 +1,9 @@
 class CookieForm < ActiveType::Object
 
-  attribute :value, :string
-  # attribute :value, :string
+  attribute :authorize_statistic, :string
+  attribute :forbid_statistic, :string
+  attribute :authorize_navigation, :string
+  attribute :forbid_navigation, :string
+
 
 end
