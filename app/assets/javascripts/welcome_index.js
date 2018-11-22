482520b42ec2845f745f372442998c744ffe948b
Maxime Devallez <maxdeval02@gmail.com>
2018-11-22 10:39:07 +0100

    had to commit

--- app/assets/javascripts/welcome_index.js
+++ app/assets/javascripts/welcome_index.js
@@ -21,23 +21,23 @@ load_js_for_page(["welcome", "index"], function() {
   });
 
 
-var roboto400 = new FontFaceObserver("Roboto", {
-  weight: 400
-});
-var roboto500 = new FontFaceObserver("Roboto", {
-  weight: 500
-});
-var roboto700 = new FontFaceObserver("Roboto", {
-  weight: 700
-});
+  var roboto400 = new FontFaceObserver("Roboto", {
+    weight: 400
+  });
+  var roboto500 = new FontFaceObserver("Roboto", {
+    weight: 500
+  });
+  var roboto700 = new FontFaceObserver("Roboto", {
+    weight: 700
+  });
 
-Promise.all([
-  roboto400.load(),
-  roboto500.load(),
-  roboto700.load()
-]).then(function() {
-  document.documentElement.className += " fonts-loaded";
-  console.log("Roboto font is available")
-});
+  Promise.all([
+    roboto400.load(),
+    roboto500.load(),
+    roboto700.load()
+  ]).then(function() {
+    document.documentElement.className += " fonts-loaded";
+    console.log("Roboto font is available")
+  });
 
 });
