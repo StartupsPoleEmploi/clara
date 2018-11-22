8fc58648dce2a8be6c50e33a41d710548bd95287
David Boureau <bdavidxyz@gmail.com>
2018-11-22 14:33:55 +0100

    cookiesjs modified

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -1,147 +1,78 @@
+_.set(window, 'clara.cookies', {
+    init_cookies_preferences: function() {
+      if($("#authorize_all").is(":checked")) {
+        $("#authorize_statistic").prop("checked", true);
+        $("#authorize_navigation").prop("checked", true);
+        $("#input_nav").prop("checked", false);
+        $("#input_stat").prop("checked", false);
+      };
+    },
+    authorize_all: function() {
+      $("#authorize_all").click(function() {
+        $("#authorize_statistic").prop("checked", true);
+        $("#authorize_navigation").prop("checked", true);
+        $("#input_nav").prop("checked", false);
+        $("#input_stat").prop("checked", false);
+      });
+    },
+    forbid_all: function() {
+      $("#forbid_all").click(function() {
+        $("#forbid_statistic").prop("checked", true);
+        $("#forbid_navigation").prop("checked", true);
+        $("#input_nav").prop("checked", true);
+        $("#input_stat").prop("checked", true);
+      });
+    },
+    authorize_navigation: function () {
+      $("#authorize_navigation").click(function() {
+        $('input[name=control_all]').prop('checked',false);
+        $("#input_nav").prop("checked", false);
+        if($("#authorize_statistic").is(":checked")) {
+          $("#authorize_all").prop("checked", true);
+        };
+      });
+    },
+    authorize_statistic: function() {
+      $("#authorize_statistic").click(function() {
+        $('input[name=control_all]').prop('checked',false);
+        $("#input_stat").prop("checked", false);
+        if($("#authorize_navigation").is(":checked")) {
+          $("#authorize_all").prop("checked", true);
+        };
+      });
+    },
+    forbid_navigation: function () {
+      $("#forbid_navigation").click(function() {
+        $('input[name=control_all]').prop('checked',false);
+        $("#input_nav").prop("checked", true);
+        if($("#forbid_statistic").is(":checked")) {
+          $("#forbid_all").prop("checked", true);
+        };
+      });
+    },
+    forbid_statistic: function () {
+      $("#forbid_statistic").click(function() {
+        $('input[name=control_all]').prop('checked',false);
+        $("#input_stat").prop("checked", true);
+        if($("#forbid_navigation").is(":checked")) {
+          $("#forbid_all").prop("checked", true);
+        };
+      });
+    }
+});
+
 clara.load_js(function only_if(){return $("body").hasClasses("cookies", "edit")}, function() {
 
-  /**
-  *         INITIAL STATE 
-  **/
-  var STATE_KEY = 'cookies_preference';
 
-  var initial_state = {
-    disable_statistic: false,
-    disable_navigation: false
-  };
-
-  var default_state = function() {
-    var previous_state = {
-      disable_statistic: $('#input_stat').is(':checked'),
-      disable_navigation: $('#input_nav').is(':checked')
-    };
-    var has_state = _.isPlainObject(previous_state) && _.isNotEmpty(previous_state);
-    return has_state ? previous_state : initial_state;
-  };
-
-  var disable_button = function (id) {
-    $(id).addClass("is-disabled");
-  }
-  var enable_button = function (id) {
-    $(id).removeClass("is-disabled");
-  }
-
-  /**
-  *         MAIN REDUCER 
-  **/
-  var main_reducer = function(state, action) {
-
-    if (state === undefined) {
-      return default_state();
-    }
-
-    // Works better than _.assign or Object.assign
-    var newState = JSON.parse(JSON.stringify(state));
-
-    if (action.type === 'AUTHORIZE_STATISTIC') {
-      newState.disable_statistic = false;
-    }
-    if (action.type === 'FORBID_STATISTIC') {
-      newState.disable_statistic = true;
-    }
-    if (action.type === 'AUTHORIZE_NAVIGATION') {
-      newState.disable_navigation = false;
-    }
-    if (action.type === 'FORBID_NAVIGATION') {
-      newState.disable_navigation = true;
-    }
-    if (action.type === 'AUTHORIZE_ALL') {
-      newState.disable_statistic = false;
-      newState.disable_navigation = false;
-    }
-    if (action.type === 'FORBID_ALL') {
-      newState.disable_statistic = true;
-      newState.disable_navigation = true;
-    }
-
-    return newState;
-  };
-
-  /**
-  *         MAIN STORE 
-  **/
-  var main_store = Redux.createStore(main_reducer, default_state());
-
-  /**
-  *         DISPATCHERS 
-  **/
-  $('#authorize_statistic').click(function(){
-    var that = this;
-    main_store.dispatch({type: 'AUTHORIZE_STATISTIC'});
-  });
-  $('#forbid_statistic').click(function(){
-    var that = this;
-    main_store.dispatch({type: 'FORBID_STATISTIC'});
-  });
-
-  $('#authorize_navigation').click(function(){
-    var that = this;
-    main_store.dispatch({type: 'AUTHORIZE_NAVIGATION'});
-  });
-  $('#forbid_navigation').click(function(){
-    var that = this;
-    main_store.dispatch({type: 'FORBID_NAVIGATION'});
-  });
-
-  $('#authorize_all').click(function(){
-    var that = this;
-    main_store.dispatch({type: 'AUTHORIZE_ALL'});
-  });
-  $('#forbid_all').click(function(){
-    var that = this;
-    main_store.dispatch({type: 'FORBID_ALL'});
-  });
-
-  /**
-  *         SUBSCRIBERS
-  **/
-  main_store.subscribe(function() {
-
-    var state = main_store.getState();
-
-    // state of simple buttons
-    if (state.disable_statistic === true) {
-      disable_button("#authorize_statistic");
-      enable_button("#forbid_statistic");
-    } else {
-      enable_button("#authorize_statistic");
-      disable_button("#forbid_statistic");
-    }
-
-    if (state.disable_navigation === true) {
-      disable_button("#authorize_navigation");
-      enable_button("#forbid_navigation");
-    } else {
-      enable_button("#authorize_navigation");
-      disable_button("#forbid_navigation");
-    }
-
-    // state aggregated buttons
-    if (_.every(state)) {
-      enable_button("#forbid_all");
-      disable_button("#authorize_all");        
-    } 
-    else if (_.none(state)) {
-      disable_button("#forbid_all");
-      enable_button("#authorize_all");        
-    } 
-    else { // some
-      disable_button("#authorize_all");
-      disable_button("#forbid_all");        
-    } 
-
-    // state of form
-    $("#input_nav").prop( "checked", state.disable_navigation );
-    $("#input_stat").prop( "checked", state.disable_statistic );
-  }); 
-
-  // fire initial state
-  main_store.dispatch({type: 'INIT'})
-
+  $("input:radio:first").focus();
+  $("#authorize_all").prop("checked", true);
+  clara.cookies.init_cookies_preferences();
+  clara.cookies.authorize_all();
+  clara.cookies.forbid_all();
+  clara.cookies.authorize_navigation();
+  clara.cookies.authorize_statistic();
+  clara.cookies.forbid_navigation();
+  clara.cookies.forbid_statistic();
 
 });
+  
