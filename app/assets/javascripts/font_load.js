64d42f869cde4b03610aa2931d3a1d7c705ef787
David Boureau <bdavidxyz@gmail.com>
2018-11-26 09:52:09 +0100

    works

--- app/assets/javascripts/font_load.js
+++ app/assets/javascripts/font_load.js
@@ -1,11 +1,10 @@
 clara.load_js(function only_if(){return true}, function() {
 
   var roboto = new FontFaceObserver('Roboto');
-
   roboto.load().then(function () {
     document.body.className += " fonts-loaded";
   });
-  
+
 });
 
 
