00e127fb9c62a1903310b12c7bee3d81bb8df2b2
Maxime Devallez <maxdeval02@gmail.com>
2018-12-03 17:01:26 +0100

    with this way param is missing

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -32,7 +32,7 @@ class CookiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cooky_params
-      params.require(:cookies_form1, :cookies_form2).permit(:value, :value)
+      params.require(:cookies_form1).permit(:value).require(:cookies_form2).permit(:value)
       #params.require(:cookies_form2).permit(:value)
     end
 end
