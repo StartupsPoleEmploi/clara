30bc08d41d041eda74506cd3291169437b1ce135
Maxime Devallez <maxdeval02@gmail.com>
2018-12-03 15:59:42 +0100

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,11 +9,13 @@ module ApplicationHelper
   end
 
   def ga_disabled?
-    session[:cookie] && session[:cookie]["forbid_statistic"] && session[:cookie]["forbid_statistic"] == "1"
+    session[:cookie] && session[:cookie]["forbid_statistic"] 
+    #&& session[:cookie]["forbid_statistic"] == "1"
   end
 
   def hj_disabled?
-    session[:cookie] && session[:cookie]["forbid_navigation"] && session[:cookie]["forbid_navigation"] == "1"
+    session[:cookie] && session[:cookie]["forbid_navigation"] 
+    #&& session[:cookie]["forbid_navigation"] == "1"
   end
 
   def empty_image
