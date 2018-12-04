de64154ee74aff408e97f0b9d80723137b3ade7b
David Boureau <bdavidxyz@gmail.com>
2018-12-04 16:31:50 +0100

    jquery_extension replaced and renamed

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -1,7 +1,7 @@
 //= require magic_lamp
 //= require lodash/lodash
 //= require jquery/jquery
-//= require jquery/jquery-extension
+//= require jquery/jquery_extension
 //= require load_js
 //= require support/jasmine-jquery
 //= require support/jasmine-matchers
