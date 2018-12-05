8cb06271bf85cb4b577b9d6e5228c9300f7667ac
Maxime Devallez <maxdeval02@gmail.com>
2018-12-05 14:10:56 +0100

    required choices

--- app/forms/cookie_form.rb
+++ app/forms/cookie_form.rb
@@ -3,4 +3,7 @@ class CookieForm < ActiveType::Object
   attribute :analytics, :string
   attribute :hotjar, :string
 
+  validates :analytics, presence: true
+  validates :hotjar, presence: true
+
 end
