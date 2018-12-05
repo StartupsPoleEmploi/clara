4fbf287a35d179938986c4787b22ae16f75873b7
Maxime Devallez <maxdeval02@gmail.com>
2018-12-05 11:46:58 +0100

    state function working

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -14,7 +14,14 @@ _.set(window, 'clara.cookies', {
     radiobuttons_state: function() {
       if( $("#ga-script").length === 1 && $("#ga-create-script").length === 1) {
         $("#authorize_statistic").prop("checked", true);
+      } else {
+        $("#forbid_statistic").prop("checked", true);
       }
+      if( $("#hj-script").length === 1 ) {
+        $("#authorize_navigation").prop("checked", true);
+      } else {
+        $("#forbid_navigation").prop("checked", true);
+      }      
     }
 });
 
@@ -26,6 +33,7 @@ clara.load_js(function only_if(){return $("body").hasClasses("cookies", "edit")}
   $("input:radio:first").focus();
   clara.cookies.authorize_all();
   clara.cookies.forbid_all();
+  clara.cookies.radiobuttons_state();
 
 });
   
