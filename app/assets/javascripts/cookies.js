cba67a6b1f58b890c8a6723c40ec1a8dfdec5143
David Boureau <bdavidxyz@gmail.com>
2018-12-06 15:20:39 +0100

    better js spec coverage

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -6,27 +6,27 @@ _.set(window, 'clara.cookies', {
 
     main_function: function() {
       $("input:radio:first").focus();
-      clara.cookies.authorize_all();
-      clara.cookies.forbid_all();
-      clara.cookies.radiobuttons_state();
+      clara.cookies.setup_authorize_all_callbacks();
+      clara.cookies.setup_forbid_all_callbacks();
+      clara.cookies.setup_initial_radiobuttons_state();
     },
 
-    authorize_all: function() {
+    setup_authorize_all_callbacks: function() {
       $("#authorize_all").click(function() {
         $("#authorize_statistic").prop("checked", true);
         $("#authorize_navigation").prop("checked", true);
       });
     },
 
-    forbid_all: function() {
+    setup_forbid_all_callbacks: function() {
       $("#forbid_all").click(function() {
         $("#forbid_statistic").prop("checked", true);
         $("#forbid_navigation").prop("checked", true);
       });
     },
 
-    radiobuttons_state: function() {
-      if( $("#ga-script").length === 1 && $("#ga-create-script").length === 1) {
+    setup_initial_radiobuttons_state: function() {
+      if( $("#ga-script").length === 1) {
         $("#authorize_statistic").prop("checked", true);
       } else {
         $("#forbid_statistic").prop("checked", true);
--- spec/javascripts/4_cookie_spec.js
+++ spec/javascripts/4_cookie_spec.js
@@ -1,4 +1,3 @@
-//= require load_js_per_page
 //= require cookies
 describe('cookies.js', function() {
 
@@ -10,16 +9,16 @@ describe('cookies.js', function() {
 
     $('body').append('\
       <div class="test-only">\
-        <input id="authorize_all" name="control_all" type="button" value="authorize_all">\
-        <input id="forbid_all" name="control_all" type="button" value="forbid_all">\
-        <input id="authorize_statistic" name="statistic" type="radio" value="authorize_statistic">\
-        <input id="forbid_statistic" name="statistic" type="radio" value="forbid_statistic">\
-        <input id="authorize_navigation" name="navigation" type="radio" value="authorize_navigation">\
-        <input id="forbid_navigation" name="navigation" type="radio" value="forbid_navigation">\
+        <input id="authorize_all"        name="control_all" type="button" value="authorize_all">\
+        <input id="forbid_all"           name="control_all" type="button" value="forbid_all">\
+        <input id="authorize_statistic"  name="statistic"   type="radio"  value="authorize_statistic">\
+        <input id="forbid_statistic"     name="statistic"   type="radio"  value="forbid_statistic">\
+        <input id="authorize_navigation" name="navigation"  type="radio"  value="authorize_navigation">\
+        <input id="forbid_navigation"    name="navigation"  type="radio"  value="forbid_navigation">\
+        <div id="hj-script">\
+        <div id="ga-script">\
       </div>\
     ');
-
-    clara.cookies.main_function();
 
   });
 
@@ -27,9 +26,30 @@ describe('cookies.js', function() {
     $(".test-only").remove();
   })
     
-  describe('authorize_all', function() {
+  describe('radiobuttons_state', function() {
+    it('Should say yes to analytics if ga-create script is already here', function() {
+      //given
+      expect($("#authorize_statistic").is(":checked")).toBe(false);
+      //when
+      clara.cookies.setup_initial_radiobuttons_state();
+      //then
+      expect($("#authorize_statistic").is(":checked")).toBe(true);
+    });
+    it('Should say yes to hotjar if hj-create script is already here', function() {
+      //given
+      expect($("#authorize_navigation").is(":checked")).toBe(false);
+      //when
+      clara.cookies.setup_initial_radiobuttons_state();
+      //then
+      expect($("#authorize_navigation").is(":checked")).toBe(true);
+    });
+
+  });
+
+  describe('setup_authorize_all_callbacks', function() {
     it('Should check authorize_statistic when you click on authorize_all', function() {
       //given
+      clara.cookies.setup_authorize_all_callbacks();
       //when
       $('#authorize_all').click();
       //then
@@ -37,6 +57,7 @@ describe('cookies.js', function() {
     });
     it('Should check authorize_navigation when you click on authorize_all', function() {
       //given
+      clara.cookies.setup_authorize_all_callbacks();
       //when
       $('#authorize_all').click();
       //then
@@ -44,6 +65,7 @@ describe('cookies.js', function() {
     });
     it('Should not check forbid_navigation when you click on authorize_all', function() {
       //given
+      clara.cookies.setup_authorize_all_callbacks();
       //when
       $('#authorize_all').click();
       //then
@@ -51,6 +73,7 @@ describe('cookies.js', function() {
     });
     it('Should not check forbid_statistic when you click on authorize_all', function() {
       //given
+      clara.cookies.setup_authorize_all_callbacks();
       //when
       $('#authorize_all').click();
       //then
@@ -62,6 +85,7 @@ describe('cookies.js', function() {
   describe('forbid_all', function() {
     it('Should check forbid_statistic when you click on forbid_all', function() {
       //given
+      clara.cookies.setup_forbid_all_callbacks();
       //when
       $('#forbid_all').click();
       //then
@@ -69,6 +93,7 @@ describe('cookies.js', function() {
     });
     it('Should check forbid_navigation when you click on forbid_all', function() {
       //given
+      clara.cookies.setup_forbid_all_callbacks();
       //when
       $('#forbid_all').click();
       //then
@@ -77,6 +102,7 @@ describe('cookies.js', function() {
 
     it('Should not check authorize_navigation when you click on forbid_all', function() {
       //given
+      clara.cookies.setup_forbid_all_callbacks();
       //when
       $('#forbid_all').click();
       //then
@@ -84,6 +110,7 @@ describe('cookies.js', function() {
     });
     it('Should not check authorize_statistic when you click on forbid_all', function() {
       //given
+      clara.cookies.setup_forbid_all_callbacks();
       //when
       $('#forbid_all').click();
       //then
