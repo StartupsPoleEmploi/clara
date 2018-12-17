4f762b8617971d9f31e96c07458fa6e1f75dd2b7
David Boureau <bdavidxyz@gmail.com>
2018-12-17 10:41:49 +0100

    removed console.log

--- app/assets/javascripts/aides_index.js
+++ app/assets/javascripts/aides_index.js
@@ -1,7 +1,5 @@
 clara.load_js(function only_if(){return $("body").hasClasses("aides", "index")}, function main_function() {
 
-  console.log("aides index");
-
   change_placeholder();
 
   $( window ).resize(function() {
