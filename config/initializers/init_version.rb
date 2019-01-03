d5a835f5c57e1c258be950961cff4eba0d00868c
David Boureau <bdavidxyz@gmail.com>
2019-01-03 11:17:08 +0100

    [1075] dependency order

--- config/initializers/init_version.rb
+++ config/initializers/init_version.rb
@@ -1,6 +1,6 @@
 major = ENV["MAJOR_VERSION"] || 17
-minor = ENV["MINOR_VERSION"] || 3
-build = ENV["BUILD_VERSION"] || 2
+minor = ENV["MINOR_VERSION"] || 4
+build = ENV["BUILD_VERSION"] || 0
 
 
 ARA_VERSION = "#{[major.to_s, minor.to_s, build.to_s].join('.')}"
