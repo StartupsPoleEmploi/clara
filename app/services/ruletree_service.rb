9642a3a48788748a6e2c0a8c343373cba42cd093
David Boureau <bdavidxyz@gmail.com>
2019-01-10 15:58:21 +0100

    boolean type is not possible !

--- app/services/ruletree_service.rb
+++ app/services/ruletree_service.rb
@@ -108,8 +108,6 @@ class RuletreeService
         the_value.to_i
       when 'string'
         the_value.to_s
-      when 'boolean'
-        the_value.to_s.downcase == "true"
       else
         the_value
     end
