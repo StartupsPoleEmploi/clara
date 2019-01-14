e71f4bb55c9f78578a1a39fecb15e41dc584abb9
David Boureau <bdavidxyz@gmail.com>
2019-01-14 17:10:17 +0100

    sort works, but not on correct field

--- app/assets/javascripts/administrate/custom/admin_rules/zu_clean_select.js
+++ app/assets/javascripts/administrate/custom/admin_rules/zu_clean_select.js
@@ -7,7 +7,7 @@ clara.js_define("zu_clean_select", {
   main_function: function(arg_h) {
     var that = this;
     that._remove_empty_option_if_only_one_choice(arg_h);
-    that._sort_options_alphabetically();
+    that._sort_options_alphabetically(arg_h);
   },
 
   _remove_empty_option_if_only_one_choice: function(arg_h) {
@@ -17,8 +17,20 @@ clara.js_define("zu_clean_select", {
     }
   },
 
-  _sort_options_alphabetically: function() {
+  _sort_options_alphabetically: function(arg_h) {
+    $select = arg_h.for_select
 
+    var my_options = $select.find("option");
+    var selected = $select.val();
+
+    my_options.sort(function(a,b) {
+        if (a.text > b.text) return 1;
+        if (a.text < b.text) return -1;
+        return 0
+    });
+
+    $select.empty().append( my_options );
+    $select.val(selected);
   }
 
 });
