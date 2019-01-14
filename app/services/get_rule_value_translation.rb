71eb1fd37a5d3dbf8bc5bd99d21a97331172e613
David Boureau <bdavidxyz@gmail.com>
2019-01-14 14:46:02 +0100

    tested

--- app/services/get_rule_value_translation.rb
+++ app/services/get_rule_value_translation.rb
@@ -3,31 +3,10 @@ class GetRuleValueTranslation < ClaraService
   is_callable
 
   def call
-    p '- - - - - - - - - - - - - - variable_id- - - - - - - - - - - - - - - -' 
-    pp variable_id
-    p ''
-    p '- - - - - - - - - - - - - - key_value- - - - - - - - - - - - - - - -' 
-    pp key_value
-    p ''
     v = Variable.find(@variable_id)
-    p '- - - - - - - - - - - - - - v- - - - - - - - - - - - - - - -' 
-    pp v
-    p ''
     translations = v.elements_translation.split(",")
-    p '- - - - - - - - - - - - - - translations- - - - - - - - - - - - - - - -' 
-    pp translations
-    p ''
     keys = v.elements.split(",")
-    p '- - - - - - - - - - - - - - keys- - - - - - - - - - - - - - - -' 
-    pp keys
-    p ''
     index_of_key = keys.find_index(@key_value)
-    p '- - - - - - - - - - - - - - index_of_key- - - - - - - - - - - - - - - -' 
-    pp index_of_key
-    p ''
-    p '- - - - - - - - - - - - - - translations- - - - - - - - - - - - - - - -' 
-    pp translations[index_of_key]
-    p ''
     translations[index_of_key]
   end
 
