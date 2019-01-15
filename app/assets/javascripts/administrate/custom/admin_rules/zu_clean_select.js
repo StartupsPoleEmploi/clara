1a8c6f88f6e0228c2fb395b704aff70219fd6885
David Boureau <bdavidxyz@gmail.com>
2019-01-15 15:29:55 +0100

    simplified

--- app/assets/javascripts/administrate/custom/admin_rules/zu_clean_select.js
+++ app/assets/javascripts/administrate/custom/admin_rules/zu_clean_select.js
@@ -5,15 +5,11 @@ clara.js_define("zu_clean_select", {
   trigger_function: _.stubFalse,
 
   main_function: function(arg_h) {
-    var that = this;
-    that._remove_empty_option_if_only_one_choice(arg_h);
-  },
-
-  _remove_empty_option_if_only_one_choice: function(arg_h) {
     var $select = arg_h.for_select
     if ($select.find("option").length === 2) {
       $select.find('option[value=""]').remove();
     }
-  },
+  }
+
 
 });
