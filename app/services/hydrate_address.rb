9a71a5da387df7d32274cd3cc3807ba399b5267f
David Boureau <bdavidxyz@gmail.com>
2019-01-18 15:05:49 +0100

    cleaned

--- app/services/hydrate_address.rb
+++ app/services/hydrate_address.rb
@@ -8,19 +8,9 @@ class HydrateAddress < ClaraService
     end
     output_asker = Asker.new(@asker_attributes)
     citycode = output_asker.v_location_citycode
-    p '- - - - - - - - - - - - - - citycode- - - - - - - - - - - - - - - -' 
-    pp citycode
-    pp citycode.blank?
-    p ''
     zipcode = output_asker.v_location_zipcode
-    p '- - - - - - - - - - - - - - zipcode- - - - - - - - - - - - - - - -' 
-    pp zipcode
-    pp zipcode.present?
-    p ''
     return output_asker if citycode.blank?
     return output_asker if zipcode.present?
-    p '- - - - - - - - - - - - - - RehydrateAddressService inside- - - - - - - - - - - - - - - -' 
-    p ''
     output_asker.v_zrr = IsZrr.call(citycode: citycode)
     zip_city_region = GetZipCityRegion.call(citycode: citycode)
     got_them = zip_city_region.is_a?(Array)
@@ -28,9 +18,6 @@ class HydrateAddress < ClaraService
     output_asker.v_location_city = got_them ? zip_city_region[1] : nil
     output_asker.v_location_label = zip_city_region[0] + " " + zip_city_region[1] if got_them
     output_asker.v_location_state = zip_city_region[2] if got_them
-    p '- - - - - - - - - - - - - - output_asker- - - - - - - - - - - - - - - -' 
-    pp output_asker
-    p ''
     output_asker
   end
 
