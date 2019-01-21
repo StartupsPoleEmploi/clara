b032896a6e219272ddbaf6ca01d83aa1b76325cd
David Boureau <bdavidxyz@gmail.com>
2019-01-21 14:30:18 +0100

    simplification

--- app/services/justification_service.rb
+++ app/services/justification_service.rb
@@ -7,7 +7,7 @@ class JustificationService
   def root_rules
     if @aid != nil && @aid.rule != nil
       return @aid.rule.slave_rules if @aid.rule.is_complex_rule?
-      return [@aid.rule] if @aid.rule.is_simple_rule?
+      return [@aid.rule] if @aid.rule.kind == "simple"
     end
     []
   end
@@ -16,7 +16,7 @@ class JustificationService
     if @aid != nil && @aid.rule != nil
       return 'and' if @aid.rule.composition_type == 'and_rule'    
       return 'or' if @aid.rule.composition_type == 'or_rule'    
-      return 'one' if @aid.rule.is_simple_rule?    
+      return 'one' if @aid.rule.kind == "simple"  
     end
     ''
   end
