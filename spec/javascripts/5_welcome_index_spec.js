365a653fe4fa4ac02d957e236d2eec2e3e78575e
Maxime Devallez <maxdeval02@gmail.com>
2019-01-22 12:13:40 +0100

    teaspoon doesn't find welcome_index_spec test

--- spec/javascripts/5_welcome_index_spec.js
+++ spec/javascripts/5_welcome_index_spec.js
@@ -1,4 +1,8 @@
 //= require welcome_index
 describe('welcome_index.js', function() {
 
+  it('Should have welcome_index mapped to clara', function() {
+    expect(clara.welcome_index).toBeDefined();
+  });
+
 }
