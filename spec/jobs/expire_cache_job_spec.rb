f1459a28d69630fa2cb2d2cc39f22a85d91c36e6
David Boureau <bdavidxyz@gmail.com>
2019-01-24 14:55:38 +0100

    jobs of cache later

--- spec/jobs/expire_cache_job_spec.rb
+++ spec/jobs/expire_cache_job_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe ExpireCacheJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe ExpireCacheJob, type: :job do
+#   describe "Nominal" do
+    
+#   end
+# end
