03f20b02c97112be1e08487279bf195967812e59
David Boureau <bdavidxyz@gmail.com>
2019-01-24 11:45:11 +0100

    ability to detect errors with JS

---
+++ app/assets/javascripts/administrate/custom/when_admin_form_has_error.js
@@ -0,0 +1,11 @@
+clara.js_define("when_admin_form_has_error", {
+
+    trigger_function: function() {
+      return $("field-unit-error-msg");
+    },
+    main_function: function() {
+      console.log("has errors!")
+    },
+
+});
+
