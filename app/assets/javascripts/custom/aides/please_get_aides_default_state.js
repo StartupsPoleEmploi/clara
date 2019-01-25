4fd5b25f43e41ac0fd70be42b0c7f106de1a4a96
David Boureau <bdavidxyz@gmail.com>
2019-01-25 16:04:55 +0100

    a correct red test first

--- app/assets/javascripts/custom/aides/please_get_aides_default_state.js
+++ app/assets/javascripts/custom/aides/please_get_aides_default_state.js
@@ -5,7 +5,7 @@ clara.js_define("please_get_aides_default_state", {
   main_function: function(the_initial_state) {
     var previous_state = store.get(clara.please_get_state_key.main_function());
     var has_state = _.isPlainObject(previous_state) && _.isNotEmpty(previous_state);
-    return has_state ? previous_state : initial_state;
+    return has_state ? previous_state : the_initial_state;
   }
 
 });
---
+++ spec/javascripts/7_aides_default_state_spec.js
@@ -0,0 +1,18 @@
+//= require custom/aides/please_get_aides_default_state.js
+describe('please_get_aides_default_state.js', function() {
+
+  it('Should have please_get_aides_default_state mapped to clara', function() {
+    expect(clara.please_get_aides_default_state).toBeDefined();
+  });
+
+  describe('State is existing', function() {
+    beforeEach(function() {
+    });
+    it('returns initial state if not yet existing', function() {
+      spyOn(clara.please_get_state_key, "main_function").and.callFake(function(){return null});
+        res = clara.please_get_aides_default_state.main_function("my_initial_state");
+        expect(res).toEqual("blabla");
+    });
+  });
+
+});
