60e567b1143e711616361f656be3f30b4ec21bc7
David Boureau <bdavidxyz@gmail.com>
2019-01-25 16:19:59 +0100

    key ok

--- spec/javascripts/7_aides_default_state_spec.js
+++ spec/javascripts/7_aides_default_state_spec.js
@@ -8,6 +8,15 @@ describe('please_get_aides_default_state.js', function() {
   describe('State is existing', function() {
     beforeEach(function() {
     });
+    it('extract data from store thanks to state_key', function() {
+      // given
+      spyOn(clara.please_get_state_key, "main_function").and.callFake(_.wrap("key_in_store"));
+      spyOn(store, "get")
+      // when
+      res = clara.please_get_aides_default_state.main_function("my_initial_state");
+      // then
+      expect(store.get).toHaveBeenCalledWith("key_in_store");
+    });
     it('returns given initial state if previous_state is not yet existing', function() {
       spyOn(store, "get").and.callFake(_.wrap(null));
       res = clara.please_get_aides_default_state.main_function("my_initial_state");
@@ -19,9 +28,9 @@ describe('please_get_aides_default_state.js', function() {
       expect(res).toEqual("my_initial_state");
     });
     it('returns previous state if previous_state exists as non-empty object', function() {
-      spyOn(store, "get").and.callFake(_.wrap({a:42}));
+      spyOn(store, "get").and.callFake(_.wrap({state:42}));
       res = clara.please_get_aides_default_state.main_function("my_initial_state");
-      expect(res).toEqual({a:42});
+      expect(res).toEqual({state:42});
     });
   });
 
