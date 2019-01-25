5068c3330c898d75f7b27b40bdb7e025890e7c16
David Boureau <bdavidxyz@gmail.com>
2019-01-25 17:41:05 +0100

    ;;

--- app/assets/javascripts/custom/aides/please_get_aides_default_state.js
+++ app/assets/javascripts/custom/aides/please_get_aides_default_state.js
@@ -4,8 +4,8 @@ clara.js_define("please_get_aides_default_state", {
 
   main_function: function(the_initial_state) {
     var previous_state = store.get(clara.please_get_state_key.main_function());
-    console.log("previous_state")
-    console.log(previous_state)
+    console.log("previous_state");
+    console.log(previous_state);
     var has_state = _.isPlainObject(previous_state) && _.isNotEmpty(previous_state);
     return has_state ? previous_state : the_initial_state;
   }
