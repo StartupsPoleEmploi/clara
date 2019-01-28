f26835edb93d1c1a9458271c205294b6dc3d0b43
David Boureau <bdavidxyz@gmail.com>
2019-01-28 10:32:05 +0100

    all green tests

--- spec/javascripts/7_aides_default_state_spec.js
+++ spec/javascripts/7_aides_default_state_spec.js
@@ -19,7 +19,12 @@ describe('aides_default_state.js', function() {
     //   res = clara.aides_default_state.please("my_initial_state");
     //   expect(res).toEqual({state:42});
     // });
-    it('returns PREVIOUS state, if previous_state exists as non empty object, and raw data of previous_state didnt change');
+    it('returns PREVIOUS state, if previous_state exists as non empty object, and raw data of previous_state didnt change', function(){      
+      spyOn(clara.aides_get_state, "please").and.callFake(_.wrap({state:42}));
+      spyOn(clara.aides_extract_raw_data, "please").and.returnValues(["a", "b"], ["a", "b"]);
+      res = clara.aides_default_state.please("my_initial_state");
+      expect(res).toEqual({state:42});      
+    });
     it('returns INITIAL state, if previous_state exists as non empty object, but actual state changed', function() {
       spyOn(clara.aides_get_state, "please").and.callFake(_.wrap({state:42}));
       spyOn(clara.aides_extract_raw_data, "please").and.returnValues(["a", "b"], ["x", "y"]);
