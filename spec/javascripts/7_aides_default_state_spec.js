a6578045cc688ae7aa3ac4bdcb99c98d8ccf34da
David Boureau <bdavidxyz@gmail.com>
2019-01-28 10:21:20 +0100

    TDDing

--- spec/javascripts/7_aides_default_state_spec.js
+++ spec/javascripts/7_aides_default_state_spec.js
@@ -4,29 +4,26 @@ describe('aides_default_state.js', function() {
   describe('State is existing', function() {
     beforeEach(function() {
     });
-    it('extract data from store thanks to state_key', function() {
-      // given
-      spyOn(clara.aides_state_key, "please").and.callFake(_.wrap("key_in_store"));
-      spyOn(store, "get")
-      // when
-      res = clara.aides_default_state.please("my_initial_state");
-      // then
-      expect(store.get).toHaveBeenCalledWith("key_in_store");
-    });
-    it('returns given initial state if previous_state is not yet existing', function() {
-      spyOn(store, "get").and.callFake(_.wrap(null));
-      res = clara.aides_default_state.please("my_initial_state");
+    it('returns given INITIAL state if previous_state is not yet existing', function() {
+      spyOn(clara.aides_get_state, "please").and.callFake(_.wrap(null));
+      var res = clara.aides_default_state.please("my_initial_state");
       expect(res).toEqual("my_initial_state");
     });
-    it('returns given initial state if previous_state exists but as empty object', function() {
-      spyOn(store, "get").and.callFake(_.wrap({}));
-      res = clara.aides_default_state.please("my_initial_state");
+    it('returns given INITIAL state if previous_state exists but as empty object', function() {
+      spyOn(clara.aides_get_state, "please").and.callFake(_.wrap({}));
+      var res = clara.aides_default_state.please("my_initial_state");
       expect(res).toEqual("my_initial_state");
     });
-    it('returns previous state if previous_state exists as non-empty object', function() {
-      spyOn(store, "get").and.callFake(_.wrap({state:42}));
-      res = clara.aides_default_state.please("my_initial_state");
-      expect(res).toEqual({state:42});
+    // it('returns previous state if previous_state exists as non-empty object', function() {
+    //   spyOn(clara.aides_get_state, "please").and.callFake(_.wrap({state:42}));
+    //   res = clara.aides_default_state.please("my_initial_state");
+    //   expect(res).toEqual({state:42});
+    // });
+    it('returns PREVIOUS state, if previous_state exists as non empty object, and raw data of previous_state didnt change');
+    it('returns INITIAL state, if previous_state exists as non empty object, and raw data of previous_state actually changed', function(){
+    //   spyOn(clara.aides_get_state, "please").and.callFake(_.wrap({state:42}));
+    //   res = clara.aides_default_state.please("my_initial_state");
+    //   expect(res).toEqual({state:42});      
     });
   });
 
