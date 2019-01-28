bea5f89990e9cd0fe324f9d831609473f91631e5
David Boureau <bdavidxyz@gmail.com>
2019-01-28 10:05:46 +0100

    a red test

--- app/assets/javascripts/custom/aides/aides_default_state.js
+++ app/assets/javascripts/custom/aides/aides_default_state.js
@@ -3,7 +3,7 @@ clara.js_define("aides_default_state", {
   please_if: _.stubFalse,
 
   please: function(the_initial_state) {
-    var previous_state = store.get(clara.aides_state_key.please());
+    var previous_state = clara.aides_get_state.please();
     console.log("previous_state");
     console.log(previous_state);
     var has_state = _.isPlainObject(previous_state) && _.isNotEmpty(previous_state);
