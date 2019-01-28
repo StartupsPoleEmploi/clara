d72e9c5ae7a022146ed9c9dae7894c9e1a86e939
David Boureau <bdavidxyz@gmail.com>
2019-01-28 10:07:55 +0100

    green test again

--- app/assets/javascripts/custom/aides/aides_get_state.js
+++ app/assets/javascripts/custom/aides/aides_get_state.js
@@ -5,7 +5,7 @@ clara.js_define("aides_get_state", {
   please: function(store_arg) {
     var key = clara.aides_state_key.please();
     var local_store = store_arg || _.get(window, "store");
-    local_store.get(key);
+    return local_store.get(key);
   }
 
 });
