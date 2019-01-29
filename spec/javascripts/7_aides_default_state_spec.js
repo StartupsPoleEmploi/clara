98dca2b9d8f5e31da67b02dbb2bd4f62522bffb1
David Boureau <bdavidxyz@gmail.com>
2019-01-29 16:38:24 +0100

    clean clean clean

--- spec/javascripts/7_aides_default_state_spec.js
+++ spec/javascripts/7_aides_default_state_spec.js
@@ -3,27 +3,28 @@ describe('aides_default_state.js', function() {
 
   describe('State is existing', function() {
     beforeEach(function() {
+      spyOn(clara.aides_initial_state, "please").and.returnValue("my_initial_state");
     });
     it('returns given INITIAL state if previous_state is not yet existing', function() {
-      spyOn(clara.aides_get_state, "please").and.callFake(_.wrap(null));
-      var res = clara.aides_default_state.please("my_initial_state");
+      spyOn(clara.aides_get_state, "please").and.returnValue(null);
+      var res = clara.aides_default_state.please();
       expect(res).toEqual("my_initial_state");
     });
     it('returns given INITIAL state if previous_state exists but as empty object', function() {
       spyOn(clara.aides_get_state, "please").and.callFake(_.wrap({}));
-      var res = clara.aides_default_state.please("my_initial_state");
+      var res = clara.aides_default_state.please();
       expect(res).toEqual("my_initial_state");
     });
     it('returns PREVIOUS state, if previous_state exists as non empty object, and raw data of previous_state didnt change', function(){      
       spyOn(clara.aides_get_state, "please").and.callFake(_.wrap({state:42}));
       spyOn(clara.aides_extract_raw_data, "please").and.returnValues(["a", "b"], ["a", "b"]);
-      res = clara.aides_default_state.please("my_initial_state");
+      res = clara.aides_default_state.please();
       expect(res).toEqual({state:42});      
     });
     it('returns INITIAL state, if previous_state exists as non empty object, but actual state changed', function() {
       spyOn(clara.aides_get_state, "please").and.callFake(_.wrap({state:42}));
       spyOn(clara.aides_extract_raw_data, "please").and.returnValues(["a", "b"], ["x", "y"]);
-      res = clara.aides_default_state.please("my_initial_state");
+      res = clara.aides_default_state.please();
       expect(res).toEqual("my_initial_state");      
     });
   });
