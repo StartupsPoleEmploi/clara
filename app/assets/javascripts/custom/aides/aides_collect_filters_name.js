71843c2c4a773774840d7be85038cbead071c2dc
David Boureau <bdavidxyz@gmail.com>
2019-01-29 15:44:45 +0100

    bugged

---
+++ app/assets/javascripts/custom/aides/aides_collect_filters_name.js
@@ -0,0 +1,11 @@
+clara.js_define("aides_collect_filters_name", {
+
+  please_if: _.stubFalse,
+
+  please: function() {
+   return 
+    clara.aides_$actual_filters.please()
+      .map(function(){return $(this).data()["name"]}).get();
+  }
+
+});
