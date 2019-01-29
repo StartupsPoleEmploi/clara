070a76ea6c3c391d993d2d544175aeba3771f143
David Boureau <bdavidxyz@gmail.com>
2019-01-29 16:04:10 +0100

    tdd fever

--- app/assets/javascripts/custom/aides/aides_collect_filters_name.js
+++ app/assets/javascripts/custom/aides/aides_collect_filters_name.js
@@ -3,7 +3,7 @@ clara.js_define("aides_collect_filters_name", {
   please_if: _.stubFalse,
 
   please: function() {
-    return clara.aides_$actual_filters.please().map(function(){return $(this).data()["name"]}).get();
+    return clara.aides_$actual_filters.please().datamap("name");
   }
 
 });
