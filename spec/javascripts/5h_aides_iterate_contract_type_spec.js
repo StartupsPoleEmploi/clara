d59c90ad48a4d6df9a95a851b874300595a79dbf
David Boureau <bdavidxyz@gmail.com>
2019-01-31 15:07:43 +0100

    cleaned

---
+++ spec/javascripts/5h_aides_iterate_contract_type_spec.js
@@ -0,0 +1,42 @@
+//= require custom/aides/aides_constants
+//= require custom/aides/aides_iterate_contract_types
+describe('aides_iterate_contract_types.js', function() {
+
+  beforeAll(function(){
+    _.set(window, "main_store", {getState: _.wrap(MagicLamp.loadJSON("nominal_aides_state"))})
+    // _.set(window, "main_store", {getState: _.noop})
+  });
+  afterAll(function(){
+    _.unset(window, "main_store")
+  });
+  it('Should iterate through contract_type', function() {
+    //given
+    var arr = []
+    var my_function = function(ely, contract){arr.push({ely:ely, contract:contract})}
+    //when
+    clara.aides_iterate_contract_types.please(my_function)
+    //then
+    expect(arr[0]).toEqual(
+      {
+        "ely": "eligibles",
+        "contract": {
+          "name": "contrat-specifique",
+          "is_collapsed": false,
+          "aids": [
+            {
+              "name": "volontariat-associatif",
+              "is_collapsed": false,
+              "filters": [
+                {
+                  "name": "s-informer-sur-contrats-specifiques",
+                  "is_collapsed": false
+                }
+              ]
+            }
+          ]
+        }
+      }
+    );
+  });
+
+});
