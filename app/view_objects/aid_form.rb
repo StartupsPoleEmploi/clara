9942dd0307dba1f70ef2d06803188dbea7aba901
David Boureau <bdavidxyz@gmail.com>
2019-01-31 11:53:27 +0100

    cleaned

--- app/view_objects/aid_form.rb
+++ app/view_objects/aid_form.rb
@@ -1,9 +1,6 @@
 class AidForm < ViewObject
 
   def after_init(args)
-    p '- - - - - - - - - - - - - - args- - - - - - - - - - - - - - - -' 
-    pp args.inspect
-    p ''
     locals = hash_for(args)
     @page = locals[:page]
     @mandatory_list = ["name", "contract_type_id", "ordre_affichage"]
