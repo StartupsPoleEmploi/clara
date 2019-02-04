a59127897605ae7712de9606c8a79a873f73a6e6
David Boureau <bdavidxyz@gmail.com>
2019-02-04 11:03:37 +0100

    wip

--- app/dashboards/domain_filter_dashboard.rb
+++ app/dashboards/domain_filter_dashboard.rb
@@ -23,10 +23,7 @@ class DomainFilterDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :id,
     :name,
-    :description,
-    # :created_at,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
