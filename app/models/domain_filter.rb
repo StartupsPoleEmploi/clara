52d56904002c4500840efae10c1b0f82e835c35c
David Boureau <bdavidxyz@gmail.com>
2019-02-04 11:34:30 +0100

    destory ok

--- app/models/domain_filter.rb
+++ app/models/domain_filter.rb
@@ -3,8 +3,7 @@ class DomainFilter < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
-
-  has_many :axle_filters
+  has_many :axle_filters, dependent: :nullify
   friendly_id :name, use: :slugged
 
   def should_generate_new_friendly_id?
