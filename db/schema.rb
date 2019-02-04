3009fc7c2ebe1827f627393b3fb6b42f9b1a477b
David Boureau <bdavidxyz@gmail.com>
2019-02-04 10:39:23 +0100

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_103119) do
+ActiveRecord::Schema.define(version: 2018_12_10_103120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2018_12_10_103119) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "ordre_affichage", default: 0
+    t.integer "ordre_affichage"
     t.string "icon"
     t.string "slug"
     t.string "category"
