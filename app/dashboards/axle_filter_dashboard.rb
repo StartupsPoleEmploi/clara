d4688017b89b4e35a204cf0e56450a62d2858a55
David Boureau <bdavidxyz@gmail.com>
2019-02-05 10:44:07 +0100

    done

--- app/dashboards/axle_filter_dashboard.rb
+++ app/dashboards/axle_filter_dashboard.rb
@@ -30,7 +30,6 @@ class AxleFilterDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :name,
-    :description,
     :need_filters,
     :created_at,
     :updated_at,
