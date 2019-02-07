1a2efe31ac646ee88e59f1d2baa0258c111d8eb6
David Boureau <bdavidxyz@gmail.com>
2019-02-07 11:25:55 +0100

    get / post ok

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -5,13 +5,13 @@ module Admin
   class PagesController < Admin::ApplicationController
 
     # load zrr
-    def zrr
+    def get_zrr
     end
-    def load_zrr
+    def post_zrr
       deleted_zrrs_cache = Rails.cache.delete("zrrs")
       Zrr.create unless Zrr.first
       z = Zrr.first
-      z.value = _get_csv_data
+      z.value = _csv_data
       z.save
       render json: {
         deleted_zrrs_cache: deleted_zrrs_cache,
@@ -20,9 +20,9 @@ module Admin
     end
 
     # cache
-    def cache
+    def get_cache
     end
-    def expire_json_objects
+    def post_cache
       ExpireCache.call
       render json: {
         status: "ok"
@@ -30,26 +30,26 @@ module Admin
     end
 
     #stats
-    def stats
+    def get_stats
     end
-    def load_stats
-      csv = CSV.parse(_get_csv_data, {headers: true})
+    def post_stats
+      csv = CSV.parse(_csv_data, {headers: true})
       json_data = _csv_to_json(csv)
       _save_stat_of("ga", json_data)
     end
-    def load_stats_from_pe
-      csv = CSV.parse(_get_csv_data, {headers: true})
+    def post_stats_from_pe
+      csv = CSV.parse(_csv_data, {headers: true})
       csv.delete("Période")
       json_data = _csv_to_json(csv)
       _save_stat_of("ga_pe", json_data)      
     end
-    def load_advisors_stats
-      csv = CSV.parse(_get_csv_data, {headers: true})
+    def post_advisors_stats
+      csv = CSV.parse(_csv_data, {headers: true})
       csv.delete("Source URL")
       json_data = _csv_to_json(csv)
       _save_stat_of("hj_ad", json_data)   
     end
-    def _get_csv_data
+    def _csv_data
       params.extract!(:csv_data).permit(:csv_data).to_h["csv_data"]
     end
     def _save_stat_of(prop, value)
@@ -66,9 +66,9 @@ module Admin
     end
 
     # refdata
-    def loadrefdata
+    def get_ref_data
     end
-    def load_ref_data
+    def post_ref_data
       Rails.application.load_seed
     end
 
