b06b64e36a1adaf966a9023a08b901ca0f1d5791
David Boureau <bdavidxyz@gmail.com>
2019-02-08 14:55:58 +0100

    works, icky

--- app/view_objects/with_variable_field_view_object.rb
+++ app/view_objects/with_variable_field_view_object.rb
@@ -3,7 +3,25 @@ class WithVariableFieldViewObject < ViewObject
   def after_init(args)
     locals = hash_for(args)
     @field = locals[:field]
-
   end
 
+  def all_allowed_options
+    input = @field.associated_resource_options.compact
+    # element is
+    # ["v_location_label",
+    #  21,
+    #  {"data-kind"=>"string",
+    #   "data-elements"=>"",
+    #   "data-elements-translation"=>"",
+    #   "data-visible"=>false}
+    # ]
+    output = input
+      .select {|element| element[2]["data-visible"] == true} 
+      .map {|element| element[2].delete("data-visible");element}
+    p '- - - - - - - - - - - - - - output- - - - - - - - - - - - - - - -' 
+    pp output.size
+    pp output
+    p ''
+    output
+  end
 end
