617cccf1714faa9e815dce99d11c4636bbe7f560
David Boureau <bdavidxyz@gmail.com>
2019-02-08 14:59:31 +0100

    done

--- app/view_objects/with_variable_field_view_object.rb
+++ app/view_objects/with_variable_field_view_object.rb
@@ -15,13 +15,8 @@ class WithVariableFieldViewObject < ViewObject
     #   "data-elements-translation"=>"",
     #   "data-visible"=>false}
     # ]
-    output = input
+    input
       .select {|element| element[2]["data-visible"] == true} 
       .map {|element| element[2].delete("data-visible");element}
-    p '- - - - - - - - - - - - - - output- - - - - - - - - - - - - - - -' 
-    pp output.size
-    pp output
-    p ''
-    output
   end
 end
