5164af95a0ba041469641daae1b56816a9faf46c
David Boureau <bdavidxyz@gmail.com>
2019-02-13 18:11:36 +0100

    name ok

--- app/assets/javascripts/administrate/custom/admin_rules/admin_rules_var_val.js
+++ app/assets/javascripts/administrate/custom/admin_rules/admin_rules_var_val.js
@@ -21,22 +21,25 @@ clara.js_define("admin_rules_var_val", {
   },
 
   _input_is_text: function() {
-    $("#rule_value_eligible_selectible").css('display','none');
-    $("#rule_value_eligible").css('display','block');
+    clara.admin_rules_var_val._input_is_number()
     $("#rule_value_eligible").attr("type", "text");
     $("#rule_value_eligible").val("");
   },
 
   _input_is_number: function() {
     $("#rule_value_eligible_selectible").css('display','none');
+    $("#rule_value_eligible_selectible").removeAttr("name", "rule[value_eligible]");
     $("#rule_value_eligible").css('display','block');
+    $("#rule_value_eligible").attr("name", "rule[value_eligible]");
     $("#rule_value_eligible").attr("type", "number");
     $("#rule_value_eligible").val("");
   },
 
   _input_is_select: function(innerHTML) {
     $("#rule_value_eligible_selectible").css('display','block');
+    $("#rule_value_eligible_selectible").attr("name", "rule[value_eligible]");
     $("#rule_value_eligible").css('display','none');
+    $("#rule_value_eligible").removeAttr("name", "rule[value_eligible]");
   },
 
 
