c1a095f78940ddb333f960a770cf9671be840d82
David Boureau <bdavidxyz@gmail.com>
2019-02-13 17:24:49 +0100

    correctly change input

--- app/assets/javascripts/administrate/custom/admin_rules/admin_rules_var_changed.js
+++ app/assets/javascripts/administrate/custom/admin_rules/admin_rules_var_changed.js
@@ -11,30 +11,42 @@ clara.js_define("admin_rules_var_changed", {
     console.log(currentVar)
     var currentType = currentVar.variable_kind
     if (currentType === "integer") {
-      that._enable_input_text()
-      that._disable_input_select()
+      that._input_is_number()
       $("#rule_value_eligible").attr("type", "number");
     } else if (currentType === "selectionnable") {
-      console.log(that)
-      that._disable_input_text()
-      that._enable_input_select()
+      that._input_is_select()
+      var innerHtml = that._populate_with(currentVar.elements.split(","))
+      that._replace_input_select_content(innerHtml)
     }
   },
 
-  _enable_input_select: function() {
-    $("#rule_value_eligible_selectible").css('display','block');
+  _populate_with: function(values) {
+    var res = "<option value></option>"
+    _.each(values, function(value) {
+      res += '<option value="' + value + '">' + value + '</option>'
+    })
+    return res;
   },
 
-  _disable_input_select: function() {
+  _replace_input_select_content: function(with_html) {
+    $('#rule_value_eligible_selectible').html(with_html)
+  },
+
+  _input_is_text: function() {
     $("#rule_value_eligible_selectible").css('display','none');
-  },
-
-  _enable_input_text: function() {
     $("#rule_value_eligible").css('display','block');
   },
-  
-  _disable_input_text: function() {
+
+  _input_is_number: function() {
+    $("#rule_value_eligible_selectible").css('display','none');
+    $("#rule_value_eligible").css('display','block');
+    $("#rule_value_eligible").attr("type", "number");
+  },
+
+  _input_is_select: function() {
+    $("#rule_value_eligible_selectible").css('display','block');
     $("#rule_value_eligible").css('display','none');
   },
+
 
  });
