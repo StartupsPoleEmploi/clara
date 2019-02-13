5337aab0132fdf264e7772950559053e43be8764
David Boureau <bdavidxyz@gmail.com>
2019-02-13 11:46:42 +0100

    better seeds

--- app/view_objects/with_variable_field_view_object.rb
+++ app/view_objects/with_variable_field_view_object.rb
@@ -7,6 +7,9 @@ class WithVariableFieldViewObject < ViewObject
 
   def all_allowed_options
     input = @field.associated_resource_options.compact
+    p '- - - - - - - - - - - - - - input- - - - - - - - - - - - - - - -' 
+    pp input
+    p ''
     # element is
     # ["v_location_label",
     #  21,
@@ -15,8 +18,12 @@ class WithVariableFieldViewObject < ViewObject
     #   "data-elements-translation"=>"",
     #   "data-visible"=>false}
     # ]
-    input
+    res = input
       .select {|element| element[2]["data-visible"] == true} 
       .map {|element| element[2].delete("data-visible");element}
+    # p '- - - - - - - - - - - - - - res- - - - - - - - - - - - - - - -' 
+    # pp res
+    # p ''
+    res
   end
 end
