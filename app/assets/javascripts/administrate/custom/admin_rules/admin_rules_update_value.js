b7f657eef80db894333360d75dde49779290f6bc
David Boureau <bdavidxyz@gmail.com>
2019-02-14 11:21:21 +0100

    clean!

--- app/assets/javascripts/administrate/custom/admin_rules/admin_rules_update_value.js
+++ app/assets/javascripts/administrate/custom/admin_rules/admin_rules_update_value.js
@@ -4,21 +4,57 @@ clara.js_define("admin_rules_update_value", {
   please_if: _.stubFalse,
 
   please: function() {
+    var that = clara.admin_rules_update_value;
     var s = main_store.getState();
     var currentVar = _.find(s.variables, function(e){return e.name == s["selected_variable"]})
     if (currentVar) {
       var currentType = currentVar.variable_kind
       console.log(currentType)
       if (currentType === "integer") {
-        clara.admin_rules_var_val._input_is_number(s.selected_value)
+        that._input_is_number(s.selected_value)
       } else if (currentType === "string") {
-        clara.admin_rules_var_val._input_is_text(s.selected_value)
+        that._input_is_text(s.selected_value)
       } else if (currentType === "selectionnable") {
-        clara.admin_rules_var_val._input_is_select()
-        var innerHtml = clara.admin_rules_var_val._populate_with(currentVar.elements.split(","))
-        clara.admin_rules_var_val._replace_input_select_content(innerHtml, s.selected_value)
+        that._input_is_select()
+        var innerHtml = that._populate_with(currentVar.elements.split(","))
+        that._replace_input_select_content(innerHtml, s.selected_value)
       }      
     }
+  },
+
+  _populate_with: function(values) {
+    var res = "<option value></option>"
+    _.each(values, function(value) {
+      res += '<option value="' + value + '">' + value + '</option>'
+    })
+    return res;
+  },
+
+  _replace_input_select_content: function(with_html, initial_value) {
+    $('#rule_value_eligible_selectible').html(with_html)
+    $("#rule_value_eligible_selectible").val(initial_value)
+  },
+
+  _input_is_text: function(initial_value) {
+    that._input_is_number()
+    $("#rule_value_eligible").attr("type", "text");
+    $("#rule_value_eligible").val(initial_value);
+  },
+
+  _input_is_number: function(initial_value) {
+    $("#rule_value_eligible_selectible").css('display','none');
+    $("#rule_value_eligible_selectible").removeAttr("name", "rule[value_eligible]");
+    $("#rule_value_eligible").css('display','block');
+    $("#rule_value_eligible").attr("name", "rule[value_eligible]");
+    $("#rule_value_eligible").attr("type", "number");
+    $("#rule_value_eligible").val(initial_value);
+  },
+
+  _input_is_select: function() {
+    $("#rule_value_eligible_selectible").css('display','block');
+    $("#rule_value_eligible_selectible").attr("name", "rule[value_eligible]");
+    $("#rule_value_eligible").css('display','none');
+    $("#rule_value_eligible").removeAttr("name", "rule[value_eligible]");
   },
 
 
