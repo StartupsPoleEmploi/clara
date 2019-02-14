c56d9376c2f63b8b8854faa0ac636c190f5b4f08
David Boureau <bdavidxyz@gmail.com>
2019-02-14 15:36:01 +0100

    done mig

--- db/migrate/20181210103126_remove_variable_type.rb
+++ db/migrate/20181210103126_remove_variable_type.rb
@@ -1,4 +1,4 @@
-class RemoveOperatorType < ActiveRecord::Migration[5.2]
+class RemoveVariableType < ActiveRecord::Migration[5.2]
   def change
     remove_column :variables, :variable_type, :integer
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_103125) do
+ActiveRecord::Schema.define(version: 2018_12_10_103126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -206,7 +206,6 @@ ActiveRecord::Schema.define(version: 2018_12_10_103125) do
 
   create_table "variables", id: :serial, force: :cascade do |t|
     t.string "name"
-    t.integer "variable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
