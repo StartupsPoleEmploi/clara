2f6aba7311819fc58d59822039e0f658db72ccc2
David Boureau <bdavidxyz@gmail.com>
2019-02-14 15:28:56 +0100

    removed comments

--- app/controllers/admin/rules_controller.rb
+++ app/controllers/admin/rules_controller.rb
@@ -40,9 +40,6 @@ module Admin
     end
 
     def edit
-      # p '- - - - - - - - - - - - - - requested_resource- - - - - - - - - - - - - - - -' 
-      # pp requested_resource
-      # p ''
       render locals: {
         page: Administrate::Page::Form.new(dashboard, requested_resource),
       }
