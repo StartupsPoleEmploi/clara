4c8821694a410b80041095cfb17f7c7fcc936b5d
David Boureau <bdavidxyz@gmail.com>
2019-02-14 10:19:45 +0100

    first found algo ok

--- app/assets/javascripts/administrate/custom/admin_rules/admin_rules_expl_changed.js
+++ app/assets/javascripts/administrate/custom/admin_rules/admin_rules_expl_changed.js
@@ -8,11 +8,34 @@ clara.js_define("admin_rules_expl_changed", {
     var s = main_store.getState()
     var all_selected = 
       _.every(
-        [s.selected_operator, s.selected_variable, s.selected_value],
+        [s.selected_operator, 
+        s.selected_variable, 
+        s.selected_value],
         function(e){return !_.isEmpty(_.trim(e))}
       );
     if (all_selected) {
       console.log("all_selected")
+      var found_value =
+        _.find(s.explicitations, function(expl) {
+          var res = false;
+          console.log(expl)
+          console.log(s.selected_variable)
+          console.log(s.selected_operator)
+          console.log(s.selected_value)
+          if (expl.variable_name === s.selected_variable) {
+            console.log("1st")
+            if (expl.operator_kind === s.selected_operator) {
+              if (expl.value_eligible === null) {
+                res = true;
+              } else if (expl.value_eligible === s.selected_value) {
+                res = true;
+              }
+            }  
+          }  
+          return res;
+        });
+      console.log("found_value is ")
+      console.log(found_value)
     }
   },
 
