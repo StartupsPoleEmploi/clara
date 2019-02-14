45bb1839888f64e2cd50ed19f43cff3a48a19e44
David Boureau <bdavidxyz@gmail.com>
2019-02-14 15:38:50 +0100

    depreciation handled

--- app/models/variable.rb
+++ app/models/variable.rb
@@ -2,9 +2,6 @@ class Variable < ApplicationRecord
   include Prefixable
   has_paper_trail ignore: [:updated_at]
 
-  # Deprecated, please remove ASAP
-  enum variable_type: [:integer, :string, :selectionnable], _prefix: true
-
   enum variable_kind: {
     integer: "integer", 
     string: "string", 
