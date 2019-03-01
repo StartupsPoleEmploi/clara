9d765c9fca8a23f5e3ca62540ae2ec3dae1e2a4f
David Boureau <bdavidxyz@gmail.com>
2019-03-01 12:05:39 +0100

    ability to modify state properly ! congrats !

--- app/assets/javascripts/administrate/custom/admin_rules/admin_rules_concerned_operators.js
+++ app/assets/javascripts/administrate/custom/admin_rules/admin_rules_concerned_operators.js
@@ -5,8 +5,16 @@ clara.js_define("admin_rules_concerned_operator", {
 
   please: function(state, given_variable) {
     var result = "";
-    console.log("admin_rules_concerned_operator")
-    console.log(state, given_variable);
+    // console.log("admin_rules_concerned_operator")
+    // console.log(state, given_variable);
+    var auth_expls = _.filter(state.explicitations, function(expl) {
+      return given_variable == expl.variable_name
+    });
+    var auth_ops = _.uniqBy(_.map(auth_expls, function(e){return e.operator_kind}));
+    console.log(auth_ops)
+    if (auth_ops.length === 1) {
+      result= auth_ops[0]
+    }
     return result;
   },
 
