9690cf16aa1fe9afa220a6d0bb82a0a2aa4e46e7
David Boureau <bdavidxyz@gmail.com>
2019-03-01 15:09:15 +0100

    red

--- spec/services/ruletree_service_calculate_selectionnable_spec.rb
+++ spec/services/ruletree_service_calculate_selectionnable_spec.rb
@@ -33,7 +33,7 @@ describe RuletreeService do
     end
     it 'Selectionnable, not_equal, same string => FALSE' do
       #given
-      operator_kind = "equal"
+      operator_kind = "not_equal"
       rule_h = build(:rule, operator_kind: operator_kind).attributes
       criterion_value = "niveau_4"
       rule_value = "niveau_4"
@@ -43,6 +43,19 @@ describe RuletreeService do
       res = sut.send :calculate, rule_h, criterion_value, rule_value, rule_type
       #then
       expect(res).to eq(false)
+    end
+    it 'Selectionnable, not_equal, diff string => TRUE' do
+      #given
+      operator_kind = "not_equal"
+      rule_h = build(:rule, operator_kind: operator_kind).attributes
+      criterion_value = "niveau_4"
+      rule_value = "niveau_5"
+      rule_type = "selectionnable"
+      sut = RuletreeService.new
+      #when
+      res = sut.send :calculate, rule_h, criterion_value, rule_value, rule_type
+      #then
+      expect(res).to eq(true)
     end
   end
 end
