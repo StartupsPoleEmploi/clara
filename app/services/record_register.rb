a4eb1e1fc7e9eff79f308edc79b8443283f8f7e5
David Boureau <bdavidxyz@gmail.com>
2019-03-05 16:39:03 +0100

    saving r

--- app/services/record_register.rb
+++ app/services/record_register.rb
@@ -3,12 +3,13 @@ class RecordRegister
   def call(session, asker, url)
     city_key = asker.v_location_citycode
     if city_key.is_a?(String) && city_key.start_with?("34")
-      Register.new(
+      r = Register.new(
         user: session.id,
         geo:  asker.v_location_citycode,
         url: url,
         moment: DateTime.now.strftime("%d-%m-%Y"),
       )
+      r.save
     end
   end
   
