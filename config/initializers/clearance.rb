0870bcfbb903fb3d97c8fdd8fdf07845138aa868
David Boureau <bdavidxyz@gmail.com>
2019-03-07 11:14:04 +0100

    rolled_back mig

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,16 @@
 Clearance.configure do |config|
+  config.allow_sign_up = false
+  config.cookie_domain = ".pole-emploi.fr"
+  config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
+  config.cookie_name = "remember_token"
+  config.cookie_path = "/"
+  config.routes = false
+  config.httponly = false
   config.mailer_sender = "reply@example.com"
-  config.rotate_csrf_on_sign_in = true
+  config.password_strategy = Clearance::PasswordStrategies::BCrypt
+  config.redirect_url = "/admin"
+  config.rotate_csrf_on_sign_in = false
+  config.secure_cookie = false
+  config.sign_in_guards = []
+  config.user_model = User
 end
