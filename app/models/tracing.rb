219463644d7c3a8c3f02c87057dbb933eac77c44
David Boureau <bdavidxyz@gmail.com>
2019-03-08 16:31:30 +0100

    name validation

--- app/models/tracing.rb
+++ app/models/tracing.rb
@@ -1,7 +1,7 @@
 class Tracing < ApplicationRecord
 
-  validates :name, uniqueness: true
-  
+  validates :name, presence: true, uniqueness: true
+
   has_many :tracizations
   has_many :aids, through: :tracizations
   
