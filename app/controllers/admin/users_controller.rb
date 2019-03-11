b8d573bb76c1ae43934313df68c2d0897d198572
David Boureau <bdavidxyz@gmail.com>
2019-03-11 14:01:14 +0100

    refuse some action on user admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,5 +17,8 @@ module Admin
 
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
+    def valid_action?(name, resource = resource_class)
+      %w[new edit].exclude?(name.to_s) && super
+    end
   end
 end
