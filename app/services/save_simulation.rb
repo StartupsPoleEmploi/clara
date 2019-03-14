e9c2d1c4624b9dab8be91b788989c150ba88bd66
David Boureau <bdavidxyz@gmail.com>
2019-03-14 15:52:48 +0100

    cleaned

--- app/services/save_simulation.rb
+++ app/services/save_simulation.rb
@@ -8,18 +8,9 @@ class SaveSimulation
     custom_rule_check = CustomRuleCheck.new
     custom_rule_check.rule = current_rule
     custom_rule_check.name = simulation_params[:name]
-    p '- - - - - - - - - - - - - - name- - - - - - - - - - - - - - - -' 
-    pp custom_rule_check.name
-    p ''
     custom_rule_check.result = simulation_params[:result]
     custom_rule_check.hsh = asker_hash
-    p '- - - - - - - - - - - - - - asker_hash- - - - - - - - - - - - - - - -' 
-    pp asker_hash
-    p ''
     creation_of_crc_worked = custom_rule_check.save
-    p '- - - - - - - - - - - - - - creation_of_crc_worked- - - - - - - - - - - - - - - -' 
-    pp creation_of_crc_worked
-    p ''
     if creation_of_crc_worked
       actual_status = CalculateRuleSimulated.new.call(current_rule)
       Rule.where(id: current_rule.id).update_all(simulated: actual_status)
