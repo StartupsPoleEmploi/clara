20983910f2a1da18ad66612353598d6cb60d9ecd
David Boureau <bdavidxyz@gmail.com>
2019-03-14 15:17:16 +0100

    save attempt

--- app/assets/javascripts/administrate/custom/admin_rule/admin_rule_save.js
+++ app/assets/javascripts/administrate/custom/admin_rule/admin_rule_save.js
@@ -4,18 +4,28 @@ clara.js_define("admin_rule_save", {
   please_if: _.stubFalse,
 
   please: function(evt, store) {
-    var that = this;
+    var state = store.getState();
+    var simul = state.current_simulation;
+
+    var url = $(evt.currentTarget).closest(".c-simulator-registration").attr("data-url");
+
     $.ajax({
-      url: $(evt.currentTarget).attr("data-url"),
-      type:'DELETE',
+      url: url,
+      type:'POST',
       dataType:'json',
       data:{
+        simulation: {
+          result: simul.result,
+          name: simul.name
+        },
+        asker: simul.params,
         authenticity_token: window._token
       },
       success:function(data){
         window.location.reload();
       }
     });
+
   },
 
 
