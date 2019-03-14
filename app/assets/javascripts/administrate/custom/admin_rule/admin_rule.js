a669972d7b54ed554ed1e69d948fc7b2231c8d52
David Boureau <bdavidxyz@gmail.com>
2019-03-14 15:11:28 +0100

    created save

--- app/assets/javascripts/administrate/custom/admin_rule/admin_rule.js
+++ app/assets/javascripts/administrate/custom/admin_rule/admin_rule.js
@@ -46,10 +46,12 @@ clara.js_define("admin_rule", {
     var $replay = $(".simulator-table-row .simulation-table-replay");
     var $simulate = $("#btn_simulate");
     var $remove = $(".simulation-table-delete");
+    var $save = $("#btn-save");
 
     $simulate.on('click', function(){clara.admin_rule_ajax_resolve.please(main_store)});
     $replay.on('click', function(e){clara.admin_rule_replay.please(e, main_store)})
     $remove.on('click', function(e){clara.admin_rule_remove.please(e, main_store)})
+    $save.on('click', function(e){clara.admin_rule_save.please(e, main_store)})
   }
 
 });
