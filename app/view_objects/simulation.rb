102740740a04f8aaa85627fa0386b36c33c52639
David Boureau <bdavidxyz@gmail.com>
2019-03-14 11:24:29 +0100

    first working draft

--- app/view_objects/simulation.rb
+++ app/view_objects/simulation.rb
@@ -6,13 +6,27 @@ class Simulation < ViewObject
   end
 
   def displayed_variables
-    # ap @page.resource[:id]
-    ap FindChildRules.new.call(@page.resource[:id])
+    # ap FindChildRules.new.call(@page.resource[:id])
+    vars = ListVariablesOfRule.new.call(@page.resource[:id])
+    res = vars.map do |v|  
+      OpenStruct.new(
+        html_id: "#{v['name']}", 
+        displayed_label: "#{v['name_translation']}",
+        form_name: "asker[#{v['name']}]",
+      )
+      # OpenStruct.new(
+      #   html_id: "v_handicap", 
+      #   displayed_label: "le handicap",
+      #   form_name: "asker[v_handicap]",
+      # )
+    end
+    ap res
     variable_1 = OpenStruct.new(
       html_id: "v_handicap", 
       displayed_label: "le handicap",
       form_name: "asker[v_handicap]",
     )
     [variable_1]
+    res
   end
 end
