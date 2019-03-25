5618b7a0723ef401922e2b8db5b13c9e5af89d37
Maxime Devallez <maxdeval02@gmail.com>
2019-03-25 16:01:50 +0100

    red test

--- spec/services/calculate_asker_service_spec.rb
+++ spec/services/calculate_asker_service_spec.rb
@@ -4,10 +4,12 @@ describe CalculateAskerService do
   describe '.calculate_zrr' do
     it 'Should calculate zrr' do
       # given
+      asker = Asker.new
+      asker.v_location_citycode = "20245"
       # when
-      CalculateAskerService.new.calculate_zrr!
+      CalculateAskerService.new(asker).calculate_zrr!
       # then
-      expect(asker.v_zrr).to eq()
+      expect(asker.v_zrr).to eq("oui")
 
     end
   end
