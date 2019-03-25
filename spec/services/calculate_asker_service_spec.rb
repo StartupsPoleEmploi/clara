68f707d215c4b02275b6e33172ebde18173e1bd3
Maxime Devallez <maxdeval02@gmail.com>
2019-03-25 16:26:18 +0100

    seems it doesn't take params because result is nil

--- spec/services/calculate_asker_service_spec.rb
+++ spec/services/calculate_asker_service_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 describe CalculateAskerService do
+
+  before do
+    _fake_is_zrr
+#    _fake_get_zip_city_region
+  end
+
   describe '.calculate_zrr' do
     it 'Should calculate zrr' do
       # given
       asker = Asker.new
       asker.v_location_citycode = "20245"
+      IsZrr.new.call("20245")
       # when
       CalculateAskerService.new(asker).calculate_zrr!
       # then
@@ -13,4 +20,14 @@ describe CalculateAskerService do
 
     end
   end
+
+  def _fake_is_zrr
+    allow_any_instance_of(IsZrr).to receive(:call)
+  end
+
+=begin  def _fake_get_zip_city_region
+    allow_any_instance_of(GetZipCityRegion).to receive(:call).and_return(['59440', 'Avesnelles', 'Hauts-de-France (Nord-Pas-de-Calais)'])
+  end
+=end
+
 end
