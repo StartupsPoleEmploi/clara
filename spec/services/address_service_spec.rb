9459cf6cf28996685387bfc80a6f0948e257244d
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 16:14:51 +0100

    created test for reset

--- spec/services/address_service_spec.rb
+++ spec/services/address_service_spec.rb
@@ -86,7 +86,7 @@ describe AddressService do
   end
 
   describe '.download' do
-    it 'Returns address_type from given asker' do
+    it 'Returns label from given asker' do
       #given
       asker = Asker.new
       address = AddressForm.new
@@ -96,6 +96,83 @@ describe AddressService do
       #then
       expect(address.label).to eq("label")
     end
+    it 'Returns route from given asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      asker.v_location_route = "route"
+      #when
+      AddressService.new.download(address, asker)
+      #then
+      expect(address.route).to eq("route")
+    end
+    it 'Returns address_type from given asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      asker.v_location_city = "city"
+      #when
+      AddressService.new.download(address, asker)
+      #then
+      expect(address.city).to eq("city")
+    end
+    it 'Returns country from given asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      asker.v_location_country = "country"
+      #when
+      AddressService.new.download(address, asker)
+      #then
+      expect(address.country).to eq("country")
+    end
+    it 'Returns zipcode from given asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      asker.v_location_zipcode = "zipcode"
+      #when
+      AddressService.new.download(address, asker)
+      #then
+      expect(address.zipcode).to eq("zipcode")
+    end
+    it 'Returns citycode from given asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      asker.v_location_citycode = "citycode"
+      #when
+      AddressService.new.download(address, asker)
+      #then
+      expect(address.citycode).to eq("citycode")
+    end
+    it 'Returns street_number from given asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      asker.v_location_street_number = "street_number"
+      #when
+      AddressService.new.download(address, asker)
+      #then
+      expect(address.street_number).to eq("street_number")
+    end
+    it 'Returns state from given asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      asker.v_location_state = "state"
+      #when
+      AddressService.new.download(address, asker)
+      #then
+      expect(address.state).to eq("state")
+    end
+
+  describe '.reset' do
+    it 'Returns nil for label asker' do
+      
+    end
+  end
+
   end
 
 end
