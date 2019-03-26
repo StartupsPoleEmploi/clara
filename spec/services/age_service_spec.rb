fefc3acb22d189fbd902d7f81d09995f89c2b711
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 14:52:16 +0100

    created asker

--- spec/services/age_service_spec.rb
+++ spec/services/age_service_spec.rb
@@ -5,6 +5,7 @@ describe AgeService do
   describe '.new_and_download' do
     it 'Returns a new age form with age_type filled from given asker' do
       #given
+      asker = Asker.new
       #when
       #then
       expect(asker.v_age).to eq('age')
@@ -14,6 +15,5 @@ describe AgeService do
 #  describe '.upload' do
 #    it 'should inject allocation_type into asker' do
 #    end
-
-  end
+#  end
 end
