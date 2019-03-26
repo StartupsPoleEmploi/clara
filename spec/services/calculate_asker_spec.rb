73ea2be6310b31deb1062cca50c56e1129698a03
David Boureau <bdavidxyz@gmail.com>
2019-03-26 10:23:43 +0100

    zrr nil by default

--- spec/services/calculate_asker_spec.rb
+++ spec/services/calculate_asker_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 describe CalculateAskerService do
 
-  it '.calculate_zrr!' do
-    res = CalculateAskerService.new(Asker.new).calculate_zrr!
-    expect(res).to eq("blabla")
+  describe '.calculate_zrr!' do
+
+    it "should update v_zrr of an asker" do |variable|
+      #given
+      asker = Asker.new
+      expect(asker.v_zrr).to eq(nil)
+      #when
+      #then
+      # res = CalculateAskerService.new(Asker.new).calculate_zrr!
+      # expect(res).to eq("blabla")      
+    end
 
   end
 
