8698f20f1a6a1b753e982b57b1df0e5f7b2b1ba7
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 14:45:58 +0100

    red test

--- spec/services/age_service_spec.rb
+++ spec/services/age_service_spec.rb
@@ -4,7 +4,10 @@ describe AgeService do
 
   describe '.new_and_download' do
     it 'Returns a new age form with age_type filled from given asker' do
-
+      #given
+      #when
+      #then
+      expect(asker.v_age).to eq('age')
     end
   end
 
