084415765cd7d88f51d34b272cece13964968d1b
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 14:33:25 +0100

    IsZrr green tests

--- spec/services/is_zrr_spec.rb
+++ spec/services/is_zrr_spec.rb
@@ -6,39 +6,53 @@ describe IsZrr do
 
     it "should return nil if there are not 5 digits" do
       #given
+      is_zrr = IsZrr.new
       #when
-      res = IsZrr.new.call("")
       #then
-      expect(res).to eq(nil)
+      expect(is_zrr.call(nil)).to eq(nil)
+      expect(is_zrr.call(1234)).to eq(nil)
+      expect(is_zrr.call(12345.0)).to eq(nil)
+      expect(is_zrr.call([1,2,3,4])).to eq(nil)
+      expect(is_zrr.call({})).to eq(nil)
+      expect(is_zrr.call("")).to eq(nil)
+      expect(is_zrr.call("5")).to eq(nil)
+      expect(is_zrr.call("1234")).to eq(nil)
+      expect(is_zrr.call("abcde")).to eq(nil)
     end
 
-    it  "should return 'oui' if this is a zrr" do
+    it  "should return 'oui' if this is a zrr in the cache" do
       #given
-      citycode = "59606"
-      allow_any_instance_of(IsZrr).to receive(:call).with("59606").and_return("oui")
-      allow(Rails.cache).to receive(:fetch).with("zrrs").and_return(citycode)
-      Rails.cache.write('zrrs', 'test')
+      allow(Rails.cache).to receive(:fetch).with("zrrs").and_return("73253,84069,59606,87079,87159,88033,88093,88419\r\n")
       #when
-      res = IsZrr.new.call(citycode)
-      all_zrr = Rails.cache.fetch("zrrs")
       #then
-      expect(all_zrr.include?(citycode)).to eq(true)
-      expect(res).to eq("oui")
+      expect(IsZrr.new.call(59606)).to eq("oui")
+      expect(IsZrr.new.call("59606")).to eq("oui")
     end
 
-    it  "should return 'non' if this is not a zrr" do
+    it  "should return 'oui' if this is no zrr in the cache, but in database" do
       #given
-      citycode = "59606"
-      allow_any_instance_of(IsZrr).to receive(:call).with("59606").and_return("non")
-      allow(Rails.cache).to receive(:fetch).with("zrrs").and_return(" ")
-      Rails.cache.write('zrrs', 'test')
       #when
-      res = IsZrr.new.call(citycode)
-      all_zrr = Rails.cache.fetch("zrrs")
+      allow(Zrr).to receive(:first).and_return(OpenStruct.new(:value => "73253,84069,59606,87079,87159,88033,88093,88419\r\n"))
       #then
-      expect(all_zrr.include?(citycode)).to eq(false)
-      expect(res).to eq("non")
+      expect(IsZrr.new.call("59606")).to eq("oui")
     end
+
+    it  "should return 'non' if this is no zrr in the cache, but in database, but value not here" do
+      #given
+      #when
+      allow(Zrr).to receive(:first).and_return(OpenStruct.new(:value => "73253,84069,87079,87159,88033,88093,88419\r\n"))
+      #then
+      expect(IsZrr.new.call("59606")).to eq("non")
+    end
+
+    it  "should return 'non' if this is no zrr in the cache, and db is empty" do
+      #given
+      #when
+      allow(Zrr).to receive(:first).and_return(nil)
+      #then
+      expect(IsZrr.new.call("59606")).to eq("non")
+    end
+
 
   end
 
