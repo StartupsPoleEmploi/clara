a49c7c19fd2458bc252b4a53bae84ffe42287274
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 15:58:25 +0100

    upload done

--- spec/services/address_service_spec.rb
+++ spec/services/address_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe AddressService do
 
   describe '.upload' do
-    it 'should inject address_type into asker' do
+    it 'should inject location_label into asker' do
       #given
       asker = Asker.new
       address = AddressForm.new
@@ -12,6 +12,76 @@ describe AddressService do
       AddressService.new.upload(address, asker)
       #then
       expect(asker.v_location_label).to eq("location_label")
+    end
+    it 'should inject location_route into asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      address.route = 'location_route' 
+      #when
+      AddressService.new.upload(address, asker)
+      #then
+      expect(asker.v_location_route).to eq("location_route")
+    end
+    it 'should inject location_city into asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      address.city = 'location_city' 
+      #when
+      AddressService.new.upload(address, asker)
+      #then
+      expect(asker.v_location_city).to eq("location_city")
+    end
+    it 'should inject location_country into asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      address.country = 'location_country' 
+      #when
+      AddressService.new.upload(address, asker)
+      #then
+      expect(asker.v_location_country).to eq("location_country")
+    end
+    it 'should inject location_zipcode into asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      address.zipcode = 'location_zipcode' 
+      #when
+      AddressService.new.upload(address, asker)
+      #then
+      expect(asker.v_location_zipcode).to eq("location_zipcode")
+    end
+    it 'should inject location_citycode into asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      address.citycode = 'location_citycode' 
+      #when
+      AddressService.new.upload(address, asker)
+      #then
+      expect(asker.v_location_citycode).to eq("location_citycode")
+    end
+    it 'should inject location_street_number into asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      address.street_number = 'location_street_number' 
+      #when
+      AddressService.new.upload(address, asker)
+      #then
+      expect(asker.v_location_street_number).to eq("location_street_number")
+    end
+    it 'should inject location_state into asker' do
+      #given
+      asker = Asker.new
+      address = AddressForm.new
+      address.state = 'location_state' 
+      #when
+      AddressService.new.upload(address, asker)
+      #then
+      expect(asker.v_location_state).to eq("location_state")
     end
   end
 
