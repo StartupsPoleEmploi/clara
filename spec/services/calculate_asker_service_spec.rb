dc35d68273202f6c8e979c13bfb961dff53e89ae
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 10:41:06 +0100

    green test

--- spec/services/calculate_asker_service_spec.rb
+++ spec/services/calculate_asker_service_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 describe CalculateAskerService do
 
-  describe '.calculate_zrr' do
-    it 'Should calculate zrr' do
-      # given
-      calculate_zrr_service = instance_double('CalculateAskerService')
+  describe '.calculate_zrr!' do
+
+    it "should update v_zrr of an asker" do
+      #given
       asker = Asker.new
-      asker.v_location_citycode = "20245"
-      asker.v_zrr = IsZrr.new.call("20245")
-      # when
-      asker = CalculateAskerService.new(asker).calculate_zrr!
-      # then
-      expect(asker.v_zrr).to eq("oui")
-
+      asker.v_location_citycode = "59606"
+      expect(asker.v_zrr).to eq(nil)
+      allow_any_instance_of(IsZrr).to receive(:call).with("59606").and_return("est_en_zrr")
+      #when
+      CalculateAskerService.new(asker).calculate_zrr!
+      #then
+      expect(asker.v_zrr).not_to eq(nil)
+      expect(asker.v_zrr).to eq("est_en_zrr")
     end
-  end
 
+  end
 
 end
