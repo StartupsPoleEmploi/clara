93e91deff93277b0f4e7bb757726de239c53b226
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 15:40:35 +0100

    created test

---
+++ spec/services/address_service_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe AgeService do
+
+  describe '.new_and_download' do
+    it 'Returns address_type from given asker' do
+
+    end
+  end
+
+#  describe '.upload' do
+#    it 'should inject age_type into asker' do
+#    end
+#  end
+end
