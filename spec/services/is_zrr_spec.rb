bd977f32534b03a957672adaed1efb5bb0a24b6b
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 11:02:51 +0100

    green test

--- spec/services/is_zrr_spec.rb
+++ spec/services/is_zrr_spec.rb
@@ -7,6 +7,7 @@ describe IsZrr do
     it "should return nul if there are not 5 digits" do
       #given
       #when
+      res = IsZrr.new.call("")
       #then
       expect(res).to eq(nil)
     end
