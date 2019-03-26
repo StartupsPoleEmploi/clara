54cf1c70a8492bcaa98ae8c8a4a86e691150039d
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 12:21:53 +0100

    adding Rails keyword

--- spec/services/is_zrr_spec.rb
+++ spec/services/is_zrr_spec.rb
@@ -17,15 +17,16 @@ describe IsZrr do
       citycode = "59606"
       allow_any_instance_of(IsZrr).to receive(:call).with("59606").and_return("oui")
       allow(Rails.cache).to receive(:fetch).with("zrrs").and_return(true)
-      expect(cache.exist?('zrrs')).to be(false)
-      cache.write('zrrs')
+      expect(Rails.cache.exist?('zrrs')).to be(false)
+      Rails.cache.write('zrrs')
       #when
       res = IsZrr.new.call(citycode)
       #then
-      expect(cache.exist?('zrrs')).to be(true)
+      expect(Rails.cache.exist?('zrrs')).to be(true)
       #expect(res.include?(citycode)).to eq(true)
       #expect(res).to eq("oui")
     end
+
   end
 
 end
