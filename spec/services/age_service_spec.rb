81858d56b41ba8bb11d2bc45c5adcea3f4ec53cd
Maxime Devallez <maxdeval02@gmail.com>
2019-03-26 14:43:13 +0100

    created test

---
+++ spec/services/age_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe AgeService do
+
+  describe '.new_and_download' do
+    it 'Returns a new age form with age_type filled from given asker' do
+
+    end
+  end
+
+#  describe '.upload' do
+#    it 'should inject allocation_type into asker' do
+#    end
+
+  end
+end
