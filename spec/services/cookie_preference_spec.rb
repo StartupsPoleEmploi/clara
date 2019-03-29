5c7e24b811f8c104056fc4eb6d5f188f7e5245c9
Maxime Devallez <maxdeval02@gmail.com>
2019-03-29 10:44:36 +0100

    init cookiepreference test

---
+++ spec/services/cookie_preference_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe CookiePreference do
+
+  describe '.accept_all_cookies' do
+    it 'Returns authorization for statistic and navigation' do
+      #given
+      #when
+      #then
+    end
+  end
+end
