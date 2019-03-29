662aa2422fe8a2101f44fa07e6c540830a0da5d2
David Boureau <bdavidxyz@gmail.com>
2019-03-29 10:39:37 +0100

    done?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_170360) do
+ActiveRecord::Schema.define(version: 2019_03_29_102403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -82,7 +82,6 @@ ActiveRecord::Schema.define(version: 2019_03_08_170360) do
 
   create_table "contract_types", force: :cascade do |t|
     t.string "name"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "ordre_affichage"
