e48509242bb9aa507afabf2962c8612b9401690d
Maxime Devallez <maxdeval02@gmail.com>
2019-03-29 10:57:19 +0100

    still red

--- spec/services/cookie_preference_spec.rb
+++ spec/services/cookie_preference_spec.rb
@@ -5,9 +5,11 @@ describe CookiePreference do
   describe '.accept_all_cookies' do
     it 'Returns authorization for statistic and navigation' do
       #given
+      res = {}
       #when
+      res = accept_all_cookies
       #then
-      expect(:cookie).to eq({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
+      expect(res).to eq({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
     end
   end
 end
