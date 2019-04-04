985d3ea22cb1d63718da7a0861e80f9de9ffebe3
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-04 12:05:47 +0200

    restarting the test from beginning

--- spec/services/cookie_preference_spec.rb
+++ spec/services/cookie_preference_spec.rb
@@ -5,13 +5,10 @@ describe CookiePreference do
   describe '.accept_all_cookies' do
     it 'Returns authorization for statistic and navigation' do
       #given
-      session = {}
-      #allow_any_instance_of(CookiePreference).to receive(:accept_all_cookies).and_return({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
-      #when
-      session = CookiePreference.new.accept_all_cookies
-      #res = session[:cookie].accept_all_cookies
+      #when 
+      cookie_preference = CookiePreference.new(session).accept_all_cookies
       #then
-      expect(session).to eq({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
+      expect(cookie_preference).to eq({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
     end
   end
 end
