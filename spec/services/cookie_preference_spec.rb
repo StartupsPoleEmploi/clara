6dc6f2330bd47378132c2353d2f3853ced4497bd
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-04 12:17:43 +0200

    renamming

--- spec/services/cookie_preference_spec.rb
+++ spec/services/cookie_preference_spec.rb
@@ -5,7 +5,7 @@ describe CookiePreference do
   describe '.accept_all_cookies' do
     it 'Returns authorization for statistic and navigation' do
       #given
-      session = {"empty" => "hash"}
+      session = {"my" => "hash"}
       #when 
       cookie_preference = CookiePreference.new(session).accept_all_cookies
       #then
