22ad30fa8d64d1c236a665306c8c8bb943a15c1d
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-04 09:32:54 +0200

    can't find session

--- spec/services/cookie_preference_spec.rb
+++ spec/services/cookie_preference_spec.rb
@@ -5,11 +5,9 @@ describe CookiePreference do
   describe '.accept_all_cookies' do
     it 'Returns authorization for statistic and navigation' do
       #given
-      res = {}
       #when
-      res = accept_all_cookies
       #then
-      expect(res).to eq({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
+      expect(CookiePreference.new(session).accept_all_cookies).to eq({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
     end
   end
 end
