e1a6741262cf78de00979b13792144d7a75a0fd9
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-04 10:16:54 +0200

    test 2

--- spec/services/cookie_preference_spec.rb
+++ spec/services/cookie_preference_spec.rb
@@ -7,6 +7,7 @@ describe CookiePreference do
       #given
       request.cookies['foo'] = 'bar'
       get :accept_all_cookies
+      
       #when
       #then
       expect(response.cookies['foo']]).accept_all_cookies).to eq({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
