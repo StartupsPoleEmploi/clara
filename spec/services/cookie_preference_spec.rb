0b0995d06d66d15aad9f526c86641a45d48d0906
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-04 11:21:12 +0200

    error with initialize(current_session)

--- spec/services/cookie_preference_spec.rb
+++ spec/services/cookie_preference_spec.rb
@@ -5,12 +5,13 @@ describe CookiePreference do
   describe '.accept_all_cookies' do
     it 'Returns authorization for statistic and navigation' do
       #given
-      #cookie_preference = CookiePreference.new
+      session = {}
       #allow_any_instance_of(CookiePreference).to receive(:accept_all_cookies).and_return({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
       #when
+      session = CookiePreference.new.accept_all_cookies
       #res = session[:cookie].accept_all_cookies
       #then
-      expect(CookiePreference.new.accept_all_cookies).to eq({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
+      expect(session).to eq({"analytics" => "authorize_statistic","hotjar" => "authorize_navigation"})
     end
   end
 end
