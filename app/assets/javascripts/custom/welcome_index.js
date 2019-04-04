38e1ff6040097b0cffc44d6817fe526370681f4b
David Boureau <bdavidxyz@gmail.com>
2019-04-04 10:44:41 +0200

    done ?

--- app/assets/javascripts/custom/welcome_index.js
+++ app/assets/javascripts/custom/welcome_index.js
@@ -3,5 +3,14 @@ clara.load_js(function only_if(){return $("body").hasClasses("welcome", "index")
   store.clearAll();
   console.log("Version : " + clara.version);
 
+  $(document).scroll(function() {
+    var scroll_position = $(document).scrollTop();
+    if (scroll_position > 0) {
+      $(".c-newhomehero__header").addClass("is-scrolled");
+    } else {
+      $(".c-newhomehero__header").removeClass("is-scrolled");
+    }
+  });
+
 
 });
