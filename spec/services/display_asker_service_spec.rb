83e11c7fd87a0e3a1d206791cb3cba48b5b7cb55
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-05 13:50:07 +0200

    init display asker service

---
+++ spec/services/display_asker_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe DisplayAskerService do
+
+  describe '.go' do
+    it 'Returns asker attritubes' do
+      #given
+      #when
+      #then
+    end
+  end
+
+end
