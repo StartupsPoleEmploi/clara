bc8afef34ab339de05aec0d20b5567ae03a02ccf
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-05 11:13:19 +0200

    red test

--- spec/services/cookie_preference_spec.rb
+++ spec/services/cookie_preference_spec.rb
@@ -69,7 +69,9 @@ describe CookiePreference do
     it 'Retuns true if hj is disabled' do
      #given
      res = false
+     session = {"hotjar" => "forbid_navigation"}
      #when
+     res = CookiePreference.new(session).hj_disabled?
      #then
      expect(res).to eq(true) 
     end
