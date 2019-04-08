fad45146c057ff7a6160c645d948c65a23b967b9
David Boureau <bdavidxyz@gmail.com>
2019-04-08 11:59:35 +0200

    wip

--- app/assets/javascripts/custom/aides/aides_main_subscriber.js
+++ app/assets/javascripts/custom/aides/aides_main_subscriber.js
@@ -113,7 +113,15 @@ clara.js_define("aides_main_subscriber", {
     // Nothing interesting was found for the user
     var nb_of_eligibles = _.sumBy(state.eligibles_zone.eligibles, function(contract){return _.count(contract.aids, function(aid){return !aid.is_collapsed;})})  
     var nb_of_uncertains = _.sumBy(state.uncertains_zone.uncertains, function(contract){return _.count(contract.aids, function(aid){return !aid.is_collapsed;})})  
-    nb_of_eligibles + nb_of_uncertains === 0  ? $('#nothing').removeClass('u-hidden') : $('#nothing').addClass('u-hidden');
+    var has_no_positive_result = nb_of_eligibles + nb_of_uncertains === 0;
+      
+    if (has_no_positive_result) {
+      $('#nothing').removeClass('u-hidden');
+      $('#ineligibles').addClass('u-hidden');
+    } else {
+      $('#nothing').addClass('u-hidden');
+      $('#ineligibles').removeClass('u-hidden');
+    }
 
   }
 
