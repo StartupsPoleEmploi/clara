6935137f797e89a621d9a894fe42d2cc14bf9a5e
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-16 15:11:26 +0200

    cleaning

--- app/view_objects/detail_void_contract_type.rb
+++ app/view_objects/detail_void_contract_type.rb
@@ -1,20 +1,11 @@
 class DetailVoidContractType < ViewObject
 
-  def after_init(args)
-    locals = hash_for(args)
-    @aides = array_for(locals[:aides])
-    @dispositifs = array_for(locals[:dispositifs])
-    @the_request = locals[:the_request]
-  end
-
-
   def links_to_all_contract_types
     ContractType.all.map do |c|
     {
       name: c.name,
       link: @context.link_to(c.name, type_path(c.slug), {"class" => "c-link-to-ct"})
     }
-
     end
   end
 
