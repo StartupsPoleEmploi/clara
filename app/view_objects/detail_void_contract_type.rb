d0c12991dbf37715d4d15b628bb61d216f28fc2d
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-17 16:23:55 +0200

    will have to remove current contract type in detail vod contract type

--- app/view_objects/detail_void_contract_type.rb
+++ app/view_objects/detail_void_contract_type.rb
@@ -1,12 +1,18 @@
 class DetailVoidContractType < ViewObject
 
+  def after_init(args)
+    @name = "Aide à la mobilité" 
+  end
+
   def links_to_all_contract_types
-    ContractType.all.map do |c|
-    {
-      name: c.name,
-      link: @context.link_to(c.name, type_path(c.slug), {"class" => "c-link-to-ct"})
-    }
-    end
+    #unless
+      ContractType.all.map do |c|
+      {
+        name: c.name,
+        link: @context.link_to(c.name, type_path(c.slug), {"class" => "c-link-to-ct"})
+      }
+      end
+    #end
   end
 
   
