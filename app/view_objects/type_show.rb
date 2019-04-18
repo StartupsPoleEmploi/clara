ce0c44beaf38de2c6f0d7a28d6285893c41f9b37
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-18 11:04:03 +0200

    trying to return the title in function of slug

--- app/view_objects/type_show.rb
+++ app/view_objects/type_show.rb
@@ -26,4 +26,23 @@ class TypeShow < ViewObject
     "#{@contract[:name]}"
   end
 
+  def title_of_tab
+    case @slug
+    when "aide-a-la-creation-ou-reprise-d-entreprise"
+      res =  "à la création ou reprise d'entreprise"
+    when "aide-a-la-mobilite"
+      res = "à la mobilité, au déplacement, garde d'enfant"
+    when "contrat-en-alternance"
+      res = "à l'alternance"
+    when "aide-a-la-definition-du-projet-professionnel"    
+      res = "à l'orientation, la reconversion professionnelle"
+    else 
+      res = ""
+    end
+    puts "res"
+    puts res
+    res
+  end
+
+
 end
