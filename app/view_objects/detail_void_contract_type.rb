d6174e4d5015482e2ff0b7604db698b3ae439b83
Maxime Devallez <maxime.devallez@pole-emploi.fr>
2019-04-18 10:16:58 +0200

    removed current contract type

--- app/view_objects/detail_void_contract_type.rb
+++ app/view_objects/detail_void_contract_type.rb
@@ -3,21 +3,18 @@ class DetailVoidContractType < ViewObject
   def after_init(args)
     locals = hash_for(args)
     @contract = hash_for(locals[:contract])
-    puts "contract"
-    puts @contract
   end
 
-  def links_to_all_contract_types
-    #unless
+  def list_of_contract_without_current
+    ContractType.all.where.not(id: @contract[:id])
+  end 
 
-    #@contract_list = 
-      ContractType.all.map do |c|
-      {
+  def links_to_all_contract_types
+      list_of_contract_without_current.map do |c| {
         name: c.name,
         link: @context.link_to(c.name, type_path(c.slug), {"class" => "c-link-to-ct"})
       }
       end
-    #end
   end
 
   
