6bdfb6158336360b5093ca772746158f56e0f943
David Boureau <bdavidxyz@gmail.com>
2019-04-23 10:32:51 +0200

    reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2019_03_29_102403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "aids", id: :serial, force: :cascade do |t|
@@ -153,7 +152,7 @@ ActiveRecord::Schema.define(version: 2019_03_29_102403) do
     t.index ["slug"], name: "index_filters_on_slug", unique: true
   end
 
-  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+  create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
     t.integer "sluggable_id", null: false
     t.string "sluggable_type", limit: 50
