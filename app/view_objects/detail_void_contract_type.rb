097679fd07a4f48da35b84bbc710c021fed69ab3
David Boureau <bdavidxyz@gmail.com>
2019-04-25 16:36:14 +0200

    done

--- app/view_objects/detail_void_contract_type.rb
+++ app/view_objects/detail_void_contract_type.rb
@@ -6,13 +6,14 @@ class DetailVoidContractType < ViewObject
   end
 
   def list_of_contract_without_current
-    all_contracts = ActivatedModelsService.instance.contracts.map do |c| { 
-      name: c["name"], 
-      link: @context.link_to(c["name"], type_path(c["slug"]), {"class" => "c-link-to-ct"})  
-    } 
-    end
-    res = all_contracts.reject{|ct| ct[:name] == @contract[:name]}
-    res
-  end 
+    Rails.cache.fetch("all_but_current_contract", expires_in: 1.hour) do
+      all_contracts = ActivatedModelsService.instance.contracts.map do |c| { 
+        name: c["name"], 
+        link: @context.link_to(c["name"], type_path(c["slug"]), {"class" => "c-link-to-ct"})  
+      } 
+      end
+      all_contracts.reject{|ct| ct[:name] == @contract[:name]}
+    end 
+  end
   
 end
