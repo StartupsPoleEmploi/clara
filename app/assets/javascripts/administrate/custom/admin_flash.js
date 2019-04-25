71fd34640c4bae6cb42d969b0d61e673f319dfe3
David Boureau <bdavidxyz@gmail.com>
2019-04-25 11:16:48 +0200

    wip

---
+++ app/assets/javascripts/administrate/custom/admin_flash.js
@@ -0,0 +1,12 @@
+clara.js_define("admin_flash", {
+
+    please_if: function() {
+      return $(".flashes").length > 0;
+    },
+
+    please: function() {
+      console.log("admin flash!!!")
+    },
+
+});
+
