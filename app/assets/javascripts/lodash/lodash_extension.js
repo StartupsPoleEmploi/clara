ff0a33bba7d6de3b3edc389acf05a51984ff5f45
David Boureau <bdavidxyz@gmail.com>
2019-05-06 10:50:23 +0200

    less js

--- app/assets/javascripts/lodash/lodash_extension.js
+++ app/assets/javascripts/lodash/lodash_extension.js
@@ -1,11 +1,5 @@
 _.mixin({
 
-  toPropArray: function (obj) {
-    return _.map(obj, function(value, prop) {
-      return { prop: prop, value: value };
-    });
-  },
-
   toPercentage: function(portion, total) {
     if (_.every(arguments, _.isNumber)) {
       return ((portion/total) * 100).toFixed(1) + '%'
@@ -13,41 +7,6 @@ _.mixin({
     return ''
   },
 
-  voidString: function(str) {
-    var result = "" 
-    if (_.isString(str)) {
-      result = str;
-    }
-    return result;
-  },
-
-  // returns next element in an array, returns first element if last is given
-  nextElementLooped: function (array, element) {
-    
-    if (!(_.isArray(array) && !_.isEmpty(array))) return null;
-
-    var current_index = _.findIndex(array, function(e) {return _.isEqual(e, element)});
-
-    if (_.isEqual(current_index, -1)) return null;
-
-    var last_index  = array.length - 1;
-    if (_.isEqual(current_index, last_index)) { // last element
-      return array[0];
-    } else {
-      return array[current_index + 1];
-    }
-  },
-
-  fromPropArray: function (propArray) {
-    return _.reduce(propArray, function(obj, e, key) {
-      var z = {};
-      z[e.prop] = e.value;
-      _.assign(obj, z);
-      return obj;
-    }, {});
-
-  },
-  
   none: function() {
     return !_.some.apply(_, arguments);
   },
@@ -58,24 +17,6 @@ _.mixin({
 
   count: function() {
     return _.defaultTo(_.countBy.apply(_, arguments)[true], 0);
-  },
-
-
-  injectToPropArray: function (source, destination) {
-
-    _.each(destination, function(e) {
-      if (_.isObject(e) && e.prop) {
-        var sourceElt = _.find(source, function(s) {
-           return _.isEqual(s.prop, e.prop)
-        });
-        if (sourceElt) {
-          e.value = sourceElt.value;
-        } else {
-          e.value = null;
-        }
-      }
-    });
-
   },
 
   isBlank: function(value) {
