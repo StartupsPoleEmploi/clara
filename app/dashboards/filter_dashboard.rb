5095aabea96c55a590e7cc9d305f955e57ca0a72
David Boureau <bdavidxyz@gmail.com>
2019-05-17 15:36:31 +0200

    3 actions

--- app/dashboards/filter_dashboard.rb
+++ app/dashboards/filter_dashboard.rb
@@ -23,8 +23,6 @@ class FilterDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :id,
-    # :aids,
     :name,
     :slug,
   ].freeze
