8bb5dcc502c5ed93f07a5a2efc18fe2edf27564e
David Boureau <bdavidxyz@gmail.com>
2019-05-27 22:02:12 +0200

    some more tests

---
+++ test/services/RuleTreeService_integer_nsw_test.rb
@@ -0,0 +1,51 @@
+require "test_helper"
+
+class RuleTreeServiceIntegerMtTest < ActiveSupport::TestCase
+
+  
+  test ".calculate integer, 34, not_starts_with, 3 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "34", "not_starts_with", "3", "integer", ""
+    #then
+    assert_equal(false, res)
+  end
+
+  test ".calculate integer, 123, not_starts_with, 12 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "123", "not_starts_with", "12", "integer", ""
+    #then
+    assert_equal(false, res)
+  end
+
+  test ".calculate integer, 123, not_starts_with, 123 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "123", "not_starts_with", "123", "integer", ""
+    #then
+    assert_equal(false, res)
+  end
+
+  test ".calculate integer, 18, not_starts_with, 2 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "18", "not_starts_with", "2", "integer", ""
+    #then
+    assert_equal(true, res)
+  end
+
+  test ".calculate integer, 99, not_starts_with, 92 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "99", "not_starts_with", "92", "integer", ""
+    #then
+    assert_equal(true, res)
+  end
+
+end
