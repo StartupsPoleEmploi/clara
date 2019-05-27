ffdff84d5eda6e7a5ac4a442ee8db4cb7c79f462
David Boureau <bdavidxyz@gmail.com>
2019-05-27 16:29:29 +0200

    wip test

---
+++ test/services/RuleTreeService_integer_moet_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+class RuleTreeServiceIntegerMoetTest < ActiveSupport::TestCase
+
+
+
+
+  test ".calculate integer, 34, more_or_equal_than, 18 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "34", "more_or_equal_than", "18", "integer", ""
+    #then
+    assert_equal(true, res)
+  end
+
+  test ".calculate integer, 99, more_or_equal_than, 18 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "99", "more_or_equal_than", "18", "integer", ""
+    #then
+    assert_equal(true, res)
+  end
+
+  test ".calculate integer, 18, more_or_equal_than, 18 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "18", "more_or_equal_than", "18", "integer", ""
+    #then
+    assert_equal(true, res)
+  end
+
+  test ".calculate integer, 3, more_or_equal_than, 18 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "3", "more_or_equal_than", "18", "integer", ""
+    #then
+    assert_equal(false, res)
+  end
+
+  test ".calculate integer, -32, more_or_equal_than, 18 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "-32", "more_or_equal_than", "18", "integer", ""
+    #then
+    assert_equal(false, res)
+  end
+
+end
---
+++ test/services/RuleTreeService_integer_mt_test.rb
@@ -0,0 +1,51 @@
+require "test_helper"
+
+class RuleTreeServiceIntegerMtTest < ActiveSupport::TestCase
+
+  
+  test ".calculate integer, 34, more_than, 18 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "34", "more_than", "18", "integer", ""
+    #then
+    assert_equal(true, res)
+  end
+
+  test ".calculate integer, 99, more_than, 18 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "99", "more_than", "18", "integer", ""
+    #then
+    assert_equal(true, res)
+  end
+
+  test ".calculate integer, 18, more_than, 18 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "18", "more_than", "18", "integer", ""
+    #then
+    assert_equal(false, res)
+  end
+
+  test ".calculate integer, 3, more_than, 18 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "3", "more_than", "18", "integer", ""
+    #then
+    assert_equal(false, res)
+  end
+
+  test ".calculate integer, -32, more_than, 18 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "-32", "more_than", "18", "integer", ""
+    #then
+    assert_equal(false, res)
+  end
+
+end
