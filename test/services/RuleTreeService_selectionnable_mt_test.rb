9e55918a007c41afdae906c84c352938667654f5
David Boureau <bdavidxyz@gmail.com>
2019-05-28 10:47:26 +0200

    116 tests

---
+++ test/services/RuleTreeService_selectionnable_mt_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+class RuleTreeServiceSelectionnableMtTest < ActiveSupport::TestCase
+
+
+  test ".calculate selectionnable, niveau_4, more_than, niveau_5 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "niveau_4", "more_than", "niveau_5", "selectionnable", "niveau_infra_5,niveau_5,niveau_4,niveau_3,niveau_2,niveau_1"
+    #then
+    assert_equal(true, res)
+  end
+
+  test ".calculate selectionnable, niveau_3, more_than, niveau_5 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "niveau_3", "more_than", "niveau_5", "selectionnable", "niveau_infra_5,niveau_5,niveau_4,niveau_3,niveau_2,niveau_1"
+    #then
+    assert_equal(true, res)
+  end
+
+  test ".calculate selectionnable, niveau_2, more_than, niveau_2 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "niveau_2", "more_than", "niveau_2", "selectionnable", "niveau_infra_5,niveau_5,niveau_4,niveau_3,niveau_2,niveau_1"
+    #then
+    assert_equal(false, res)
+  end
+
+  test ".calculate selectionnable, niveau_3, more_than, niveau_2 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "niveau_3", "more_than", "niveau_2", "selectionnable", "niveau_infra_5,niveau_5,niveau_4,niveau_3,niveau_2,niveau_1"
+    #then
+    assert_equal(false, res)
+  end
+
+  test ".calculate selectionnable, niveau_infra_5, more_than, niveau_2 => false" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "niveau_infra_5", "more_than", "niveau_2", "selectionnable", "niveau_infra_5,niveau_5,niveau_4,niveau_3,niveau_2,niveau_1"
+    #then
+    assert_equal(false, res)
+  end
+
+
+
+end
