07ba25cb6c19a8c9e18dfd8f93aeee8cd4266112
David Boureau <bdavidxyz@gmail.com>
2019-05-28 12:30:47 +0200

    ok

--- test/services/RuleTreeService_calc_noruletype_test.rb
+++ test/services/RuleTreeService_calc_noruletype_test.rb
@@ -11,4 +11,12 @@ class RuleTreeServiceCalcNoRuleTypeTest < ActiveSupport::TestCase
     #then
     assert_equal(false, res)
   end
+  test ".calculate string, 34, starts_with, 3 => true" do
+    #given
+    sut = RuletreeService.new
+    #when
+    res = sut.send :calculate, "34", "starts_with", "3", "string", ""
+    #then
+    assert_equal(true, res)
+  end
 end
