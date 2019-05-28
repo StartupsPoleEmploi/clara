448e852f0692a8534387068412d59aa9dd820e67
David Boureau <bdavidxyz@gmail.com>
2019-05-28 08:13:49 +0200

    red tests

--- test/services/RuleTreeService_integer_loet_test.rb
+++ test/services/RuleTreeService_integer_loet_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class RuleTreeServiceIntegerLoetest < ActiveSupport::TestCase
+class RuleTreeServiceIntegerLoetTest < ActiveSupport::TestCase
   
 
   test ".calculate integer, 34, less_or_equal_than, 18 => false" do
