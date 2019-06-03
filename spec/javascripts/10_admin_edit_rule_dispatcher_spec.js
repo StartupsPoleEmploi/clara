17fa7fba2093f92261f46e9a9e27caa238b12f26
David Boureau <bdavidxyz@gmail.com>
2019-06-03 21:05:11 +0200

    another test

--- spec/javascripts/10_admin_edit_rule_dispatcher_spec.js
+++ spec/javascripts/10_admin_edit_rule_dispatcher_spec.js
@@ -8,7 +8,7 @@ describe("admin_edit_rule_dispatcher.js", function() {
       // given
       var store = create_store(); spyOn(store, "dispatch");
       // when
-      clara.admin_edit_rule_dispatcher.please(store, {editor_what:create_ckeditor("what")});
+      sut().please(store, {editor_what:create_ckeditor("what")});
       // then
       expect(store.dispatch).toHaveBeenCalledWith({ type: "WHAT_CHANGED", value: "data_of_what" });
     });
@@ -17,10 +17,41 @@ describe("admin_edit_rule_dispatcher.js", function() {
       // given
       var store = create_store(); spyOn(store, "dispatch");
       // when
-      clara.admin_edit_rule_dispatcher.please(store, {editor_additionnal_conditions: create_ckeditor("additionnal_conditions")});
+      sut().please(store, {editor_additionnal_conditions: create_ckeditor("additionnal_conditions")});
       // then
       expect(store.dispatch).toHaveBeenCalledWith({ type: "ADDITIONNAL_CONDITIONS_CHANGED", value: "data_of_additionnal_conditions" });
     });
+
+    describe("contract change", function() {
+      beforeEach(function() { 
+        $('body').append('\
+          <select class="test-only" name="aid[contract_type_id]" id="aid_contract_type_id">\
+            <option value=""></option>\
+            <option selected="selected" value="1">Aide à la mobilité</option>\
+            <option value="10">Aide régionale</option>\
+          </select>\
+        ');
+      });
+      afterEach(function() {
+        $(".test-only").remove();
+      })
+      it("Dispatch CONTRACT_CHANGED if contract changed", function() {
+        // given
+        var $aid_contract_type_id = $("#aid_contract_type_id")
+        var store = create_store(); spyOn(store, "dispatch");
+        // when
+        sut().please(store, {$aid_contract_type_id: $aid_contract_type_id});
+        $aid_contract_type_id.trigger('change');
+        // then
+        expect(store.dispatch).toHaveBeenCalledWith(
+          { type: "CONTRACT_CHANGED", value: "Aide à la mobilité" }
+        );
+      });      
+    })
+
+    function sut() {
+      return clara.admin_edit_rule_dispatcher;
+    }
 
     function create_ckeditor(name) {
       return {
