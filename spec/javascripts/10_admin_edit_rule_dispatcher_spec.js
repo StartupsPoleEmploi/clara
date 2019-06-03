7edbb7d54c48af1040974a8037e2d824c51b6746
David Boureau <bdavidxyz@gmail.com>
2019-06-03 16:31:59 +0200

    dispatcher spec

---
+++ spec/javascripts/10_admin_edit_rule_dispatcher_spec.js
@@ -0,0 +1,10 @@
+//= require_tree ../../app/assets/javascripts/administrate/custom/admin_edit_rule
+describe('admin_edit_rule_dispatcher.js', function() {
+
+  describe('.please', function() {
+    it('Dispatch ADDITIONAL_CONDITION_CHANGED if additionnal_conditions changed', function() {
+      expect(true).toEqual(true)
+    });
+});
+});
+  
