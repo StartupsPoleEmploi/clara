07b329247c86a1b0e9bdb4b6f5eaa1d807ff70fd
David Boureau <bdavidxyz@gmail.com>
2019-06-03 17:15:59 +0200

    another green test

--- spec/javascripts/10_admin_edit_rule_dispatcher_spec.js
+++ spec/javascripts/10_admin_edit_rule_dispatcher_spec.js
@@ -18,20 +18,30 @@ describe('admin_edit_rule_dispatcher.js', function() {
       }
     }
 
-    it('Dispatch ADDITIONAL_CONDITION_CHANGED if additionnal_conditions changed', function() {
+    it('Dispatch ADDITIONNAL_CONDITIONS_CHANGED if additionnal_conditions changed', function() {
       // given
-      var obs = create_obs("what")
-      // var editor = obs.editor_what
+      var obs = create_obs("additionnal_conditions")
       var store = create_store();
       spyOn(store, 'dispatch');
-      console.log(obs)
+      spyOn(obs.editor_additionnal_conditions, 'on').and.callThrough();
+      // when
+      clara.admin_edit_rule_dispatcher.please(store, obs);
+      // then
+      expect(store.dispatch).toHaveBeenCalledWith({ type: 'ADDITIONNAL_CONDITIONS_CHANGED', value: 42 });
+      expect(obs.editor_additionnal_conditions.on).toHaveBeenCalledWith('change', jasmine.any(Function));
+    });
+
+    it('Dispatch WHAT_CHANGED if additionnal_conditions changed', function() {
+      // given
+      var obs = create_obs("what")
+      var store = create_store();
+      spyOn(store, 'dispatch');
       spyOn(obs.editor_what, 'on').and.callThrough();
       // when
       clara.admin_edit_rule_dispatcher.please(store, obs);
       // then
       expect(store.dispatch).toHaveBeenCalledWith({ type: 'WHAT_CHANGED', value: 42 });
       expect(obs.editor_what.on).toHaveBeenCalledWith('change', jasmine.any(Function));
-      // expect(obs.on).toHaveBeenCalledWith("blabla");
     });
 });
 });
