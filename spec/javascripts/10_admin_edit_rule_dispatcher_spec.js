0e46abe65528bc2d028b080a1d49cffef1bd0697
David Boureau <bdavidxyz@gmail.com>
2019-06-03 18:04:33 +0200

    simplified

--- spec/javascripts/10_admin_edit_rule_dispatcher_spec.js
+++ spec/javascripts/10_admin_edit_rule_dispatcher_spec.js
@@ -1,15 +1,19 @@
 //= require_tree ../../app/assets/javascripts/administrate/custom/admin_edit_rule
-describe('admin_edit_rule_dispatcher.js', function() {
+describe("admin_edit_rule_dispatcher.js", function() {
 
-  describe('.please', function() {
+  describe(".please", function() {
 
-    function create_obs(name) {
-      var obs = { };
-      obs["editor_" + name] = {
-          on: function(evt, func){func()},
-          getData: function(){return 42}
-        };
-      return obs;
+    function create_ckeditor(name) {
+      return {
+        on: function(evt, func){
+          if(evt === "change") {
+            func();
+          }
+        },
+        getData: function(){
+          return "data_of_" + name;
+        }
+      }
     }
 
     function create_store() {
@@ -18,31 +22,27 @@ describe('admin_edit_rule_dispatcher.js', function() {
       }
     }
 
-    it('Dispatch ADDITIONNAL_CONDITIONS_CHANGED if additionnal_conditions changed', function() {
+
+    it("Dispatch WHAT_CHANGED if what changed", function() {
       // given
-      var obs = create_obs("additionnal_conditions")
-      var store = create_store();
-      spyOn(store, 'dispatch');
-      spyOn(obs.editor_additionnal_conditions, 'on').and.callThrough();
+      var fake_ckeditor = create_ckeditor("what"); spyOn(fake_ckeditor, "on").and.callThrough();
+      var store = create_store(); spyOn(store, "dispatch");
       // when
-      clara.admin_edit_rule_dispatcher.please(store, obs);
+      clara.admin_edit_rule_dispatcher.please(store, {editor_what:fake_ckeditor});
       // then
-      expect(store.dispatch).toHaveBeenCalledWith({ type: 'ADDITIONNAL_CONDITIONS_CHANGED', value: 42 });
-      expect(obs.editor_additionnal_conditions.on).toHaveBeenCalledWith('change', jasmine.any(Function));
+      expect(store.dispatch).toHaveBeenCalledWith({ type: "WHAT_CHANGED", value: "data_of_what" });
     });
 
-    it('Dispatch WHAT_CHANGED if additionnal_conditions changed', function() {
+    it("Dispatch ADDITIONNAL_CONDITIONS_CHANGED if additionnal_conditions changed", function() {
       // given
-      var obs = create_obs("what")
-      var store = create_store();
-      spyOn(store, 'dispatch');
-      spyOn(obs.editor_what, 'on').and.callThrough();
+      var fake_ckeditor = create_ckeditor("additionnal_conditions"); spyOn(fake_ckeditor, "on").and.callThrough();
+      var store = create_store(); spyOn(store, "dispatch");
       // when
-      clara.admin_edit_rule_dispatcher.please(store, obs);
+      clara.admin_edit_rule_dispatcher.please(store, {editor_additionnal_conditions:fake_ckeditor});
       // then
-      expect(store.dispatch).toHaveBeenCalledWith({ type: 'WHAT_CHANGED', value: 42 });
-      expect(obs.editor_what.on).toHaveBeenCalledWith('change', jasmine.any(Function));
+      expect(store.dispatch).toHaveBeenCalledWith({ type: "ADDITIONNAL_CONDITIONS_CHANGED", value: "data_of_additionnal_conditions" });
     });
+
 });
 });
   
