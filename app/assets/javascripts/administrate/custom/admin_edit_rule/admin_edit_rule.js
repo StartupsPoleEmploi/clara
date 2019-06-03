9c2e0cd9d8c42351e17a46e04a491cd309add2fa
David Boureau <bdavidxyz@gmail.com>
2019-06-03 16:03:26 +0200

    more unit tests

--- app/assets/javascripts/administrate/custom/admin_edit_rule/admin_edit_rule.js
+++ app/assets/javascripts/administrate/custom/admin_edit_rule/admin_edit_rule.js
@@ -1,4 +1,4 @@
-clara.js_define("edit_admin_aid", {
+clara.js_define("admin_edit_rule", {
 
   please_if: function() {
     return $(".js-aid-edition").exists();
@@ -30,30 +30,7 @@ clara.js_define("edit_admin_aid", {
     };
     
     // REDUCER
-    var reducer = function(state, action) { 
-
-      // Deep copy of previous state to avoid side-effects
-      var newState = _.cloneDeep(state);
-
-      if (action.type === 'INIT') {
-        // do nothing
-      } else if (action.type === 'ADDITIONNAL_CONDITIONS_CHANGED') {
-        newState.additionnal_conditions = action.value
-      } else if (action.type === 'HOW_AND_WHEN_CHANGED') {
-        newState.how_and_when = action.value
-      } else if (action.type === 'HOW_MUCH_CHANGED') {
-        newState.how_much = action.value
-      } else if (action.type === 'LIMITATIONS_CHANGED') {
-        newState.limitations = action.value
-      } else if (action.type === 'WHAT_CHANGED') {
-        newState.what = action.value
-      } else if (action.type === 'TITLE_CHANGED') {
-        newState.title = action.value
-      } else if (action.type === 'CONTRACT_CHANGED') {
-        newState.contract = action.value
-      }
-      return newState;
-    };
+    var reducer = clara.admin_edit_rule_reducer.please;
 
     // STORE
     window.main_store = Redux.createStore(reducer, global_state);
