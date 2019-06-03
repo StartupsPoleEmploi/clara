5e0ae67412a714aacaaf1ad8a99ec48f426d14d0
David Boureau <bdavidxyz@gmail.com>
2019-06-03 15:39:07 +0200

    cleaned

--- app/assets/javascripts/administrate/custom/admin_edit_rule/admin_edit_rule.js
+++ app/assets/javascripts/administrate/custom/admin_edit_rule/admin_edit_rule.js
@@ -6,27 +6,6 @@ clara.js_define("edit_admin_aid", {
 
   please: function() {
 
-
-//     CKEDITOR.replace( 'aid_additionnal_conditions', {
-//     toolbar: [
-//     // { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
-//     // { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-//     // { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
-//     // { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
-//     '/',
-//     { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
-//     // { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
-//     // { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-//     // { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
-//     '/',
-//     // { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-//     // { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-//     // { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
-//     // { name: 'others', items: [ '-' ] },
-//     // { name: 'about', items: [ 'About' ] }
-// ]
-// });
-
     var editor_additionnal_conditions = CKEDITOR.instances['aid_additionnal_conditions']
     var editor_how_and_when = CKEDITOR.instances['aid_how_and_when']
     var editor_how_much = CKEDITOR.instances['aid_how_much']
