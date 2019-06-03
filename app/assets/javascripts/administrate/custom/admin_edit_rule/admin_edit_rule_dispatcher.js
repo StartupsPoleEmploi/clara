af062143dba3c89412b954b219e5ea885994dae8
David Boureau <bdavidxyz@gmail.com>
2019-06-03 17:06:28 +0200

    a green test

--- app/assets/javascripts/administrate/custom/admin_edit_rule/admin_edit_rule_dispatcher.js
+++ app/assets/javascripts/administrate/custom/admin_edit_rule/admin_edit_rule_dispatcher.js
@@ -3,50 +3,64 @@ clara.js_define("admin_edit_rule_dispatcher", {
   please_if: _.stubFalse,
 
   please: function(store, observables) {
-    observables.editor_additionnal_conditions.on('change', function(){
-      store.dispatch({
-        type: 'ADDITIONNAL_CONDITIONS_CHANGED', 
-        value: observables.editor_additionnal_conditions.getData() 
-      })
-    });
-    observables.editor_how_and_when.on('change', function(){
-      store.dispatch({
-        type: 'HOW_AND_WHEN_CHANGED', 
-        value: observables.editor_how_and_when.getData() 
-      })
-    });
-    observables.editor_how_much.on('change', function(){
-      store.dispatch({
-        type: 'HOW_MUCH_CHANGED', 
-        value: observables.editor_how_much.getData() 
-      })
-    });
-    observables.editor_limitations.on('change', function(){
-      store.dispatch({
-        type: 'LIMITATIONS_CHANGED', 
-        value: observables.editor_limitations.getData() 
-      })
-    });
-    observables.editor_what.on('change', function(){
-      store.dispatch({
-        type: 'WHAT_CHANGED', 
-        value: observables.editor_what.getData() 
-      })
-    });
-    observables.$aid_name.on('keyup', function(e){
-      var newVal = this.value;
-      store.dispatch({
-        type: 'TITLE_CHANGED', 
-        value: newVal 
-      })
-    });
-    observables.$aid_contract_type_id.on('change', function(e){
-      var newVal = $(this).find("option:selected").text();
-      store.dispatch({
-        type: 'CONTRACT_CHANGED', 
-        value: newVal
-      })
-    });
+    if (observables.editor_additionnal_conditions) {
+      observables.editor_additionnal_conditions.on('change', function(){
+        store.dispatch({
+          type: 'ADDITIONNAL_CONDITIONS_CHANGED', 
+          value: observables.editor_additionnal_conditions.getData() 
+        })
+      });      
+    }
+    if (observables.editor_how_and_when) {
+      observables.editor_how_and_when.on('change', function(){
+        store.dispatch({
+          type: 'HOW_AND_WHEN_CHANGED', 
+          value: observables.editor_how_and_when.getData() 
+        })
+      });
+    }
+    if (observables.editor_how_much) {
+      observables.editor_how_much.on('change', function(){
+        store.dispatch({
+          type: 'HOW_MUCH_CHANGED', 
+          value: observables.editor_how_much.getData() 
+        })
+      });
+    }
+    if (observables.editor_limitations) {
+      observables.editor_limitations.on('change', function(){
+        store.dispatch({
+          type: 'LIMITATIONS_CHANGED', 
+          value: observables.editor_limitations.getData() 
+        })
+      });      
+    }
+    if (observables.editor_what) {
+      observables.editor_what.on('change', function(){
+        store.dispatch({
+          type: 'WHAT_CHANGED', 
+          value: observables.editor_what.getData() 
+        })
+      });
+    }
+    if (observables.$aid_name) {
+      observables.$aid_name.on('keyup', function(e){
+        var newVal = this.value;
+        store.dispatch({
+          type: 'TITLE_CHANGED', 
+          value: newVal 
+        })
+      });
+    }
+    if (observables.$aid_contract_type_id) {    
+      observables.$aid_contract_type_id.on('change', function(e){
+        var newVal = $(this).find("option:selected").text();
+        store.dispatch({
+          type: 'CONTRACT_CHANGED', 
+          value: newVal
+        })
+      });
+    }
   }
 
 
--- spec/javascripts/10_admin_edit_rule_dispatcher_spec.js
+++ spec/javascripts/10_admin_edit_rule_dispatcher_spec.js
@@ -2,8 +2,32 @@
 describe('admin_edit_rule_dispatcher.js', function() {
 
   describe('.please', function() {
+
+    function create_obs_for(name) {
+
+    }
+
+    function create_store() {
+      return {
+        dispatch: function(arg1, arg2){}
+      }
+    }
+
     it('Dispatch ADDITIONAL_CONDITION_CHANGED if additionnal_conditions changed', function() {
-      expect(true).toEqual(true)
+      // given
+      var obs = {
+        editor_what: {
+          on: function(evt, func){func()},
+          getData: function(){return 42}
+        }
+      };
+      var store = create_store();
+
+      spyOn(store, 'dispatch');
+      // when
+      clara.admin_edit_rule_dispatcher.please(store, obs);
+      // then
+      expect(store.dispatch).toHaveBeenCalledWith({ type: 'WHAT_CHANGED', value: 42 });
     });
 });
 });
