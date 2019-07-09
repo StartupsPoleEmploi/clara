7d8a30e3cbfeda372e37e07bcf18cf9b0acc0eda
David Boureau <bdavidxyz@gmail.com>
2019-07-09 08:17:13 +0200

    cleaning tests

--- test/services/QuestionManager_test.rb
+++ test/services/QuestionManager_test.rb
@@ -31,4 +31,34 @@ class QuestionManagerTest < ActiveSupport::TestCase
     assert_equal("/allocation_questions/new", res)
   end
 
+  test ".getNextPath go from category to allocation" do
+    #given
+    referer = 'category'
+    form = Struct.new(:value).new('')
+    #when
+    res = QuestionManager.new.getNextPath(referer, form)
+    #then
+    assert_equal("/allocation_questions/new", res)
+  end
+
+  test ".getNextPath go from allocation to age, by default" do
+    #given
+    referer = 'allocation'
+    form = Struct.new(:type).new('')
+    #when
+    res = QuestionManager.new.getNextPath(referer, form)
+    #then
+    assert_equal("/age_questions/new", res)
+  end
+
+  test ".getNextPath go from allocation to A.R.E, if type of allocation is 'ASS_AER_APS_AS-FNE'" do
+    #given
+    referer = 'allocation'
+    form = Struct.new(:type).new('ASS_AER_APS_AS-FNE')
+    #when
+    res = QuestionManager.new.getNextPath(referer, form)
+    #then
+    assert_equal("/are_questions/new", res)
+  end
+
 end
