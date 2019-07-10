dca6907fc8a1e8f299260e0dec4de6376e871c87
David Boureau <bdavidxyz@gmail.com>
2019-07-10 10:40:17 +0200

    red test first

---
+++ test/services/SerializeResultsService_api_eligible_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class SerializeResultsServiceApiEligibleTest < ActiveSupport::TestCase
+  
+  test '_.api_eligible' do
+    #given
+    #when
+    res = SerializeResultsService.api_eligible(asker, filters, need_filters, custom_filters, custom_parent_filters)
+    #then
+  end
+
+end
