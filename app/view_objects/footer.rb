db5e7e99e887bedb711d4c756dad6055135a2082
David Boureau <bdavidxyz@gmail.com>
2019-07-11 11:44:08 +0200

    cleaning footer again

--- app/view_objects/footer.rb
+++ app/view_objects/footer.rb
@@ -1,16 +1,15 @@
 class Footer < ViewObject
 
   def after_init(args)
-    locals = hash_for(args)
     _init_splitted_array_of_contract
   end
 
   def links_to_all_contract_types 
     Rails.cache.fetch("footer_links", expires_in: 1.hour) do
       ActivatedModelsService.instance.contracts.map do |c| 
-        { 
-          name: c["name"], 
-          link: @context.link_to(c["name"], type_path(c["slug"]), {"class" => "c-link-to-ct"})  
+        {
+          "name" => c["name"],
+          "slug" => c["slug"]
         } 
       end
     end 
@@ -25,14 +24,16 @@ class Footer < ViewObject
   end
 
   def _init_splitted_array_of_contract 
+    p '- - - - - - - - - - - - - - _init_splitted_array_of_contract- - - - - - - - - - - - - - - -' 
+    p ''
     @array1 = []  
     @array2 = []  
     links_to_all_contract_types.each_with_index do |ct, indx|  
       if indx < links_to_all_contract_types.size / 2 
-        @array1.push(ct[:link]) 
+        @array1.push(ct) 
       end 
       if indx >= links_to_all_contract_types.size / 2  
-        @array2.push(ct[:link]) 
+        @array2.push(ct) 
       end     
     end 
   end 
--- test/services/Footer_test.rb
+++ test/services/Footer_test.rb
@@ -10,10 +10,10 @@ class FooterTest < ActiveSupport::TestCase
 
   def contracts
     [
-      {"name" => "Emploi international", "id" => 3,  "slug" => "emploi-international"}
-      {"name" => "Contrat spécifique",   "id" => 5,  "slug" => "contrat-specifique"}
-      {"name" => "Dispositif séniors",   "id" => 9,  "slug" => "dispositif-seniors"}
-      {"name" => "Contrat en alternance", "id" => 7,  "slug" => "contrat-en-alternance"}
+      {"name" => "Emploi international", "id" => 3,  "slug" => "emploi-international"},
+      {"name" => "Contrat spécifique",   "id" => 5,  "slug" => "contrat-specifique"},
+      {"name" => "Dispositif séniors",   "id" => 9,  "slug" => "dispositif-seniors"},
+      {"name" => "Contrat en alternance", "id" => 7,  "slug" => "contrat-en-alternance"},
       {"name" => "Aide régionale", "id" => 10,  "slug" => "aide-regionale"}
     ]
   end
