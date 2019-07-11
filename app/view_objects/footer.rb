27da39fc2be4310f9b996b5434bf88563f39787c
David Boureau <bdavidxyz@gmail.com>
2019-07-11 11:05:55 +0200

    cleaning footer

--- app/view_objects/footer.rb
+++ app/view_objects/footer.rb
@@ -4,32 +4,11 @@ class Footer < ViewObject
     locals = hash_for(args)
     @aides = array_for(locals[:aides])
     @dispositifs = array_for(locals[:dispositifs])
-    @the_request = locals[:the_request]
     init_splitted_array_of_contract
-  end
-
-  def type_aides
-    @aides
-  end
-
-  def should_display_footer
-    @the_request.respond_to?('path') && !@the_request.path.include?("/stats")
   end
 
   def link_to_all_aides
     @context.link_to("Toutes nos aides", aides_path, {"class" => "c-link-to-all-aides"})
-  end
-
-  def type_dispositifs
-    @dispositifs
-  end
-
-  def display_title_aides
-    @aides.size > 0
-  end
-
-  def display_title_dispositifs
-    @dispositifs.size > 0
   end
 
   def links_to_all_contract_types 
---
+++ test/services/Footer_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class FooterTest < ActiveSupport::TestCase
+  
+  test ".root_condition " do
+  end
+
+end
