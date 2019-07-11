2bc66bc57db86f0ac8bfcc7554c84101df738609
David Boureau <bdavidxyz@gmail.com>
2019-07-11 12:05:24 +0200

    4 more tests

--- test/services/Navigation_test.rb
+++ test/services/Navigation_test.rb
@@ -6,12 +6,13 @@ class NavigationTest < ActiveSupport::TestCase
     #given
     active_path = "admin_users_path"
     allow_any_instance_of(GetCurrentPathService).to receive(:call).and_return(active_path)
+    navigation = Navigation.new(OpenStruct.new(request: ""), nil)
     #when
-    res = Navigation.new(OpenStruct.new(request: ""), nil)
+    res = navigation.admin_clazz
     #then
     assert_equal(
       'navigation__link navigation__link--active', 
-      res.admin_clazz
+      res
     )
   end
 
@@ -19,12 +20,41 @@ class NavigationTest < ActiveSupport::TestCase
     #given
     inactive_path = "inactive_path"
     allow_any_instance_of(GetCurrentPathService).to receive(:call).and_return(inactive_path)
+    navigation = Navigation.new(OpenStruct.new(request: ""), nil)
     #when
-    res = Navigation.new(OpenStruct.new(request: ""), nil)
+    res = navigation.admin_clazz
     #then
     assert_equal(
       'navigation__link navigation__link--inactive', 
-      res.admin_clazz
+      res
+    )
+  end
+
+  test '.filter_clazz returns "navigation__link navigation__link--active" is path is active' do
+    #given
+    active_path = "admin_filter_path"
+    allow_any_instance_of(GetCurrentPathService).to receive(:call).and_return(active_path)
+    navigation = Navigation.new(OpenStruct.new(request: ""), nil)
+    #when
+    res = navigation.filter_clazz
+    #then
+    assert_equal(
+      'navigation__link navigation__link--active', 
+      res
+    )
+  end
+
+  test '.filter_clazz returns "navigation__link navigation__link--inactive" is path is NOT active' do
+    #given
+    inactive_path = "inactive_path"
+    allow_any_instance_of(GetCurrentPathService).to receive(:call).and_return(inactive_path)
+    navigation = Navigation.new(OpenStruct.new(request: ""), nil)
+    #when
+    res = navigation.filter_clazz
+    #then
+    assert_equal(
+      'navigation__link navigation__link--inactive', 
+      res
     )
   end
 
