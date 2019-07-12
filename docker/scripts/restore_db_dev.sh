b3c910a7174573c9d9f4b83f287442838aaeec96
David Boureau <bdavidxyz@gmail.com>
2019-07-12 16:31:00 +0200

    more scripts

--- docker/scripts/restore_db_dev.sh
+++ docker/scripts/restore_db_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec clara_db pg_restore --verbose --clean -x -h localhost -S ara -d ara_dev  /home/dumped/latest.dump
+docker exec clara_db pg_restore --verbose --clean -x -h localhost -S ara -d ara_dev /home/dumped/latest.dump
