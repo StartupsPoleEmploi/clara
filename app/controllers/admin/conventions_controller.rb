9f4961eea1ec6e9414cf5bc866f235a05eb442f9
David Boureau <bdavidxyz@gmail.com>
2019-07-12 12:22:18 +0200

    cleaned

--- app/controllers/admin/conventions_controller.rb
+++ app/controllers/admin/conventions_controller.rb
@@ -1,25 +1,9 @@
 module Admin
   class ConventionsController < Admin::ApplicationController
-    # To customize the behavior of this controller,
-    # you can overwrite any of the RESTful actions. For example:
-    #
-    # def index
-    #   super
-    #   @resources = Convention.
-    #     page(params[:page]).
-    #     per(10)
-    # end
 
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   Convention.find_by!(slug: param)
-    # end
-
-    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
-    # for more information
-    # disable 'new' and 'destroy' links
     def valid_action?(name, resource = resource_class)
       %w[new destroy].exclude?(name.to_s) && super
     end
+    
   end
 end
