a8c076b3737b6dca51a3ce0e5507010cf28ca969
David Boureau <bdavidxyz@gmail.com>
2019-07-12 11:43:38 +0200

    removed find_filters

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,6 @@ Rails.application.routes.draw do
       post 'save_simulation', on: :member
       delete 'delete_simulation', on: :member
     end
-    get 'find_filters' => 'aids#find_filters'
     controller 'pages' do
       get 'get_all_filters_menu'
       get 'get_need_menu'
