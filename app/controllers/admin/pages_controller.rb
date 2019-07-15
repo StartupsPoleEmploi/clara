f20efb49d27dcb48c778c68e197e41ce89bcbb2b
David Boureau <bdavidxyz@gmail.com>
2019-07-15 14:19:50 +0200

    deleted stats in admin

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -35,42 +35,6 @@ module Admin
       }
     end
 
-    #stats
-    def get_stats
-    end
-    def post_stats
-      csv = CSV.parse(_csv_data, {headers: true})
-      json_data = _csv_to_json(csv)
-      _save_stat_of("ga", json_data)
-    end
-    def post_stats_from_pe
-      csv = CSV.parse(_csv_data, {headers: true})
-      csv.delete("Période")
-      json_data = _csv_to_json(csv)
-      _save_stat_of("ga_pe", json_data)      
-    end
-    def post_stats_advisors
-      csv = CSV.parse(_csv_data, {headers: true})
-      csv.delete("Source URL")
-      json_data = _csv_to_json(csv)
-      _save_stat_of("hj_ad", json_data)   
-    end
-    def _csv_data
-      params.extract!(:csv_data).permit(:csv_data).to_h["csv_data"]
-    end
-    def _save_stat_of(prop, value)
-      Stat.create unless Stat.first
-      s = Stat.first
-      s[prop] = value
-      s.save
-    end
-    def _csv_to_json(csv)
-      csv_hash = csv.map(&:to_h)
-      root_hash = {}
-      root_hash["json_data"] = csv_hash
-      root_hash
-    end
-
     # refdata
     def get_ref_data
     end
