48c42e9a613698b1ffedeb469287b356b590fe72
David Boureau <bdavidxyz@gmail.com>
2019-07-15 19:08:43 +0200

    ok for towns

--- app/assets/javascripts/administrate/custom/admin_geo/admin_geowhere.js
+++ app/assets/javascripts/administrate/custom/admin_geo/admin_geowhere.js
@@ -6,43 +6,45 @@ clara.js_define("admin_geowhere", {
 
     please: function() {
 
-        var department_options = {        
-          valueField: 'name',
-          labelField: 'name',
-          searchField: 'name',
-          options: [],
-          create: false,
-          load: function(query, callback) {
-            if (!query.length) return callback();
-            $.get({
-                url: 'https://api-adresse.data.gouv.fr/search/',
-                type: 'GET',
-                dataType: 'json',
-                data: {
-                  q: query,
-                  type: "context"
-                },
-                error: function() {
-                    callback();
-                },
-                success: function(res) {
-                  callback([]);
-                  var uniq_context = _.uniqBy(res.features, function(e) {
-                    return e.properties.context
-                  });
-                  var rez = _.map(uniq_context, function(e){
-                    var displayed = e.properties.context
-                    return {
-                      value: _.slugify(displayed),
-                      name: displayed            
-                    }
-                  })
-                  callback(rez);
-                }
-            });
-          }
-        };
-        $('.c-geoselect--department').selectize(department_options);
+        // var department_options = {        
+        //   valueField: 'name',
+        //   labelField: 'name',
+        //   searchField: 'name',
+        //   options: [],
+        //   create: false,
+        //   load: function(query, callback) {
+        //     if (!query.length) return callback();
+        //     $.get({
+        //         url: 'https://api-adresse.data.gouv.fr/search/',
+        //         type: 'GET',
+        //         dataType: 'json',
+        //         data: {
+        //           q: query,
+        //           type: "municipality"
+        //         },
+        //         error: function() {
+        //             callback();
+        //         },
+        //         success: function(res) {
+        //           var uniq_context = _.uniqBy(res.features, function(e) {
+        //             return e.properties.context
+        //           });
+        //           console.log(uniq_context);
+        //           var rez = _.map(uniq_context, function(e){
+        //             var displayed = e.properties.context
+        //             return {
+        //               value: _.slugify(displayed),
+        //               name: displayed.split(",")[0] + ", " + displayed.split(",")[1]         
+        //             }
+        //           })
+        //           callback(rez);
+        //           // selectize.refreshItems()
+        //         }
+        //     });
+        //   }
+        // };
+        // $('.c-geoselect--department').selectize()[0].selectize.destroy();
+        // $('.c-geoselect--department').selectize(department_options);
 
         // var region_options = {        
         //   valueField: 'name',
@@ -92,48 +94,43 @@ clara.js_define("admin_geowhere", {
         // };
         // $('.c-geoselect--region').selectize(region_options);
       
-        var region_options = {        
-          valueField: 'name',
-          labelField: 'name',
-          searchField: 'name',
-          options: [],
-          create: false,
-          load: function(query, callback) {
-            if (!query.length) return callback();
-            $.get({
-                url: 'https://api-adresse.data.gouv.fr/search/',
-                type: 'GET',
-                dataType: 'json',
-                data: {
-                  q: query,
-                  type: "municipality"
-                },
-                error: function() {
-                    callback();
-                },
-                success: function(res) {
-                  callback([]);
-                  var uniq_context = _.uniqBy(res.features, function(e) {
-                    return e.properties.context
-                  });
-                  var rez = _.map(uniq_context, function(e){
-                    if (e && e.properties && e.properties.context && e.properties.context.indexOf(",") > 0 && e.properties.context.indexOf("(") > 0) {
-                      var displayed = e.properties.context.split(",")[2].split("(")[0]
-                      console.log(displayed);
-                      return {
-                        value: _.slugify(displayed),
-                        name: displayed            
-                      }                      
-                    } else {
-                      return {value: "a", name:"a"}
-                    }
-                  })
-                  callback(rez);
-                }
-            });
-          }
-        };
-        $('.c-geoselect--region').selectize(region_options);
+        // var region_options = {        
+        //   valueField: 'name',
+        //   labelField: 'name',
+        //   searchField: 'name',
+        //   options: [],
+        //   create: false,
+        //   load: function(query, callback) {
+        //     if (!query.length) return callback();
+        //     $.get({
+        //         url: 'https://api-adresse.data.gouv.fr/search/',
+        //         type: 'GET',
+        //         dataType: 'json',
+        //         data: {
+        //           q: query,
+        //           type: "municipality"
+        //         },
+        //         error: function() {
+        //             callback();
+        //         },
+        //         success: function(res) {
+        //           callback([]);
+        //           var uniq_context = _.uniqBy(res.features, function(e) {
+        //             return e.properties.context
+        //           });
+        //           var rez = _.map(uniq_context, function(e){
+        //             var displayed = e.properties.context
+        //             return {
+        //               value: _.slugify(displayed),
+        //               name: displayed            
+        //             }
+        //           })
+        //           callback(rez);
+        //         }
+        //     });
+        //   }
+        // };
+        // $('.c-geoselect--region').selectize(region_options);
 
 
 
