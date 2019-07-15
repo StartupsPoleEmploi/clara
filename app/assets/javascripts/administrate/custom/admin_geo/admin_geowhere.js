88724734ce79bf52eb24ffac6001de48f5841e53
David Boureau <bdavidxyz@gmail.com>
2019-07-15 17:06:23 +0200

    wip

--- app/assets/javascripts/administrate/custom/admin_geo/admin_geowhere.js
+++ app/assets/javascripts/administrate/custom/admin_geo/admin_geowhere.js
@@ -5,6 +5,28 @@ clara.js_define("admin_geowhere", {
     },
 
     please: function() {
+
+        var department_options = {        
+          placeholder: "",
+          options: [
+            {value: "ain", name: "Ain" },
+            {value: "loire-atlantique", name: "Loire-Atlantique" },
+          ],
+          labelField: 'name',
+          searchField: ['name'],
+        };
+        $('.c-geoselect--department').selectize(department_options);
+
+        var region_options = {        
+          placeholder: "",
+          options: [
+            {value: "bretagne", name: "Bretagne" },
+            {value: "ara", name: "Auvergne-Rhone-Alpes" },
+          ],
+          labelField: 'name',
+          searchField: ['name'],
+        };
+        $('.c-geoselect--region').selectize(region_options);
       
     }
 
