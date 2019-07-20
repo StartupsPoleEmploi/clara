5e49fb407fb537c540e22112584a87d966a413b5
David Boureau <bdavidxyz@gmail.com>
2019-07-20 09:16:45 +0200

    wip

--- app/assets/javascripts/administrate/custom/admin_geo/admin_trundle_subscriber.js
+++ app/assets/javascripts/administrate/custom/admin_geo/admin_trundle_subscriber.js
@@ -21,15 +21,16 @@ clara.js_define("admin_trundle_subscriber", {
     walk_nodes: function(obj) {
       var that = clara.admin_trundle_subscriber;
       var parent_name = obj.name;
+      var parent_combination = obj.subcombination;
       if (_.size(obj.subboxes) > 0) {
         _.each(obj.subboxes, function(subbox) {
-          that.paint_node(subbox, parent_name);
+          that.paint_node(subbox, parent_name, parent_combination);
           that.walk_nodes(subbox);
         })
       }
     },
 
-    paint_node: function(node, parent_name) {
+    paint_node: function(node, parent_name, parent_combination) {
       console.log("painting " + node.name + " with parent " + parent_name + " editing " + node.is_editing)
       var that = clara.admin_trundle_subscriber;
       var $parent = $("." + parent_name)
@@ -41,7 +42,7 @@ clara.js_define("admin_trundle_subscriber", {
       } else {
         console.log('node.subcombination')
         console.log(node.subcombination)
-        $(that.first_template(node.xtxt, node.name, parent_name, node.subcombination)).appendTo($parent)
+        $(that.first_template(node.xtxt, node.name, parent_name, parent_combination)).appendTo($parent)
       }
 
     },
@@ -56,38 +57,23 @@ clara.js_define("admin_trundle_subscriber", {
                 ',
 
     first_template: function(title, name, parent_name, combination) {
-      var combined_with = combination ? '<span class="c-comb c-comb--' + _.toLower(combination) + '">' + combination + ' </span>' : ''
+      var french_combination = combination ? (combination === "AND" ? "ET" : "OU") : ""
+      var combined_with = combination ? '<span class="c-comb c-comb--' + combination.toLowerCase() + '">' + french_combination + ' </span>' : ''
+
       console.log(combined_with);
-      return '<ul class="unsortable ui-sortable ' + name + '" data-box="' + name + '">\
-                <li class="sortable ui-sortable-handle pos-relative">' + combined_with + '\
-                  <span class="combinator-container">\
-                    <button class="js-tooltip like-a-link add-condition" data-tooltip-content-id="tooltip_id_condition" data-tooltip-title="' + title +'" data-tooltip-prefix-class="combinator" data-tooltip-close-text="x" data-tooltip-close-title="Ferme la fenêtre" id="label_tooltip_2">' +
-                      title +
-                    '</button>\
-                  </span>\
-                  <div id="tooltip_id_condition" class="hidden">\
+      return '\
+              <li class="ui-sortable-handle" style="">\
+                ET\
+                <ul class="sortable ui-sortable pos-relative">\
+                  <button class="js-tooltip like-a-link add-condition" data-tooltip-content-id="tooltip_id_condition_' + name + '" data-tooltip-title="' + title +'" data-tooltip-prefix-class="combinator" data-tooltip-close-text="x" data-tooltip-close-title="Ferme la fenêtre" id="label_tooltip_' + name + '">' + title + '</button>\
+                </ul>\
+                <div id="tooltip_id_condition_' + name + '" class="hidden">\
                     <div>\
                       <button class="like-a-link add-condition-and" onclick=\'store_trundle.dispatch({ type: "ADD_CONDITION", combination: "AND", parent_box: "' + parent_name + '" });\'>ET une nouvelle condition</button>\
                     </div>\
-                    <div>\
-                      <button class="like-a-link add-condition-or">OU une nouvelle condition</button>\
-                    </div>\
-                    <div>\
-                      <button class="like-a-link add-condition-andsub" onclick=\'store_trundle.dispatch({ type: "ADD_CONDITION", combination: "AND", parent_box: "' + name + '" });\'>regrouper avec une nouvelle sous-condition, liée par un ET</button>\
-                    </div>\
-                    <div>\
-                      <button class="like-a-link add-condition-orsub">regrouper avec une nouvelle sous-condition, liée par un OU</button>\
-                    </div>\
-                    <div>\
-                      <button class="like-a-link edit-condition">Editer cette condition</button>\
-                    </div>\
-                    <div>\
-                      <button class="like-a-link close-window">Fermer cette fenêtre</button>\
-                    </div>\
                   </div>\
-                </li>\
-              </ul>\
-      '
+              </li>\
+              '
   },
 });
 
