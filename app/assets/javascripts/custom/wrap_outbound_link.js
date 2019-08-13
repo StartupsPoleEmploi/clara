9356e3c43fc54ed5f14b7fd3871e79cd1dda8dc7
David Boureau <bdavidxyz@gmail.com>
2019-08-13 14:34:53 +0200

    done

--- app/assets/javascripts/custom/wrap_outbound_link.js
+++ app/assets/javascripts/custom/wrap_outbound_link.js
@@ -10,12 +10,17 @@ clara.js_define("wrap_outbound_link", {
 
     var THE_URL = "https://candidat.pole-emploi.fr/prestations/servicescarte";
 
-    $("a").filter(function() {
-      return this.href === THE_URL;
-    }).each(function() {
-      var actual_target = $(this).attr('target');
-      $(this).attr("onclick", "clara.track_outbound_link.please('" + THE_URL + "', '" + actual_target + "');return false;");
-    });
+    $("a")
+      .filter(function() {
+        return this.href === THE_URL;
+      })
+      .each(function() {
+        var actual_target = $(this).attr('target');
+        $(this).attr(
+          "onclick", 
+          "clara.track_outbound_link.please('" + THE_URL + "', '" + actual_target + "');return false;"
+        );
+      });
 
   }
 
