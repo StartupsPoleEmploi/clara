dc8dec7019bf44321727b0b4ac60382d070659c1
David Boureau <bdavidxyz@gmail.com>
2019-09-13 15:02:39 +0200

    separated templates

---
+++ app/view_objects/detail_why_new.rb
@@ -0,0 +1,70 @@
+
+class DetailWhyNew < ViewObject
+
+  def after_init(args)
+    locals          = hash_for(args)
+    @ability        = string_for(locals[:ability])
+    @root_condition = string_for(locals[:root_condition])
+    @root_rules     = array_for(locals[:root_rules])
+  end
+
+  def ability
+    @ability
+  end
+
+  def root_condition
+    @root_condition
+  end
+
+  def root_rules
+    if @ability == 'eligible'
+      return @root_rules.sort_by{|rule| rule[:status] == 'eligible' ? 0 : 1}
+    elsif @ability == 'ineligible'
+      return @root_rules.sort_by{|rule| rule[:status] == 'ineligible' ? 0 : 1}
+    elsif @ability == 'uncertain'
+      return @root_rules.sort_by{|rule| rule[:status] == 'uncertain' ? 0 : 1}
+    end
+    return []
+  end
+
+  def uncertain_sentence
+    result = ""
+    if @ability == 'uncertain'
+      uncertain_rules_length = number_of_uncertain_rules
+      number_of_rules = @root_rules.length
+      result = "single-alone"   if number_of_rules == 1 && uncertain_rules_length == 1
+      result = "single-amongst" if number_of_rules > 1 && uncertain_rules_length == 1 
+      result = "plural-all"     if uncertain_rules_length > 1 && number_of_rules == uncertain_rules_length
+      result = "plural"         if uncertain_rules_length > 1 && number_of_rules != uncertain_rules_length
+    end
+    return result
+  end
+
+  def eligible_sentence
+    result = ""
+    if @ability == 'eligible'
+      eligible_rules_length = number_of_eligible_rules
+      number_of_rules = @root_rules.length
+      result = "single-amongst" if number_of_rules > 1 && eligible_rules_length == 1 
+      result = "plural"         if eligible_rules_length > 1 && number_of_rules != eligible_rules_length
+    end
+    return result
+  end
+
+  def number_of_uncertain_rules
+    @root_rules.select{|rule| rule[:status] == 'uncertain'}.length
+  end
+
+  def number_of_eligible_rules
+    @root_rules.select{|rule| rule[:status] == 'eligible'}.length
+  end
+
+  def all_conditions
+    @root_rules.size > 1 && @root_rules.all? {|e| e[:status] == 'eligible'}
+  end
+
+  def no_condition
+    @root_rules.size > 1 && @root_rules.all? {|e| e[:status] == 'ineligible'}
+  end
+
+end
