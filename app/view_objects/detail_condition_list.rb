ccac0f7b750c57fe18cc8bbd8e2cbb9baa149441
David Boureau <bdavidxyz@gmail.com>
2019-09-16 14:10:46 +0200

    works!

--- app/view_objects/detail_condition_list.rb
+++ app/view_objects/detail_condition_list.rb
@@ -6,47 +6,67 @@ class DetailConditionList < ViewObject
   end
 
   def html_output
-    raw(_node_for(@ability_tree, {}))
+    res = ""
+    if @ability_tree[:slave_rules].size == 0
+      res = "il faut remplir la condition suivante : " + "<ul><li>" + _and_or(@ability_tree) + _eligibility(@ability_tree) + @ability_tree[:description] + "</li></ul>"
+    else
+      res = "il faut réunir " + _node_for(@ability_tree, {}, 0, true)
+    end
+    raw(res)
   end
 
 
-  def _node_for(ability, parent_ability)
-    res = ""
-    res = 
-    _and_or(parent_ability) + _all_or_at_least(ability) +
-    "<ul>" +
-      ability[:slave_rules].map do |sub_ability|
+  def _node_for(ability, parent_ability, i, skip_img=false)
+    _and_or(parent_ability, i) + _eligibility(ability, skip_img)  + _all_or_at_least(ability) +
+    "<ol>" +
+      ability[:slave_rules].map.with_index do |sub_ability, indx|
         if sub_ability[:composition_type].blank?
-          "<li>" + _and_or(ability) + sub_ability[:description] + "</li>"
+          "<li>" + _and_or(ability, indx) + _eligibility(sub_ability) + sub_ability[:description] + "</li>"
         else
-          "<li>" + _node_for(sub_ability, ability) + "</li>"
+          "<li>" + _node_for(sub_ability, ability, indx) +  "</li>"
         end
       end.join +
-    "</ul>"
-    res
+    "</ol>"
   end
 
 
   def _all_or_at_least(ability)
     res = ""
     if ability[:composition_type] == "and_rule"
-      res = "Toutes les conditions suivantes"
+      res = "l'ensemble des #{ability[:slave_rules].size} conditions suivantes"
     elsif ability[:composition_type] == "or_rule"
-      res = "Au moins une des conditions suivantes"
+      res = "au moins une des conditions suivantes"
     end
     res
   end
 
-  def _and_or(ability)
+  def _and_or(ability, number=-1)
     res = ""
+    # if ability[:composition_type] == "and_rule" && number == 0
+    #   res = "<strong>1) </strong>"
+    # elsif ability[:composition_type] == "and_rule"
+    #   res = "<strong>et #{number+1}) </strong>"
     if ability[:composition_type] == "and_rule"
-      res = "<strong>et </strong>"
+      res = "<strong> </strong>"
     elsif ability[:composition_type] == "or_rule"
       res = "<strong>soit </strong>"
     end
     res
   end
 
+  def _eligibility(ability, skip_img=false)
+    res = ""
+    unless skip_img
+      if ability[:ability] == "eligible"
+        res = "✅"
+      elsif ability[:ability] == "uncertain"
+        res = "<strong class='orangify'>❓</strong>"
+      elsif ability[:ability] == "ineligible"
+        res = "❌"
+      end
+    end
+    res
+  end
 
   # def after_init(args)
   #   locals     = hash_for(args)
