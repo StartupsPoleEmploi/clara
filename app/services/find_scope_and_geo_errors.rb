a708a2a9828a2c3c0040460606554f99c0b7d1ae
David Boureau <bdavidxyz@gmail.com>
2019-09-23 16:48:41 +0200

    almost done

--- app/services/find_scope_and_geo_errors.rb
+++ app/services/find_scope_and_geo_errors.rb
@@ -2,28 +2,33 @@ class FindScopeAndGeoErrors
 
   def call(trundle, geo)
     res = ""    
-    sel = geo["selection"]
-    editions = trundle.deep_find("is_editing")
+    sel = geo[:selection]
+    editions = _deep_find(:is_editing, trundle, [])
     has_complex_geo = sel == "rien_sauf" || sel == "tout_sauf"
+    
+    ap geo
+    ap editions
+    ap editions.any?
+    ap has_complex_geo && geo[:town].blank? && geo[:department].blank? && geo[:region].blank?
 
-    if has_complex_geo
-      if geo["town"].blank? && geo["department"].blank? && geo["region"].blank?
-        res = "Veuillez renseigner un critère géographique"
-      end
+    if has_complex_geo && geo[:town].blank? && geo[:department].blank? && geo[:region].blank?
+      res = "Veuillez renseigner un critère géographique"
     elsif editions.any?
       res = "Veuillez terminer l'édition d'une condition avant de valider le champ d'application"
     end
+
+    # fail "bah ah ah"
 
     res
   end
 
   # https://stackoverflow.com/a/40743132/2595513
-  def deep_find(key, object, found=[])
+  def _deep_find(key, object, found=[])
     if object.respond_to?(:key?) && object.key?(key)
       found << object[key]
     end
     if object.is_a? Enumerable
-      found << object.collect { |*a| deep_find(key, a.last) }
+      found << object.collect { |*a| _deep_find(key, a.last) }
     end
     found.flatten.compact
   end
