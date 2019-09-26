736f6b154f1a9c47890c952930b890463b174648
David Boureau <bdavidxyz@gmail.com>
2019-09-26 09:26:02 +0200

    done

--- app/assets/javascripts/custom/expand_tree.js
+++ app/assets/javascripts/custom/expand_tree.js
@@ -1,11 +1,20 @@
 clara.js_define("expand_tree", {
 
     please_if: function() {
+      this.clicked = false;
       return $(".c-expand-tree").exists();
     },
 
     please: function() {
-      console.log("hello")
+      var that = this;
+      $('.c-expand-tree').click(function(e) {
+        if (!that.clicked) {
+          if (typeof ga === "function") {
+            ga('send', 'event', 'results', 'expand', 'yes');
+            that.clicked = true;
+          }
+        }
+      });        
     }
 });
 
