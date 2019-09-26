a6b79faaf517b275163a3b546a086fb4bcef0595
David Boureau <bdavidxyz@gmail.com>
2019-09-26 09:17:04 +0200

    dedicated js for expanding tree

---
+++ app/assets/javascripts/custom/expand_tree.js
@@ -0,0 +1,11 @@
+clara.js_define("expand_tree", {
+
+    please_if: function() {
+      return $(".c-expand-tree").exists();
+    },
+
+    please: function() {
+      console.log("hello")
+    }
+});
+
