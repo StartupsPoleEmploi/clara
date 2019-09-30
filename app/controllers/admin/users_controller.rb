3565e87b7ec9d526a42d93faf6eef3de505922e0
David Boureau <bdavidxyz@gmail.com>
2019-09-30 14:12:03 +0200

    restricting, again and again...

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,6 +15,9 @@ module Admin
     #   User.find_by!(slug: param)
     # end
 
+    before_action :require_superadmin
+
+
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
     def valid_action?(name, resource = resource_class)
