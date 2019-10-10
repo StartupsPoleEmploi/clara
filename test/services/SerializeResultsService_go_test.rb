9687ac2b95ea7fe954383a76b4619cd0ca1eb10d
David Boureau <bdavidxyz@gmail.com>
2019-10-10 15:49:16 +0200

    green unit tests

--- test/services/SerializeResultsService_go_test.rb
+++ test/services/SerializeResultsService_go_test.rb
@@ -17,7 +17,7 @@ class SerializeResultsServiceGoTest < ActiveSupport::TestCase
         :flat_all_ineligible=>"every_ineligible", 
         :flat_all_contract=>[{"contract_types"=>42}], 
         :flat_all_filter=>[{"id"=> 42}], 
-        :asker=>{"v_handicap"=>"oui", "v_spectacle"=>"oui", "v_cadre"=>"non", "v_diplome"=>"niveau_4", "v_category"=>"cat_12345", "v_duree_d_inscription"=>"moins_d_un_an", "v_allocation_value_min"=>"434", "v_allocation_type"=>"RPS_RFPA_RFF_pensionretraite", "v_qpv"=>nil, "v_zrr"=>"oui", "v_age"=>"33", "v_location_label"=>nil, "v_location_route"=>nil, "v_location_city"=>nil, "v_location_country"=>nil, "v_location_zipcode"=>nil, "v_location_citycode"=>"02004", "v_location_street_number"=>nil, "v_location_state"=>nil}
+        :asker=>{"v_inscrit"=>"oui", "v_handicap"=>"oui", "v_spectacle"=>"oui", "v_cadre"=>"non", "v_diplome"=>"niveau_4", "v_category"=>"cat_12345", "v_duree_d_inscription"=>"moins_d_un_an", "v_allocation_value_min"=>"434", "v_allocation_type"=>"RPS_RFPA_RFF_pensionretraite", "v_qpv"=>nil, "v_zrr"=>"oui", "v_age"=>"33", "v_location_label"=>nil, "v_location_route"=>nil, "v_location_city"=>nil, "v_location_country"=>nil, "v_location_zipcode"=>nil, "v_location_citycode"=>"02004", "v_location_street_number"=>nil, "v_location_state"=>nil}
       }, 
       res
     )
@@ -34,6 +34,7 @@ class SerializeResultsServiceGoTest < ActiveSupport::TestCase
   def asker
     Asker.new({
                   "v_handicap" => "oui",
+                  "v_inscrit" => "oui",
                  "v_spectacle" => "oui",
                      "v_cadre" => "non",
                    "v_diplome" => "niveau_4",
