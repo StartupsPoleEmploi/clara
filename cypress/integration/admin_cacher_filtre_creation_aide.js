edf5302e354572e11cd1d62d9f30cd7899895e0d
David Boureau <bdavidxyz@gmail.com>
2019-10-11 14:18:15 +0200

    icky, but works

--- cypress/integration/admin_cacher_filtre_creation_aide.js
+++ cypress/integration/admin_cacher_filtre_creation_aide.js
@@ -1,23 +1,25 @@
 describe("Cacher les filtres inutiles à la création de l'aide pour un simple admin", function() {
-  before('On peut se connecter comme simple admin', function() {
-    cy.visit('/sign_in')
-    cy.get('#session_email')
-      .type('admin@clara.com').should('have.value', 'admin@clara.com')
-    cy.get('#session_password')
-      .type('foo').should('have.value', 'foo')
-    cy.get('.c-login-connect input').click()
-    cy.location('pathname').should('include', '/admin')
-    cy.get('body').should('have.attr', 'data-path', 'admin_aids_path')
-  })
-  it("Comme simple admin on a accès à la création d'aide", function() {
+  it("lors de la création", function() {
+    cy.connect_as_simple_admin()
     cy.visit('/admin/aids/new')
+    cy.stage_3_has_few_fields()
   })
-  it("En cliquant sur l'étape 3, on ne voit que 2 champs : résumé et filtres de pages de résultats", function() {
-    cy.get('#label_tab_3').click()
-    cy.get('#tab_3 .field-unit').should('have.length', 2)
-    cy.get('#tab_3 .field-unit .field-unit__label label').first().should('have.text', "Résumé")
-    cy.get('#tab_3 .field-unit .field-unit__label label').last().should('have.text', "Filtres sur la page de résultat")
-
+  it("lors de la modification", function() {
+    cy.connect_as_simple_admin()
+    cy.visit('/admin/aids/erasmus/edit')
+    cy.stage_3_has_few_fields()
+  })
+})
+describe("Montrer les filtres inutiles de l'aide pour un super-admin", function() {
+  it("lors de la création", function() {
+    cy.connect_as_super_admin()
+    cy.visit('/admin/aids/new')
+    cy.stage_3_has_all_fields()
+  })
+  it("lors de la modification", function() {
+    cy.connect_as_super_admin()
+    cy.visit('/admin/aids/erasmus/edit')
+    cy.stage_3_has_all_fields()
   })
 })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,44 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('connect_as_simple_admin', () => { 
+    cy.visit('/conditions-generales-d-utilisation')
+    cy.clearCookies()
+    cy.visit('/sign_in')
+    cy.get('#session_email')
+      .type('admin@clara.com').should('have.value', 'admin@clara.com')
+    cy.get('#session_password')
+      .type('foo').should('have.value', 'foo')
+    cy.get('.c-login-connect input').click()
+    cy.location('pathname').should('include', '/admin')
+    cy.get('body').should('have.attr', 'data-path', 'admin_aids_path')
+ });
+Cypress.Commands.add('stage_3_has_all_fields', () => { 
+    cy.get('#label_tab_3').click()
+    cy.get('#tab_3 .field-unit').should('have.length', 4)
+    cy.get('#tab_3 .field-unit .field-unit__label label').eq(0).should('have.text', "Résumé")
+    cy.get('#tab_3 .field-unit .field-unit__label label').eq(1).should('have.text', "Filtres sur la page de résultat")
+    cy.get('#tab_3 .field-unit .field-unit__label label').eq(2).should('have.text', "Filtres personnalisés")
+    cy.get('#tab_3 .field-unit .field-unit__label label').eq(3).should('have.text', "Filtres par besoins")
+ });
+Cypress.Commands.add('stage_3_has_few_fields', () => { 
+    cy.get('#label_tab_3').click()
+    cy.get('#tab_3 .field-unit').should('have.length', 2)
+    cy.get('#tab_3 .field-unit .field-unit__label label').first().should('have.text', "Résumé")
+    cy.get('#tab_3 .field-unit .field-unit__label label').last().should('have.text', "Filtres sur la page de résultat")
+ });
+Cypress.Commands.add('connect_as_super_admin', () => { 
+    cy.visit('/conditions-generales-d-utilisation')
+    cy.clearCookies()
+    cy.visit('/sign_in')
+    cy.get('#session_email')
+      .type('superadmin@clara.com').should('have.value', 'superadmin@clara.com')
+    cy.get('#session_password')
+      .type('foo').should('have.value', 'foo')
+    cy.get('.c-login-connect input').click()
+    cy.location('pathname').should('include', '/admin')
+    cy.get('body').should('have.attr', 'data-path', 'admin_aids_path')
+ });
+Cypress.Commands.add('disconnect_from_admin', () => { 
+    cy.get('.js-sign-out').click()
+ });
