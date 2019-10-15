55fed50fceb4a51ce74724bf97413d5ff6b28668
David Boureau <bdavidxyz@gmail.com>
2019-10-15 16:20:19 +0200

    should work?

--- docker/scripts/restore_db_dev.sh
+++ docker/scripts/restore_db_dev.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-docker exec clara_db pg_restore --verbose --clean -x -h localhost -S ara -d ara_dev /home/dumped/latest.dump
+docker exec clara_db pg_restore --verbose --clean -x -h localhost -S ara -d ara_dev ../db/r7.dumped
