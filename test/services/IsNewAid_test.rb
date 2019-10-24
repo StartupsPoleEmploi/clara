ab13081c0c168b809375de5d858771084c7dd38b
Rémy Marronnier <rmarronnier@gmail.com>
2019-10-24 18:03:19 +0200

    Specs fix

--- test/services/IsNewAid_test.rb
+++ test/services/IsNewAid_test.rb
@@ -3,28 +3,36 @@ require "test_helper"
 class IsNewAidTest < ActiveSupport::TestCase
   
 test "Newly created aid input in IsNewAid call returns true" do
+  #given
   new_aid = Aid.new(
     :name => "test",
     :what => "test")
-    res = IsNewAid.new.call(new_aid)
-    assert_equal(true, res)
+  #when
+  res = IsNewAid.new.call(new_aid)
+  #then
+  assert_equal(true, res)
 end
 
 test "Updated aid input in IsNewAid call returns false" do
+  #given
   new_aid2 = Aid.new(
     :name => "test",
     :what => "test")
-    new_aid2.what = "description updated"
-    new_aid2.save
-    res = IsNewAid.new.call(new_aid2)
-    res = new_aid2.created_at
-    assert_equal(false, res)
+  new_aid2.updated_at = Date.new
+  #when
+  res = IsNewAid.new.call(new_aid2)
+  #then
+  assert_equal(false, res)
 end
 
 test "Wrong input in IsNewAid call raises error" do
-not_an_aid = "Je ne suis pas une aide"
-# IsNewAid.new.call(not_an_aid)
-    expect { raise ArgumentError.new("Only aid is allowed") }.to raise_error(ArgumentError.new("Only aid is allowed"))
+  #given
+  not_an_aid = "Je ne suis pas une aide"
+  #then
+  assert_raises ArgumentError do
+    #when
+    IsNewAid.new.call(not_an_aid)
+  end
 end
 
 end
