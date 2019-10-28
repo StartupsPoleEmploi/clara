da4169979e8cba60e6f6a7e0fa0fbe87a817e097
David Boureau <bdavidxyz@gmail.com>
2019-10-28 17:42:06 +0100

    added step to have some possible errors

--- app/controllers/admin/aid_creation_controller.rb
+++ app/controllers/admin/aid_creation_controller.rb
@@ -14,5 +14,22 @@ module Admin
       }      
     end
 
+    # from https://github.com/thoughtbot/administrate/blob/master/app/controllers/administrate/application_controller.rb
+    def create_aid_stage_1
+      resource = resource_class.new(resource_params)
+      authorize_resource(resource)
+
+      if resource.save
+        redirect_to(
+          [namespace, resource],
+          notice: translate_with_resource("create.success"),
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource),
+        }
+      end     
+    end
+
   end
 end
