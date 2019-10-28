e05e7ab296cbbf2602d13219420da477b57568f6
David Boureau <bdavidxyz@gmail.com>
2019-10-28 17:28:21 +0100

    error field etc ok

--- app/view_objects/new_aid_one.rb
+++ app/view_objects/new_aid_one.rb
@@ -3,6 +3,7 @@ class NewAidOne < ViewObject
   def after_init(args)
     locals = hash_for(args)
     @page = locals[:page]
+    @errors_h = _init_errors_messages(@page)
   end
 
   def attr_name(attribute)
@@ -17,6 +18,19 @@ class NewAidOne < ViewObject
   def mandatory_field?(attribute)
     name = attr_name(attribute)
     ["name", "contract_type", "ordre_affichage"].include?(name)
+  end
+
+  def errored?(attribute)
+    actual_attr = attribute.attribute
+    @errors_h.key?(actual_attr) && @errors_h[actual_attr].size > 0
+  end
+
+  def _init_errors_messages(page)
+    res_h = page.resource.errors.messages
+    res_h.transform_keys! do |key|
+      key.to_s.end_with?("_id") ? key.to_s.chars.first(key.size - 3).join.to_sym : key
+    end
+    res_h
   end
 
 end
