0622e4cdefd94db4ab3e1ff33ee13fb9e7470a12
Rémy Marronnier <rmarronnier@gmail.com>
2019-10-28 17:03:11 +0100

    Removed unused functions

--- app/assets/javascripts/lodash/lodash_extension.js
+++ app/assets/javascripts/lodash/lodash_extension.js
@@ -1,46 +1,27 @@
 _.mixin({
 
-  // See https://gist.github.com/Yimiprod/7ee176597fef230d1451
-  objDiff: function(object, base) {
-    function changes(object, base) {
-      return _.transform(object, function(result, value, key) {
-        if (!_.isEqual(value, base[key])) {
-          result[key] = (_.isObject(value) && _.isObject(base[key])) ? changes(value, base[key]) : value;
-        }
-      });
-    }
-    return changes(object, base);
-  },
-
-  toPercentage: function(portion, total) {
-    if (_.every(arguments, _.isNumber)) {
-      return ((portion/total) * 100).toFixed(1) + '%'
-    }
-    return ''
-  },
-
-  none: function() {
+  none: function () {
     return !_.some.apply(_, arguments);
   },
 
-  isNotEmpty: function() {
+  isNotEmpty: function () {
     return _.negate(_.isEmpty).apply(_, arguments)
   },
 
-  count: function() {
+  count: function () {
     return _.defaultTo(_.countBy.apply(_, arguments)[true], 0);
   },
 
-  isNotBlank: function() {
+  isNotBlank: function () {
     return _.negate(_.isBlank).apply(_, arguments)
   },
 
   // https://stackoverflow.com/a/15643382/2595513
-  findNested: function(obj, key, memo) {
+  findNested: function (obj, key, memo) {
     var i,
-        proto = Object.prototype,
-        ts = proto.toString,
-        hasOwn = proto.hasOwnProperty.bind(obj);
+      proto = Object.prototype,
+      ts = proto.toString,
+      hasOwn = proto.hasOwnProperty.bind(obj);
 
     if ('[object Array]' !== ts.call(memo)) memo = [];
 
@@ -57,7 +38,7 @@ _.mixin({
     return memo;
   },
 
-  deepSearch: function(object, key, predicate) {
+  deepSearch: function (object, key, predicate) {
     if (object && object.hasOwnProperty(key) && predicate(key, object[key]) === true) return object
 
     for (var i = 0; i < _.keys(object).length; i++) {
@@ -69,7 +50,7 @@ _.mixin({
     return null
   },
 
-  isBlank: function(value) {
+  isBlank: function (value) {
     if (_.isNumber(value)) {
       return value.toString() === "0" || value.toString() === "NaN";
     } else if (_.isString(value)) {
@@ -80,25 +61,9 @@ _.mixin({
       return _.isEmpty(value);
     }
   },
-  slugify: function(str) {
-    res = "";
-    if(_.isString(str)) {
-      str = str.replace(/^\s+|\s+$/g, ''); // trim
-      str = str.toLowerCase();
-      
-      str = _.deburr(str);
-
-      str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-          .replace(/\s+/g, '-') // collapse whitespace and replace by -
-          .replace(/-+/g, '-'); // collapse dashes
-
-      res = str;
-    }
-    return res;
-  },
-  insertAt: function(array, index, item) {
+  insertAt: function (array, index, item) {
     if (_.isArray(array) && _.isInteger(index)) {
-      array.splice( index, 0, item );
+      array.splice(index, 0, item);
     }
   },
 
