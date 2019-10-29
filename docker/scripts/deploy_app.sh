7393fb6e760738170a0b7784633fbf5c37d0bfa4
David Boureau <bdavidxyz@gmail.com>
2019-10-29 14:53:28 +0100

    [1410b] essai r7 dockerisée

--- docker/scripts/deploy_app.sh
+++ docker/scripts/deploy_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker exec -w /var/git/ara.git clara_app rm -rf /var/www/ara/shared/tmp/
-docker exec -w /var/git/ara.git clara_app  bundle exec mina production2 setup 
-docker exec -w /var/git/ara.git clara_app  bundle exec mina production2 deploy 
+docker-compose exec -w /var/git/ara.git srv_app rm -rf /var/www/ara/shared/tmp/
+docker-compose exec -w /var/git/ara.git srv_app bundle exec mina production2 setup 
+docker-compose exec -w /var/git/ara.git srv_app bundle exec mina production2 deploy 
