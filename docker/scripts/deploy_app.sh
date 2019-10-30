4f6c6b7c2be84aa9e738a9a128b29f2a2010f59d
David Boureau <bdavidxyz@gmail.com>
2019-10-30 09:12:01 +0100

    [1410l]

--- docker/scripts/deploy_app.sh
+++ docker/scripts/deploy_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker-compose exec -w /var/git/ara.git srv_app rm -rf /var/www/ara/shared/tmp/
+docker-compose exec -w /var/git/ara.git srv_app bundle install
 docker-compose exec -w /var/git/ara.git srv_app bundle exec mina production2 setup 
 docker-compose exec -w /var/git/ara.git srv_app bundle exec mina production2 deploy 
