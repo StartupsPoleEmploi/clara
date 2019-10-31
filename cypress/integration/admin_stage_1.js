d70b371e4a879b203cac472f106c2f6e8c9406a8
David Boureau <bdavidxyz@gmail.com>
2019-10-31 16:53:29 +0100

    new tests

--- cypress/integration/admin_stage_1.js
+++ cypress/integration/admin_stage_1.js
@@ -16,12 +16,39 @@ describe("Étape 1", function() {
       cy.get('.field-unit').should('have.length', 4)
     })
     it("Tous les champs sont vides", function() {
-      cy.get('input#aid_name').should('have.value', '')
-      cy.get('select#aid_contract_type_id').should('have.value', '')
-      cy.get('input#aid_ordre_affichage').should('have.value', '')
-      cy.get('textarea#aid_source').should('have.value', '')
+      cy.get('.field-unit input#aid_name')             .should('have.value', '')
+      cy.get('.field-unit select#aid_contract_type_id').should('have.value', '')
+      cy.get('.field-unit input#aid_ordre_affichage')  .should('have.value', '')
+      cy.get('.field-unit textarea#aid_source')        .should('have.value', '')
     })
     it("si on valide directement, on a 3 champs en erreur", function() {
+      cy.get('button.c-newaid-actionrecord').click()
+      cy.get('.field-unit--errored-true input#aid_name')             .should('have.length', 1)
+      cy.get('.field-unit--errored-true select#aid_contract_type_id').should('have.length', 1)
+      cy.get('.field-unit--errored-true input#aid_ordre_affichage')  .should('have.length', 1)
+      cy.get('.field-unit--errored-false textarea#aid_source')        .should('have.length', 1)
+    })
+    it("si on renseigne le contract_type, on a 2 champs en erreur", function() {
+      // Given
+      cy.get('select#aid_contract_type_id').select('1')
+      // When
+      cy.get('button.c-newaid-actionrecord').click()
+      // Then
+      cy.get('.field-unit--errored-true input#aid_name')             .should('have.length', 1)
+      cy.get('.field-unit--errored-false select#aid_contract_type_id').should('have.length', 1)
+      cy.get('.field-unit--errored-true input#aid_ordre_affichage')  .should('have.length', 1)
+      cy.get('.field-unit--errored-false textarea#aid_source')        .should('have.length', 1)
+    })
+    it("si on renseigne l'ordre d'affichage en plus, on a 1 champs en erreur", function() {
+      // Given
+      cy.get('#aid_ordre_affichage').type('42')
+      // When
+      cy.get('button.c-newaid-actionrecord').click()
+      // Then
+      cy.get('.field-unit--errored-true input#aid_name')             .should('have.length', 1)
+      cy.get('.field-unit--errored-false select#aid_contract_type_id').should('have.length', 1)
+      cy.get('.field-unit--errored-false input#aid_ordre_affichage')  .should('have.length', 1)
+      cy.get('.field-unit--errored-false textarea#aid_source')        .should('have.length', 1)
     })
   })
   
