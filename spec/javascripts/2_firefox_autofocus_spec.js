f6fef9cdc2b00a2ab61f1612467cc467ea4210ca
Rémy Marronnier <rmarronnier@gmail.com>
2019-10-31 17:41:52 +0100

    beforeEach

--- spec/javascripts/2_firefox_autofocus_spec.js
+++ spec/javascripts/2_firefox_autofocus_spec.js
@@ -1,13 +1,17 @@
-describe('firefox_autofocus.js', function() {
+//= require custom/autofocus_firefox
+describe('autofocus_firefox.js', function() {
+
+  beforeEach(function(){
+    $(document.body).append("<form><input></input><input></input></form>")
+    $("input:first").addClass("without_autofocus c-field")
+    $("input:last").addClass("with_autofocus c-field")
+    $("input.with_autofocus").attr('autofocus', 'autofocus');
+  });
     it("Should not focus on input without autofocus", function() {
-      $(document.body).append("<form><input></input></form>")
-      $("input").addClass("without_autofocus")
       expect($('input.without_autofocus').is(":focus")).toEqual(false)
     });
     it("Should focus on input with autofocus", function() {
-      $(document.body).append("<form><input></input></form>")
-      $("input").addClass("with_autofocus")
-      $("input.with_autofocus").attr('autofocus', 'autofocus');
+      //expect($('input.with_autofocus').hasAttribute("autofocus")).toEqual(true)
       expect($('input.with_autofocus').is(":focus")).toEqual(true)
     });
 });
