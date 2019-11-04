ba090f61271e48376a9119fbf01cf4fd4b0d0b36
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-04 10:29:36 +0100

    wip

--- spec/javascripts/2_firefox_autofocus_spec.js
+++ spec/javascripts/2_firefox_autofocus_spec.js
@@ -1,17 +1,17 @@
 //= require custom/autofocus_firefox
-describe('autofocus_firefox.js', function() {
-
-  beforeEach(function(){
+describe('autofocus_firefox.js', function () {
+  beforeEach(function () {
     $(document.body).append("<form><input></input><input></input></form>")
     $("input:first").addClass("without_autofocus c-field")
     $("input:last").addClass("with_autofocus c-field")
     $("input.with_autofocus").attr('autofocus', 'autofocus');
   });
-    it("Should not focus on input without autofocus", function() {
-      expect($('input.without_autofocus').is(":focus")).toEqual(false)
-    });
-    it("Should focus on input with autofocus", function() {
-      //expect($('input.with_autofocus').hasAttribute("autofocus")).toEqual(true)
-      expect($('input.with_autofocus').is(":focus")).toEqual(true)
-    });
+  it("Should not focus on input without autofocus", function () {
+    expect($('input.without_autofocus').is(":focus")).toEqual(false)
+  });
+  it("Should focus on input with autofocus", function () {
+    clara.autofocus_firefox.please(my_function)
+    //expect($('input.with_autofocus').hasAttribute("autofocus")).toEqual(true)
+    expect($('input.with_autofocus').is(":focus")).toEqual(true)
+  });
 });
