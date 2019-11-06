2ba87b2c4319b99950717ab1347e678d0728236a
David Boureau <bdavidxyz@gmail.com>
2019-11-06 11:17:26 +0100

    working functional testing, yet uncomplete

---
+++ cypress/integration/admin_stage_2.js
@@ -0,0 +1,52 @@
+describe("Étape 2", function() {
+
+  before(function() {
+    cy.connect_as_super_admin()
+  })
+
+  after(function() {
+    cy.visit('/admin/aids?aid%5Bdirection%5D=desc&aid%5Border%5D=updated_at')
+    cy.get('a.js-delete-aid[href="/admin/aids/test-stage-2?locale=fr"]').click()
+  })
+
+  before(function() {
+    cy.visit('/admin/aid_creation/new_aid_stage_1')
+  })
+
+  it("si on vient de créer une aide, on arrive sur un écran avec 5 champs éditables, tous vides", function() {
+    cy.get('.field-unit input#aid_name').type('test stage 2')
+    cy.get('.field-unit select#aid_contract_type_id').select("1")
+    cy.get('.field-unit input#aid_ordre_affichage').type(42)
+
+    cy.get('button.c-newaid-actionrecord').click()
+
+    cy.location().should((loc) => {expect(loc.pathname).to.eq('/admin/aid_creation/new_aid_stage_2')})
+
+    cy.get('.tooltip__closetext__container').click()
+
+    cy.get('.field-unit--ckeditor').should('have.length', 5)
+    cy.get('.field-unit--ckeditor .cke_contents').eq(0).should('have.text', "Press ALT 0 for help")
+    cy.get('.field-unit--ckeditor .cke_contents').eq(1).should('have.text', "Press ALT 0 for help")
+    cy.get('.field-unit--ckeditor .cke_contents').eq(2).should('have.text', "Press ALT 0 for help")
+    cy.get('.field-unit--ckeditor .cke_contents').eq(3).should('have.text', "Press ALT 0 for help")
+    cy.get('.field-unit--ckeditor .cke_contents').eq(4).should('have.text', "Press ALT 0 for help")
+
+  })
+
+  it("on peut renseigner une description, elle se mets à jour en temps réel", function() {
+
+    cy.get('#accordion_0-0_tab').click()
+    cy.window().then(win => {win.CKEDITOR.instances["aid_what"].setData("<p>Ma description</p>");});
+    cy.get('.js-what').contains('Ma description')
+  })
+  
+  it("on peut renseigner des conditions à remplir, avec mise à jour en temps réel", function() {
+
+    cy.get('#accordion_0-1_tab').click()
+    cy.window().then(win => {win.CKEDITOR.instances["aid_additionnal_conditions"].setData("<p>Mes conditions à remplir</p>");});
+    cy.get('.js-additionnal-conditions').contains('Mes conditions à remplir')
+  })
+
+
+})
+
