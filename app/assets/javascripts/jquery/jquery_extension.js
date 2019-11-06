0a7f89e2029d9924c4618274a88eedb992fff7e5
David Boureau <bdavidxyz@gmail.com>
2019-11-06 09:40:13 +0100

    better replaceTag

--- app/assets/javascripts/jquery/jquery_extension.js
+++ app/assets/javascripts/jquery/jquery_extension.js
@@ -38,6 +38,26 @@ $.currentUrl = function () {
 }
 
 
+$.replaceTag = function (currentElem, newTagObj, keepProps) {
+  var $currentElem = $(currentElem);
+  var i, $newTag = $(newTagObj).clone();
+  if (keepProps) {
+      newTag = $newTag[0];
+      newTag.className = currentElem.className;
+      $.extend(newTag.classList, currentElem.classList);
+      $.extend(newTag.attributes, currentElem.attributes);
+  }
+  $currentElem.wrapAll($newTag);
+  $currentElem.contents().unwrap();
+  return this;
+}
+
+$.extend({
+  // See https://stackoverflow.com/a/20469901/2595513
+  
+});
+
+
 jQuery.fn.extend({
   // See https://stackoverflow.com/a/20469901/2595513
   replaceTag: function (newTagObj, keepProps) {
