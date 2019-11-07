80897432d3e5c9a7f094ea7feed1b5e3561bdad5
David Boureau <bdavidxyz@gmail.com>
2019-11-07 11:36:30 +0100

    done, with tests

---
+++ app/assets/javascripts/administrate/custom/admin_steps/admin_stage_2.js
@@ -0,0 +1,12 @@
+clara.js_define("admin_stage_2", {
+
+    please_if: function() {
+      return $(".c-newaid--stage2").exists();
+    },
+
+    please: function() {
+      $("#accordion_0-0_tab").click();
+    }
+
+});
+
--- cypress/integration/admin_stage_2.js
+++ cypress/integration/admin_stage_2.js
@@ -13,17 +13,22 @@ describe("Étape 2", function() {
     cy.visit('/admin/aid_creation/new_aid_stage_1')
   })
 
-  it("si on vient de créer une aide, on arrive sur un écran avec 5 champs éditables, tous vides", function() {
+  it("si on vient de créer une aide, on arrive sur un écran avec 5 champs éditables, tous vides, le premier est déplié", function() {
+    // GIVEN
     cy.get('.field-unit input#aid_name').type('test stage 2')
     cy.get('.field-unit select#aid_contract_type_id').select("1")
     cy.get('.field-unit input#aid_ordre_affichage').type(42)
-
+    
+    // WHEN
     cy.get('button.c-newaid-actionrecord').click()
-
+    
+    // THEN
     cy.location().should((loc) => {expect(loc.pathname).to.eq('/admin/aid_creation/new_aid_stage_2')})
 
+    // close notice
     cy.get('.tooltip__closetext__container').click()
 
+    // Should have 5 empty editors
     cy.get('.field-unit--ckeditor').should('have.length', 5)
     cy.get('.field-unit--ckeditor .cke_contents').eq(0).should('have.text', "Press ALT 0 for help")
     cy.get('.field-unit--ckeditor .cke_contents').eq(1).should('have.text', "Press ALT 0 for help")
@@ -31,6 +36,8 @@ describe("Étape 2", function() {
     cy.get('.field-unit--ckeditor .cke_contents').eq(3).should('have.text', "Press ALT 0 for help")
     cy.get('.field-unit--ckeditor .cke_contents').eq(4).should('have.text', "Press ALT 0 for help")
 
+    // First must be expanded
+    cy.get('#accordion_0-0_tab').should('have.attr', 'aria-expanded', 'true')
   })
 
   it("on peut renseigner une description, elle se mets à jour en temps réel", function() {
