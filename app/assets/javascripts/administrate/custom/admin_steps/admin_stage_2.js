4974fefca6df5f6cfda2e38cf30d5efeb09ef93c
David Boureau <bdavidxyz@gmail.com>
2019-11-07 13:59:01 +0100

    commented

--- app/assets/javascripts/administrate/custom/admin_steps/admin_stage_2.js
+++ app/assets/javascripts/administrate/custom/admin_steps/admin_stage_2.js
@@ -5,7 +5,10 @@ clara.js_define("admin_stage_2", {
     },
 
     please: function() {
+      // unfold description field
       $("#accordion_0-0_tab").click();
+
+      // give focus to description field
       setTimeout(function(){CKEDITOR.instances["aid_what"].focus()}, 500)
     }
 
