8b783ed7a6c28d01d8aef9dca474c36815dcf089
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-08 00:13:08 +0100

    More tests

--- test/services/HydrateAddress_test.rb
+++ test/services/HydrateAddress_test.rb
@@ -3,26 +3,26 @@ require "test_helper"
 class HydrateAddressnTest < ActiveSupport::TestCase
   def asker_attributes_test
     {
-          "v_handicap" => "non",
-          "v_spectacle" => "oui",
-          "v_cadre" => "non",
-          "v_diplome" => "niveau_1",
-          "v_category" => nil,
-          "v_duree_d_inscription" => "non_inscrit",
-          "v_allocation_value_min" => "43",
-          "v_allocation_type" => "ASS_AER_APS_AS-FNE",
-          "v_qpv" => nil,
-          "v_zrr" => nil,
-          "v_age" => "54",
-          "v_location_label" => nil,
-          "v_location_route" => nil,
-          "v_location_city" => nil,
-          "v_location_country" => nil,
-          "v_location_zipcode" => nil,
-          "v_location_citycode" => "44047",
-          "v_location_street_number" => nil,
-          "v_location_state" => nil,
-        }
+      "v_handicap" => "non",
+      "v_spectacle" => "oui",
+      "v_cadre" => "non",
+      "v_diplome" => "niveau_1",
+      "v_category" => nil,
+      "v_duree_d_inscription" => "non_inscrit",
+      "v_allocation_value_min" => "43",
+      "v_allocation_type" => "ASS_AER_APS_AS-FNE",
+      "v_qpv" => nil,
+      "v_zrr" => nil,
+      "v_age" => "54",
+      "v_location_label" => nil,
+      "v_location_route" => nil,
+      "v_location_city" => nil,
+      "v_location_country" => nil,
+      "v_location_zipcode" => nil,
+      "v_location_citycode" => "44047",
+      "v_location_street_number" => nil,
+      "v_location_state" => nil,
+    }
   end
 
   test ".call should return an Asker instance" do
@@ -44,5 +44,34 @@ class HydrateAddressnTest < ActiveSupport::TestCase
     result = HydrateAddress.new
     #then
     assert_raises("Arguments must be attributes of an Asker") { result.call(asker_attributes_hash) }
+  end
+  test ".call should return unchanged Asker instance if v_location_citycode.blank?" do
+    #given
+    asker_attributes_hash = self.asker_attributes_test
+    asker_attributes_hash.v_location_citycode = nil
+    asker_test = Asker.new(asker_attributes_hash)
+    #when
+    result = HydrateInscription.new.call(asker_attributes_hash)
+    #then
+    assert_same(asker_test, result)
+  end
+  test ".call should return unchanged Asker instance if v_location_zipcode.present?" do
+    #given
+    asker_attributes_hash = self.asker_attributes_test
+    asker_attributes_hash.v_location_zipcode = 25454
+    asker_test = Asker.new(asker_attributes_hash)
+    #when
+    result = HydrateInscription.new.call(asker_attributes_hash)
+    #then
+    assert_same(asker_test, result)
+  end
+  test ".call should return Asker instance with new v_zrr value " do
+    #given
+    asker_attributes_hash = self.asker_attributes_test
+    zrr_test = IsZrr.new.call(asker_attributes_hash.v_location_citycode)
+    #when
+    result = HydrateInscription.new.call(asker_attributes_hash)
+    #then
+    assert_equal(zrr_test, result.v_location_citycode)
   end
 end
