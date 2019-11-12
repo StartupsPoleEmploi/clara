8721f7c5a97313ef157f2db1d1ea2d357248c61e
David Boureau <bdavidxyz@gmail.com>
2019-11-12 11:30:10 +0100

    ok done

--- spec/javascripts/0b_jquery_extension_spec.js
+++ spec/javascripts/0b_jquery_extension_spec.js
@@ -65,95 +65,45 @@ describe("jQuery extension", function () {
     });
   });
 
-
-
-  // describe("$.urlParam", function () {
-  //   it("Should return null when url contains no parameter", function () {
-  //     //given
-  //     spyOn($, "currentUrl").and.callFake(function(){return "http://clara.com"});
-  //     var name = ""
-  //     //when
-  //     var res = $.urlParam(name)
-  //     //then
-  //     expect(res).toEqual(null)
-  //   });
-  //   // it("Should return blank string when url parameter has no value", function () {
-  //   //   //given
-  //   //   spyOn($, "currentUrl").and.callFake(function(){return "http://clara.com?empty_param"});
-  //   //   var name = ""
-  //   //   //when
-  //   //   var res = $.urlParam(name)
-  //   //   //then
-  //   //   expect(res).toEqual("")
-  //   // });
-  //   // it("Should return parameter value when url parameter has a value (example 1)", function () {
-  //   //   //given
-  //   //   spyOn($, "currentUrl").and.callFake(function(){return "http://clara.com?filled_param=filled_value"});
-  //   //   var name = ""
-  //   //   //when
-  //   //   var res = $.urlParam(name)
-  //   //   //then
-  //   //   expect(res).toEqual("filled_value")
-  //   // });
-  //   // it("Should return parameter value when url parameter has a value (example 2)", function () {
-  //   //   //given
-  //   //   spyOn($, "currentUrl").and.callFake(function(){return "http://clara.com?named_parameter=param_value"});
-  //   //   var name = "named_parameter"
-  //   //   //when
-  //   //   var example_1 = $.urlParam(name)
-  //   //   //then
-  //   //   expect(example_1).toEqual("param_value")
-  //   // });
-  //   // it("Should return url parameter value when name given corresponds among several parameters", function () {
-  //   //   //given
-  //   //   spyOn($, "currentUrl").and.callFake(function(){return "http://clara.com?a=b&c=d&e=f&g=h"});
-  //   //   var name = "c"
-  //   //   //when
-  //   //   var res = $.urlParam(name)
-  //   //   //then
-  //   //   expect(res).toEqual("d")
-  //   // });
-  // });
-
-    describe('$.hasClasses', function () {
-      beforeEach(function () {
-        $(document.body).append('\
-          <div id="tested_div" class="aaa bbb ccc">\
-        ')
-      });
-      afterEach(function () {
-        $('#tested_div').remove()
-      });
-      it("Should return true if given element has all classes", function () {
-        expect($('#tested_div').hasClasses("aaa", "bbb", "ccc")).toEqual(true)
-      });
-      it("Should return true if given element has all classes (random order)", function () {
-        expect($('#tested_div').hasClasses("ccc", "bbb", "aaa")).toEqual(true)
-      });
-      it("Should return true if given element has some (but not all) classes", function () {
-        expect($('#tested_div').hasClasses("bbb", "aaa")).toEqual(true)
-      });
-      it("Should return true if given element has the only given class", function () {
-        expect($('#tested_div').hasClasses("bbb")).toEqual(true)
-      });
-      it("Should return false if none of the given classes are present", function () {
-        expect($('#tested_div').hasClasses("xxx", "yyy", "zzz")).toEqual(false)
-      });
-      it("Should return false if the given class is not present", function () {
-        expect($('#tested_div').hasClasses("yyy")).toEqual(false)
-      });
-      it("Should return false if one of the given class is not present", function () {
-        expect($('#tested_div').hasClasses("ccc", "bbb", "zzz", "aaa")).toEqual(false)
-      });
-      it("Should return false if wrong param (a Date) is given", function () {
-        expect($('#tested_div').hasClasses(new Date(123456789))).toEqual(false)
-      });
-      it("Should return false if undefined is given", function () {
-        expect($('#tested_div').hasClasses(undefined)).toEqual(false)
-      });
-      it("Should return false if empty String is given", function () {
-        expect($('#tested_div').hasClasses(" ")).toEqual(false)
-      });
+  describe('$.hasClasses', function () {
+    beforeEach(function () {
+      $(document.body).append('\
+        <div id="tested_div" class="aaa bbb ccc">\
+      ')
     });
+    afterEach(function () {
+      $('#tested_div').remove()
+    });
+    it("Should return true if given element has all classes", function () {
+      expect($('#tested_div').hasClasses("aaa", "bbb", "ccc")).toEqual(true)
+    });
+    it("Should return true if given element has all classes (random order)", function () {
+      expect($('#tested_div').hasClasses("ccc", "bbb", "aaa")).toEqual(true)
+    });
+    it("Should return true if given element has some (but not all) classes", function () {
+      expect($('#tested_div').hasClasses("bbb", "aaa")).toEqual(true)
+    });
+    it("Should return true if given element has the only given class", function () {
+      expect($('#tested_div').hasClasses("bbb")).toEqual(true)
+    });
+    it("Should return false if none of the given classes are present", function () {
+      expect($('#tested_div').hasClasses("xxx", "yyy", "zzz")).toEqual(false)
+    });
+    it("Should return false if the given class is not present", function () {
+      expect($('#tested_div').hasClasses("yyy")).toEqual(false)
+    });
+    it("Should return false if one of the given class is not present", function () {
+      expect($('#tested_div').hasClasses("ccc", "bbb", "zzz", "aaa")).toEqual(false)
+    });
+    it("Should return false if wrong param (a Date) is given", function () {
+      expect($('#tested_div').hasClasses(new Date(123456789))).toEqual(false)
+    });
+    it("Should return false if undefined is given", function () {
+      expect($('#tested_div').hasClasses(undefined)).toEqual(false)
+    });
+    it("Should return false if empty String is given", function () {
+      expect($('#tested_div').hasClasses(" ")).toEqual(false)
+    });
+  });
 
 });
