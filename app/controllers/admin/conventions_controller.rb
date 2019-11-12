c5e31d54995d2c3af4071a02d3d3013bde508984
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-12 17:30:05 +0100

    Adds controller exclusion

--- app/controllers/admin/conventions_controller.rb
+++ app/controllers/admin/conventions_controller.rb
@@ -1,9 +1,12 @@
 module Admin
   class ConventionsController < Admin::ApplicationController
-
     def valid_action?(name, resource = resource_class)
       %w[new destroy].exclude?(name.to_s) && super
+      if current_user.role != "superadmin" && current_user.role != "relecteur"
+        %w[edit].exclude?(name.to_s) && super
+      else
+        true
+      end
     end
-    
   end
 end
