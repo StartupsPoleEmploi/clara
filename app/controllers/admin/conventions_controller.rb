f6ec6b6a022ce0429c5d7af8f7a3022f61a604b7
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-13 11:22:04 +0100

    code review controller fix

--- app/controllers/admin/conventions_controller.rb
+++ app/controllers/admin/conventions_controller.rb
@@ -1,11 +1,10 @@
 module Admin
   class ConventionsController < Admin::ApplicationController
     def valid_action?(name, resource = resource_class)
-      %w[new destroy].exclude?(name.to_s) && super
       if current_user.role != "superadmin" && current_user.role != "relecteur"
-        %w[edit].exclude?(name.to_s) && super
+        %w[new destroy edit].exclude?(name.to_s) && super
       else
-        true
+        %w[new destroy].exclude?(name.to_s) && super
       end
     end
   end
