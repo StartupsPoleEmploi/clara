0ad2e0ac324dcced0711fcf7d7136f947f4ffd8d
David Boureau <bdavidxyz@gmail.com>
2019-11-14 15:03:22 +0100

    acces direct par URL interdit

--- app/controllers/admin/conventions_controller.rb
+++ app/controllers/admin/conventions_controller.rb
@@ -1,5 +1,10 @@
 module Admin
   class ConventionsController < Admin::ApplicationController
+
+
+    before_action :require_superadmin, only: [:new, :create, :destroy]
+    before_action :require_superadmin_or_relecteur, only: [:edit]
+
     def valid_action?(name, resource = resource_class)
       if current_user.role != "superadmin" && current_user.role != "relecteur"
         %w[new destroy edit].exclude?(name.to_s) && super
@@ -7,5 +12,6 @@ module Admin
         %w[new destroy].exclude?(name.to_s) && super
       end
     end
+
   end
 end
