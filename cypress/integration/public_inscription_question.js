9311a555efa5d56407d197c2e08df05f323d9c70
David Boureau <bdavidxyz@gmail.com>
2019-11-15 12:04:03 +0100

    reintegration

---
+++ cypress/integration/public_inscription_question.js
@@ -0,0 +1,47 @@
+
+
+describe("Quand on arrive sur la question inscription", function () {
+
+  before(function () {
+    cy.visit('/inscription_questions/new')
+  })
+
+  it("Aucun radiobutton coché", function () {
+    cy.get('input[type="radio"]').should('not.be.checked')
+  })
+
+  it("focus sur le premier radiobutton ", function () {
+    cy.get('input[type="radio"]').first().should('have.focus')
+  })
+
+  it("Absence d'erreur lorqu'on arrive sur la page", function () {
+    cy.get('.c-label.is-error').should('not.exist')
+  })
+
+  it("Erreur si on ne coche rien ", function () {
+    // when
+    cy.get('input[value="Continuer"]').click()
+    // then
+    cy.get('.c-label.is-error').should('exist')
+  })
+
+  it("Cas nominal : on coche une case et on valide, ", function () {
+    // given
+    cy.visit('/inscription_questions/new')
+    cy.get('input[type="radio"]').first().check()
+    // when
+    cy.get('input[value="Continuer"]').click()
+    // then
+    cy.location('pathname').should('not.contain', 'inscription_questions')
+  })
+
+  it("On peut revenir à l'écran précédent", function () {
+    // given
+    cy.visit('/inscription_questions/new')
+    // when
+    cy.get('input[value="Revenir"]').click()
+    // then
+    cy.location('pathname').should('not.contain', 'inscription_questions')
+  })
+})
+
