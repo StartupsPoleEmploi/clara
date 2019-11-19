18a08373a13c54c61fed62e86628e159ab2c92da
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-19 15:14:45 +0100

    Buggy

--- app/assets/javascripts/administrate/custom/admin_accordion.js
+++ app/assets/javascripts/administrate/custom/admin_accordion.js
@@ -1,14 +1,17 @@
 clara.js_define("admin_accordion", {
 
-    please_if: function() {
-      return $(".js-accordion").length > 0;
-    },
+  please_if: function () {
+    return $(".js-accordion").length > 0;
+  },
 
-    please: function() {
-      $(".field-unit").addClass("js-accordion__panel");
-      $(".field-unit__label label[for]").addClass("js-accordion__header");
-      $('.js-accordion').accordion();
-    }
+  please: function () {
+    $(".field-unit").addClass("js-accordion__panel");
+    $(".field-unit__label label[for]").addClass("js-accordion__header");
+    $('.js-accordion').accordion();
+    $("button.js-hide").click(function () {
+      $(".alert-convention").hide()
+    })
+  }
 
 });
 
