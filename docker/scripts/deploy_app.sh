3a8397cf61e1e5ffca7cba1e6e489c99e4770b22
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-22 11:21:38 +0100

    test

--- docker/scripts/deploy_app.sh
+++ docker/scripts/deploy_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker exec -w /var/git/ara.git srv_app git pull origin master
-docker exec -w /var/git/ara.git srv_app bundle install
-docker exec -w /var/git/ara.git srv_app export RUBYOPT="-KU -E utf-8:utf-8"
-docker exec -w /var/git/ara.git srv_app bundle exec mina production2 setup 
-docker exec -w /var/git/ara.git srv_app bundle exec mina production2 deploy 
+docker exec -w /var/git/ara.git clara_app git pull origin master
+docker exec -w /var/git/ara.git clara_app bundle install
+docker exec -w /var/git/ara.git clara_app export RUBYOPT="-KU -E utf-8:utf-8"
+docker exec -w /var/git/ara.git clara_app bundle exec mina production2 setup 
+docker exec -w /var/git/ara.git clara_app bundle exec mina production2 deploy 
