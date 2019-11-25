9a15cfddbd15fa299e7816ea94123019d3a97a0b
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-25 14:14:24 +0100

    Migration working

--- db/migrate/20191125102348_add_cascade_delete_to_foreign_keys.rb
+++ db/migrate/20191125102348_add_cascade_delete_to_foreign_keys.rb
@@ -1,9 +1,8 @@
 class AddCascadeDeleteToForeignKeys < ActiveRecord::Migration[5.2]
   def change
     if foreign_key_exists?(:compound_rules, :rules)
-      remove_foreign_key :compound_rules, :rules, column: "slave_rule_id"
-      add_foreign_key "compound_rules", "rules", on_delete: :cascade
-      add_foreign_key "compound_rules", "rules", column: "slave_rule_id"
+      remove_foreign_key :compound_rules, column: "slave_rule_id"
+      add_foreign_key "compound_rules", "rules", column: "slave_rule_id", on_delete: :cascade
     end
   end
 end
