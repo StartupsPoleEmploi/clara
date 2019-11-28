8900c60d919b333710eeea10b3da99baa1cb19c0
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-28 15:12:10 +0100

    Working but needs UI input from Marion

--- app/assets/javascripts/administrate/custom/admin_steps/admin_stage_steps.js
+++ app/assets/javascripts/administrate/custom/admin_steps/admin_stage_steps.js
@@ -19,6 +19,9 @@ clara.js_define("admin_stage_steps", {
 
       if (has_reached_stage2 && current_stage_nb < 2) {
         $(".c-newaid-stage--2 .c-newaid-stageinside").html('<a href="/admin/aid_creation/new_aid_stage_2?locale=fr&amp;modify=' + is_modifying + '&slug=' + slug + '">Étape 2</a>')
+        $(".js-aid-edition").on('DOMSubtreeModified', ".c-detail-list", function () { // If we have a polyfill for Mutation Observer, we can change this deprecated function
+          warn_user_to_save()
+        })
       }
       if (has_reached_stage3 && current_stage_nb < 3) {
         $(".c-newaid-stage--3 .c-newaid-stageinside").html('<a href="/admin/aid_creation/new_aid_stage_3?locale=fr&amp;modify=' + is_modifying + '&slug=' + slug + '">Étape 3</a>')
@@ -32,16 +35,30 @@ clara.js_define("admin_stage_steps", {
       }
       if (has_reached_stage4 && current_stage_nb < 4) {
         $(".c-newaid-stage--4 .c-newaid-stageinside").html('<a href="/admin/aid_creation/new_aid_stage_4?locale=fr&amp;modify=' + is_modifying + '&slug=' + slug + '">Étape 4</a>')
+        if (current_stage_nb === 3) {
+          $("#aid_short_description").change(function () {
+            warn_user_to_save()
+          })
+          $("form").on('DOMSubtreeModified', "#aid_filter_ids-selectized", function () { // If we have a polyfill for Mutation Observer, we can change this deprecated function
+            warn_user_to_save()
+          })
+        }
       }
       if (has_reached_stage5 && current_stage_nb < 5) {
         $(".c-newaid-stage--5 .c-newaid-stageinside").html('<a href="/admin/aid_creation/new_aid_stage_5?locale=fr&amp;modify=' + is_modifying + '&slug=' + slug + '">Étape 5</a>')
+        if (current_stage_nb === 4) {
+          $("#main-apprule-expl").on('DOMSubtreeModified', "ul.c-parentexpl-root_box", function () { // If we have a polyfill for Mutation Observer, we can change this deprecated function
+            warn_user_to_save()
+          })
+        }
       }
 
 
 
       function warn_user_to_save() {
         setTimeout(function () {
-          console.log("changed !")
+          var warning_msg = "⚠ Attention, vos modifications ne seront enregistrées que si vous cliquez sur Continuer"
+          $(".c-newaid-belowstages").append(warning_msg)
         }, 1200)
 
       }
