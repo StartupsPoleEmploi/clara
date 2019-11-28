577cfa677cb0054161d0f5b77018f9b79e145533
Rémy Marronnier <rmarronnier@gmail.com>
2019-11-28 13:11:26 +0100

    test

--- app/assets/javascripts/administrate/custom/admin_steps/admin_stage_2.js
+++ app/assets/javascripts/administrate/custom/admin_steps/admin_stage_2.js
@@ -1,35 +1,43 @@
 clara.js_define("admin_stage_2", {
 
-    please_if: function() {
-      return $(".c-newaid--stage2").exists();
-    },
+  please_if: function () {
+    return $(".c-newaid--stage2").exists();
+  },
 
-    please: function() {
+  please: function () {
 
-      // unfold description field
-      $("#accordion_0-0_tab").click();
-
-
-      clara.admin_stage_steps.please(2);
+    // unfold description field
+    $("#accordion_0-0_tab").click();
 
 
-      // give focus to description field
-      setTimeout(function(){
+    clara.admin_stage_steps.please(2);
 
-        function give_focus_to_description() {
-          CKEDITOR.instances["aid_what"].focus()
-        }
 
-        if (document.activeElement.id === "js-tooltip-close") {
-          $("#js-tooltip-close").on("click", function() {
-            give_focus_to_description()
-          })
-        } else  {
+    // give focus to description field
+    setTimeout(function () {
+
+      function give_focus_to_description() {
+        CKEDITOR.instances["aid_what"].focus()
+      }
+
+      if (document.activeElement.id === "js-tooltip-close") {
+        $("#js-tooltip-close").on("click", function () {
           give_focus_to_description()
-        }
+        })
+      } else {
+        give_focus_to_description()
+      }
 
 
-      }, 1200)
+    }, 1200)
+
+    function warn_user_to_save() {
+      CKEDITOR.instances["aid_what"].change(function () {
+        alert("The text has been changed.");
+      })
     }
+
+
+  }
 
 });
