4814b8052b60b4f53424c52b8dc620f5d21c96aa
David Boureau <bdavidxyz@gmail.com>
2019-12-03 11:46:04 +0100

    test ok

--- cypress/integration/admin_show_aid.js
+++ cypress/integration/admin_show_aid.js
@@ -1,14 +1,35 @@
 describe("Voir une aide", function() {
 
-  before(function() {
+  describe("Pour un contributeur", function() {
+    before(function() {
+      // given
+      cy.connect_as_contributeur1()
+      // when
+      cy.visit('/admin/aids/erasmus')
+
+    })
+
+    it("Pour un contributeur, la rubrique n'est pas cliquable", function() {
+      // then
+      const $contract_type_elt = Cypress.$("#contract_type").next().children()
+      expect($contract_type_elt.prop("tagName")).to.equal("DIV")
+    })  
   })
 
-  it("Il y a 3 champs", function() {
-    // given
-    cy.visit('/admin/aids/erasmus')
-    // when
-    // then
-    cy.connect_as_contributeur1()
+  describe("Pour un superadmin", function() {
+    before(function() {
+      // given
+      cy.connect_as_superadmin()
+      // when
+      cy.visit('/admin/aids/erasmus')
+
+    })
+
+    it("Pour un contributeur, la rubrique est cliquable", function() {
+      // then
+      const $contract_type_elt = Cypress.$("#contract_type").next().children()
+      expect($contract_type_elt.prop("tagName")).to.equal("A")
+    })  
   })
 
 
