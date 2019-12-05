53cea12587a17e7614303f19c4a5a9bcc30c2ddf
David Boureau <bdavidxyz@gmail.com>
2019-12-05 16:36:16 +0100

    clearer given/when/then

--- cypress/integration/admin_stage_3.js
+++ cypress/integration/admin_stage_3.js
@@ -23,35 +23,45 @@ describe("Étape 3", function() {
   })
 
   it("Si on vient de l'étape 2, on arrive sur l'étape 3 en création", function() {
+    // then
     cy.get(MAIN_TITLE_SELECTOR).should('have.text', 'Créer  une aide')
     cy.location().should((loc) => {expect(loc.pathname).to.eq('/admin/aid_creation/new_aid_stage_3')})
   })
 
   it("L'étape 3 est optionnelle : si on valide directement, on arrive sur l'étape 4", function() {
+    // when
     cy.get('button.c-newaid-actionrecord').click()
+    // then
     cy.location().should((loc) => {expect(loc.pathname).to.eq('/admin/aid_creation/new_aid_stage_4')})
   })
 
   it("On peut accéder à l'écran directement en modification", function() {
+    // when
     cy.visit('/admin/aid_creation/new_aid_stage_3?modify=true&slug=test-stage-3').then((contentWindow) => {
+      // then
       cy.get(MAIN_TITLE_SELECTOR).should('have.text', 'Modifier  une aide')
     })
   })
 
 
   it("On peut renseigner du texte : il se met à jour dans l'aperçu", function() {
+    // given
     cy.get(".c-resultaid__smalltxt").should('have.text', "")
+    // when
     cy.get('#aid_short_description').type("Un résumé possible de l'aide")
+    // then
     cy.get(".c-resultaid__smalltxt").should('have.text', "Un résumé possible de l'aide")
   })
 
   it("On peut renseigner des filtres", function() {
-    
+    // given
     cy.get("#aid_filter_ids").invoke("val").should('equal', null)
 
+    // when
     cy.get("#aid_filter_ids-selectized").click()
     cy.get("#aid_filter_ids-selectized").type("{enter}")
 
+    // then
     cy.get("#aid_filter_ids").invoke("val").should('not.equal', null)
   })
 
