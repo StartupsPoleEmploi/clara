2e0e7daabb4ea9ac87fbccb3fba6440f55bb5bd8
Rémy Marronnier <rmarronnier@gmail.com>
2019-12-09 12:04:37 +0100

    date dump

--- docker/scripts/dump_db.sh
+++ docker/scripts/dump_db.sh
@@ -1,1 +1,2 @@
-docker exec clara_db pg_dump -Fc --no-owner --dbname=ara_production > latest.dump
+$date = date +%F
+docker exec clara_db pg_dump -Fc --no-owner --dbname=ara_production > "$date".dump
