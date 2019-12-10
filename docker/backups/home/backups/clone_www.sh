ddc3a535037d58b2ac3387d29555ed6938f90d57
Rémy Marronnier <rmarronnier@gmail.com>
2019-12-10 14:09:53 +0100

    backup.sh

--- docker/backups/home/backups/clone_www.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-date=`date +%d`
-
-NAME=www_"$date".bz2
-
-ENV_NAME=`docker exec srv_app printenv ARA_DATABASE_NAME`
-
-scp -r srv_app /home/clara | bzip2 -cq9 | tee /home/backups/$ENV_NAME/$NAME /mnt/backups/$ENV_NAME/$NAME
--- docker/backups/home/backups/dump_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-date=`date +%d`
-
-NAME=database_"$date".dump.bz2
-
-DATABASE_NAME=`docker exec srv_app printenv ARA_DATABASE_NAME`
-
-pg_dump postgres://backup@srv_db:5432/$DATABASE_NAME | bzip2 -cq9 | tee /home/backups/$DATABASE_NAME/$NAME /mnt/backups/$DATABASE_NAME/$NAME
