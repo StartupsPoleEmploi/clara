d8a3c7ac1a2dca48e0979b8d099e1062d0fc914d
Rémy Marronnier <rmarronnier@gmail.com>
2019-12-10 14:14:45 +0100

    fix

--- docker/backups/home/backups/backup.sh
+++ docker/backups/home/backups/backup.sh
@@ -6,8 +6,9 @@ DUMP_NAME=database_"$date".dump.bz2
 WWW_NAME=www_"$date".bz2
 
 #DATABASE_NAME=`docker exec srv_app printenv ARA_DATABASE_NAME`
-DATABASE_NAME=production
+DATABASE_NAME=ara_production
+ENV_NAME=production
 
 
-pg_dump postgres://backup@srv_db:5432/$DATABASE_NAME | bzip2 -cq9 | tee /home/backups/$DATABASE_NAME/$DUMP_NAME /mnt/backups/$DATABASE_NAME/$DUMP_NAME
+pg_dump postgres://backup@srv_db:5432/$DATABASE_NAME | bzip2 -cq9 | tee /home/backups/$ENV_NAME/$DUMP_NAME /mnt/backups/$ENV_NAME/$DUMP_NAME
 scp -r srv_app /home/clara | bzip2 -cq9 | tee /home/backups/$ENV_NAME/$WWW_NAME /mnt/backups/$ENV_NAME/$WWW_NAME
