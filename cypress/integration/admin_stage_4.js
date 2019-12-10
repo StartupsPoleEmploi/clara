34a7cbf8e9c9d8d030a32ec1d165b1a1c7a7d362
David Boureau <bdavidxyz@gmail.com>
2019-12-10 14:21:44 +0100

    multiple criteria

--- cypress/integration/admin_stage_4.js
+++ cypress/integration/admin_stage_4.js
@@ -6,9 +6,9 @@ describe("Étape 4", function() {
     cy.connect_as_contributeur1()
   })
 
-  // after(function() {
-  //   cy.delete_an_aid("test-stage-4")
-  // })
+  after(function() {
+    cy.delete_an_aid("test-stage-4")
+  })
 
   before(function() {
     cy.visit('/admin/aid_creation/new_aid_stage_1')
@@ -47,29 +47,84 @@ describe("Étape 4", function() {
   })
 
   describe("Enregistrement d'une règle", function() {
-    before(function() {
+    beforeEach(function() {
       cy.visit('/admin/aid_creation/new_aid_stage_4?modify=true&slug=test-stage-4')
     })
 
-    it("On peut enregistrer une règle simple, elle apparaît en lecture après vidage du cache", function() {
+    it("On peut enregistrer une règle simple ...", function() {
+      // given
       cy.get('select#rule_variable_id').select('v_category')
       cy.get('select#rule_value_eligible_selectible').select('cat_12345')
       cy.get('.c-apprule-button.is-validation').click()
       // when
       cy.get('#record_root_rule').click()
       cy.visit('/admin/get_cache')
-      cy.wait(500)
       cy.get('#button-empty-cache').click()
       cy.wait(2000)
       cy.visit('/admin/aids/test-stage-4')
       cy.get('#label_tab_4').click()
-      
-      // cy.get(".c-display-appfield").should($el => expect($el.text().trim()).to.equal('il faut remplir la condition suivante : Être en catégorie 1, 2, 3, 4 ou 5'));
+      // then
       cy.get(".c-display-appfield").shouldHaveTrimmedText('il faut remplir la condition suivante : Être en catégorie 1, 2, 3, 4 ou 5');
-
     })
+    it("... avec un critère géographique hors DOM", function() {
+      // given
+      cy.get('#tout_sauf_domtom').click()
+
+      // when
+      cy.get('#record_root_rule').click()
+      cy.visit('/admin/get_cache')
+      cy.get('#button-empty-cache').click()
+      cy.wait(2000)
+      cy.visit('/admin/aids/test-stage-4')
+      cy.get('#label_tab_4').click()
+
+      // then
+      cy.get(".c-display-appfield").shouldHaveTrimmedText("il faut réunir l'ensemble des 2 conditions suivantes Être en catégorie 1, 2, 3, 4 ou 5 l'ensemble des 6 conditions suivantes Ne pas résider dans le département Guadeloupe Ne pas résider dans le département Martinique Ne pas résider dans le département Guyane Ne pas résider dans le département La Réunion Ne pas résider dans le département Saint-Pierre-et-Miquelon Ne pas résider dans le département Mayotte");
+    })
+    it("... avec un critère géographique DOM uniquement", function() {
+      // given
+      cy.get('#domtom_seulement').click()
+
+      // when
+      cy.get('#record_root_rule').click()
+      cy.visit('/admin/get_cache')
+      cy.get('#button-empty-cache').click()
+      cy.wait(2000)
+      cy.visit('/admin/aids/test-stage-4')
+      cy.get('#label_tab_4').click()
+
+      // then
+      cy.get(".c-display-appfield").shouldHaveTrimmedText("il faut réunir l'ensemble des 2 conditions suivantes Être en catégorie 1, 2, 3, 4 ou 5 au moins une des conditions suivantessoit Résider dans le département Guadeloupesoit Résider dans le département Martiniquesoit Résider dans le département Guyanesoit Résider dans le département La Réunionsoit Résider dans le département Saint-Pierre-et-Miquelonsoit Résider dans le département Mayotte");
+    })
+    it("On peut enregistrer une règle composite", function() {
+      // given
+      cy.visit('/admin/aid_creation/new_aid_stage_4?modify=true&slug=test-stage-4')
+
+      cy.get('.js-or:last').click()
+      cy.get('select#rule_variable_id').select('v_qpv')
+      cy.get('select#rule_value_eligible_selectible').select('oui')
+      cy.get('.c-apprule-button.is-validation').click()
+      cy.get('input#tout').click() // toute la france
+
+      cy.get('.js-and:first').click()
+      cy.get('select#rule_variable_id').select('v_age')
+      cy.get('select#rule_operator_kind').select('more_than')
+      cy.get('input#rule_value_eligible').type('18')
+
+      cy.get('.c-apprule-button.is-validation').click()
+
+      // when
+      cy.get('#record_root_rule').click()
+      cy.visit('/admin/get_cache')
+      cy.get('#button-empty-cache').click()
+      cy.wait(2000)
+      cy.visit('/admin/aids/test-stage-4')
+      cy.get('#label_tab_4').click()
+      // then
+      cy.get(".c-display-appfield").shouldHaveTrimmedText("il faut réunir au moins une des conditions suivantessoit l'ensemble des 2 conditions suivantes Être en catégorie 1, 2, 3, 4 ou 5 Avoir un âge strictement supérieur à 18 anssoit Être en Quartier Prioritaire de la Ville");
+    })
+
+
   })
-
-
 })
 
