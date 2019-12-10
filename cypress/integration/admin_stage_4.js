d7186e2fe654985522ab9e3526e1d2ef6b4be766
David Boureau <bdavidxyz@gmail.com>
2019-12-10 11:54:39 +0100

    wip

--- cypress/integration/admin_stage_4.js
+++ cypress/integration/admin_stage_4.js
@@ -6,9 +6,9 @@ describe("Étape 4", function() {
     cy.connect_as_contributeur1()
   })
 
-  after(function() {
-    cy.delete_an_aid("test-stage-4")
-  })
+  // after(function() {
+  //   cy.delete_an_aid("test-stage-4")
+  // })
 
   before(function() {
     cy.visit('/admin/aid_creation/new_aid_stage_1')
@@ -24,14 +24,16 @@ describe("Étape 4", function() {
   })
 
   it("Si on vient de l'étape 3, on arrive sur l'étape 4 en création", function() {
+    // when
+    cy.get('button.c-newaid-actionrecord').click()
     // then
-    cy.get(MAIN_TITLE_SELECTOR).should('have.text', 'Créer  une aide')
+    cy.get(MAIN_TITLE_SELECTOR).should('have.text', 'Créer une aide')
     cy.location().should((loc) => {expect(loc.pathname).to.eq('/admin/aid_creation/new_aid_stage_4')})
   })
 
   it("L'étape 4 est optionnelle : si on valide directement, on arrive sur l'étape 5", function() {
     // when
-    cy.get('button.c-newaid-actionrecord').click()
+    cy.get('#record_root_rule').click()
     // then
     cy.location().should((loc) => {expect(loc.pathname).to.eq('/admin/aid_creation/new_aid_stage_5')})
   })
@@ -40,7 +42,31 @@ describe("Étape 4", function() {
     // when
     cy.visit('/admin/aid_creation/new_aid_stage_4?modify=true&slug=test-stage-4').then((contentWindow) => {
       // then
-      cy.get(MAIN_TITLE_SELECTOR).should('have.text', 'Modifier  une aide')
+      cy.get(MAIN_TITLE_SELECTOR).should('have.text', 'Modifier une aide')
+    })
+  })
+
+  describe("Enregistrement d'une règle", function() {
+    before(function() {
+      cy.visit('/admin/aid_creation/new_aid_stage_4?modify=true&slug=test-stage-4')
+    })
+
+    it("On peut enregistrer une règle simple, elle apparaît en lecture après vidage du cache", function() {
+      cy.get('select#rule_variable_id').select('v_category')
+      cy.get('select#rule_value_eligible_selectible').select('cat_12345')
+      cy.get('.c-apprule-button.is-validation').click()
+      // when
+      cy.get('#record_root_rule').click()
+      cy.visit('/admin/get_cache')
+      cy.wait(500)
+      cy.get('#button-empty-cache').click()
+      cy.wait(2000)
+      cy.visit('/admin/aids/test-stage-4')
+      cy.get('#label_tab_4').click()
+      
+      // cy.get(".c-display-appfield").should($el => expect($el.text().trim()).to.equal('il faut remplir la condition suivante : Être en catégorie 1, 2, 3, 4 ou 5'));
+      cy.get(".c-display-appfield").shouldHaveTrimmedText('il faut remplir la condition suivante : Être en catégorie 1, 2, 3, 4 ou 5');
+
     })
   })
 
