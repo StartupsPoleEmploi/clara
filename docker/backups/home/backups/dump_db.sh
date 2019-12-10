d5751cf94ccc347c4f7c60ea8d06439189060704
Rémy Marronnier <rmarronnier@gmail.com>
2019-12-10 11:24:09 +0100

    dump working

--- docker/backups/home/backups/dump_db.sh
+++ docker/backups/home/backups/dump_db.sh
@@ -4,4 +4,4 @@ date=`date +%d`
 
 NAME=database_"$date".dump.bz2
 
-pg_dump -Fc --no-owner --dbname=ara_production | bzip2 -cq9 | tee /home/backups/$NAME /mnt/backups/$NAME
+pg_dump postgres://backup@srv_db:5432/ara_production | bzip2 -cq9 | tee /home/backups/$NAME /mnt/backups/$NAME
