9a2e338e66ddf73b86491e375f0c4847bbfc21f6
Rémy Marronnier <rmarronnier@gmail.com>
2019-12-10 13:51:10 +0100

    clone_www

--- docker/backups/home/backups/dump_db.sh
+++ docker/backups/home/backups/dump_db.sh
@@ -4,4 +4,6 @@ date=`date +%d`
 
 NAME=database_"$date".dump.bz2
 
-pg_dump postgres://backup@srv_db:5432/ara_production | bzip2 -cq9 | tee /home/backups/$NAME /mnt/backups/$NAME
+DATABASE_NAME=`docker exec srv_app printenv ARA_DATABASE_NAME`
+
+pg_dump postgres://backup@srv_db:5432/$DATABASE_NAME | bzip2 -cq9 | tee /home/backups/$DATABASE_NAME/$NAME /mnt/backups/$DATABASE_NAME/$NAME
