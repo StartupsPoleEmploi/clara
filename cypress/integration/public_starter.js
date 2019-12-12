4e49bf5c277fc1feaa8c36f4ebceb1ce18137ca6
David Boureau <bdavidxyz@gmail.com>
2019-12-12 15:06:08 +0100

    gt working

--- cypress/integration/public_starter.js
+++ cypress/integration/public_starter.js
@@ -17,6 +17,10 @@ describe("Pour un visiteur", function () {
     cy.get('#moins_d_un_an').click()
     cy.get('.js-next').click()
 
+    cy.get('#cat_12345').should("exist")
+    cy.get('#cat_12345').click()
+    cy.get('.js-next').click()
+
     cy.get('#radio_ARE_ASP').should("exist")
     cy.get('#radio_ARE_ASP').click()
     cy.get('.js-next').click()
@@ -26,7 +30,7 @@ describe("Pour un visiteur", function () {
     cy.get('.js-next').click()
 
     cy.get('#age').should("exist")
-    cy.get('#age').type("33")
+    cy.get('#age').type("16")
     cy.get('.js-next').click()
     
     cy.get('#niveau_4').should("exist")
@@ -37,6 +41,7 @@ describe("Pour un visiteur", function () {
     cy.get('.js-next').click()
     
     cy.get('#val_spectacle').should("exist")
+    cy.get('#val_spectacle').click()
 
     //when
     cy.get('.js-next').click()
@@ -44,6 +49,10 @@ describe("Pour un visiteur", function () {
     //then 
     cy.get("body.c-body.aides").should("exist")
 
+  })
+  it("Sur l'écran de résultat il y a les aides éligibles et non éligibles", function () {
+    cy.get('.c-resultcard--green').its('length').should('be.gt', 0)
+    cy.get('.c-resultcard--red').its('length').should('be.gt', 0)
   })
 })
 
