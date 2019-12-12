2271e37b8963de8f6db5d93dee1d34d8236d8d09
clara-test <clara-test@none.none>
2019-12-12 15:09:02 +0000

    wip

--- docker/backups/home/backups/backup.sh
+++ docker/backups/home/backups/backup.sh
@@ -2,12 +2,17 @@
 
 date=`date +%d`
 
-DUMP_NAME=database_"$date".dump.bz2
-WWW_NAME=www_"$date".bz2
-
-DATABASE_NAME=ara_production
 ENV_NAME=production
 
+DUMP_NAME="${ENV_NAME}_database_$date.dump.bz2"
+WWW_NAME="${ENV_NAME}_www_$date.bz2"
+DOCKER_NAME="${ENV_NAME}_docker_$date.tar.bz2"
 
-pg_dump postgres://backup@srv_db:5432/$DATABASE_NAME | bzip2 -cq9 | tee /home/backups/$ENV_NAME/$DUMP_NAME /mnt/backups/$ENV_NAME/$DUMP_NAME
-scp -r clara_rails /home/clara | bzip2 -cq9 | tee /home/backups/$ENV_NAME/$WWW_NAME /mnt/backups/$ENV_NAME/$WWW_NAME
+DATABASE_NAME=ara_production
+
+pg_dump postgres://backup@srv_db:5432/$DATABASE_NAME | bzip2 -cq9 | tee /home/backups/$ENV_NAME/$DUMP_NAME /mnt/backups/$ENV_NAME/$DUMP_NAME >/dev/null
+
+#scp -r srv_app /home/clara | bzip2 -cq9 | tee /home/backups/$ENV_NAME/$WWW_NAME /mnt/backups/$ENV_NAME/$WWW_NAME
+
+tar --exclude=*.bz2 --exclude=*.log -jcPf - /home/docker | tee /home/backups/$DOCKER_NAME /mnt/backups/$DOCKER_NAME >/dev/null
+
