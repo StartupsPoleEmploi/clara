93cd1da0a4d6f4c3799f38ef1548f1ccd9bd7a2f
Rémy Marronnier <rmarronnier@gmail.com>
2019-12-13 10:19:07 +0100

    multisteps beginning

---
+++ app/controllers/admin/vuejs_aid_creation_controller.rb
@@ -0,0 +1,223 @@
+
+module Admin
+  class VuejsAidCreationController < Admin::ApplicationController
+    # def dashboard
+    #   @dashboard ||= AidDashboard.new
+    # end
+
+    # def _hidden(prop)
+    #   params.require(prop).permit(:value).to_h[:value]
+    # end
+
+    def choucroute
+      "sdfsdf"
+    end
+
+    # def new_aid_stage_1
+    #   aid = params[:slug] ? Aid.find_by(slug: params[:slug]) : Aid.new
+    #   authorize_resource(aid)
+    #   render locals: {
+    #            page: Administrate::Page::Form.new(dashboard, aid),
+    #          }
+    # end
+
+    # def create_stage_1
+    #   new_attributes = params.require(:aid).permit(:source, :name, :contract_type_id, :ordre_affichage).to_h
+    #   new_ordre_affichage = new_attributes[:ordre_affichage] || 99
+    #   new_attributes[:ordre_affichage] = new_ordre_affichage
+    #   slug = _hidden(:slug)
+    #   if !slug.blank?
+    #     aid = Aid.find_by(slug: slug)
+    #     aid.assign_attributes(new_attributes)
+    #   else
+    #     aid = Aid.new(new_attributes)
+    #   end
+
+    #   was_new = aid.id == nil
+
+    #   if aid.save
+    #     # Hack to consider it as a "draft"
+    #     if was_new
+    #       aid.archived_at = aid.created_at
+    #     end
+    #     # end of hack
+    #     aid.update_status
+    #     if slug.blank?
+    #       redirect_to(
+    #         admin_aid_creation_new_aid_stage_2_path(slug: aid.slug, modify: _hidden(:modify)),
+    #         notice: "L'aide a bien été enregistrée en tant que brouillon.",
+    #       )
+    #     else
+    #       redirect_to(
+    #         admin_aid_creation_new_aid_stage_2_path(slug: aid.slug, modify: _hidden(:modify)),
+    #         notice: "Les modifications ont bien été enregistrées.",
+    #       )
+    #     end
+    #   else
+    #     render :new_aid_stage_1, locals: {
+    #                                page: Administrate::Page::Form.new(dashboard, aid),
+    #                              }
+    #   end
+    # end
+
+    # def new_aid_stage_2
+    #   aid = Aid.find_by(slug: params[:slug])
+    #   authorize_resource(aid)
+    #   render locals: {
+    #            page: Administrate::Page::Form.new(dashboard, aid),
+    #          }
+    # end
+
+    # def create_stage_2
+    #   slug = _hidden(:slug)
+    #   aid = Aid.find_by(slug: slug)
+    #   old_attributes = aid.attributes.with_indifferent_access
+    #   new_attributes = params.require(:aid).permit(:what, :additionnal_conditions, :how_much, :how_and_when, :limitations).to_h
+    #   all_attributes = old_attributes.merge(new_attributes)
+    #   aid.assign_attributes(new_attributes)
+    #   aid.save
+    #   aid.update_status
+
+    #   redirect_to(
+    #     admin_aid_creation_new_aid_stage_3_path(slug: aid.slug, modify: _hidden(:modify)),
+    #     notice: "Le contenu a été mis à jour",
+    #   )
+    # end
+
+    # def new_aid_stage_3
+    #   aid = Aid.find_by(slug: params[:slug])
+    #   ct = aid.contract_type
+    #   contract_type = ct.attributes.with_indifferent_access
+
+    #   render locals: {
+    #            page: Administrate::Page::Form.new(dashboard, aid),
+    #            contract_type: contract_type,
+    #            aid_attributes: aid.attributes.with_indifferent_access,
+    #          }
+    # end
+
+    # def create_stage_3
+    #   slug = _hidden(:slug)
+    #   new_attributes = params.require(:aid).permit(:short_description, filter_ids: []).to_h
+    #   filters_ids = new_attributes[:filter_ids].reject { |f| f.blank? }.map { |f| f.to_i }
+    #   filters = Filter.where(id: filters_ids)
+    #   aid = Aid.find_by(slug: slug)
+    #   aid.filters = filters
+    #   aid.short_description = new_attributes[:short_description]
+
+    #   aid.save
+    #   aid.update_status
+
+    #   redirect_to(
+    #     admin_aid_creation_new_aid_stage_4_path(slug: aid.slug, modify: _hidden(:modify)),
+    #     notice: "Le contenu a été mis à jour",
+    #   )
+    # end
+
+    # def new_aid_stage_4
+    #   aid = Aid.find_by(slug: params[:slug])
+    #   authorize_resource(aid)
+
+    #   gon.global_state = {
+    #     explicitations: _all_explicitations,
+    #     operator_kinds: _all_operator_kinds,
+    #     variables: _all_variables,
+    #   }
+
+    #   gon.initial_scope = ExtractScopeForAid.new.call(aid)
+    #   gon.initial_geo = ExtractGeoForAid.new.call(aid)
+
+    #   render locals: {
+    #            page: Administrate::Page::Form.new(dashboard, aid),
+    #            aid_attributes: aid.attributes.with_indifferent_access,
+    #          }
+    # end
+
+    # def create_stage_4
+    #   aid_slug = params["aid"]
+    #   # Need to parse JSON in order to preserve arrays as correct arrays
+    #   trundle = JSON.parse(params["trundle"], symbolize_names: true)
+    #   modify = params["modify"]
+    #   geo = JSON.parse(params["geo"], symbolize_names: true)
+
+    #   error_message = FindScopeAndGeoErrorsToo.new.call(trundle, geo)
+
+    #   is_void = error_message == "Étape non renseignée."
+
+    #   if (error_message.blank? || is_void)
+    #     url = admin_aid_creation_new_aid_stage_5_path(slug: aid_slug, modify: modify)
+    #     aid = Aid.find_by(slug: aid_slug)
+
+    #     CreateScopeAndGeoForAidToo.new.call(trundle: trundle, aid: aid, geo: geo.with_indifferent_access)
+    #     aid.update_status
+
+    #     msg = is_void ? "Mise à jour du champ d'application effectué, celui-ci est vide." : "Mise à jour du champ d'application effectué."
+    #     flash[:notice] = msg
+    #     flash.keep(:notice)
+    #     render js: "document.location = '#{url}'"
+    #   else
+    #     render :json => error_message, :status => 422
+    #   end
+    # end
+
+    # def new_aid_stage_5
+    #   aid = params[:slug] ? Aid.find_by(slug: params[:slug]) : Aid.new
+    #   authorize_resource(aid)
+    #   render locals: {
+    #            page: Administrate::Page::Form.new(dashboard, aid),
+    #            filters_size: aid.filters.size,
+    #            whodunnit: aid.versions.first[:whodunnit],
+    #            aid_status: aid.status,
+    #          }
+    # end
+
+    # def create_stage_5
+    #   slug = _hidden(:slug)
+    #   action_asked = _hidden(:action_asked)
+    #   aid = Aid.find_by(slug: slug)
+
+    #   please_save_aid = true
+
+    #   notice_message = ""
+    #   if action_asked == "archive"
+    #     aid.archived_at = DateTime.now
+    #     notice_message = "L'aide a bien été archivée, elle n'apparaît plus sur le site."
+    #   elsif action_asked == "publish"
+    #     aid.archived_at = nil
+    #     notice_message = "L'aide a été publiée sur le site."
+    #   elsif action_asked == "reread"
+    #     aid.is_rereadable = true
+    #     notice_message = "L'aide a été demandée pour relecture."
+    #   elsif action_asked == "keep"
+    #     notice_message = "L'aide a été conservée en tant que brouillon."
+    #   elsif action_asked == "discard"
+    #     please_save_aid = false
+    #     notice_message = "Le brouillon a été supprimé."
+    #   end
+
+    #   if please_save_aid
+    #     aid.save
+    #     aid.update_status
+    #   else
+    #     aid.destroy
+    #   end
+
+    #   redirect_to(
+    #     admin_root_path("aid[direction]" => "desc", "aid[order]" => "updated_at"),
+    #     notice: notice_message,
+    #   )
+    # end
+
+    # def _all_explicitations
+    #   JSON.parse(Explicitation.all.to_json(:only => [:id, :value_eligible, :operator_kind, :template], :include => { variable: { only: [:name] } })).map { |e| e["variable_name"] = e["variable"]["name"]; e.delete("variable"); e }
+    # end
+
+    # def _all_operator_kinds
+    #   ListOperatorKind.new.call
+    # end
+
+    # def _all_variables
+    #   JSON.parse(Variable.all.to_json(:only => [:id, :name, :variable_kind, :elements, :elements_translation, :is_visible, :name_translation]))
+    # end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   resources :passwords, controller: "passwords", only: [:create, :new]
   resource :session, controller: "sessions", only: [:create]
 
@@ -13,32 +12,30 @@ Rails.application.routes.draw do
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "users#new", as: "sign_up"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  
+
   namespace :api do
     namespace :v1 do
-      post 'api_user_token' => 'api_user_token#create'
-      get 'filters'  => 'api_aides#filters'
-      get 'need_filters'  => 'api_aides#need_filters'
-      get 'aids/detail/:aid_slug'   => 'api_aides#detail'
-      get 'aids/eligible'   => 'api_aides#eligible'
-      get 'aids/ineligible' => 'api_aides#ineligible'
-      get 'aids/uncertain'  => 'api_aides#uncertain'
-      get 'ping' => 'api_aides#ping'
+      post "api_user_token" => "api_user_token#create"
+      get "filters" => "api_aides#filters"
+      get "need_filters" => "api_aides#need_filters"
+      get "aids/detail/:aid_slug" => "api_aides#detail"
+      get "aids/eligible" => "api_aides#eligible"
+      get "aids/ineligible" => "api_aides#ineligible"
+      get "aids/uncertain" => "api_aides#uncertain"
+      get "ping" => "api_aides#ping"
     end
   end
 
-
   namespace :stats do
-    root 'stats#index'
-    get '/index' => 'stats#index'
-    get '/time' => 'stats#time'
+    root "stats#index"
+    get "/index" => "stats#index"
+    get "/time" => "stats#time"
   end
-
 
   mount MagicLamp::Genie, at: "/magic_lamp" if defined?(MagicLamp)
 
-  root 'welcome#index'
-  post 'accept_all_cookies' => 'welcome#accept_all_cookies'
+  root "welcome#index"
+  post "accept_all_cookies" => "welcome#accept_all_cookies"
 
   namespace :admin do
     resources :explicitations
@@ -59,41 +56,53 @@ Rails.application.routes.draw do
       get :export, on: :collection
     end
     namespace :aid_creation do
-      get  "new_aid_stage_1"
+      get "new_aid_stage_1"
       post "create_stage_1"
-      get  "new_aid_stage_2"
+      get "new_aid_stage_2"
       post "create_stage_2"
-      get  "new_aid_stage_3"
+      get "new_aid_stage_3"
       post "create_stage_3"
       get "new_aid_stage_4"
       post "create_stage_4"
       get "new_aid_stage_5"
       post "create_stage_5"
     end
-    resources :rules do 
-      get 'resolve', on: :member
-      post 'save_simulation', on: :member
-      delete 'delete_simulation', on: :member
+    namespace :vuejs_aid_creation do
+      get "multisteps"
+      # post "create_stage_1"
+      # get  "new_aid_stage_2"
+      # post "create_stage_2"
+      # get  "new_aid_stage_3"
+      # post "create_stage_3"
+      # get "new_aid_stage_4"
+      # post "create_stage_4"
+      # get "new_aid_stage_5"
+      # post "create_stage_5"
     end
-    controller 'pages' do
-      get 'get_all_filters_menu'
-      get 'get_need_menu'
-      get 'get_custom_filter_menu'
-      get 'get_reinit'
-      post 'post_reinit'
-      get 'get_zrr'
-      post 'post_zrr'
-      get 'get_cache'
-      post 'post_cache'
-      get 'get_ref_data'
-      post 'post_ref_data'
-      get 'get_transfer_descr'
-      post 'post_transfer_descr'
-      get 'get_delete_trace'
-      post 'post_delete_trace'
-      get 'get_hidden_admin'
+    resources :rules do
+      get "resolve", on: :member
+      post "save_simulation", on: :member
+      delete "delete_simulation", on: :member
     end
-    get 'status', to: 'status#index'
+    controller "pages" do
+      get "get_all_filters_menu"
+      get "get_need_menu"
+      get "get_custom_filter_menu"
+      get "get_reinit"
+      post "post_reinit"
+      get "get_zrr"
+      post "post_zrr"
+      get "get_cache"
+      post "post_cache"
+      get "get_ref_data"
+      post "post_ref_data"
+      get "get_transfer_descr"
+      post "post_transfer_descr"
+      get "get_delete_trace"
+      post "post_delete_trace"
+      get "get_hidden_admin"
+    end
+    get "status", to: "status#index"
     resources :variables
     resources :contract_types
     resources :rule_checks
@@ -101,51 +110,48 @@ Rails.application.routes.draw do
   end
 
   # a route to check exception are propertly sent
-  resources :divide_by_zero,         only: [:index]
+  resources :divide_by_zero, only: [:index]
 
-  resources :age_questions,         only: [:new, :create]
-  resources :address_questions,     only: [:new, :create]
-  resources :allocation_questions,  only: [:new, :create]
-  resources :category_questions,    only: [:new, :create]
-  resources :grade_questions,       only: [:new, :create]
+  resources :age_questions, only: [:new, :create]
+  resources :address_questions, only: [:new, :create]
+  resources :allocation_questions, only: [:new, :create]
+  resources :category_questions, only: [:new, :create]
+  resources :grade_questions, only: [:new, :create]
   resources :inscription_questions, only: [:new, :create]
-  resources :are_questions,         only: [:new, :create]
-  resources :other_questions,       only: [:new, :create]
+  resources :are_questions, only: [:new, :create]
+  resources :other_questions, only: [:new, :create]
 
-  resources :contact,               only: [:index, :create]
-  resources :contact_sent,          only: [:index]
-  
-  resources :cookies,               only: [:edit, :update]
-  resources :confidentiality,       only: [:index]
-  
-  
+  resources :contact, only: [:index, :create]
+  resources :contact_sent, only: [:index]
+
+  resources :cookies, only: [:edit, :update]
+  resources :confidentiality, only: [:index]
+
   resources :aides, only: [:index] do
     collection do
       resources :type, only: [:show]
       resources :detail, only: [:show]
     end
   end
-  post 'aides/search_for_aids'
-  get 'get_search_front', to: 'aides#get_search_front'
-  post 'post_search_front', to: 'aides#post_search_front'
+  post "aides/search_for_aids"
+  get "get_search_front", to: "aides#get_search_front"
+  post "post_search_front", to: "aides#post_search_front"
 
-  resources :req,       only: [:index]
-  resources :reqip,    only: [:index]
-  
-  get 'conditions-generales-d-utilisation', to: 'welcome#terms'
-  get 'welcome/index'
-  post 'welcome/start_wizard'
+  resources :req, only: [:index]
+  resources :reqip, only: [:index]
 
-  get 'accessibilite', to: 'welcome#accessibility'
+  get "conditions-generales-d-utilisation", to: "welcome#terms"
+  get "welcome/index"
+  post "welcome/start_wizard"
 
-  get 'errors/not_found'
-  get 'errors/internal_server_error'
-  get 'sitemap.xml', :to => 'sitemap#index', :as => 'main_sitemap'
-  get 'aides-sitemap.xml', :to => 'sitemap#aides', :as => 'aides_sitemap'
-  get 'types-aides-sitemap.xml', :to => 'sitemap#types', :as => 'types_sitemap'
+  get "accessibilite", to: "welcome#accessibility"
 
+  get "errors/not_found"
+  get "errors/internal_server_error"
+  get "sitemap.xml", :to => "sitemap#index", :as => "main_sitemap"
+  get "aides-sitemap.xml", :to => "sitemap#aides", :as => "aides_sitemap"
+  get "types-aides-sitemap.xml", :to => "sitemap#types", :as => "types_sitemap"
 
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
-
 end
