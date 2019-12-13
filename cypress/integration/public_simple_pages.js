5b7b8cb4f963d82c384633059c218a6613a57bd8
David Boureau <bdavidxyz@gmail.com>
2019-12-13 11:48:28 +0100

    3tests

--- cypress/integration/public_simple_pages.js
+++ cypress/integration/public_simple_pages.js
@@ -18,4 +18,20 @@ describe("Pour un visiteur", function () {
     // then
     cy.get("body.c-body.welcome.accessibility").should("exist")
   })
+  it("On peut consulter la page de confidentialité depuis la page d'accueil", function () {
+    // given
+    cy.visit('/')
+    // when
+    cy.get('.c-footer-cgu').contains("Cookies et politique de confidentialité").click()
+    // then
+    cy.get("body.c-body.confidentiality.index").should("exist")
+  })
+  it("On peut consulter la page de contact depuis la page d'accueil", function () {
+    // given
+    cy.visit('/')
+    // when
+    cy.get('.c-footer-cgu').contains("Nous contacter").click()
+    // then
+    cy.get("body.c-body.contact.index").should("exist")
+  })
 })
