c5958b9b47b6db458827a1c3df64d27922e28518
David Boureau <bdavidxyz@gmail.com>
2019-12-16 13:31:28 +0100

    on peut saisir un montant à virgule

--- app/forms/are_form.rb
+++ app/forms/are_form.rb
@@ -3,5 +3,5 @@ class AreForm < ActiveType::Object
   attribute :minimum_income, :string
 
   validates :minimum_income, presence: true
-  validates :minimum_income, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :minimum_income, numericality: { greater_than_or_equal_to: 0}
 end
