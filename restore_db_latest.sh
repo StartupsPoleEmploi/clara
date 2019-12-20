497ca90f90c594b418647de686cf706f46eabb5d
sreg <sreg@freemobinaute.com>
2019-12-20 11:19:00 +0100

    Pointe ara_production sauf developpement

--- restore_db_latest.sh
+++ restore_db_latest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec clara_db bash -c "DB=\$ENV_TYPE; if [ \"\$DB\" = \"developpement\" ]; then DB=dev; fi;  pg_restore --verbose --clean -x -h localhost -S ara -d ara_\$DB /home/db/latest.dump"
+docker exec clara_db bash -c "DB=production; if [ \"\$DB\" = \"developpement\" ]; then DB=dev; fi;  pg_restore --verbose --clean -x -h localhost -S ara -d ara_\$DB /home/db/latest.dump"
