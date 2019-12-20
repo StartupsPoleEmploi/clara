9bb2f79fcf8cb1e4cfbc5a718ff06a2e1f8c363b
David Boureau <bdavidxyz@gmail.com>
2019-12-20 11:59:02 +0100

    working remove last char ruby

--- app/view_objects/history.rb
+++ app/view_objects/history.rb
@@ -15,23 +15,9 @@ class History < ViewObject
 
   def _remove_last_given_char_from_string(str, char)
     reversed_array = str.reverse.split("")
-    # p '- - - - - - - - - - - - - - reversed_array- - - - - - - - - - - - - - - -' 
-    # pp reversed_array
-    # p ''
     index_of_char_to_remove = reversed_array.find_index{|e| e == char}
-    # p '- - - - - - - - - - - - - - index_of_char_to_remove- - - - - - - - - - - - - - - -' 
-    # pp index_of_char_to_remove
-    # p ''
     reversed_array.delete_at(index_of_char_to_remove)
-    p '- - - - - - - - - - - - - - reversed_array- - - - - - - - - - - - - - - -' 
-    pp reversed_array
-    p ''
     reversed_array.reverse.join("")
-    # index_of_char_to_remove = str.reverse.split("").find_index{|e| e == char}
-    # p '- - - - - - - - - - - - - - index_of_char_to_remove- - - - - - - - - - - - - - - -' 
-    # pp index_of_char_to_remove
-    # p ''
-    # str.reverse.slice(index_of_char_to_remove).reverse
   end
 
 
