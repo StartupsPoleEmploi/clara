c79a18797c8c979bd0785908337a381d26f31e7b
David Boureau <bdavidxyz@gmail.com>
2019-12-20 11:55:53 +0100

    simple space ok

--- app/view_objects/history.rb
+++ app/view_objects/history.rb
@@ -9,7 +9,29 @@ class History < ViewObject
     arr.insert(3, "à")
     arr.pop
     joined = arr.join(" ")
-    joined.delete_suffix('min')
+    cleaned = joined.delete_suffix('min')
+    _remove_last_given_char_from_string(cleaned, ' ')
+  end
+
+  def _remove_last_given_char_from_string(str, char)
+    reversed_array = str.reverse.split("")
+    # p '- - - - - - - - - - - - - - reversed_array- - - - - - - - - - - - - - - -' 
+    # pp reversed_array
+    # p ''
+    index_of_char_to_remove = reversed_array.find_index{|e| e == char}
+    # p '- - - - - - - - - - - - - - index_of_char_to_remove- - - - - - - - - - - - - - - -' 
+    # pp index_of_char_to_remove
+    # p ''
+    reversed_array.delete_at(index_of_char_to_remove)
+    p '- - - - - - - - - - - - - - reversed_array- - - - - - - - - - - - - - - -' 
+    pp reversed_array
+    p ''
+    reversed_array.reverse.join("")
+    # index_of_char_to_remove = str.reverse.split("").find_index{|e| e == char}
+    # p '- - - - - - - - - - - - - - index_of_char_to_remove- - - - - - - - - - - - - - - -' 
+    # pp index_of_char_to_remove
+    # p ''
+    # str.reverse.slice(index_of_char_to_remove).reverse
   end
 
 
