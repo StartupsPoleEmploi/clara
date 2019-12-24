e356390c69130a18a3954269aafbae0820e27dac
David Boureau <bdavidxyz@gmail.com>
2019-12-24 15:05:52 +0100

    ok bummer

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,9 @@
 module Admin
   class UsersController < Admin::ApplicationController
-    # To customize the behavior of this controller,
-    # you can overwrite any of the RESTful actions. For example:
-    #
-    # def index
-    #   super
-    #   @resources = User.
-    #     page(params[:page]).
-    #     per(10)
-    # end
 
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   User.find_by!(slug: param)
-    # end
 
-    before_action :require_superadmin, only: [:udpate, :destroy, :edit, :show]
-    before_action :require_superadmin_or_relecteur, only: [:index]
+    before_action :require_superadmin_or_relecteur, only: [:update]
+    before_action :require_superadmin, only: [:update, :destroy, :edit, :show]
 
 
     def valid_action?(name, resource = resource_class)
