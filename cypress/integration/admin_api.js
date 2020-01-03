59e2216e18800cdf548c475616439bed45f53690
David Boureau <bdavidxyz@gmail.com>
2020-01-03 17:02:53 +0100

    wip

---
+++ cypress/integration/admin_api.js
@@ -0,0 +1,54 @@
+describe("Utiliser l'API", function() {
+
+  const ANSWER_SEL = "td.col.response-col_description div h5"
+  const EXEC_SEL = "button.btn.execute"
+  const TRY_SEL = "button.try-out__btn"
+
+  function first_steps(cy) {
+    cy.get(TRY_SEL).click()
+    cy.get(EXEC_SEL).should("exist")
+  }
+
+  function fire(cy) {
+    cy.get(TRY_SEL).click()
+    cy.get(EXEC_SEL).should("exist")
+  }
+
+  describe("Un simple ping", function () {
+    before(function () {
+      // given
+      cy.visit('/apidocs#/ping/getPing')
+    })
+    it("Doit répondre ok", function () {
+      // when
+      first_steps(cy)
+      cy.get(EXEC_SEL).click()
+      cy.get(ANSWER_SEL).first().should("exist")
+      cy.get(ANSWER_SEL).first().siblings().invoke('text').should(
+        (txt) => {expect(txt.replace(/[\s\n\r]+/g, '')).to.eq('{"status":"ok"}')}
+      )
+    })
+  })
+  describe("Authentification", function () {
+    before(function () {
+      // given
+      cy.visit('/apidocs#/user/api_user_token')
+    })
+    it("Doit répondre ok", function () {
+      first_steps(cy)
+      cy.get(".body-param__text").click()
+      cy.get('.body-param__text').clear().invoke('val', '{"auth":\n {\n   "email": "foo@bar.com",\n   "password": "Zecret+237"\n}\n}\n').trigger('input').then(
+(window) => {console.log("bla");}
+        );
+      cy.get(".body-param__text").click()
+      cy.get(".body-param__text").type(" ")
+      // cy.get(".body-param__text").type("text")
+      cy.get("a.tablinks").first().click()
+
+    })
+  })
+
+
+
+})
+
