f3a4c507895e1e788cc8a6a508e43ba4251cbd04
David Boureau <bdavidxyz@gmail.com>
2020-01-03 17:27:48 +0100

    filters works...

--- cypress/integration/admin_api.js
+++ cypress/integration/admin_api.js
@@ -3,17 +3,32 @@ describe("Utiliser l'API", function() {
   const ANSWER_SEL = "td.col.response-col_description div h5"
   const EXEC_SEL = "button.btn.execute"
   const TRY_SEL = "button.try-out__btn"
+  const INPUT_JWT_SEL = 'input[placeholder="Authorization - Bearer [your JWT token here]"]'
 
   let reusable_jwt_token;
 
   function first_steps(cy) {
-    cy.get(TRY_SEL).click()
+    cy.get(TRY_SEL).first().click()
     cy.get(EXEC_SEL).should("exist")
   }
 
   function fire(cy) {
     cy.get(EXEC_SEL).click()
     cy.get(ANSWER_SEL).first().should("exist")
+  }
+
+  function authenticate(cy) {
+    cy.get(INPUT_JWT_SEL)
+      .clear()
+      .invoke('val', reusable_jwt_token)
+      .trigger('input')
+    cy.get(INPUT_JWT_SEL).click()
+    cy.get(INPUT_JWT_SEL).type(" ")
+    cy.get("a.tablinks").first().click()
+  }
+
+  function actual_answer(cy) {
+    cy.get(ANSWER_SEL).first().siblings().invoke('text')
   }
 
   describe("Un simple ping", function () {
@@ -51,6 +66,21 @@ describe("Utiliser l'API", function() {
           reusable_jwt_token = jwt
           expect(reusable_jwt_token).to.have.length(140)
         }
+      )
+    })
+  })
+  describe("La liste des filtres", function () {
+    before(function () {
+      // given
+      cy.visit('/apidocs#/filters/getAllFilters')
+    })
+    it("Doit renvoyer la liste des filtres", function () {
+      // when
+      first_steps(cy)
+      authenticate(cy)
+      fire(cy)
+      cy.get(ANSWER_SEL).first().siblings().invoke('text').should(
+        (txt) => {expect(txt.replace(/[\s\n\r]+/g, '').indexOf('{"filters":[')).to.eq(0)}
       )
     })
   })
