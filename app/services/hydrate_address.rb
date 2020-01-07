e4d761bba66d9aa59a3e19f254242513dcb4487a
David Boureau <bdavidxyz@gmail.com>
2020-01-07 17:08:31 +0100

    green tests

--- app/services/hydrate_address.rb
+++ app/services/hydrate_address.rb
@@ -4,17 +4,14 @@ class HydrateAddress
     unless _valid_args(asker_attributes)
       raise ArgumentError.new("Arguments must be attributes of an Asker")
     end
-    p '- - - - - - - - - - - - - - asker_attributes- - - - - - - - - - - - - - - -' 
-    pp asker_attributes
-    p ''
     output_asker = Asker.new(asker_attributes)
-    citycode = output_asker.v_location_citycode
-    zipcode = output_asker.v_location_zipcode
+    citycode = output_asker.v_location_citycode || ""
+    zipcode = output_asker.v_location_zipcode || ""
     return output_asker if citycode.blank?
     output_asker.v_zrr = IsZrr.new.call(citycode)
-    # zip_city_region = GetZipCityRegion.new.call(citycode)
     city_name = GetCityName.new.call(citycode)
     output_asker.v_location_label = zipcode + " " + city_name
+    output_asker.v_location_city = city_name
     output_asker
   end
 
