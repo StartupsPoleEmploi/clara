ea60bb6c39adb19f5d11b208c6e793e014e7b695
David Boureau <bdavidxyz@gmail.com>
2020-01-13 15:49:15 +0100

    wip

--- app/assets/javascripts/administrate/custom/admin_steps/admin_stage_4.js
+++ app/assets/javascripts/administrate/custom/admin_steps/admin_stage_4.js
@@ -6,6 +6,32 @@ clara.js_define("admin_stage_4", {
 
     please: function() {
       clara.admin_stage_steps.please(4);
+
+
+      // detection change
+      function sth_changed() {
+        $("button#record_root_rule").removeAttr("disabled");
+      }
+      // hack to access to store_trundle modification
+      window.decision_tree_changed = function(){
+        sth_changed();
+      }
+      // hack to access to selectize modification
+      window.detect_selectize_change = function(){
+        sth_changed();
+      }
+      // radiobutton change
+      var originally_checked = $('.c-geowhere input[type="radio"]:checked').attr("id");
+      $('.c-geowhere input[type="radio"]').change(function(){
+        var now_selected_id = $('.c-geowhere input[type="radio"]:checked').attr("id")
+        if (now_selected_id !== originally_checked) {
+          sth_changed();
+        }
+      }) 
+
+
+
+
     }
     
 });
