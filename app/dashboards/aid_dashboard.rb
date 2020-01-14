cd118fbc6e461f98f43fafd6f3f229de94956f82
David Boureau <bdavidxyz@gmail.com>
2020-01-14 14:26:35 +0100

    better field size

--- app/dashboards/aid_dashboard.rb
+++ app/dashboards/aid_dashboard.rb
@@ -14,7 +14,7 @@ class AidDashboard < Administrate::BaseDashboard
     id: Field::Number.with_options(export: false),
     name: Field::String,
     status: Field::String,
-    zone_geo: Field::String,
+    zone_geo: Field::Text.with_options(truncate: 50000),
     source: Field::Text.with_options(export: false),
     ordre_affichage: Field::Number.with_options(export: false),
     archived_at: Field::DateTime,
