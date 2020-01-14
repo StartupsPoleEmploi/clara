bbe0b2efec1b1ce6959056fd1ad6bc186e6a8ba3
David Boureau <bdavidxyz@gmail.com>
2020-01-14 11:25:55 +0100

    another test

--- test/services/GetGeoZone_test.rb
+++ test/services/GetGeoZone_test.rb
@@ -35,12 +35,26 @@ class GetGeoZoneTest < ActiveSupport::TestCase
     assert_equal("Toute la France, à l'exception des villes Île-Tudy 29, Renescure 59.", res)
   end
 
+  test '.call a nice sentence if 2 departements only are filled' do
+    #given
+    allow_any_instance_of(ExtractGeoForAid).to receive(:call).and_return(_except_two_departements)
+    sut = GetGeoZone.new
+    #when
+    res = sut.call(nil)
+    #then
+    assert_equal("Toute la France, à l'exception des départements 01 Ain, 44 Loire-Atlantique.", res)
+  end
+
   def _except_one_per_type
     {"selection"=>"tout_sauf", "town"=>[{"29085"=>"Île-Tudy 29"}], "department"=>[{"01"=>"01 Ain"}], "region"=>[{"BRE"=>"Bretagne"}]}
   end
 
   def _except_two_towns
     {"selection"=>"tout_sauf", "town"=>[{"29085"=>"Île-Tudy 29"}, {"59497"=>"Renescure 59"}]}
+  end
+
+  def _except_two_departements
+    {"selection"=>"tout_sauf", "department"=>[{"01"=>"01 Ain"}, {"44"=>"44 Loire-Atlantique"}]}
   end
 
 end
