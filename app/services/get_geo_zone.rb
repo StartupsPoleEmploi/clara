a1deba7aba0547677ba7ad0d59138b4362021244
David Boureau <bdavidxyz@gmail.com>
2020-01-14 11:49:37 +0100

    working tests

--- app/services/get_geo_zone.rb
+++ app/services/get_geo_zone.rb
@@ -12,6 +12,8 @@ class GetGeoZone
       res = "Toute la France, à l'exception des DOM-TOM"
     elsif h["selection"] == "domtom_seulement"
       res = "Uniquement les DOM-TOM"
+    elsif h["selection"] == "rien_sauf"
+      res = "Uniquement #{_des_regions(h["region"], h["department"], h["town"], true)}#{_des_departements(h["department"], h["town"], true)}#{_des_villes(h["town"], true)}"
     end
     res
   end
@@ -20,12 +22,20 @@ class GetGeoZone
     array.is_a?(Array) && array.size > 0
   end
 
+  def _pronoum(type1, type2, type3, type4, is_defined, uniq)
+    if is_defined
+      uniq ? type3 : type4
+    else
+      uniq ? type1 : type2
+    end
+  end
 
-  def _des_regions(regions_array, departements_array, towns_array)
+
+  def _des_regions(regions_array, departements_array, towns_array, is_defined=false)
     res = ""
     if _isnt_empty(regions_array)
       uniq = regions_array.size === 1
-      res = "#{uniq ? "de la" : "des"} région#{uniq ? "" : "s"} #{regions_array.map{|d| d.values[0]}.join(', ')}"
+      res = "#{_pronoum("de la", "des", "la", "les", is_defined, uniq)} région#{uniq ? "" : "s"} #{regions_array.map{|d| d.values[0]}.join(', ')}"
       if (_isnt_empty(departements_array)) || (_isnt_empty(towns_array))
         res += "; "
       else
@@ -35,11 +45,11 @@ class GetGeoZone
     res
   end
 
-  def _des_departements(departements_array, towns_array)
+  def _des_departements(departements_array, towns_array, is_defined=false)
     res = ""
     if _isnt_empty(departements_array)
       uniq = departements_array.size === 1
-      res = "#{uniq ? "du" : "des"} département#{uniq ? "" : "s"} #{departements_array.map{|d| d.values[0]}.join(', ')}"
+      res = "#{_pronoum("du", "des", "le", "les", is_defined, uniq)} département#{uniq ? "" : "s"} #{departements_array.map{|d| d.values[0]}.join(', ')}"
       if _isnt_empty(towns_array)
         res += "; "
       else
@@ -48,11 +58,11 @@ class GetGeoZone
     end
   end
 
-  def _des_villes(towns_array)
+  def _des_villes(towns_array, is_defined=false)
     res = ""
     if _isnt_empty(towns_array)
       uniq = towns_array.size === 1
-      res = "#{uniq ? "de la" : "des"} ville#{uniq ? "" : "s"} #{towns_array.map{|d| d.values[0]}.join(', ')}."
+      res = "#{_pronoum("de la", "des", "la", "les", is_defined, uniq)} ville#{uniq ? "" : "s"} #{towns_array.map{|d| d.values[0]}.join(', ')}."
     end
     res
   end
