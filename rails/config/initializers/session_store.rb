abc062c556c74f6d67dfc4feeb4132fcb296f02b
David Boureau <bdavidxyz@gmail.com>
2020-01-23 09:34:07 +0100

    corrected cooky

--- rails/config/initializers/session_store.rb
+++ rails/config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_mae_session', expire_after: nil
-Rails.application.config.session_store :cookie_store, key: 'display_convention', expire_after: 24.hours
+Rails.application.config.session_store :cookie_store, key: 'hide_convention', expire_after: 24.hours
