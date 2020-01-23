215c389c94058ecac34cf16d3e9ac26c3307e556
David Boureau <bdavidxyz@gmail.com>
2020-01-23 12:11:43 +0100

    stage 2 full ok

--- rails/app/assets/javascripts/administrate/custom/admin_accordion.js
+++ rails/app/assets/javascripts/administrate/custom/admin_accordion.js
@@ -11,6 +11,9 @@ clara.js_define("admin_accordion", {
     $("#accordion_0-0_tab").html("<span class='field-unit__label-mandatory'>*</span><span>&nbsp;</span><span>" + $("#accordion_0-0_tab").html() + "</span>")
     $("#accordion_0-2_tab").html("<span class='field-unit__label-mandatory'>*</span><span>&nbsp;</span><span>" + $("#accordion_0-2_tab").html() + "</span>")
     $("#accordion_0-3_tab").html("<span class='field-unit__label-mandatory'>*</span><span>&nbsp;</span><span>" + $("#accordion_0-3_tab").html() + "</span>")
+
+    $("#accordion_0-1_tab").html("<span>" + $("#accordion_0-1_tab").html() + "</span><span>&nbsp;</span><span class='field-unit__label-optional'>Facultatif</span>")
+    $("#accordion_0-4_tab").html("<span>" + $("#accordion_0-4_tab").html() + "</span><span>&nbsp;</span><span class='field-unit__label-optional'>Facultatif</span>")
   }
 
 });
