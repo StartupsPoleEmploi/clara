a3a6aca106bae26cd035ac65c9cefc7fe4a80217
David Boureau <bdavidxyz@gmail.com>
2020-01-27 14:58:46 +0100

    with correct question

---
+++ rails/app/services/percentage_of_progression.rb
@@ -0,0 +1,10 @@
+class PercentageOfProgression
+
+ def call(the_question)
+    p '- - - - - - - - - - - - - - the_question- - - - - - - - - - - - - - - -' 
+    pp the_question
+    p ''
+   0.4
+ end
+  
+end
