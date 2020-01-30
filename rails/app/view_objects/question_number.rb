77d8e3fb743ddceee968fa173753bd12ad1c6469
David Boureau <bdavidxyz@gmail.com>
2020-01-30 12:05:26 +0100

    wip, reaching number correctly, but the the right one

--- rails/app/view_objects/question_number.rb
+++ rails/app/view_objects/question_number.rb
@@ -1,10 +1,91 @@
 class QuestionNumber < ViewObject
 
   def after_init(args)
+    @asker = _get_asker
   end
 
   def value
-    2.to_s
+    res = self.public_send('_value_for_' + _current_question)
+  end
+
+  def _get_asker
+    Asker.new(JSON.parse(@context.session[:asker].to_s))
+  end
+
+  def _current_question
+    @context.request.path.split("_questions")[0][1..-1]
+  end
+
+  def _value_for_inscription
+    1
+  end
+
+  def _value_for_category
+    2
+  end
+
+  def _value_for_allocation
+    res = 3
+    if _isnt_inscrit(@asker)
+      res = 2
+    end
+    res
+  end
+
+  def _value_for_are
+    res = 4
+    if _isnt_inscrit(@asker)
+      res = 3
+    end
+    res
+  end
+
+  def _value_for_age
+    res = 5
+    if _isnt_inscrit(@asker) && _isnt_montant(@asker)
+      res = 3
+    elsif _isnt_inscrit(@asker) || _isnt_montant(@asker)
+      res = 4
+    end
+    res
+  end
+
+  def _value_for_grade
+    res = 6
+    if _isnt_inscrit(@asker) && _isnt_montant(@asker)
+      res = 4
+    elsif _isnt_inscrit(@asker) || _isnt_montant(@asker)
+      res = 5
+    end
+    res
+  end
+
+  def _value_for_address
+    res = 7
+    if _isnt_inscrit(@asker) && _isnt_montant(@asker)
+      res = 5
+    elsif _isnt_inscrit(@asker) || _isnt_montant(@asker)
+      res = 6
+    end
+    res
+  end
+
+  def _value_for_other
+    res = 8
+    if _isnt_inscrit(@asker) && _isnt_montant(@asker)
+      res = 6
+    elsif _isnt_inscrit(@asker) || _isnt_montant(@asker)
+      res = 7
+    end
+    res
+  end
+
+  def _isnt_inscrit(asker)
+    asker.v_duree_d_inscription == 'non_inscrit'
+  end
+
+  def _isnt_montant(asker)
+    asker.v_allocation_type == 'ASS_AER_APS_AS-FNE' || asker.v_allocation_type == 'ARE_ASP'
   end
 
 end
