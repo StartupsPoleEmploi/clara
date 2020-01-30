1e8be388bc15239cf3b1ac7be3dafcdb7eb8fa12
David Boureau <bdavidxyz@gmail.com>
2020-01-30 14:58:23 +0100

    ok

--- rails/test/services/QuestionNumber_test.rb
+++ rails/test/services/QuestionNumber_test.rb
@@ -29,6 +29,15 @@ class QuestionNumberTest < ActiveSupport::TestCase
     #then
     assert_equal(2, res)
   end
+  test '.value for question "allocation" is 3 if asker is "inscrit"' do
+    #given
+    allow_any_instance_of(QuestionNumber).to receive(:_get_asker).and_return(Asker.new)
+    allow_any_instance_of(QuestionNumber).to receive(:_current_question).and_return("allocation")
+    #when
+    res = QuestionNumber.new(nil).value
+    #then
+    assert_equal(3, res)
+  end
 
   def _unsubcribed_asker(existing_asker=nil)
     res = existing_asker || Asker.new
