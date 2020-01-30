90341ea0c85f7597c619644acc62500cf320f2f8
David Boureau <bdavidxyz@gmail.com>
2020-01-30 14:54:20 +0100

    more tests

---
+++ rails/test/services/QuestionNumber_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class QuestionNumberTest < ActiveSupport::TestCase
+  
+  test '.value for question "inscription" is always 1' do
+    #given
+    allow_any_instance_of(QuestionNumber).to receive(:_get_asker).and_return(Asker.new)
+    allow_any_instance_of(QuestionNumber).to receive(:_current_question).and_return("inscription")
+    #when
+    res = QuestionNumber.new(nil).value
+    #then
+    assert_equal(1, res)
+  end
+  test '.value for question "category" is always 2' do
+    #given
+    allow_any_instance_of(QuestionNumber).to receive(:_get_asker).and_return(Asker.new)
+    allow_any_instance_of(QuestionNumber).to receive(:_current_question).and_return("inscription")
+    #when
+    res = QuestionNumber.new(nil).value
+    #then
+    assert_equal(1, res)
+  end
+end
