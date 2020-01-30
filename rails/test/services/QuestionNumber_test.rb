80c93ee1c7f202472ee61bccc50787e156ec1611
David Boureau <bdavidxyz@gmail.com>
2020-01-30 15:04:43 +0100

    ok 2 tests

--- rails/test/services/QuestionNumber_test.rb
+++ rails/test/services/QuestionNumber_test.rb
@@ -11,15 +11,15 @@ class QuestionNumberTest < ActiveSupport::TestCase
     #then
     assert_equal(1, res)
   end
-  # test '.value for question "category" is always 2' do
-  #   #given
-  #   allow_any_instance_of(QuestionNumber).to receive(:_get_asker).and_return(Asker.new)
-  #   allow_any_instance_of(QuestionNumber).to receive(:_current_question).and_return("category")
-  #   #when
-  #   res = QuestionNumber.new(nil).value
-  #   #then
-  #   assert_equal(2, res)
-  # end
+  test '.value for question "category" is always 2' do
+    #given
+    allow_any_instance_of(QuestionNumber).to receive(:_get_asker).and_return(Asker.new)
+    allow_any_instance_of(QuestionNumber).to receive(:_current_question).and_return("category")
+    #when
+    res = QuestionNumber.new(nil).value
+    #then
+    assert_equal(2, res)
+  end
   # test '.value for question "allocation" is 2 if asker is "non_inscrit"' do
   #   #given
   #   allow_any_instance_of(QuestionNumber).to receive(:_get_asker).and_return(_unsubcribed_asker)
