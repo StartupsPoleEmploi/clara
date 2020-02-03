d45391c51c8732a4ea244553db43188ac56b1a92
David Boureau <bdavidxyz@gmail.com>
2020-02-03 11:52:12 +0100

    allok ok

--- rails/app/assets/javascripts/custom/allocation_questions.js
+++ rails/app/assets/javascripts/custom/allocation_questions.js
@@ -1,5 +1,16 @@
-clara.load_js(function only_if(){return $("body").hasClasses("allocation_questions", "new")}, function() {
+// clara.load_js(function only_if(){return $("body").hasClasses("allocation_questions", "new")}, function() {
 
-  $("input:radio:first").focus();
+//   $("input:radio:first").focus();
   
+// });
+
+clara.js_define("new_allocation_question", {
+
+    please: function() {
+      if ($(".c-error-in-form").exists()) {
+            $($(".c-error-in-form").detach()).appendTo(".c-fieldset");
+      }
+    },
+
 });
+
