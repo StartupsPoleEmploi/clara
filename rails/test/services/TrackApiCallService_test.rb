f7f4abf329d9ad472ac5db2e12b0b5bdaf872705
David Boureau <bdavidxyz@gmail.com>
2020-02-03 16:44:51 +0100

    with tests

---
+++ rails/test/services/TrackApiCallService_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class TrackApiCallServiceTest < ActiveSupport::TestCase
+  
+  test '._tracked return given arg if it is a string' do
+    #given
+    given_arg = "a String"
+    #when
+    result = TrackApiCallService.new._tracked(given_arg)
+    #then
+    assert_equal(given_arg, result)
+  end
+  test '._tracked do not return given arg if it ISNT a string' do
+    #given
+    given_arg = 42
+    #when
+    result = TrackApiCallService.new._tracked(given_arg)
+    #then
+    assert_not_equal(given_arg, result)
+  end
+  test '._tracked returns user_not_found@clara.com if input is nil' do
+    #given
+    given_arg = nil
+    #when
+    result = TrackApiCallService.new._tracked(given_arg)
+    #then
+    assert_not_equal("user_not_found@clara.com", result)
+  end
+end
