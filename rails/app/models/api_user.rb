ef1e466c9607c2d34aae198a0c62de1a9a339a4b
David Boureau <bdavidxyz@gmail.com>
2020-02-04 08:25:58 +0100

    wip, not working

--- rails/app/models/api_user.rb
+++ rails/app/models/api_user.rb
@@ -14,6 +14,8 @@ class ApiUser < ApplicationRecord
 
   # See https://github.com/nsarno/knock/blob/v2.1.1/README.md#customization
   def self.from_token_payload payload
+    p '- - - - - - - - - - - - - - from_token_payload- - - - - - - - - - - - - - - -' 
+    p ''
     user_id = payload["sub"]
     # Returns a valid user, `nil` or raise
     Rails.cache.fetch("apiuser:#{user_id}") { self.find(user_id) if payload['class'] && payload['class'] == to_s }
@@ -21,6 +23,8 @@ class ApiUser < ApplicationRecord
 
   # See https://github.com/nsarno/knock/blob/v2.1.1/README.md#customization
   def self.from_token_request request
+    p '- - - - - - - - - - - - - - from_token_request- - - - - - - - - - - - - - - -' 
+    p ''
     return nil unless request.respond_to?(:params) && request.params["auth"] && request.params["auth"]["email"]
     email = request.params["auth"]["email"]
     Rails.cache.fetch("apiuser_email:#{email}") { self.find_by(email: email) }
@@ -29,6 +33,8 @@ class ApiUser < ApplicationRecord
   private
   
   def invalidate_cache
+    p '- - - - - - - - - - - - - - invalidate_cache- - - - - - - - - - - - - - - -' 
+    p ''
     Rails.cache.delete("apiuser:#{id}")
     Rails.cache.delete("apiuser_email:#{email}")
   end
