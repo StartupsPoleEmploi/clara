9454acf5942c4704ecaa79446aca5655eaa5dc3f
David Boureau <bdavidxyz@gmail.com>
2020-02-04 12:22:04 +0100

    ok done

--- rails/app/view_objects/back_question.rb
+++ rails/app/view_objects/back_question.rb
@@ -1,6 +1,8 @@
 class BackQuestion < ViewObject
 
   def after_init(args)
+    locals = hash_for(args)
+    @back_to_previous = string_for(locals[:back_to_previous])
   end
 
   def _get_asker
@@ -13,6 +15,22 @@ class BackQuestion < ViewObject
 
   def url
     QuestionManager.new.public_send('before_' + _current_question, _get_asker)
+  end
+
+  def onclick_value
+    res = "window.location.href='#{url}'"
+    if @back_to_previous == "yes"
+      res = "window.history.back()"
+    end
+    res
+  end
+
+  def actual_text
+    res = "Retour à la question précédente"
+    if @back_to_previous == "yes"
+      res = "Retour à la page précédente"
+    end
+    res
   end
 
 end
