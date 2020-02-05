c66b5a304e9ab709e7a58ac7429d9ea448f66a4d
David Boureau <bdavidxyz@gmail.com>
2020-02-05 09:39:15 +0100

    wip

--- rails/app/assets/javascripts/custom/new_address_question.js
+++ rails/app/assets/javascripts/custom/new_address_question.js
@@ -1,9 +1,9 @@
 clara.js_define("new_address_question", {
 
     please: function() {
-      if ($(".c-error-in-form").exists()) {
-        $($(".c-error-in-form").detach()).appendTo(".c-address__content");
-      }
+      // if ($(".c-error-in-form").exists()) {
+      //   $($(".c-error-in-form").detach()).appendTo(".c-address__content");
+      // }
     },
 
 });
