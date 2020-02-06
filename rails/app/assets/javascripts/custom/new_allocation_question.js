abd8efc6ad46332da1ddf2eaa8e295f43b5af1e5
David Boureau <bdavidxyz@gmail.com>
2020-02-06 15:33:25 +0100

    ageform redo

--- rails/app/assets/javascripts/custom/new_allocation_question.js
+++ rails/app/assets/javascripts/custom/new_allocation_question.js
@@ -3,7 +3,8 @@ clara.js_define("new_allocation_question", {
     please: function() {
       $("input:radio:first").focus();
       if ($(".c-error-in-form").exists()) {
-            $($(".c-error-in-form").detach()).appendTo(".c-fieldset");
+        $($(".c-error-in-form").detach()).appendTo(".c-fieldset");
+        $(".c-error-in-form").css("margin-top", "13px")
       }
     },
 
