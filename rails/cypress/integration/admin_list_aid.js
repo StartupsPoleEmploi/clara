3fc3e0bdc956ff5aee124b98647a499b7f92cbcc
David Boureau <bdavidxyz@gmail.com>
2020-02-25 14:45:00 +0100

    well-tested

--- rails/cypress/integration/admin_list_aid.js
+++ rails/cypress/integration/admin_list_aid.js
@@ -1,5 +1,24 @@
 describe("Lister les aides", function() {
 
+  describe("Pour un superadmin", function() {
+    before(function() {
+      // given
+      cy.connect_as_superadmin()
+      // when
+      cy.visit('/admin/aids')
+
+    })
+
+    it("Pour un superadmin, la suppression d'une aide est possible", function() {
+      // then
+      cy.get('a.js-delete-aid').should('exist')
+    })  
+    it("Pour un superadmin, l'archivage d'une aide est possible", function() {
+      // then
+      cy.get('a.js-archive-aid').should('exist')
+    })  
+  })
+
   describe("Pour un relecteur", function() {
     before(function() {
       // given
@@ -9,9 +28,13 @@ describe("Lister les aides", function() {
 
     })
 
-    it("Pour un relecteur, la suppression d'une aide est possible", function() {
+    it("Pour un relecteur, la suppression d'une aide n'est pas possible", function() {
       // then
-      cy.get('a.js-delete-aid').should('exist')
+      cy.get('a.js-delete-aid').should('not.exist')
+    })  
+    it("Pour un relecteur, l'archivage d'une aide est pas possible", function() {
+      // then
+      cy.get('a.js-archive-aid').should('exist')
     })  
   })
 
@@ -21,13 +44,16 @@ describe("Lister les aides", function() {
       cy.connect_as_contributeur1()
       // when
       cy.visit('/admin/aids')
-
     })
 
-    it("Pour un contributeur, aucune aide ne peut être supprimée", function() {
+    it("Pour un contributeur, la suppression d'une aide n'est pas possible", function() {
       // then
       cy.get('a.js-delete-aid').should('not.exist')
     })  
+    it("Pour un contributeur, l'archivage d'une aide n'est pas possible", function() {
+      // then
+      cy.get('a.js-archive-aid').should('not.exist')
+    }) 
   })
 
 })
