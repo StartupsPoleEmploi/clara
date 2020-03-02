c41e5af8e3b53253c3ed716ac918b4bf49ff786b
David Boureau <bdavidxyz@gmail.com>
2020-03-02 16:13:33 +0100

    scope ok

--- rails/app/models/filter.rb
+++ rails/app/models/filter.rb
@@ -21,7 +21,22 @@ class Filter < ApplicationRecord
 
   has_and_belongs_to_many :aids
   validates :name, presence: true, uniqueness: true
+
+  # scope :by_role,  ->(role) { joins(:roles).where(roles: { name: role }) }
+  # scope :without_feed, joins('left outer join authors_feeds on authors.id=authors_feeds.author_id').where('authors_feeds.feed_id is null')
+  # scope :without_aid, joins('left outer join aids_filters on filters.id=aids_filters.filter_id').where('aids_filters.aid_id is null')
   
+  # scope :not_in_any_group, -> {
+  #     joins("LEFT JOIN groups_users ON users.id = groups_users.user_id")
+  #     .where("groups_users.user_id IS NULL")
+  # }
+
+  scope :not_in_any_group, -> {
+    joins("LEFT JOIN aids_filters ON filters.id = aids_filters.filter_id")
+    .where("aids_filters.filter_id IS NULL")
+  }
+  
+  scope :with_aid_attached, -> { where.not(id: not_in_any_group) }
 
   friendly_id :name, use: :slugged
 
