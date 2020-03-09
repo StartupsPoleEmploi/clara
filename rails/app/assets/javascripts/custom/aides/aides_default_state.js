e4e71f1c230683a574cbdc86ba832930c393ed28
David Boureau <bdavidxyz@gmail.com>
2020-03-09 10:52:32 +0100

    ok works

--- rails/app/assets/javascripts/custom/aides/aides_default_state.js
+++ rails/app/assets/javascripts/custom/aides/aides_default_state.js
@@ -3,6 +3,7 @@ clara.js_define("aides_default_state", {
   please_if: _.stubFalse,
 
   please: function() {
+    var that = clara.aides_default_state;
     var current_state = clara.aides_initial_state.please();
     var previous_state = clara.aides_get_state.please();
     var has_state = _.isPlainObject(previous_state) && _.isNotEmpty(previous_state);
@@ -14,7 +15,21 @@ clara.js_define("aides_default_state", {
         result = previous_state;
       }
     }
+    result = that._merge_with_filters_state(result);
     return result;
+  },
+
+  _merge_with_filters_state: function(state) {
+    var localState = _.cloneDeep(state);
+    var filters = _.get(localState, "filters_zone.filters");
+    var choosen_filters = []
+    if (window.localStorage) {
+      choosen_filters = JSON.parse(localStorage.getItem("choosen_filters"));
+    }
+    _.each(filters, function(filter) {
+      filter.is_checked = _.includes(choosen_filters, filter.name)
+    })
+    return localState;
   }
 
 });
--- rails/app/assets/javascripts/custom/aides/aides_initial_state.js
+++ rails/app/assets/javascripts/custom/aides/aides_initial_state.js
@@ -3,7 +3,6 @@ clara.js_define("aides_initial_state", {
   please_if: _.stubFalse,
 
   please: function(eligy) {
-    var that = clara.aides_initial_state;
     return {
       width: $( window ).width(),
       eligibles_zone: {
@@ -18,22 +17,12 @@ clara.js_define("aides_initial_state", {
       },
       filters_zone: {
         is_collapsed: true,
-        filters: _.map(clara.aides_collect_filters_name.please(), function(e){return {name: e, is_checked: that._is_checked(e), updated_at : 0}})
+        filters: _.map(clara.aides_collect_filters_name.please(), function(e){return {name: e, is_checked: false, updated_at : 0}})
       },
       recap_zone: {
         is_collapsed: true
       }
     }
-  },
-
-  _is_checked: function(filter_slug) {
-    var result = false;
-    var choosen_filters = [];
-    if (window.localStorage) {
-      choosen_filters = JSON.parse(localStorage.getItem("choosen_filters"));
-      result = _.includes(choosen_filters, filter_slug)
-    }
-    return result;
   }
 
 });
