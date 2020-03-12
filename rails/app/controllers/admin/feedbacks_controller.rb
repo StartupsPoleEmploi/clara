b1166a57922b76eb966643fb8f85e4eea48385bf
David Boureau <bdavidxyz@gmail.com>
2020-03-12 15:50:25 +0100

    ok done

--- rails/app/controllers/admin/feedbacks_controller.rb
+++ rails/app/controllers/admin/feedbacks_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class FeedbacksController < Admin::ApplicationController
 
     def valid_action?(name, resource = resource_class)
-      %w[new destroy edit].exclude?(name.to_s) && super
+      %w[new edit].exclude?(name.to_s) && super
     end
 
   end
