1756d11a1b1fa9c8c0b93114c962d19f3f947f18
David Boureau <bdavidxyz@gmail.com>
2020-03-25 18:40:24 +0100

    debug date

--- rails/app/dashboards/recall_dashboard.rb
+++ rails/app/dashboards/recall_dashboard.rb
@@ -22,7 +22,6 @@ class RecallDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  id
   aid
   trigger_at
   email
@@ -31,7 +30,6 @@ class RecallDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  id
   aid
   trigger_at
   email
