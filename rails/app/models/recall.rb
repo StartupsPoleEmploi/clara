217cb54ce04036ae4939b1b8e6b56eec32bc24da
David Boureau <bdavidxyz@gmail.com>
2020-03-25 18:23:16 +0100

    various screens ok

--- rails/app/models/recall.rb
+++ rails/app/models/recall.rb
@@ -1,5 +1,6 @@
 class Recall  < ApplicationRecord
   belongs_to :aid, optional: false
   validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :trigger_at, presence: true
 end
