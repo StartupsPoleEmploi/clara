a40c0a214b17f1db31100072026cbed93e4d54d9
David Boureau <bdavidxyz@gmail.com>
2020-03-26 22:07:34 +0100

    a working scope

--- rails/app/dashboards/recall_dashboard.rb
+++ rails/app/dashboards/recall_dashboard.rb
@@ -23,6 +23,7 @@ class RecallDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+  status
   aid
   trigger_at
   email
@@ -43,6 +44,7 @@ class RecallDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+  status
   aid
   trigger_at
   email
