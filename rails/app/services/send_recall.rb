387b91d9d2967c06d8dfab904230d6d04759eaf6
David Boureau <bdavidxyz@gmail.com>
2020-03-27 10:55:57 +0100

    time on the hompage

--- rails/app/services/send_recall.rb
+++ rails/app/services/send_recall.rb
@@ -1,7 +1,14 @@
 class SendRecall
 
   def call
-    
+    if _time_to_send_email?
+      UserMailer.with(contact: @contact, origin: @origin).contact_email.deliver_now            
+    end
+  end
+
+  def _time_to_send_email?
+    now = DateTime.now
+    now.between?(DateTime.now.change(hour:7, min:30), DateTime.now.change(hour:8, min:30))    
   end
 
 end
