f9bc542bc5aa842035c35a28c93044d214d017ad
David Boureau <bdavidxyz@gmail.com>
2020-03-30 17:32:44 +0200

    changed time

--- rails/app/services/send_recall.rb
+++ rails/app/services/send_recall.rb
@@ -36,7 +36,7 @@ class SendRecall
 
   def _time_to_send_email?
     now = DateTime.now
-    now.between?(DateTime.now.change(hour:7, min:30), DateTime.now.change(hour:8, min:30))    
+    now.between?(DateTime.now.change(hour:7, min:15), DateTime.now.change(hour:8, min:30))    
   end
 
 end
