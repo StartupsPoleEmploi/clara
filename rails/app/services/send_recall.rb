034d4877f5c7c8e3919aff0e4941e47570314c3a
David Boureau <bdavidxyz@gmail.com>
2020-04-01 16:04:56 +0200

    ok working delta hours

--- rails/app/services/send_recall.rb
+++ rails/app/services/send_recall.rb
@@ -1,6 +1,8 @@
 class SendRecall
 
   def call(is_forced, root_url)
+    p "- - - - - - - - - - - - - - _time_to_send_email? #{_time_to_send_email?}- - - - - - - - - - - - - - - -"
+    p "- - - - - - - - - - - - - - is_forced #{is_forced}- - - - - - - - - - - - - - - -"
     if _time_to_send_email? || is_forced
       recall_to_be_sent = Recall.please_send.first
       if recall_to_be_sent
@@ -20,7 +22,14 @@ class SendRecall
   def _time_to_send_email?
     now = DateTime.now
     delta = Clockdiff.first.value
-    now.between?(DateTime.now.change(hour: 7+delta, min: 15), DateTime.now.change(hour: 8+delta, min: 30))    
+    now_delta = now.change(hour: now.hour + delta)
+    lo = now.change(hour: 7, min: 15)
+    hi = now.change(hour: 8, min: 30)
+    p "- - - - - - - - - - - - - - now #{now}- - - - - - - - - - - - - - - -"
+    p "- - - - - - - - - - - - - - now_delta #{now_delta}- - - - - - - - - - - - - - - -"
+    p "- - - - - - - - - - - - - - lo #{lo}- - - - - - - - - - - - - - - -"
+    p "- - - - - - - - - - - - - - hi #{hi}- - - - - - - - - - - - - - - -"
+    now_delta.between?(lo, hi)    
   end
 
 end
