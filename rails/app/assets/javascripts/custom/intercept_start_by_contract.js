e6ec84145ceef20295178343a3573cb505ae246d
David Boureau <bdavidxyz@gmail.com>
2020-04-29 16:23:00 +0200

    filter table

--- rails/app/assets/javascripts/custom/intercept_start_by_contract.js
+++ rails/app/assets/javascripts/custom/intercept_start_by_contract.js
@@ -5,16 +5,21 @@ clara.js_define("intercept_start_by_contract", {
     },
 
     please: function() {
+      var FILTER_TABLE = {
+        "aide-a-la-mobilite": "se-deplacer",
+        
+      }
       $(".c-detail-cta.js-contract").click(function(e){
           e.preventDefault();
           var href = this.href;
 
-          localStorage.setItem("choosen_filters", JSON.stringify(new_choosen_filters));          
+          localStorage.setItem("choosen_filters", JSON.stringify({}));          
 
           console.log("hello")
+          console.log(_.last(window.location.pathname.split("/")))
           location.href = href;
       });
-    }
+    },
 
 });
 
