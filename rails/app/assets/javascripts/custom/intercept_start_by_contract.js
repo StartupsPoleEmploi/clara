74b9f16ff91751dba0319d706433ffd9cc904db6
David Boureau <bdavidxyz@gmail.com>
2020-04-29 17:33:44 +0200

    ok done ?

--- rails/app/assets/javascripts/custom/intercept_start_by_contract.js
+++ rails/app/assets/javascripts/custom/intercept_start_by_contract.js
@@ -6,13 +6,15 @@ clara.js_define("intercept_start_by_contract", {
 
     please: function() {
       var FILTER_TABLE = {
-        "aide-a-la-mobilite": "se-deplacer",
-        "aide-a-la-creation-ou-reprise-d-entreprise": "creer-entreprise",
-        "contrat-specifique": "s-informer-sur-contrats-specifiques",
-        "aide-regionale": "pres_de_chez_vous",
-        "emploi-international": "travailler-a-l-international",
+        "appui-a-l-embauche": "accompagne-recherche-emploi",
         "aide-a-la-definition-du-projet-professionnel": "trouver-change-de-metier",
-
+        "aide-a-la-mobilite": "se-deplacer",
+        "dispositif-de-conversion-d-experience-en-titre": "se-former-valoriser-ses-competences",
+        "aide-a-la-creation-ou-reprise-d-entreprise": "creer-entreprise",
+        "emploi-international": "travailler-a-l-international",
+        "contrat-specifique": "s-informer-sur-contrats-specifiques",
+        "financement-aide-a-la-formation": "se-former-valoriser-ses-competences",
+        "aide-regionale": "pres_de_chez_vous",
       }
       var current_contract = _.last(window.location.pathname.split("/"))
       var matching_filter = FILTER_TABLE[current_contract]
