ce8366d1bf0b06fcb815139d5d08b7ef5cbe9c6a
David Boureau <bdavidxyz@gmail.com>
2020-05-05 15:38:32 +0200

    red test first

--- rails/cypress/integration/public_prechecking.js
+++ rails/cypress/integration/public_prechecking.js
@@ -12,13 +12,20 @@ describe("En tant que visiteur DDCT", function () {
     // cy.get('input#travailler-a-l-international').should('be.checked') 
     // cy.get('input#pres_de_chez_vous').should('be.checked') 
 
-  it("Si je clique JE COMMENCE, le filtre correspondant à la formation est précoché à la question filtres", function () {
+  it("Financement, aide à la formation : Si je clique JE COMMENCE, le(s) filtre(s) correspondant(s) à la formation est précoché à la question filtres", function () {
     cy.visit('/aides/type/financement-aide-a-la-formation')
     cy.get('.c-detail-cta.js-contract').click()
     cy.visit('/filter_questions/new')
     cy.get('input#se-former-valoriser-ses-competences').should('be.checked') 
     cy.get('input#percevoir-une-remuneration-pendant-la-formation').should('be.checked') 
     cy.get('input#se-deplacer').should('be.checked') 
+  })
+
+  it("Aide pour être accompagné dans sa recherche : Si je clique JE COMMENCE, le(s) filtre(s) correspondant(s) à la formation est précoché à la question filtres", function () {
+    cy.visit('/aides/type/appui-a-l-embauche')
+    cy.get('.c-detail-cta.js-contract').click()
+    cy.visit('/filter_questions/new')
+    cy.get('input#se-former-valoriser-ses-competences').should('be.checked') 
   })
 
 })
