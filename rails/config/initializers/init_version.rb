cfcd1e5954cb2357ed8edf0ea7c81924608e9874
David Boureau <bdavidxyz@gmail.com>
2020-05-18 19:37:07 +0200

    undo tools checking for tests

--- rails/config/initializers/init_version.rb
+++ rails/config/initializers/init_version.rb
@@ -5,5 +5,6 @@ lines = File.foreach(file_location).first(10)
 ARA_VERSION = lines.detect {|e| e.start_with?("## ")}.split("## ")[1].split(" (")[0]
 ARA_EXT_URL = ENV.to_h.select { |key, value| key.to_s.match(/^ARA_URL/) }
 
-
-Rake::Task['about'].invoke
+unless Rails.env.test?
+  Rake::Task['about'].invoke
+end
