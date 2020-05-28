39c1d102d44baf6b6ced562df19091b6af001fa3
David Boureau <bdavidxyz@gmail.com>
2020-05-28 09:43:28 +0200

    ok done?

--- rails/app/assets/javascripts/custom/welcome_index.js
+++ rails/app/assets/javascripts/custom/welcome_index.js
@@ -1,5 +1,6 @@
 clara.load_js(function only_if(){return $("body").hasClasses("welcome", "index")}, function() {
 
+  var local_ga = _.get(window, "ga");
   store.clearAll();
   console.log("Version : " + clara.version);
 
@@ -24,6 +25,27 @@ clara.load_js(function only_if(){return $("body").hasClasses("welcome", "index")
     var html = '<iframe id="video-clara" width="720" height="405" src="https://www.youtube.com/embed/8IDnLjfAt5U?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
     $(".c-video-container").html(html);
     setTimeout(setFocusIframe, 500);
+  });
+
+  $(".c-chip-crea").click(function(){
+    if (typeof local_ga === "function") {
+      local_ga('send', 'event', 'home', 'chip', 'creation');
+    }    
+  });
+  $(".c-chip-amob").click(function(){
+    if (typeof local_ga === "function") {
+      local_ga('send', 'event', 'home', 'chip', 'mobilite');
+    }    
+  });
+  $(".c-chip-formation").click(function(){
+    if (typeof local_ga === "function") {
+      local_ga('send', 'event', 'home', 'chip', 'formation');
+    }    
+  });
+  $(".c-chip-projetpro").click(function(){
+    if (typeof local_ga === "function") {
+      local_ga('send', 'event', 'home', 'chip', 'projetpro');
+    }    
   });
 
 
