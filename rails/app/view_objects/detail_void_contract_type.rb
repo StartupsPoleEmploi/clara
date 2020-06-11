05bd70b23ce0f320354efdadfebc3155bea7cfc1
David Boureau <bdavidxyz@gmail.com>
2020-06-11 17:44:31 +0200

    renamed c-link-to-ct to c-footer-link

--- rails/app/view_objects/detail_void_contract_type.rb
+++ rails/app/view_objects/detail_void_contract_type.rb
@@ -9,7 +9,7 @@ class DetailVoidContractType < ViewObject
     Rails.cache.fetch("all_but_current_contract", expires_in: 1.hour) do
       all_contracts = ActivatedModelsService.instance.contracts.map do |c| { 
         name: c["name"], 
-        link: @context.link_to(c["name"], type_path(c["slug"]), {"class" => "c-link-to-ct"})  
+        link: @context.link_to(c["name"], type_path(c["slug"]), {"class" => "c-footer-link"})  
       } 
       end
       all_contracts.reject{|ct| ct[:name] == @contract[:name]}
