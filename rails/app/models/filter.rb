b0c89178f76a9fac49d31847134db669c4c4d010
David Boureau <bdavidxyz@gmail.com>
2020-06-23 16:09:38 +0200

    wip by validation

--- rails/app/models/filter.rb
+++ rails/app/models/filter.rb
@@ -9,7 +9,7 @@
 #  description :string
 #  slug        :string
 #
-
+require "image_processing/mini_magick"
 class Filter < ApplicationRecord
   extend FriendlyId
 
@@ -18,7 +18,23 @@ class Filter < ApplicationRecord
   after_destroy { ExpireCacheJob.perform_later } if Rails.env.production?
   after_create  { ExpireCacheJob.perform_later } if Rails.env.production?
 
+  # validate :attachment_format
+  validates :attachment, attached: true, content_type: { in: 'application/pdf', message: 'is not a PDF' }
   has_one_attached :attachment
+
+  def resize_image
+    # p '- - - - - - - - - - - - - - attachment_changes- - - - - - - - - - - - - - - -' 
+    # ap attachment_changes['attachment'].attachable
+    # p ''
+    # unless attachment_changes['attachment'].attachable.is_a?(Hash)
+    #   resized_image = MiniMagick::Image.read(attachment_changes['attachment'].attachable)
+    #   resized_image = resized_image.resize "240x240"
+    #   v_filename = attachment.filename
+    #   v_content_type = attachment.content_type
+    #   attachment.purge
+    #   attachment.attach(io: File.open(resized_image.path), filename:  v_filename, content_type: v_content_type)
+    # end
+  end
 
   has_and_belongs_to_many :aids
   validates :name, presence: true, uniqueness: true
@@ -34,6 +50,23 @@ class Filter < ApplicationRecord
 
   def should_generate_new_friendly_id?
     slug.blank?
+  end
+
+  private
+
+  def attachment_format
+   return unless attachment.attached?
+   if attachment.blob.content_type.start_with? 'image/'
+     if attachment.blob.byte_size > 5.megabytes
+       errors.add(:attachment, 'La taille doit être inférieur à 5 Mo')
+       attachment.purge
+     else
+       resize_image
+     end
+    else
+      attachment.purge
+      errors.add(:attachment, 'La photo doit être une image JPG')
+    end
   end
 
 end
