bb1504654e348a78f4f45975dd82430981b187a5
David Boureau <bdavidxyz@gmail.com>
2020-06-25 14:27:16 +0200

    wip, displaying by pair on frontpage

--- rails/app/controllers/welcome_controller.rb
+++ rails/app/controllers/welcome_controller.rb
@@ -3,22 +3,23 @@ class WelcomeController < ApplicationController
   def index
     SendRecallJob.perform_later(request && request.params[:force] == "true", request.try(:original_url))
     clean_asker_params
-    all_home_filters = Filter.homable.map { |e| {slug: e.slug, url: url_for(e.attachment)} }
-
+    all_home_filters = Filter.homable.map { |e| {name: e.name, slug: e.slug, url: url_for(e.attachment)} }
+    p '- - - - - - - - - - - - - - all_home_filters- - - - - - - - - - - - - - - -' 
+    pp all_home_filters
+    p ''
     view_params = Rails.cache.fetch("view_data_for_welcome_page", expires_in: 1.hour) do
      {} 
     end
-    hydrate_view(      {
-        nb_of_active_aids:  Aid.activated.count,
-        type_aides:         ContractType.aides.map{|e| e.attributes},
-        type_dispositifs:   ContractType.dispositifs.map{|e| e.attributes},
-        slug_of_creation:   ContractType.find_by(slug: "aide-a-la-creation-ou-reprise-d-entreprise"),
-        slug_of_amob:       ContractType.find_by(slug: "aide-a-la-mobilite"),
-        slug_of_formation:  ContractType.find_by(slug: "financement-aide-a-la-formation"),
-        slug_of_project:    ContractType.find_by(slug: "aide-a-la-definition-du-projet-professionnel"),        
-        all_home_filters:   all_home_filters,
-        remaining_filters:  all_home_filters.drop(6).map { |filter| url_for(filter.attachment) },
-      })
+    hydrate_view({
+      nb_of_active_aids:  Aid.activated.count,
+      type_aides:         ContractType.aides.map{|e| e.attributes},
+      type_dispositifs:   ContractType.dispositifs.map{|e| e.attributes},
+      slug_of_creation:   ContractType.find_by(slug: "aide-a-la-creation-ou-reprise-d-entreprise"),
+      slug_of_amob:       ContractType.find_by(slug: "aide-a-la-mobilite"),
+      slug_of_formation:  ContractType.find_by(slug: "financement-aide-a-la-formation"),
+      slug_of_project:    ContractType.find_by(slug: "aide-a-la-definition-du-projet-professionnel"),        
+      all_home_filters:   all_home_filters,
+    })
   end
 
   def start_wizard
