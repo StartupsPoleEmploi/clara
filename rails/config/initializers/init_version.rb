9ede790d37b88646b73e74230a76a6d2af4bb405
David Boureau <bdavidxyz@gmail.com>
2020-06-26 12:12:26 +0200

    no chnlg for docker

--- rails/config/initializers/init_version.rb
+++ rails/config/initializers/init_version.rb
@@ -2,10 +2,5 @@ file_location = File.file?('CHANGELOG.md') ? 'CHANGELOG.md' : '../CHANGELOG.md'
 
 lines = File.foreach(file_location).first(10)
 
-begin
-  ARA_VERSION = lines.detect {|e| e.start_with?("## ")}.split("## ")[1].split(" (")[0]
-rescue => e
-  puts e.class
-  ARA_VERSION="development"
-end
+ARA_VERSION = lines.detect {|e| e.start_with?("## ")}.split("## ")[1].split(" (")[0]
 ARA_EXT_URL = ENV.to_h.select { |key, value| key.to_s.match(/^ARA_URL/) }
