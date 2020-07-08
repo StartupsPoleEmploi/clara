584ef681b0c0b9699278e1d8a801d930a2c51bb0
David Boureau <bdavidxyz@gmail.com>
2020-07-08 16:54:50 +0200

    age ok

--- rails/app/controllers/peconnect_controller.rb
+++ rails/app/controllers/peconnect_controller.rb
@@ -18,13 +18,25 @@ class PeconnectController < ApplicationController
     ap '*************************************************************************************'
     ap info
     ap statut
+    ap birth
     hydrate_view({
       "family_name" => info["family_name"],
       "given_name" => info["given_name"],
       "code_statut_individu" => statut["codeStatutIndividu"],
       "libelle_statut_individu" => statut["libelleStatutIndividu"],
-      "date_de_naissance" => birth["dateDeNaissance"],
+      "date_de_naissance" => _actual_age(birth["dateDeNaissance"]),
     }.with_indifferent_access)
+  end
+
+  def _actual_age(str_birth)
+    res = nil
+    if str_birth
+      dob = DateTime.strptime(str_birth, '%Y-%m-%dT%H:%M:%S%z')
+      now = Date.today
+      res = now.year - dob.year - (now.strftime('%m%d') < dob.strftime('%m%d') ? 1 : 0)
+      # res = (Date.today.to_s(:number).to_i - birthday.to_date.to_s(:number).to_i)/1e4.to_i
+    end
+    res
   end
 
 end
