08e66996d8a558c5ec7203f3d5208ef0ffbb4219
David Boureau <bdavidxyz@gmail.com>
2020-07-10 15:49:31 +0200

    last changes

--- rails/app/assets/javascripts/custom/callback_question.js
+++ rails/app/assets/javascripts/custom/callback_question.js
@@ -5,13 +5,27 @@ clara.js_define("callback_question", {
   },
 
   please: function () {
-    var count = 0;
-    $("input[type='checkbox']").click(function () {
-      count = $("input[type='checkbox']:checked").length
-    })
-    $("input[type='radio']").click(function () {
-      count = $("input[type='radio']:checked").length
-    })
+
+    function check_count(count) {
+      console.log(`count ${count}`)
+      if (count === 0) {
+        $(".c-callback-submit").hide();
+      } else {
+        $(".c-callback-submit").show();
+      }
+    }
+
+    function check_all() {
+      var count = 0
+      count = $("input[type='checkbox']:checked").length;
+      count += $("input[type='radio']:checked").length;
+      check_count(count)
+    }
+
+    $("input[type='checkbox']").click(check_all)
+    $("input[type='radio']").click(check_all)
+
+    setTimeout(check_all, 1000)
   }
 });
 
