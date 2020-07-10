79e05952bd10377cd70595997520fd59cf0caa86
David Boureau <bdavidxyz@gmail.com>
2020-07-10 14:23:57 +0200

    extract subparam

--- rails/app/services/enrich_asker_from_params.rb
+++ rails/app/services/enrich_asker_from_params.rb
@@ -1,15 +1,7 @@
 class EnrichAskerFromParams
 
   def call(asker, params)
-    asker.v_duree_d_inscription = _extract_duree_d_inscription(params)
-  end
-
-  def _extract_duree_d_inscription(params)
-    res = nil
-    if params.has_key?(:inscription_form)
-      res = params.require(:inscription_form).permit(:value).to_h[:value]
-    end
-    res
+    asker.v_duree_d_inscription = ExtractSubparam.new.call(params, :inscription_form, :value)
   end
 
 end
