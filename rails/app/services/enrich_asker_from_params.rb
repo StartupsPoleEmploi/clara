f16c8f4e5bcf48157d8763f64d3d037805fa2089
David Boureau <bdavidxyz@gmail.com>
2020-07-13 11:37:12 +0200

    cleaned

--- rails/app/services/enrich_asker_from_params.rb
+++ rails/app/services/enrich_asker_from_params.rb
@@ -1,9 +1,6 @@
 class EnrichAskerFromParams
 
   def call(asker, params)
-    p '- - - - - - - - - - - - - - params- - - - - - - - - - - - - - - -' 
-    pp params
-    p ''
     asker.v_duree_d_inscription = ExtractSubparam.new.call(params, :inscription_form, :value) if params[:inscription_form]
     asker.v_category = ExtractSubparam.new.call(params, :category_form, :value) if params[:category_form]
     asker.v_allocation_type = ExtractSubparam.new.call(params, :allocation_form, :type) if params[:allocation_form]
