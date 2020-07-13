5c4a21e28fc82cce03cd99287ce9b8b186c35088
David Boureau <bdavidxyz@gmail.com>
2020-07-13 11:13:51 +0200

    ok 2 questions

--- rails/app/services/enrich_asker_from_params.rb
+++ rails/app/services/enrich_asker_from_params.rb
@@ -4,8 +4,8 @@ class EnrichAskerFromParams
     p '- - - - - - - - - - - - - - EnrichAskerFromParams, before- - - - - - - - - - - - - - - -' 
     ap asker
     p ''
-    asker.v_duree_d_inscription = ExtractSubparam.new.call(params, :inscription_form, :value)
-    asker.v_category = ExtractSubparam.new.call(params, :category_form, :value)
+    asker.v_duree_d_inscription = ExtractSubparam.new.call(params, :inscription_form, :value) if params[:inscription_form]
+    asker.v_category = ExtractSubparam.new.call(params, :category_form, :value) if params[:category_form]
     p '- - - - - - - - - - - - - - EnrichAskerFromParams, after- - - - - - - - - - - - - - - -' 
     ap asker
     p ''
