2887a8461621052891d8cb1c0f53c06cf1a81292
David Boureau <bdavidxyz@gmail.com>
2020-07-16 15:27:07 +0200

    cleaned

--- rails/app/services/pull_asker_from_session.rb
+++ rails/app/services/pull_asker_from_session.rb
@@ -1,10 +1,6 @@
 class PullAskerFromSession
 
   def call(session)
-    p '- - - - - - - - - - - - - - session- - - - - - - - - - - - - - - -' 
-    ap session[:asker]
-    ap JSON.parse(session[:asker])
-    p ''
     Asker.new(JSON.parse(session[:asker])) || Asker.new  
   end
   
