3559f0880bd022093eb045f396c1c0305459cb40
David Boureau <bdavidxyz@gmail.com>
2020-07-16 15:26:33 +0200

    [#43d][BUG] Profil PE ok

--- rails/app/services/build_callback_hash.rb
+++ rails/app/services/build_callback_hash.rb
@@ -2,20 +2,20 @@ class BuildCallbackHash
 
   def call(session, params, request)
     
-    session.clear
-
     fake = ExtractParam.new(params).call("fake")
     code = ExtractParam.new(params).call("code")
     base_url = "https://#{request.host}"    
 
 
     if _pull_or_stub_data_from_api?(session, fake, code)
+      ap 'not in the cache'
       extraction_h = PeConnectExtraction.new.call(base_url, code, fake)
       asker = BuildAskerFromPeconnect.new.call(extraction_h.slice(:statut, :birth, :formation, :coord, :alloc))
       meta = BuildMetaFromPeconnect.new.call(extraction_h.slice(:info))
       SaveMetaToSession.new.call(session, meta)
       SaveAskerToSession.new.call(session, asker)
     else
+      ap 'in the cache'
       asker = PullAskerFromSession.new.call(session)
       meta = PullMetaFromSession.new.call(session)
     end
@@ -29,7 +29,7 @@ class BuildCallbackHash
   end
   
   def _pull_or_stub_data_from_api?(session, fake, code)
-    (fake || code)
+    (fake || code) && session[:asker].blank?
   end
 
 end
--- rails/app/services/pull_asker_from_session.rb
+++ rails/app/services/pull_asker_from_session.rb
@@ -1,6 +1,10 @@
 class PullAskerFromSession
 
   def call(session)
+    p '- - - - - - - - - - - - - - session- - - - - - - - - - - - - - - -' 
+    ap session[:asker]
+    ap JSON.parse(session[:asker])
+    p ''
     Asker.new(JSON.parse(session[:asker])) || Asker.new  
   end
   
