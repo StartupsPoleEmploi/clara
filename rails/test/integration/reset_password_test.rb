9ce14607ed1ff883c2a7b5a71467d8f5685c9313
David Boureau <bdavidxyz@gmail.com>
2020-08-10 16:35:48 +0200

    transfer of cypress testing to integration testing

--- rails/test/integration/reset_password_test.rb
+++ rails/test/integration/reset_password_test.rb
@@ -2,23 +2,22 @@ require 'test_helper'
 
 class ResetPasswordTest < ActionDispatch::IntegrationTest
 
-  # test "connect to session : refused if unexisting user" do
-  #   post session_url, params: { session: { email: 'fake@email.com', password: 'any' }}
-  #   assert_response :unauthorized
-  # end
+  test "connect to session : refused if unexisting user" do
+    post session_url, params: { session: { email: 'fake@email.com', password: 'any' }}
+    assert_response :unauthorized
+  end
 
-  # test "connect to session : accepted if existing user" do
-  #   user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
-  #   post session_url, params: { session: { email: user.email, password: user.password }}
-  #   assert_response :found
-  # end
+  test "connect to session : accepted if existing user" do
+    user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
+    post session_url, params: { session: { email: user.email, password: user.password }}
+    assert_response :found
+  end
 
   test "reset password : fail if less than 8 characters" do
     user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
     allow_any_instance_of(ClearanceFindByUserAndToken).to receive(:call).and_return(user)
     put user_password_url(user.id), params: { password_reset: { password: 'short' }}
     assert text_of('.c-flash-error').include?("Le mot de passe doit avoir au moins 8 caractères")
-    # assert Nokogiri::HTML(response.body).css('.c-flash-error').text.include?("Le mot de passe doit avoir au moins 8 caractères")
   end
 
 end
