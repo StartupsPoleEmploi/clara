e72e4f0d69cecc2ccb7bf232e974e0b22827154f
David Boureau <bdavidxyz@gmail.com>
2020-08-17 17:12:23 +0200

    added stage 4, integration

--- rails/test/integration/access_aid_test.rb
+++ rails/test/integration/access_aid_test.rb
@@ -85,7 +85,29 @@ class AccessAidTest < ActionDispatch::IntegrationTest
     assert_equal 'résumé', last_aid.short_description
     assert_equal 1, last_aid.filters.size
     assert_equal 'se-deplacer', last_aid.filters.first.slug
+  end
 
+  test "Un contributeur peut créer une aide, étape 4" do
+    #given
+    contributeur      = User.create(role: "contributeur", email:"a@b.c", password: "p")
+    filter            = Filter.create!(name: "Se déplacer")
+    contract          = ContractType.create!(name: "mobilite", ordre_affichage: 42)
+    variable_age      = Variable.create!(name: "age", variable_kind: "integer")
+    variable_alloc    = Variable.create!(name: "allocation_value_min", variable_kind: "integer")
+    variable_location = Variable.create!(name: "location_state", variable_kind: "string")
+    aid               = Aid.create!(name: "aaa", contract_type: contract, ordre_affichage: 3)
+    post session_url, params: { session: { email: contributeur.email, password: contributeur.password }}
+    # when 
+    post admin_aid_creation_create_stage_4_path, params: {
+      "aid"=>"aaa", 
+      "trundle"=>"{\"name\":\"root_box\",\"subcombination\":\"OR\",\"subboxes\":[{\"name\":\"box_1597675281336\",\"subcombination\":\"AND\",\"subboxes\":[{\"name\":\"box_1597675721380b\",\"subcombination\":\"\",\"subboxes\":[],\"xval\":\"18\",\"xop\":\"more_than\",\"xvar\":\"v_age\",\"xtxt\":\"Avoir un âge strictement supérieur à 18 ans\",\"is_editing\":false},{\"name\":\"box_1597675721380a\",\"subcombination\":\"\",\"subboxes\":[],\"xval\":\"28\",\"xop\":\"less_than\",\"xvar\":\"v_age\",\"xtxt\":\"Avoir un âge strictement inférieur à 28 ans\",\"is_editing\":false}],\"xval\":\"\",\"xop\":\"\",\"xvar\":\"\",\"xtxt\":\"\",\"is_editing\":false},{\"name\":\"box_1597675679784\",\"subcombination\":\"\",\"subboxes\":[],\"xval\":\"30\",\"xop\":\"less_than\",\"xvar\":\"v_allocation_value_min\",\"xtxt\":\"Recevoir une allocation strictement inférieure à 30 euros net journalier\",\"is_editing\":false}]}", "modify"=>"", "geo"=>"{\"selection\":\"rien_sauf\",\"town\":[],\"department\":[],\"region\":[{\"BRE\":\"Bretagne\"},{\"COR\":\"Corse\"}]}", 
+      "locale"=>"fr"}
+    #then
+    assert_response :ok
+    root_rule = Aid.last.rule
+    assert_equal 'composite', root_rule.kind
+    assert_equal 'and_rule', root_rule.composition_type
+    assert_equal 2, root_rule.slave_rules.size
   end
 
 end
