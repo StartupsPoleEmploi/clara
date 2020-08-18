e89adf2d50f3cebd2f6446e211a2f02e91b58887
David Boureau <bdavidxyz@gmail.com>
2020-08-18 11:22:42 +0200

    refacto

--- rails/app/services/pe_connect_service.rb
+++ rails/app/services/pe_connect_service.rb
@@ -1,5 +1,15 @@
 class PeConnectService
 
+  def call(full_url, access_token)
+    url = URI.parse(full_url)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(url.request_uri)
+    req["Authorization"] = "Bearer #{access_token}"
+    response = http.request(req)
+    JSON.parse(response.body)
+  end
+
   def peconnect_oauth2_auth
     ENV['PECONNECT_OAUTH2_AUTH']
   end
@@ -11,5 +21,6 @@ class PeConnectService
   def clientid
     ENV['ESD_CLIENTID']
   end
+
 
  end
