1cfd7b17ee467c1b45354a2d5019aa87dbe516d8
David Boureau <bdavidxyz@gmail.com>
2020-08-18 16:00:59 +0200

    testing

---
+++ rails/test/integration/question_age_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+class QuestionAreTest < ActionDispatch::IntegrationTest
+  
+  test "Question AGE, by default age is nil" do
+    #given
+    #when
+    get new_age_question_path
+    #then
+    assert_response :ok
+    assert_select 'input#age', 1
+    assert_select 'input#age[value]', 0
+  end
+  
+  test "Question AGE, age is extracted from asker if already here" do
+    #given
+    allow_any_instance_of(AgeService).to receive(:new_and_download).and_return(AgeForm.new(number_of_years: '42'))
+    #when
+    get new_age_question_path
+    #then
+    assert_response :ok
+    assert_select 'input#age', 1
+    assert_select 'input#age[value]', 1
+    assert_select 'input#age[value=?]', '42'
+  end
+
+  test "Question AGE, user submits a valid AGE" do
+    #given
+    #when
+    post age_questions_path, params: {"age_form"=>{"minimum_income"=>"42"}}
+    #then
+    assert_response :redirect
+    assert_no_match /are/, response.redirect_url
+  end
+
+  test "Question AGE, user submits an invalid AGE" do
+    #given
+    #when
+    post age_questions_path, params: {"age_form"=>{"minimum_income"=>"invalid"}}
+    #then
+    assert_response :redirect
+    assert_match /are/, response.redirect_url
+    assert_equal ["n'est pas un nombre"], flash[:error] 
+  end
+
+
+end
