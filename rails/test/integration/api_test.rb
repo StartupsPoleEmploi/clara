a3faedd93c65e4beaa4d50b7dc47e24f1b606ff6
David Boureau <bdavidxyz@gmail.com>
2020-08-20 09:17:36 +0200

    more API tests

--- rails/test/integration/api_test.rb
+++ rails/test/integration/api_test.rb
@@ -38,6 +38,25 @@ class ApiTest < ActionDispatch::IntegrationTest
     assert_equal ({"filters"=>[{"name" => "Se déplacer", "slug" => "se-deplacer"}]}), JSON.parse(response.body)
   end
 
+  test "API : aid-slug, refused if not authenticated" do
+    #given
+    #when
+    get api_v1_aid_slug_path('aaa')
+    #then
+    assert_response 401
+  end
+
+  test "API : aid-slug, when authenticated" do
+    #given
+    contract = ContractType.create!(name: "mobilite", ordre_affichage: 42)
+    aid = Aid.create!(name: "aaa", contract_type: contract, ordre_affichage: 3)
+    #when
+    get api_v1_aid_slug_path('aaa'), headers: {:Authorization => "Bearer #{_jwt}"}
+    #then
+    assert_response :success
+    assert_equal ({'aid'=>{'name'=>'aaa', 'slug'=>'aaa'}}), JSON.parse(response.body)
+  end
+
   def _jwt
     api_user = ApiUser.create!(email:"a@b.c", password: "p")
     post api_v1_api_user_token_path, params: { auth: {email: "a@b.c", password: "p"}}
