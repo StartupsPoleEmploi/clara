a3c07c19c012a39a03637a81b5dc7043b449862c
David Boureau <bdavidxyz@gmail.com>
2020-08-20 09:51:21 +0200

    more tests for API

--- rails/test/integration/api_test.rb
+++ rails/test/integration/api_test.rb
@@ -73,14 +73,50 @@ class ApiTest < ActionDispatch::IntegrationTest
     get api_v1_aids_eligible_path(age: 22), headers: {:Authorization => "Bearer #{_jwt}"}
     #then
     assert_response :success
-    assert_equal ({'input'=>{'asker'=>{'age' => '22'}}, 'aids'=>['blabla']}), JSON.parse(response.body)
+    assert_equal ({'input'=>{'asker'=>{'age' => '22'}}, 'aids'=>[{'name'=>'aaa', 'slug'=>'aaa', 'filters'=>[], 'contract_type'=>'mobilite'}]}), JSON.parse(response.body)
+  end
+
+  test "API : ineligible, refused if not authenticated" do
+    #given
+    #when
+    get api_v1_aids_ineligible_path
+    #then
+    assert_response 401
+  end
+
+  test "API : ineligible, when authenticated" do
+    #given
+    _create_realistic_aid
+    #when
+    get api_v1_aids_ineligible_path(age: 17), headers: {:Authorization => "Bearer #{_jwt}"}
+    #then
+    assert_response :success
+    assert_equal ({'input'=>{'asker'=>{'age' => '17'}}, 'aids'=>[{'name'=>'aaa', 'slug'=>'aaa', 'filters'=>[], 'contract_type'=>'mobilite'}]}), JSON.parse(response.body)
+  end
+
+  test "API : uncertain, refused if not authenticated" do
+    #given
+    #when
+    get api_v1_aids_uncertain_path
+    #then
+    assert_response 401
+  end
+
+  test "API : uncertain, when authenticated" do
+    #given
+    _create_realistic_aid
+    #when
+    get api_v1_aids_uncertain_path(spectacle: 'true'), headers: {:Authorization => "Bearer #{_jwt}"}
+    #then
+    assert_response :success
+    assert_equal ({'input'=>{'asker'=>{'spectacle' => 'true'}}, 'aids'=>[{'name'=>'aaa', 'slug'=>'aaa', 'filters'=>[], 'contract_type'=>'mobilite'}]}), JSON.parse(response.body)
   end
 
   def _create_realistic_aid
     variable_age = Variable.create!(name: "age", variable_kind: "integer")
     contract = ContractType.create!(name: "mobilite", ordre_affichage: 42)
     rule = Rule.create!({name: "r_majorite", value_eligible: "18", variable: variable_age, description: "descr", kind: "simple", operator_kind: "more_than"})
-    aid = Aid.create!(name: "aaa", contract_type: contract, rule: rule, ordre_affichage: 3)
+    aid = Aid.create!(name: "aaa", contract_type: contract, rule: rule, ordre_affichage: 3, what: 'x', how_and_when: 'y', how_much: 'z')
   end
 
   def _jwt
