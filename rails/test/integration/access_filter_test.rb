e15543abfc928808c8584f82fd028cf638aac0c0
David Boureau <bdavidxyz@gmail.com>
2020-08-21 09:52:48 +0200

    access_filter_Test

---
+++ rails/test/integration/access_filter_test.rb
@@ -0,0 +1,84 @@
+require 'test_helper'
+
+class AccessFilterTest < ActionDispatch::IntegrationTest
+  
+
+  test "Un simple visiteur ne peut pas lister les filters" do
+    get admin_filters_path
+    assert_redirected_to sign_in_path(locale: 'fr')
+  end
+  test "Un simple visiteur ne peut pas lire un filter" do
+    get admin_filter_path(1)
+    assert_redirected_to sign_in_path(locale: 'fr')
+  end
+  test "Un simple visiteur ne peut pas éditer un filter" do
+    get edit_admin_filter_path(1)
+    assert_redirected_to sign_in_path(locale: 'fr')
+  end
+  test "Un simple visiteur ne peut pas modifier un filter" do
+    put admin_filter_path(1)
+    assert_redirected_to sign_in_path(locale: 'fr')
+  end
+  test "Un simple visiteur ne peut pas supprimer un filter" do
+    delete admin_filter_path(1)
+    assert_redirected_to sign_in_path(locale: 'fr')
+  end
+  
+  test "Un superadmin peut lister les filters" do
+    #given
+    filter, superadmin = _filter_and_superadmin
+    post session_url, params: { session: { email: superadmin.email, password: superadmin.password }}
+    #when
+    get admin_filters_path
+    #then
+    assert_response :ok
+    assert_select 'table[aria-labelledby="page-title"] tbody tr', count: 1
+  end
+
+  test "Un superadmin peut lire un filter" do
+    #given
+    filter, superadmin = _filter_and_superadmin
+    post session_url, params: { session: { email: superadmin.email, password: superadmin.password }}
+    #when
+    get admin_filter_path(filter.id)
+    #then
+    assert_response :ok
+  end  
+
+  test "Un superadmin peut accéder à l'édition d'un filter" do
+    #given
+    filter, superadmin = _filter_and_superadmin
+    post session_url, params: { session: { email: superadmin.email, password: superadmin.password }}
+    #when
+    get edit_admin_filter_path(filter.id)
+    #then
+    assert_response :ok
+  end
+
+  test "Un superadmin peut modifier un filter" do
+    #given
+    filter, superadmin = _filter_and_superadmin
+    post session_url, params: { session: { email: superadmin.email, password: superadmin.password }}
+    #when
+    put admin_filter_path(filter.id), params: { filter: { trigger_at: DateTime.new }}
+    #then
+    assert_response :found
+  end
+
+  test "Un superadmin peut supprimer un filter" do
+    #given
+    filter, superadmin = _filter_and_superadmin
+    post session_url, params: { session: { email: superadmin.email, password: superadmin.password }}
+    #when
+    delete admin_filter_path(filter.id)
+    #then
+    assert_response :found
+  end
+
+  def _filter_and_superadmin
+    superadmin = User.create!(role: "superadmin", email:"a@b.c", password: "p")
+    filter  = Filter.create!(name: "Se déplacer")
+    [filter, superadmin]
+  end
+
+end
