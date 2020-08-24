ff109bc7769e04645a7b3d3b90b866eccf5c464c
David Boureau <bdavidxyz@gmail.com>
2020-08-24 14:39:19 +0200

    removed print, share ok

--- rails/app/assets/javascripts/custom/aides/aides.js
+++ rails/app/assets/javascripts/custom/aides/aides.js
@@ -2,6 +2,25 @@ clara.js_define("aides", {
 
   please: function() {
 
+    
+    $(".c-resulteligy-share").click(function(e) {
+      if (typeof ga === "function") {
+        ga("send", "event", "results", "share", document.location.pathname);
+      }
+      var lenTwo = function(val){
+        if (val.toString().length === 1) {
+          return ('0'+val)
+        } else {
+          return val;
+        }
+      }
+      var d = new Date();
+      var now = lenTwo(d.getDay()) +"/"+lenTwo(d.getMonth())+"/"+d.getFullYear();
+      var subject = 'Ma simulation d\'aides Clara du ' + now;
+      var body = window.location.href;
+      window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
+    });
+
 
     window.main_store = 
       Redux.createStore(
