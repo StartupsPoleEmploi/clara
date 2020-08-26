2246c6effc6b085bc11b15b32c033974ec2ef640
David Boureau <bdavidxyz@gmail.com>
2020-08-26 17:27:11 +0200

    Gemfile massively modified

--- rails/app/assets/config/manifest.js
+++ rails/app/assets/config/manifest.js
@@ -1,3 +1,8 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+//= link administrate/application.css
+//= link administrate-field-ckeditor/application.css
+//= link administrate/application.js
+//= link administrate-field-ckeditor/application.js
