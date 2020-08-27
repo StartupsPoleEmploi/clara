ef8f86a837cdb0dfda99fcd257a158dc1c43b991
David Boureau <bdavidxyz@gmail.com>
2020-08-27 15:18:06 +0200

    welltested

--- rails/test/services/DisconnectFromPeconnect_test.rb
+++ rails/test/services/DisconnectFromPeconnect_test.rb
@@ -4,17 +4,32 @@ class DisconnectFromPeconnectTest < ActiveSupport::TestCase
 
   test ".call clear the session, and renders json with the redirection url if required by the params" do
     #given
-    request = OpenStruct.new(host: 'myhost')
-    session = OpenStruct.new(id_token: 'my_id_token', clear: 'ok')
-    params = OpenStruct.new(json: 'truthy')
+    request = fake(host: 'host')
+    session = fake(id_token: 'token', clear: 'ok')
+    params = fake(json: 'truthy')
     # expectations
     expect(session).to receive(:clear).once
-    expect_any_instance_of(UrlAfterDisconnect).to receive(:call).and_return("http://redirected.to")
+    expect_any_instance_of(UrlAfterDisconnect).to receive(:call).with('token', 'host').and_return("http://redirected.to")
     #when
     res = DisconnectFromPeconnect.new.call(request, session, params)
     #then
     assert_equal("render", res[:method])
     assert_equal({:json=>{:redirection_url=>"http://redirected.to"}}, res[:arg])
+  end
+
+  test ".call clear the session, and redirects to page if json param is false" do
+    #given
+    request = fake(host: 'host')
+    session = fake(id_token: 'token', clear: 'ok')
+    params = fake(json: false)
+    # expectations
+    expect(session).to receive(:clear).once
+    expect_any_instance_of(UrlAfterDisconnect).to receive(:call).with('token', 'host').and_return("http://redirected.to")
+    #when
+    res = DisconnectFromPeconnect.new.call(request, session, params)
+    #then
+    assert_equal("redirect_to", res[:method])
+    assert_equal("http://redirected.to", res[:arg])
   end
 
 end
