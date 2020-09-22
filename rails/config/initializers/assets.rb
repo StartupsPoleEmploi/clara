1b77bfe6864899946ef0855b6ff20f8abedbcc21
David Boureau <bdavidxyz@gmail.com>
2020-09-22 10:13:00 +0200

    [#67b][FEATURE] alertes auto, assets

--- rails/config/initializers/assets.rb
+++ rails/config/initializers/assets.rb
@@ -21,3 +21,20 @@ copy_file("#{Rails.root}/vendor/assets/javascripts/logster/client-app.js", "#{Ra
 copy_file("#{Rails.root}/vendor/assets/javascripts/logster/vendor.js", "#{Rails.root}/public/logs/javascript/vendor.js")
 copy_file("#{Rails.root}/vendor/assets/stylesheets/logster/client-app.css", "#{Rails.root}/public/logs/stylesheets/client-app.css")
 copy_file("#{Rails.root}/vendor/assets/stylesheets/logster/vendor.css", "#{Rails.root}/public/logs/stylesheets/vendor.css")
+
+
+sk_dir = "#{Rails.root}/public/sidekiq"
+sk_dir_img = "#{Rails.root}/public/sidekiq/images/"
+sk_dir_js = "#{Rails.root}/public/sidekiq/javascripts/"
+sk_dir_css = "#{Rails.root}/public/sidekiq/stylesheets/"
+Dir.mkdir(sk_dir) unless File.exists?(sk_dir)
+Dir.mkdir(sk_dir_img) unless File.exists?(sk_dir_img)
+Dir.mkdir(sk_dir_js) unless File.exists?(sk_dir_js)
+Dir.mkdir(sk_dir_css) unless File.exists?(sk_dir_css)
+
+copy_file("#{Rails.root}/vendor/assets/javascripts/sidekiq/application.js", "#{Rails.root}/public/sidekiq/javascripts/application.js")
+copy_file("#{Rails.root}/vendor/assets/javascripts/sidekiq/dashboard.js", "#{Rails.root}/public/sidekiq/javascripts/dashboard.js")
+copy_file("#{Rails.root}/vendor/assets/images/sidekiq/status.png", "#{Rails.root}/public/sidekiq/images/status.png")
+copy_file("#{Rails.root}/vendor/assets/stylesheets/sidekiq/bootstrap.css", "#{Rails.root}/public/sidekiq/stylesheets/bootstrap.css")
+copy_file("#{Rails.root}/vendor/assets/stylesheets/sidekiq/application.css", "#{Rails.root}/public/sidekiq/stylesheets/application.css")
+copy_file("#{Rails.root}/vendor/assets/stylesheets/sidekiq/application-dark.css", "#{Rails.root}/public/sidekiq/stylesheets/application-dark.css")
