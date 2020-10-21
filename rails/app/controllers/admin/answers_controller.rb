818c7bf0ff84462517ef55a7b37b413aff7a9808
David Boureau <bdavidxyz@gmail.com>
2020-10-21 08:57:24 +0200

    updated services

--- rails/app/controllers/admin/answers_controller.rb
+++ rails/app/controllers/admin/answers_controller.rb
@@ -1,5 +1,6 @@
 module Admin
   class AnswersController < Admin::ApplicationController
+    include AdministrateExportable::Exporter
 
     def valid_action?(name, resource = resource_class)
       %w[new edit].exclude?(name.to_s) && super
