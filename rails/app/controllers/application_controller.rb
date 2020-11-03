8ad724ff7802d3654aa178c49dcd47d5f2a3b8c1
David Boureau <bdavidxyz@gmail.com>
2020-11-03 08:38:11 +0100

    ok removed

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -10,8 +10,6 @@ class ApplicationController < ActionController::Base
 
   # back button that works, see http://jacopretorius.net/2014/01/force-page-to-reload-on-browser-back-in-rails.html
   before_action :set_cache_headers
-  # See https://sentry.io/pole-emploi/ara/getting-started/ruby-rails/
-  before_action :set_raven_context
   # See https://stackoverflow.com/a/38003702/2595513
   after_action :set_response_language_header
 
@@ -56,12 +54,6 @@ class ApplicationController < ActionController::Base
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
-  end
-
-  # See https://sentry.io/pole-emploi/ara/getting-started/ruby-rails/
-  def set_raven_context
-    Raven.user_context(id: session[:asker]) 
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 
 end
