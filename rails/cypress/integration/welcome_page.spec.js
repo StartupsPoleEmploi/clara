bf5ee7c360a709ef80660ab92ffec5407ac7b2af
David Boureau <bdavidxyz@gmail.com>
2020-11-16 16:16:29 +0100

    2 first tests ok

--- rails/cypress/integration/welcome_page.spec.js
+++ rails/cypress/integration/welcome_page.spec.js
@@ -1,17 +1,31 @@
 /// <reference types="cypress" />
 
-context('Welcome page', () => {
+context('Sans PEID', () => {
 
-  beforeEach(() => {
-    cy.visit('/')
-  })
 
-  describe('Title section', () => {
+  describe("Pour un visiteur", () => {
 
-    it('should contain a welcome message', () => {
-      cy.get('.c-newhomehero__title').contains('Boostez')
+    const AMOB_SPECTACLE = '[data-aslug="aide-a-la-mobilite-professionnelle-des-artistes-et-technicien-ne-s-du-spectacle"]'
+
+    it("Quand on arrive sur la page principale, on peut ouvrir la modale de démarrage le formulaire", () => {
+      // given
+      cy.visit('/')
+      cy.get('#login-form').should('not.be.visible')
+      // when
+      cy.get('#start_wizard_form1').click()
+      // then
+      cy.get('#login-form').should('be.visible')
     })
 
+    it("On peut commencer sans se connecter", () => {
+      // given
+      // when
+      cy.get('#start-clara').click()
+      // then
+      cy.location().should((loc) => { expect(loc.pathname).contains('question') })
+    })
+  
   })
+
 
 })
