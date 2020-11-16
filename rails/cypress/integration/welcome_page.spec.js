46857802a1c83bca6c52ac08babdf7c029c23579
David Boureau <bdavidxyz@gmail.com>
2020-11-16 16:38:11 +0100

    a simpler test that should works

--- rails/cypress/integration/welcome_page.spec.js
+++ rails/cypress/integration/welcome_page.spec.js
@@ -1,30 +1,14 @@
 /// <reference types="cypress" />
 
-context('Sans PEID', () => {
+context('Parcours du formulaire', () => {
 
 
-  describe("Pour un visiteur", () => {
+  beforeEach(() => {
+    cy.visit('/inscription_questions/new')
+  })
 
-    const AMOB_SPECTACLE = '[data-aslug="aide-a-la-mobilite-professionnelle-des-artistes-et-technicien-ne-s-du-spectacle"]'
 
-    it("Quand on arrive sur la page principale, on peut ouvrir la modale de démarrage le formulaire", () => {
-      // given
-      cy.visit('/')
-      cy.get('#login-form').should('not.be.visible')
-      // when
-      cy.get('#start_wizard_form1').click()
-      // then
-      cy.get('#login-form').should('be.visible')
-    })
-
-    it("On peut commencer sans se connecter", () => {
-      // given
-      // when
-      cy.get('#start-clara').click()
-      // then
-      cy.location().should((loc) => { expect(loc.pathname).contains('question') })
-    })
-  
+  describe("Pour un visiteur qui commence par la question inscription", () => {
 
     it("On peut remplir toutes les questions, on arrive sur l'écran de résultat", () => {
 
@@ -72,7 +56,7 @@ context('Sans PEID', () => {
       // cy.get("body.c-body.aides").should("exist")
 
     })
-  
+
   })
 
 
