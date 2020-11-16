1826283aebc79d42e87c34b9a3536e281256cabd
David Boureau <bdavidxyz@gmail.com>
2020-11-16 18:04:08 +0100

    all the form e2e test

--- rails/cypress/integration/welcome_page.spec.js
+++ rails/cypress/integration/welcome_page.spec.js
@@ -21,51 +21,40 @@ context('Parcours du formulaire', () => {
       cy.get('#cat_12345').click()
       cy.get('.js-next').click()
 
-      cy.get(".c-question-title").then(function($elem) {
-        cy.log($elem.text())
-      })
-
       cy.get('input#ARE_ASP').should("exist")
       cy.get('input#ARE_ASP').click()
       cy.get('.js-next').click()
 
+      // cy.get(".c-question-title").then(function($elem) {
+      //   cy.log($elem.text())
+      // })
+
+      cy.get('#montant').should("exist")
+      cy.get('#montant').type("842")
+      cy.get('.js-next').click()
+
+      cy.get('#age').should("exist")
+      cy.get('#age').type("16")
+      cy.get('.js-next').click()
+
+      cy.get('#niveau_4').should("exist")
+      cy.get('#niveau_4').click()
+      cy.get('.js-next').click()
+
+      cy.get('#search').should("exist")
+      cy.get('#search').type("49490")
+      cy.get('li.autocomplete-item').should("exist")
+      cy.get('li.autocomplete-item').first().click()
+      cy.get('.js-next').click()
+
+      cy.get('#val_spectacle').should("exist")
+      cy.get('#val_spectacle').click()
+      cy.get('.js-next').click()
+
       cy.wait(2000)
+      cy.get('.js-next').click()
 
-      cy.get(".c-question-title").then(function($elem) {
-        cy.log($elem.text())
-      })
-      // cy.log(cy.get('.c-question-title').invoke('text'))
-
-
-      // cy.get('#montant').should("exist")
-      // cy.get('#montant').type("842")
-      // cy.get('.js-next').click()
-
-      // cy.get('#age').should("exist")
-      // cy.get('#age').type("16")
-      // cy.get('.js-next').click()
-
-      // cy.get('#niveau_4').should("exist")
-      // cy.get('#niveau_4').click()
-      // cy.get('.js-next').click()
-
-      // cy.get('#search').should("exist")
-      // cy.get('#search').type("49490")
-      // cy.get('li.autocomplete-item').should("exist")
-      // cy.get('li.autocomplete-item').first().click()
-      // cy.get('.js-next').click()
-
-      // cy.get('#val_spectacle').should("exist")
-      // cy.get('#val_spectacle').click()
-
-      // //when
-      // cy.get('.js-next').click()
-
-      // cy.get('label[for="se-deplacer"]').click()
-      // cy.get('.js-next').click()
-
-      // //then 
-      // cy.get("body.c-body.aides").should("exist")
+      cy.get("body.c-body.aides").should("exist")
 
     })
 
