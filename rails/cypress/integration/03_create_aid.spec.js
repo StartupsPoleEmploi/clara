0f96315aa2d24e36acbad104dce3b5b8a36100ec
David Boureau <bdavidxyz@gmail.com>
2020-11-23 09:12:06 +0100

    stage 2 ok

--- rails/cypress/integration/03_create_aid.spec.js
+++ rails/cypress/integration/03_create_aid.spec.js
@@ -67,9 +67,34 @@ context("Création et publication d'une aide", () => {
         cy.get('.field-unit textarea#aid_source')        .should('not.have.value', '')
         cy.get('.field-unit input#aid_ordre_affichage')  .should('not.have.value', '')
       })
-
-
     })
+
+    describe("Il rempli l'étape 2", () => {
+      before(() => {
+        cy.visit('/admin/aid_creation/new_aid_stage_2?locale=fr&slug=erasmus42')
+      })
+
+      it("si on vient de créer une aide, on arrive sur un écran avec 5 champs éditables, tous vides, le premier est déplié", function() {
+        // Should have 5 empty editors
+        cy.get('.field-unit--ckeditor').should('have.length', 5)
+        cy.get('.field-unit--ckeditor .cke_contents').eq(0).should('have.text', "Press ALT 0 for help")
+        cy.get('.field-unit--ckeditor .cke_contents').eq(1).should('have.text', "Press ALT 0 for help")
+        cy.get('.field-unit--ckeditor .cke_contents').eq(2).should('have.text', "Press ALT 0 for help")
+        cy.get('.field-unit--ckeditor .cke_contents').eq(3).should('have.text', "Press ALT 0 for help")
+        cy.get('.field-unit--ckeditor .cke_contents').eq(4).should('have.text', "Press ALT 0 for help")
+
+        // First must be expanded
+        cy.get('#accordion_0-0_tab').should('have.attr', 'aria-expanded', 'true')
+      })
+
+      it("on peut renseigner des conditions à remplir, avec mise à jour en temps réel", function() {
+
+        cy.get('#accordion_0-1_tab').click()
+        cy.window().then(win => {win.CKEDITOR.instances["aid_additionnal_conditions"].setData("<p>Mes conditions à remplir</p>");});
+        cy.get('.js-additionnal-conditions').contains('Mes conditions à remplir')
+      })
+    })
+
 
   })
 
