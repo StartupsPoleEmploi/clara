446efb3a73ba281f36239b5efdc46bbfd45e873c
David Boureau <bdavidxyz@gmail.com>
2020-11-24 15:48:41 +0100

    ok done

--- rails/config/environments/test.rb
+++ rails/config/environments/test.rb
@@ -66,6 +66,6 @@ Rails.application.configure do
   # OmniAuth.config.test_mode = true
   config.action_mailer.default_url_options = { :host => ENV["ARA_HOST"] }
 
-  config.active_storage.service = :local
+  config.active_storage.service = :test
 
 end
