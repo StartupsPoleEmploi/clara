1cf7ef7089b0d74998f86de99d8e3fdadab2dc5f
David Boureau <bdavidxyz@gmail.com>
2020-12-01 15:03:08 +0100

    track_search made optional

--- rails/app/services/track_search.rb
+++ rails/app/services/track_search.rb
@@ -3,19 +3,19 @@ require 'securerandom'
 require 'net/http'
 
 class TrackSearch
-  
+
   def call(user_search)
-    if user_search.is_a?(String) && !user_search.blank?
+    if user_search.is_a?(String) && !user_search.blank? && ENV['ARA_GOOGLE_ANALYTICS_COLLECT'].present?
       uri = URI.parse(ENV['ARA_GOOGLE_ANALYTICS_COLLECT'])
       params = {
-                "v" => "1",
-                 "tid" => ENV['ARA_GOOGLE_ANALYTICS_ID'] ,
-                 "uid" => SecureRandom.hex,
-                 "t" => "event",
-                 "ec" => "aids",
-                 "ea" => "search",
-                 "el" => user_search,
-               }
+        "v" => "1",
+        "tid" => ENV['ARA_GOOGLE_ANALYTICS_ID'] ,
+        "uid" => SecureRandom.hex,
+        "t" => "event",
+        "ec" => "aids",
+        "ea" => "search",
+        "el" => user_search,
+      }
       HttpService.new.post_form(uri, params)
     else
       "not called"
