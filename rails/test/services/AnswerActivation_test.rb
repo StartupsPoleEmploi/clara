07c816f6f4fd6fc54499ee193c9edca99a14dc5f
David Boureau <bdavidxyz@gmail.com>
2020-12-07 09:35:28 +0100

    tested, ok

--- rails/test/services/AnswerActivation_test.rb
+++ rails/test/services/AnswerActivation_test.rb
@@ -1,31 +1,31 @@
 require "test_helper"
 
-class PeconnectActivationTest < ActiveSupport::TestCase
+class AnswerActivationTest < ActiveSupport::TestCase
 
   test '.get_value, By default, returns false' do
     #given
     #when
-    res = PeconnectActivation.new.get_value
+    res = AnswerActivation.new.get_value
     #then
     assert_equal res, false
   end
 
   test '.get_value, when switching, returns true' do
     # given
-    PeconnectActivation.new.get_value
-    PeconnectActivation.new.switch_value
+    AnswerActivation.new.get_value
+    AnswerActivation.new.switch_value
     #when
-    res = PeconnectActivation.new.get_value
+    res = AnswerActivation.new.get_value
     #then
     assert_equal res, true
   end
   test '.get_value, when switching twice, returns false' do
     #given
-    PeconnectActivation.new.get_value
-    PeconnectActivation.new.switch_value
-    PeconnectActivation.new.switch_value
+    AnswerActivation.new.get_value
+    AnswerActivation.new.switch_value
+    AnswerActivation.new.switch_value
     #when
-    res = PeconnectActivation.new.get_value
+    res = AnswerActivation.new.get_value
     #then
     assert_equal res, false
   end
