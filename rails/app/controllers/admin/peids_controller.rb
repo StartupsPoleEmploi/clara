e8b6c343aacd4b261800c450a73d99d88f5a5b37
David Boureau <bdavidxyz@gmail.com>
2020-12-28 15:09:49 +0100

    peid test

--- rails/app/controllers/admin/peids_controller.rb
+++ rails/app/controllers/admin/peids_controller.rb
@@ -2,8 +2,10 @@ module Admin
   class PeidsController < Admin::ApplicationController
     include AdministrateExportable::Exporter
 
+    before_action :require_superadmin
+
     def valid_action?(name, resource = resource_class)
-      %w[new edit].exclude?(name.to_s) && super
+      %w[new edit show update edit].exclude?(name.to_s) && super
     end
     
   end
