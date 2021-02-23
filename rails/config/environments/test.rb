dbe3616cbdc8318266c37ef302c2c567d875261b
David Boureau <bdavidxyz@gmail.com>
2021-02-23 09:23:29 +0100

    simpler test.rb

--- rails/config/environments/test.rb
+++ rails/config/environments/test.rb
@@ -15,7 +15,6 @@ Rails.application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
-  config.autoloader = :classic
   
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
