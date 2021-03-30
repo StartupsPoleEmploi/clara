0db185e857ef78444cca2614b1eef2a085607801
David Boureau <bdavidxyz@users.noreply.github.com>
2021-03-30 09:48:59 +0200

    [115] bug active storage

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_120056) do
+ActiveRecord::Schema.define(version: 2021_03_30_073855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -35,7 +35,14 @@ ActiveRecord::Schema.define(version: 2021_01_11_120056) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "aids", id: :serial, force: :cascade do |t|
@@ -304,6 +311,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_120056) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "aids", "contract_types"
   add_foreign_key "compound_rules", "rules"
   add_foreign_key "compound_rules", "rules", column: "slave_rule_id"
