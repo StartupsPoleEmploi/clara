d87df81f71565978c8e76b4e59c31c8b8e41f9c7
David Boureau <bdavidxyz@gmail.com>
2021-07-19 11:56:03 +0200

    X-CSRF-Token only in R7_MODE

--- rails/app/assets/javascripts/jquery/jquery_extension.js
+++ rails/app/assets/javascripts/jquery/jquery_extension.js
@@ -99,14 +99,3 @@ jQuery.fn.extend({
 })(window.jQuery);
 
 
-(function() {
-  if ($) {
-    var token = $( 'meta[name="csrf-token"]' ).attr( 'content' );
-
-    $.ajaxSetup( {
-      beforeSend: function ( xhr ) {
-        xhr.setRequestHeader( 'X-CSRF-Token', token );
-      }
-    });      
-  }
-})();
