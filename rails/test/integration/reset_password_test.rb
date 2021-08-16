338434c73ea10ac29552d202fab304992c02e1da
David Boureau <bdavidxyz@gmail.com>
2021-08-16 17:55:43 +0200

    test create pwd ok

--- rails/test/integration/reset_password_test.rb
+++ rails/test/integration/reset_password_test.rb
@@ -12,4 +12,22 @@ class ResetPasswordTest < ActionDispatch::IntegrationTest
     assert text_of('.c-flash-error').include?("Le mot de passe ne peut être vide")
   end
 
+  test "create password : deliver email if forgotten" do
+    #given
+    user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
+
+    #See https://stackoverflow.com/a/32127013/2595513
+    mail = double
+    expect(mail).to receive(:deliver_later).with(no_args)
+    expect(CustomClearanceMailer).to receive(:change_password)
+      .with('superadmin@clara.com', URI::regexp(%w(http https)))
+      .and_return(mail)
+
+    #when
+    post user_password_url(user.id), params: { password: { email: 'superadmin@clara.com' }}
+    #then
+    # expect(email).to receive(:deliver_later).with(no_args)
+    assert text_of('h1').include?("Consultez vos emails !")
+  end
+
 end
