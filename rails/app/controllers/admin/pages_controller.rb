1ee3020e1a21e8b12519a22019c8213cdd90a69b
David Boureau <bdavidxyz@gmail.com>
2021-08-30 10:32:11 +0200

    added more tests

--- rails/app/controllers/admin/pages_controller.rb
+++ rails/app/controllers/admin/pages_controller.rb
@@ -121,10 +121,6 @@ module Admin
       count = ActiveRecord::SessionStore::Session.count
       Mae::Application.load_tasks
       Rake::Task["db:sessions:clear"].invoke
-
-      # Rake::Task['db:schema:cache:clear'].invoke
-      # ap ActiveRecord::SessionStore::Session.count
-      # ActiveRecord::SessionStore::Session.delete_all
       render json: {status: "ok", count: count}
     end
 
--- rails/test/integration/admin_pages_test.rb
+++ rails/test/integration/admin_pages_test.rb
@@ -5,8 +5,7 @@ class AdminPagesTest < ActionDispatch::IntegrationTest
   test "Start a broken link detection" do
     #given
     allow(DetectBrokenLinksJob).to receive(:perform_later)
-    user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
-    post session_url, params: { session: { email: user.email, password: user.password }}
+    connect_as_superadmin
     #when
     post admin_post_broken_path
     #then
@@ -18,10 +17,8 @@ class AdminPagesTest < ActionDispatch::IntegrationTest
 
   test "List broken links" do
     #given
-    allow_any_instance_of(DetectBrokenLinksJob).to receive(:perform_later)
-    user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
     Broken.create(url: "https://example.com", new_url: "https://other.com", aids_slug: '["aid1", "aid2"]', code: 502)
-    post session_url, params: { session: { email: user.email, password: user.password }}
+    connect_as_superadmin
     #when
     get admin_get_relink_path
     #then
@@ -31,6 +28,29 @@ class AdminPagesTest < ActionDispatch::IntegrationTest
     assert_equal 'https://other.com', text_of('.c-relink-newurl-0').strip
     assert_equal '/admin/aids/aid1', text_of('.c-relink-slug-0-0').strip
     assert_equal '/admin/aids/aid2', text_of('.c-relink-slug-0-1').strip
+  end
+
+  test "Record answers, see the status" do
+    #given
+    connect_as_superadmin
+    #when
+    get admin_get_switch_answer_path
+    #then
+    assert_response :success
+    assert_equal "L'enregistrement des réponses est actuellement désactivé", text_of('.c-get-switch-answer-text').strip
+  end
+
+  test "Record answers, set the status" do
+    #given
+    connect_as_superadmin
+    #when
+    post admin_post_switch_answer_path
+    #then
+    assert_redirected_to admin_get_switch_answer_path + '?locale=fr'
+    #when
+    get admin_get_switch_answer_path
+    #then
+    assert_equal "L'enregistrement des réponses est actuellement activé", text_of('.c-get-switch-answer-text').strip
   end
 
 end
