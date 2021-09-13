984ad4105d76f88558d656a8e2d324134f94d3ed
David Boureau <bdavidxyz@gmail.com>
2021-09-13 10:17:43 +0200

    one green test

--- rails/test/services/CreateStage5_test.rb
+++ rails/test/services/CreateStage5_test.rb
@@ -4,12 +4,25 @@ class CreateStage5Test < ActiveSupport::TestCase
 
   test ".call, nominal" do
     #given
-    slug = 'a'
-    action_asked = 'b'
+    slug = 'fake_aid'
+    _create_aid_with_name(slug)
+    action_asked = 'archive'
     #when
     res = CreateStage5.new.call(slug, action_asked)
     #then
-    assert_equal('foo', res)
+    assert_equal("L'aide a bien été archivée, elle n'apparaîtra plus sur le site d'ici quelques secondes.", res)
+  end
+
+  def _create_aid_with_name(the_name)
+    c = ContractType.new(name: the_name, ordre_affichage: 21).tap(&:save!)
+    Aid.new("name"=>"fake_aid", 
+        "what"=>"<p>https://example3.com</p><p>https://example4.com</p>\r\n", 
+        "additionnal_conditions"=>"<p>conditions</p>\r\n", 
+        "how_much"=>"<p>contenu</p>\r\n", 
+        "how_and_when"=>"<p>comment</p>\r\n", 
+        "limitations"=>"<p>infos</p>\r\n",
+        "ordre_affichage" => 12,
+        "contract_type" => c).tap(&:save!)
   end
 
 end
