0b11be6c5876306715a4393b3888cff6207ff8eb
David Boureau <bdavidxyz@gmail.com>
2021-09-13 09:40:49 +0200

    stage 5 out

--- rails/app/controllers/admin/aid_creation_controller.rb
+++ rails/app/controllers/admin/aid_creation_controller.rb
@@ -203,33 +203,8 @@ module Admin
     def create_stage_5
       slug = _hidden(:slug)
       action_asked = _hidden(:action_asked)
-      aid = Aid.find_by(slug: slug)
 
-      please_save_aid = true
-
-      notice_message = ""
-      if action_asked == "archive"
-        aid.archived_at = DateTime.now
-        notice_message = "L'aide a bien été archivée, elle n'apparaîtra plus sur le site d'ici quelques secondes."
-      elsif action_asked == "publish"
-        aid.archived_at = nil
-        notice_message = "<strong>L'aide va être publiée sur le site web ! </strong><br> Cela peut prendre quelques secondes..."
-      elsif action_asked == "reread"
-        aid.is_rereadable = true
-        notice_message = "L'aide a été demandée pour relecture."
-      elsif action_asked == "keep"
-        notice_message = "L'aide a été conservée en tant que brouillon."
-      elsif action_asked == "discard"
-        please_save_aid = false
-        notice_message = "Le brouillon a été supprimé."
-      end
-
-      if please_save_aid
-        aid.save
-        aid.update_status
-      else
-        aid.destroy
-      end
+      notice_message = CreateStage5.new.call(slug, action_asked)
   
       redirect_to(
         admin_root_path("aid[direction]" => "desc", "aid[order]" => "updated_at"),
