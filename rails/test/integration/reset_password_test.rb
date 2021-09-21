652430ebd5d2c8bbd053b0b6ff1a3ed5b9b80a2b
David Boureau <bdavidxyz@gmail.com>
2021-09-21 10:49:08 +0200

    one new green test about reset pwd

--- rails/test/integration/reset_password_test.rb
+++ rails/test/integration/reset_password_test.rb
@@ -12,6 +12,16 @@ class ResetPasswordTest < ActionDispatch::IntegrationTest
     assert text_of('.c-flash-error').include?("Le mot de passe ne peut être vide")
   end
 
+  test "reset password : success" do
+    #given
+    user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
+    allow_any_instance_of(ClearanceFindByUserAndToken).to receive(:call).and_return(user)
+    #when
+    put user_password_url(user.id), params: { password_reset: { password: 'A3Fresh!NewPwd' }}
+    #then
+    assert_redirected_to admin_root_url
+  end
+
   test "create password : deliver email if forgotten" do
     #given
     user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
