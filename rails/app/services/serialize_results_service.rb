7b38152797f7fe9a5d7021662703c2d20bb44cdd
David Boureau <bdavidxyz@gmail.com>
2021-09-21 11:08:07 +0200

    subparam tested

--- rails/app/services/serialize_results_service.rb
+++ rails/app/services/serialize_results_service.rb
@@ -46,45 +46,8 @@ class SerializeResultsService
     aids.map {|aid| WhitelistAidService.new.for_aid_in_list(aid)}
   end
 
-  def _extract_custom_childrens(custom_parent_slug_list)
-    res = ""
-
-    return res if !(custom_parent_slug_list.is_a?(String) && !custom_parent_slug_list.empty?) 
-
-    active = ActivatedModelsService.instance
-    
-    parent_slugs = custom_parent_slug_list.split(",")
-    parent_ids = parent_slugs.map { |parent_slug|  active.custom_parent_filters.find { |c| c["slug"] == parent_slug }["id"] }
-    selection = active.custom_filters.select { |custom_filter|  parent_ids.include?(custom_filter["custom_parent_filter_id"]) }
-    if selection.is_a?(Array) && !selection.empty?
-      res = selection.map { |e| e["slug"]  }.join(",")
-    end
-    res
-  end
-
-  def _find_elies(property, initial_filters, all_elies)
-    resulting_elies = []
-    if (initial_filters.is_a?(String) && !initial_filters.empty?)
-      active = ActivatedModelsService.instance
-      filters_array = initial_filters.split(",")
-      resulting_elies += all_elies.select do |ely|
-        ely[property] = [] if ely[property] == nil
-        current_filter_array = ely[property].map do |ely_filter|
-          a=active.public_send(property)
-          b=a.find do |active_filter| 
-            active_filter["id"] == ely_filter["id"]
-          end
-          b["slug"]
-        end
-        intersection_array = current_filter_array & filters_array
-        !intersection_array.empty?
-      end
-    end
-    resulting_elies    
-  end
-
   def _filter(elies, filters)
-    regular_elies              = _find_elies("filters", filters, elies)
+    regular_elies = _find_elies("filters", filters, elies)
 
     selected_elies = []
 
