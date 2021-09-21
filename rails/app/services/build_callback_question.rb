b4e125c8faeebf8c83989dc18e43d17d085fccbb
David Boureau <bdavidxyz@gmail.com>
2021-09-21 15:30:58 +0200

    removed peconnect_question

--- rails/app/services/build_callback_question.rb
+++
@@ -1,9 +0,0 @@
-class BuildCallbackQuestion
-
-  def call(session, params, request)
-    asker = PullAskerFromSession.new.call(session)
-    EnrichAskerFromParams.new.call(asker, params)
-    SaveAskerToSession.new.call(session, asker)
-  end
-  
-end
