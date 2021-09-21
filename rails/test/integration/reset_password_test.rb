f2b4f71acb8daeb2a90cd8d5bd80403d090789c2
David Boureau <bdavidxyz@gmail.com>
2021-09-21 11:03:40 +0200

    simpler code, more tests

--- rails/test/integration/reset_password_test.rb
+++ rails/test/integration/reset_password_test.rb
@@ -2,12 +2,34 @@ require 'test_helper'
 
 class ResetPasswordTest < ActionDispatch::IntegrationTest
 
+  test "reset password : do not allow non-existing user" do
+    #given
+    unexisting_user_id = 99999
+    #when
+    put user_password_url(unexisting_user_id), params: { password_reset: { password: '' }}
+    #then
+    # nothing happens, re-render the same page
+    assert text_of('h1').include?("Récupérer votre mot de passe")
+  end
+
+
   test "reset password : fail if empty" do
     #given
     user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
     allow_any_instance_of(ClearanceFindByUserAndToken).to receive(:call).and_return(user)
     #when
     put user_password_url(user.id), params: { password_reset: { password: '' }}
+    #then
+    assert text_of('.c-flash-error').include?("Le mot de passe ne peut être vide")
+  end
+
+
+  test "reset password : fail if empty, warning if passing user params" do
+    #given
+    user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
+    allow_any_instance_of(ClearanceFindByUserAndToken).to receive(:call).and_return(user)
+    #when
+    put user_password_url(user.id), params: { user: { password: '' }}
     #then
     assert text_of('.c-flash-error').include?("Le mot de passe ne peut être vide")
   end
