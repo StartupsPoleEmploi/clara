ebde0f41c821967dbf2281b7b7dd37ecf7b221d7
David Boureau <bdavidxyz@gmail.com>
2021-09-21 10:54:09 +0200

    one more line covered

--- rails/test/integration/reset_password_test.rb
+++ rails/test/integration/reset_password_test.rb
@@ -15,20 +15,43 @@ class ResetPasswordTest < ActionDispatch::IntegrationTest
   test "reset password : success" do
     #given
     user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
+    initial_encrypted_password = user.encrypted_password
+    assert_not_nil initial_encrypted_password
     allow_any_instance_of(ClearanceFindByUserAndToken).to receive(:call).and_return(user)
     #when
     put user_password_url(user.id), params: { password_reset: { password: 'A3Fresh!NewPwd' }}
     #then
+    modified_user = User.find_by(email: 'superadmin@clara.com')
+    # assert password was changed, there is no way to access directly the password value for security purposes
+    assert_not_equal initial_encrypted_password, modified_user.encrypted_password
     assert_redirected_to admin_root_url
   end
 
-  test "create password : deliver email if forgotten" do
+  test "create password : deliver email LATER if forgotten" do
     #given
     user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
 
     #See https://stackoverflow.com/a/32127013/2595513
     mail = double
     expect(mail).to receive(:deliver_later).with(no_args)
+    expect(CustomClearanceMailer).to receive(:change_password)
+      .with('superadmin@clara.com', URI::regexp(%w(http https)))
+      .and_return(mail)
+
+    #when
+    post user_password_url(user.id), params: { password: { email: 'superadmin@clara.com' }}
+    #then
+    # expect(email).to receive(:deliver_later).with(no_args)
+    assert text_of('h1').include?("Consultez vos emails !")
+  end
+
+  test "create password : deliver email IMMEDIATELY if forgotten" do
+    #given
+    user = User.create(email: "superadmin@clara.com", password: "bar", role: "superadmin")
+
+    #See https://stackoverflow.com/a/32127013/2595513
+    mail = double
+    expect(mail).to receive(:deliver).with(no_args)
     expect(CustomClearanceMailer).to receive(:change_password)
       .with('superadmin@clara.com', URI::regexp(%w(http https)))
       .and_return(mail)
