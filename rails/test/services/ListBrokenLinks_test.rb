c4b3318fc785c9075c89f211cf3b3e2a2f4a0e46
David Boureau <bdavidxyz@gmail.com>
2021-09-27 15:28:01 +0200

    welcome_test

--- rails/test/services/ListBrokenLinks_test.rb
+++ rails/test/services/ListBrokenLinks_test.rb
@@ -14,4 +14,37 @@ class ListBrokenLinksTest < ActiveSupport::TestCase
     assert_equal [{:url=>"http://example.com", :code=>301, :new_url=>"http://new_location", :aids_slug=>["mon-aide"]}], result
   end
 
+  test '.call is able to handle a 200 (success) response' do
+    #given
+    faraday_stub = double("faraday_stub")
+    allow(faraday_stub).to receive(:head).with('http://example.com').and_return(OpenStruct.new(status: 200))
+    allow_any_instance_of(ListAidLinks).to receive(:call).and_return([{aid_slug: 'mon-aide', links:['http://example.com']}])
+    #when
+    result = ListBrokenLinks.new.call(faraday_stub)
+    #then
+    assert_equal [], result
+  end
+
+  test '.call may raise an error' do
+    #given
+    faraday_stub = double("faraday_stub")
+    allow_any_instance_of(ListAidLinks).to receive(:call).and_return([{aid_slug: 'mon-aide', links:['http://example.com']}])
+    allow(faraday_stub).to receive(:head).with('http://example.com').and_raise(ZeroDivisionError)
+    #when
+    result = ListBrokenLinks.new.call(faraday_stub)
+    #then
+    assert_equal([{:url=>"http://example.com", :code=>520, :aids_slug=>["mon-aide"]}], result)
+  end
+
+  test '.call may time out' do
+    #given
+    faraday_stub = double("faraday_stub")
+    allow_any_instance_of(ListAidLinks).to receive(:call).and_return([{aid_slug: 'mon-aide', links:['http://example.com']}])
+    allow(faraday_stub).to receive(:head).with('http://example.com').and_raise(Timeout::Error)
+    #when
+    result = ListBrokenLinks.new.call(faraday_stub)
+    #then
+    assert_equal([{:url=>"http://example.com", :code=>408, :aids_slug=>["mon-aide"]}], result)
+  end
+
 end
