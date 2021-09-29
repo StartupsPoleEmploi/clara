b5e02cde32513551f6ba8bae47ac017877ffcbfa
David Boureau <bdavidxyz@gmail.com>
2021-09-29 13:11:01 +0200

    resetsession ok

--- rails/test/integration/reset_sessions_test.rb
+++ rails/test/integration/reset_sessions_test.rb
@@ -7,13 +7,13 @@ class ResetSessionsTest < ActionDispatch::IntegrationTest
     connect_as_superadmin
     expect(ActiveRecord::SessionStore::Session).to receive(:count).and_return(42)
     expect(Mae::Application).to receive(:load_tasks)
+    expect(Rake::Task).to receive(:[]).with('db:sessions:clear').and_return(OpenStruct.new(invoke: 'any'))
     #when
     post admin_post_session_path
     #then
     assert_response :success
     json_response = JSON.parse(response.body)
     assert_equal({"status"=>"ok", "count"=>42}, json_response)
-
   end
 
 end
