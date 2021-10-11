0341d82dc2c174c34be89130d74216e32f84ec28
David Boureau <bdavidxyz@gmail.com>
2021-10-11 10:47:47 +0200

    more controller tests

--- rails/cypress/integration/user_wizard.spec.js
+++ rails/cypress/integration/user_wizard.spec.js
@@ -10,14 +10,37 @@ context('Simple visiteur : Parcours du formulaire', () => {
 
     it("On peut remplir toutes les questions, on arrive sur l'écran de résultat", () => {
 
-      // given
+
+      // Inscription question : error scenario
+      cy.get('.c-label.is-error').should("not.exist")
+      cy.get('#moins_d_un_an').should("exist")
+      cy.get('.js-next').click()
+      cy.get('.c-label.is-error').should("exist")
+      // Inscription : Go back
+      cy.get('.c-back-question').click()
+      cy.get('#moins_d_un_an').should("not.exist")
+      cy.visit('/inscription_questions/new')  
+      // Inscription: valid scenario
       cy.get('#moins_d_un_an').should("exist")
       cy.get('#moins_d_un_an').click()
       cy.get('.js-next').click()
 
+      // Category : error case
+      cy.get('.c-label.is-error').should("not.exist")
       cy.get('#cat_12345').should("exist")
+      cy.get('.js-next').click()
+      cy.get('.c-label.is-error').should("exist")
+      // Category : go back
+      cy.get('.c-back-question').click()
+      cy.get('#cat_12345').should("not.exist")
+      cy.get('.js-next').click()
+      cy.get('#cat_12345').should("exist")
+      // Category : nominal, valid scenario
       cy.get('#cat_12345').click()
       cy.get('.js-next').click()
+      cy.get('#cat_12345').should("not.exist")
+
+
 
       cy.get('input#ARE_ASP').should("exist")
       cy.get('input#ARE_ASP').click()
@@ -39,10 +62,10 @@ context('Simple visiteur : Parcours du formulaire', () => {
       cy.get('#niveau_4').click()
       cy.get('.js-next').click()
 
-      cy.window().then((win) => {
-        cy.log('----------------------win.clara.env.ARA_URL_GEO_API--------------------------------------')
-        cy.log(win.clara.env.ARA_URL_GEO_API)
-      })
+      // cy.window().then((win) => {
+      //   cy.log('----------------------win.clara.env.ARA_URL_GEO_API--------------------------------------')
+      //   cy.log(win.clara.env.ARA_URL_GEO_API)
+      // })
 
       cy.get('#search').should("exist")
       cy.get('#search').type("49490")
@@ -50,9 +73,25 @@ context('Simple visiteur : Parcours du formulaire', () => {
       cy.get('li.autocomplete-item').first().click()
       cy.get('.js-next').click()
 
+
+      // Other question : go back, error scenario, nominal scenario, go next
+      // Go to "other" question
+      cy.get('.js-next').click()
       cy.get('#val_spectacle').should("exist")
+      // Go back
+      cy.get('.c-back-question').click()
+      cy.get('#val_spectacle').should("not.exist")
+      // Come back to the question
+      cy.get('.js-next').click()
+      cy.get('#val_spectacle').should("exist")
+      // Error case
+      cy.get('.c-label.is-error').should("not.exist")
+      cy.get('.js-next').click()
+      cy.get('.c-label.is-error').should("exist")
+      // Nominal case
       cy.get('#val_spectacle').click()
       cy.get('.js-next').click()
+      cy.get('#val_spectacle').should("not.exist")
 
       cy.location().should((loc) => {expect(loc.pathname).to.eq('/filter_questions/new')})
       cy.get('.js-next').click()
